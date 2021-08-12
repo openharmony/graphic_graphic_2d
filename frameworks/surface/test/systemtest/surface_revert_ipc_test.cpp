682658cdad3e46b4ea1a2295a02a26a53607b894
luwh0708 <luwh0708@thundersoft.com>
2021-08-12 20:14:50 +0800

    add systemtest
    Change-Id: I282f8c4ab9ef80bf00d2e38d1ab3cca0d7712e9f
    Signed-off-by: luwh0708 <1632083718@qq.com>

---
+++ frameworks/surface/test/systemtest/surface_revert_ipc_test.cpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "surface_revert_ipc_test.h"
+
+#include <display_type.h>
+
+namespace OHOS {
+void SurfaceRevertIPCTest::SetUpTestCase()
+{
+    GTEST_LOG_(INFO) << getpid();
+    requestConfig = {
+        .width = 0x100,  // small
+        .height = 0x100, // small
+        .strideAlignment = 0x8,
+        .format = PIXEL_FMT_RGBA_8888,
+        .usage = HBM_USE_CPU_READ | HBM_USE_CPU_WRITE | HBM_USE_MEM_DMA,
+        .timeout = 0,
+    };
+    flushConfig = { .damage = { .w = 0x100, .h = 0x100 } };
+}
+
+void SurfaceRevertIPCTest::OnBufferAvailable()
+{
+}
+
+namespace {
+HWTEST_F(SurfaceRevertIPCTest, BufferIPC, testing::ext::TestSize.Level0)
+{
+    pipe(pipeFd);
+    pid_t pid = fork();
+    ASSERT_GE(pid, 0);
+    if (pid == 0) {
+        GTEST_LOG_(INFO) << getpid();
+        auto csurface = Surface::CreateSurfaceAsConsumer("test");
+        csurface->RegisterConsumerListener(this);
+        auto producer = csurface->GetProducer();
+        auto sam = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+        sam->AddSystemAbility(ipcSystemAbilityID, producer->AsObject());
+        auto psurface = Surface::CreateSurfaceAsProducer(producer);
+
+        int64_t data;
+        sptr<SurfaceBuffer> buffer = nullptr;
+        int32_t fence;
+        auto sret = psurface->RequestBuffer(buffer, fence, requestConfig);
+        if (sret != SURFACE_ERROR_OK) {
+            data = sret;
+            write(pipeFd[1], &data, sizeof(data));
+            exit(0);
+        }
+
+        buffer->ExtraSet("123", 0x123);
+        buffer->ExtraSet("345", 0x345ll);
+        buffer->ExtraSet("567", "567");
+
+        sret = psurface->FlushBuffer(buffer, -1, flushConfig);
+        if (sret != SURFACE_ERROR_OK) {
+            data = sret;
+            write(pipeFd[1], &data, sizeof(data));
+            exit(0);
+        }
+
+        Rect damage;
+        sret = csurface->AcquireBuffer(buffer, fence, data, damage);
+        if (sret != SURFACE_ERROR_OK) {
+            data = sret;
+            write(pipeFd[1], &data, sizeof(data));
+            exit(0);
+        }
+
+        sret = csurface->ReleaseBuffer(buffer, -1);
+        data = sret;
+        write(pipeFd[1], &data, sizeof(data));
+        sleep(0);
+        read(pipeFd[0], &data, sizeof(data));
+        sam->RemoveSystemAbility(ipcSystemAbilityID);
+        exit(0);
+    }
+
+    int64_t data;
+    read(pipeFd[0], &data, sizeof(data));
+    GTEST_LOG_(INFO) << getpid();
+    ASSERT_EQ(data, SURFACE_ERROR_OK);
+
+    auto sam = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    auto robj = sam->GetSystemAbility(ipcSystemAbilityID);
+    auto producer = iface_cast<IBufferProducer>(robj);
+    auto psurface = Surface::CreateSurfaceAsProducer(producer);
+
+    sptr<SurfaceBuffer> buffer = nullptr;
+    int32_t fence;
+    auto sret = psurface->RequestBuffer(buffer, fence, requestConfig);
+    EXPECT_EQ(sret, SURFACE_ERROR_OK);
+    EXPECT_NE(buffer, nullptr);
+    if (buffer != nullptr) {
+        int32_t int32;
+        int64_t int64;
+        std::string str;
+        buffer->ExtraGet("123", int32);
+        buffer->ExtraGet("345", int64);
+        buffer->ExtraGet("567", str);
+
+        EXPECT_EQ(int32, 0x123);
+        EXPECT_EQ(int64, 0x345);
+        EXPECT_EQ(str, "567");
+    }
+    write(pipeFd[0], &data, sizeof(data));
+}
+} // namespace
+} // namespace OHOS
