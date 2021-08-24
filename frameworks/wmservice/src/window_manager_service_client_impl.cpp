9f28c9c9200cde9f387898a8160dcffd60affd69
luwh0708 <luwh0708@thundersoft.com>
2021-08-24 16:35:41 +0800

    WMService: impl global window event
    Change-Id: I122df7c1446140efb1c4c62c8322fbd4e1402f9c
    Signed-off-by: luwh0708 <1632083718@qq.com>

--- frameworks/wmservice/src/window_manager_service_client_impl.cpp
+++ frameworks/wmservice/src/window_manager_service_client_impl.cpp
@@ -79,6 +79,11 @@ void OnWindowShotError(void *, struct wms *, uint32_t a, uint32_t b)
     WindowManagerServiceProxy::OnWindowShot(static_cast<wms_error>(a), b, -1, 0, 0, 0, 0, 0, 0);
 }
 
+void OnGlobalWindowStatus(void *, struct wms *, uint32_t a, uint32_t b, uint32_t c)
+{
+    WindowManagerServiceProxy::OnGlobalWindowStatus(a, b, c);
+}
+
 void RegistryGlobal(void *pwms, struct wl_registry *registry,
     uint32_t id, const char *interface, uint32_t version)
 {
@@ -95,6 +100,7 @@ void RegistryGlobal(void *pwms, struct wl_registry *registry,
             OnScreenShotError,
             OnWindowShotDone,
             OnWindowShotError,
+            OnGlobalWindowStatus,
         };
         wms_add_listener(*wms, &listener, nullptr);
     }
