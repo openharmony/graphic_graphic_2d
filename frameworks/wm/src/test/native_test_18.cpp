8c82d7f4779273459570423980d9c01940fba6e8
luwh0708 <luwh0708@thundersoft.com>
2021-08-24 19:14:50 +0800

    wmtest: refactor wmtest, add test23 for global window event
    Change-Id: I9fd6b9d7375ed575f80cc759b3f25689a9c2a81b
    Signed-off-by: luwh0708 <1632083718@qq.com>

--- frameworks/wm/src/test/native_test_18.cpp
+++ frameworks/wm/src/test/native_test_18.cpp
@@ -50,6 +50,13 @@ public:
 
     void Run(int32_t argc, const char **argv) override
     {
+        auto initRet = WindowManager::GetInstance()->Init();
+        if (initRet) {
+            printf("init failed with %s\n", WMErrorStr(initRet).c_str());
+            ExitTest();
+            return;
+        }
+
         csurface1 = Surface::CreateSurfaceAsConsumer();
         window1 = NativeTestFactory::CreateWindow(WINDOW_TYPE_NORMAL, csurface1);
         if (window1 == nullptr) {
