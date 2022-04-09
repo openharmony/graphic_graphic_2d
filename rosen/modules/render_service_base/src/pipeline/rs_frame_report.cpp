b87c11fb2f1d6c51205638987ad28158c7d965f7
zff <zhangfanfan2@huawei.com>
2022-04-09 19:37:08 +0800

    feat: 系统组件支持64位集成
    close: #I5113F
    
    Signed-off-by: zff <zhangfanfan2@huawei.com>
    Change-Id: I7cd99502dc32d4439cc3693a5f23f15483365333

--- rosen/modules/render_service_base/src/pipeline/rs_frame_report.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_frame_report.cpp
@@ -25,7 +25,11 @@
 namespace OHOS {
 namespace Rosen {
 namespace {
+#ifdef __aarch64__
+    const std::string FRAME_AWARE_SO_PATH = "/system/lib64/libframe_ui_intf.z.so";
+#else
     const std::string FRAME_AWARE_SO_PATH = "/system/lib/libframe_ui_intf.z.so";
+#endif
 }
 RsFrameReport& RsFrameReport::GetInstance()
 {
