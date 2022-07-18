c561b414f830e7fc5fe0428e6a36703a23a43e52
litiantian <litiantian23@h-partners.com>
2022-07-18 18:11:53 +0800

    fix bianyigaojing
    Signed-off-by: litiantian <litiantian23@h-partners.com>
    Signed-off-by: litiantian <litiantian23@huawei.com>

--- frameworks/opengl_wrapper/src/thread_private_data_ctl.cpp
+++ frameworks/opengl_wrapper/src/thread_private_data_ctl.cpp
@@ -16,7 +16,7 @@
 
 #include "wrapper_log.h"
 namespace OHOS {
-constexpr uint32_t PTHREAD_KEY_T_NOT_INITIALIZED = -1;
+constexpr int32_t PTHREAD_KEY_T_NOT_INITIALIZED = -1;
 pthread_key_t ThreadPrivateDataCtl::key_ = PTHREAD_KEY_T_NOT_INITIALIZED;
 pthread_once_t ThreadPrivateDataCtl::onceCtl_ = PTHREAD_ONCE_INIT;
 
