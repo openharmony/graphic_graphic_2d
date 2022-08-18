43652865c178a96ab3926dbfc452f872d89bde57
yaozhupeng <yaozhupeng@huawei.com>
2022-08-18 10:07:06 +0800

    fixcode
    Signed-off-by: yaozhupeng <yaozhupeng@huawei.com>

--- rosen/modules/render_service_client/core/modifier/rs_property.cpp
+++ rosen/modules/render_service_client/core/modifier/rs_property.cpp
@@ -71,10 +71,9 @@ RSProperty<T>::RSProperty(const T& value) : stagingValue_(value), id_(GenerateId
 template<typename T>
 void RSProperty<T>::Set(const T& value)
 {
-    if (value == stagingValue_ || !IsValid(value)) {
+    if (ROSEN_EQ(value, stagingValue_) || !IsValid(value)) {
         return;
     }
-
     if (!hasAddToNode_) {
         stagingValue_ = value;
         return;
@@ -87,7 +86,7 @@ void RSProperty<T>::Set(const T& value)
 template<typename T>
 void RSAnimatableProperty<T>::Set(const T& value)
 {
-    if (value == this->stagingValue_ || !IsValid(value)) {
+    if (ROSEN_EQ(value, this->stagingValue_) || !IsValid(value)) {
         return;
     }
 
