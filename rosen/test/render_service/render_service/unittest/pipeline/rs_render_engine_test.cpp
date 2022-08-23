d65de53bc655c7f1842901daefceb5b50e709cb7
andrew0229 <zhangzhao62@huawei.com>
2022-08-23 06:26:12 +0000

    add ut for Accessibility
    Signed-off-by: andrew0229 <zhangzhao62@huawei.com>

---
+++ rosen/test/render_service/render_service/unittest/pipeline/rs_render_engine_test.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+#include "pipeline/rs_base_render_util.h"
+#include "pipeline/rs_render_engine.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+class RSRenderEngineTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+
+    static inline std::shared_ptr<RSRenderEngine> renderEngine_;
+};
+
+void RSRenderEngineTest::SetUpTestCase()
+{
+    renderEngine_ = std::make_shared<RSRenderEngine>();
+}
+
+void RSRenderEngineTest::TearDownTestCase()
+{
+    renderEngine_ = nullptr;
+}
+
+void RSRenderEngineTest::SetUp() {}
+void RSRenderEngineTest::TearDown() {}
+
+/**
+ * @tc.name: SetColorFilterMode001
+ * @tc.desc:SetColorFilterMode INVERT_MODE
+ * @tc.type:FUNC
+ * @tc.require:issueI5NJLC
+ */
+HWTEST_F(RSRenderEngineTest, SetColorFilterMode001, TestSize.Level1)
+{
+    ColorFilterMode mode = ColorFilterMode::INVERT_MODE;
+    renderEngine_->SetColorFilterMode(mode);
+    ASSERT_EQ(renderEngine_->GetColorFilterMode(), mode);
+}
+
+/**
+ * @tc.name: SetColorFilterMode002
+ * @tc.desc:SetColorFilterMode PROTANOMALY_MODE
+ * @tc.type:FUNC
+ * @tc.require:issueI5NM41
+ */
+HWTEST_F(RSRenderEngineTest, SetColorFilterMode002, TestSize.Level1)
+{
+    ColorFilterMode mode = ColorFilterMode::PROTANOMALY_MODE;
+    renderEngine_->SetColorFilterMode(mode);
+    ASSERT_EQ(renderEngine_->GetColorFilterMode(), mode);
+}
+} // namespace OHOS::Rosen
