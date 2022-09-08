a435ecfcd603876264ba5dd67dca7ca6603d5c6c
lee <liwei576@huawei.com>
2022-09-08 14:41:17 +0800

    Gravity transform once
    Signed-off-by: lee <liwei576@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -698,12 +698,14 @@ void RSUniRenderVisitor::ProcessRootRenderNode(RSRootRenderNode& node)
     } else {
         canvas_->save();
     }
-    const float rootWidth = property.GetFrameWidth() * property.GetScaleX();
-    const float rootHeight = property.GetFrameHeight() * property.GetScaleY();
-    SkMatrix gravityMatrix;
-    (void)RSPropertiesPainter::GetGravityMatrix(frameGravity_,
-        RectF { 0.0f, 0.0f, boundsRect_.width(), boundsRect_.height() }, rootWidth, rootHeight, gravityMatrix);
-    canvas_->concat(gravityMatrix);
+    if (node.GetParent().lock() == curSurfaceNode_) {
+        const float rootWidth = property.GetFrameWidth() * property.GetScaleX();
+        const float rootHeight = property.GetFrameHeight() * property.GetScaleY();
+        SkMatrix gravityMatrix;
+        (void)RSPropertiesPainter::GetGravityMatrix(frameGravity_,
+            RectF { 0.0f, 0.0f, boundsRect_.width(), boundsRect_.height() }, rootWidth, rootHeight, gravityMatrix);
+        canvas_->concat(gravityMatrix);
+    }
     ProcessCanvasRenderNode(node);
     canvas_->restore();
 }
