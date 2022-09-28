d3093dbb63d42a7cbe4f114534d47450d5608af2
mingzhu_pearl <limingzhu10@huawei.com>
2022-09-28 02:51:32 +0000

    fix codex
    Signed-off-by: mingzhu_pearl <limingzhu10@huawei.com>
    Change-Id: I17fcd95553cd0f5beeb7f59ef90dee06fbf594ec

--- frameworks/surface/src/native_window.cpp
+++ frameworks/surface/src/native_window.cpp
@@ -147,7 +147,7 @@ static int32_t InternalHandleNativeWindowOpt(OHNativeWindow *window, int code, v
 {
     switch (code) {
         case SET_USAGE: {
-            int32_t usage = va_arg(args, int32_t);
+            uint64_t usage = va_arg(args, uint64_t);
             window->config.usage = usage;
             break;
         }
@@ -189,8 +189,8 @@ static int32_t InternalHandleNativeWindowOpt(OHNativeWindow *window, int code, v
             break;
         }
         case GET_USAGE: {
-            int32_t *value = va_arg(args, int32_t*);
-            int32_t usage = window->config.usage;
+            uint64_t *value = va_arg(args, uint64_t*);
+            uint64_t usage = window->config.usage;
             *value = usage;
             break;
         }
