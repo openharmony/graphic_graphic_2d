9f8ef08ad495ce96f6fe2b3b096e4a9d9a45be8d
lingkang <lingkang3@huawei.com>
2022-11-15 16:31:15 +0800

    fixed 9c1e20f from https://gitee.com/ling-kang/graphic_graphic_2d/pulls/2944 Set dirty when add child
    Signed-off-by: lingkang <lingkang3@huawei.com>
    Change-Id: I7ffda1968ad1baa0079132e34e7aab5700b4b2a5

--- rosen/modules/render_service_base/src/pipeline/rs_base_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_base_render_node.cpp
@@ -49,6 +49,7 @@ void RSBaseRenderNode::AddChild(SharedPtr child, int index)
     if (isOnTheTree_) {
         child->SetIsOnTheTree(true);
     }
+    SetDirty();
 }
 
 void RSBaseRenderNode::MoveChild(SharedPtr child, int index)
@@ -137,6 +138,7 @@ void RSBaseRenderNode::AddCrossParentChild(const SharedPtr& child, int32_t index
     if (isOnTheTree_) {
         child->SetIsOnTheTree(true);
     }
+    SetDirty();
 }
 
 void RSBaseRenderNode::RemoveCrossParentChild(const SharedPtr& child, const WeakPtr& newParent)
