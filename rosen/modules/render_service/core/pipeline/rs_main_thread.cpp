859d4f1b2e151e92257523992c946076e03f6a03
gavin <m18724298828@163.com>
2022-11-17 20:12:35 +0800

    change frame trace position
    Signed-off-by: gavin <m18724298828@163.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -146,7 +146,6 @@ void RSMainThread::Init()
         ROSEN_TRACE_END(HITRACE_TAG_GRAPHIC_AGP);
         SetRSEventDetectorLoopFinishTag();
         rsEventManager_.UpdateParam();
-        QuickEndFrameTrace(RS_INTERVAL_NAME);
         RS_LOGD("RsDebug mainLoop end");
     };
 
