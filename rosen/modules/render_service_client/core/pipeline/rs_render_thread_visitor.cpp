b18191e942f61c817ccf7d2d7a258fbbacc946a3
lijj01 <lijunjie29@huawei.com>
2022-11-21 21:17:17 +0800

    reduce useless loge tag
    Signed-off-by: lijj01 <lijunjie29@huawei.com>
    Change-Id: I875babf1b032e0d70f8778b8e315daf72102c5bb

--- rosen/modules/render_service_client/core/pipeline/rs_render_thread_visitor.cpp
+++ rosen/modules/render_service_client/core/pipeline/rs_render_thread_visitor.cpp
@@ -63,7 +63,7 @@ bool RSRenderThreadVisitor::IsValidRootRenderNode(RSRootRenderNode& node)
         return false;
     }
     if (node.GetSuggestedBufferWidth() <= 0 || node.GetSuggestedBufferHeight() <= 0) {
-        ROSEN_LOGE("Root %s: Negative width or height [%d %d]", ptr->GetName().c_str(),
+        ROSEN_LOGD("Root %s: Negative width or height [%d %d]", ptr->GetName().c_str(),
             node.GetSuggestedBufferWidth(), node.GetSuggestedBufferHeight());
         return false;
     }
