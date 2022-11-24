85ef74e86983e4860b193cb22863d615b38a42b1
xiongdawei <xiongwei84@huawei.com>
2022-11-24 17:42:31 +0800

    add effect and render animation unittest
    Signed-off-by: xiongdawei <xiongwei84@huawei.com>

--- rosen/modules/effect/test/unittest/effect_chain_unittest.cpp
+++ rosen/modules/effect/test/unittest/effect_chain_unittest.cpp
@@ -14,6 +14,10 @@
  */
 
 #include "effect_chain_unittest.h"
+
+#include <iostream>
+#include <fstream>
+
 #include "builder.h"
 #include "image_chain.h"
 
@@ -22,6 +26,7 @@ using namespace testing::ext;
 
 namespace OHOS {
 namespace Rosen {
+
 /**
  * @tc.name: BuilderCreateFromConfigTest001
  * @tc.desc: Ensure the ability of creating effect chain from config file.
@@ -83,6 +88,153 @@ HWTEST_F(EffectChainUnittest, BuilderCreateFromConfigTest003, TestSize.Level1)
      */
     ImageChain* imageChain = builder->CreateFromConfig("");
     EXPECT_EQ(imageChain, nullptr);
+}
+
+/**
+ * @tc.name: BuilderCreateFromConfigTest004
+ * @tc.desc: Ensure the ability of creating effect chain from config file.
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(EffectChainUnittest, BuilderCreateFromConfigTest004, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "EffectChainUnittest BuilderCreateFromConfigTest004 start";
+    /**
+     * @tc.steps: step1. Create a builder pointer
+     */
+    std::unique_ptr<Builder> builder = std::make_unique<Builder>();
+    /**
+     * @tc.steps: step2. Call createFromConfig to load file
+     */
+    const std::string jsonStr_ = R"({ "filters": [ { "type" : "Input", "name" : "myJPGInput", "params":
+        { "format": "png", "src": "/data/accounts/account_0/appdata/com.example.myapplication/files/
+        milk.png" } } ], "connections" : [ { "from" : "myJPGInput", "to" : "myJPGOutput" } ] })";
+    std::ofstream outFile;
+    outFile.open("test.json");
+    outFile << jsonStr_.c_str() << std::endl;
+    outFile.close();
+
+    ImageChain* imageChain = builder->CreateFromConfig("test.json");
+    EXPECT_TRUE(imageChain != nullptr);
+    system("rm -rf test.json");
+}
+
+/**
+ * @tc.name: BuilderCreateFromConfigTest005
+ * @tc.desc: Ensure the ability of creating effect chain from config file.
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(EffectChainUnittest, BuilderCreateFromConfigTest005, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "EffectChainUnittest BuilderCreateFromConfigTest005 start";
+    /**
+     * @tc.steps: step1. Create a builder pointer
+     */
+    std::unique_ptr<Builder> builder = std::make_unique<Builder>();
+    /**
+     * @tc.steps: step2. Call createFromConfig to load file
+     */
+    const std::string jsonStr_ = R"({"connections" : [ { "from" : "myJPGInput", "to" : "myJPGOutput" } ] })";
+    std::ofstream outFile;
+    outFile.open("test.json");
+    outFile << jsonStr_.c_str() << std::endl;
+    outFile.close();
+
+    ImageChain* imageChain = builder->CreateFromConfig("test.json");
+    EXPECT_EQ(imageChain, nullptr);
+    system("rm -rf test.json");
+}
+
+/**
+ * @tc.name: BuilderCreateFromConfigTest006
+ * @tc.desc: Ensure the ability of creating effect chain from config file.
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(EffectChainUnittest, BuilderCreateFromConfigTest006, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "EffectChainUnittest BuilderCreateFromConfigTest006 start";
+    /**
+     * @tc.steps: step1. Create a builder pointer
+     */
+    std::unique_ptr<Builder> builder = std::make_unique<Builder>();
+    /**
+     * @tc.steps: step2. Call createFromConfig to load file
+     */
+    const std::string jsonStr_ = R"({ "filters": [ { "type" : "Input", "name" : "myJPGInput", "params":
+        { "format": "png", "src": "/data/accounts/account_0/appdata/com.example.myapplication/files/
+        milk.png" } } ] })";
+    std::ofstream outFile;
+    outFile.open("test.json");
+    outFile << jsonStr_.c_str() << std::endl;
+    outFile.close();
+
+    ImageChain* imageChain = builder->CreateFromConfig("test.json");
+    EXPECT_EQ(imageChain, nullptr);
+    system("rm -rf test.json");
+}
+
+/**
+ * @tc.name: BuilderCreateFromConfigTest007
+ * @tc.desc: Ensure the ability of creating effect chain from config file.
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(EffectChainUnittest, BuilderCreateFromConfigTest007, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "EffectChainUnittest BuilderCreateFromConfigTest007 start";
+    /**
+     * @tc.steps: step1. Create a builder pointer
+     */
+    std::unique_ptr<Builder> builder = std::make_unique<Builder>();
+    /**
+     * @tc.steps: step2. Call createFromConfig to load file
+     */
+    const std::string jsonStr_ = R"({ "filters": [ { "type" : "Input", "name" : "myJPGInput"
+        } ], "connections" : [ { "from" : "myJPGInput", "to" : "myJPGOutput" } ] })";
+    std::ofstream outFile;
+    outFile.open("test.json");
+    outFile << jsonStr_.c_str() << std::endl;
+    outFile.close();
+
+    ImageChain* imageChain = builder->CreateFromConfig("test.json");
+    EXPECT_TRUE(imageChain != nullptr);
+    system("rm -rf test.json");
+}
+
+/**
+ * @tc.name: BuilderCreateFromConfigTest008
+ * @tc.desc: Ensure the ability of creating effect chain from config file.
+ * @tc.type: FUNC
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(EffectChainUnittest, BuilderCreateFromConfigTest008, TestSize.Level1)
+{
+    GTEST_LOG_(INFO) << "EffectChainUnittest BuilderCreateFromConfigTest008 start";
+    /**
+     * @tc.steps: step1. Create a builder pointer
+     */
+    std::unique_ptr<Builder> builder = std::make_unique<Builder>();
+    /**
+     * @tc.steps: step2. Call createFromConfig to load file
+     */
+    const std::string jsonStr_ = R"({ "filters": [ { "type" : "Input", "name" : "myJPGInput", "params":
+        { "format": "png", "src": "/data/accounts/account_0/appdata/com.example.myapplication/files/
+        milk.png" } } ], "connections" : [ {} ] })";
+    std::ofstream outFile;
+    outFile.open("test.json");
+    outFile << jsonStr_.c_str() << std::endl;
+    outFile.close();
+
+    ImageChain* imageChain = builder->CreateFromConfig("test.json");
+    EXPECT_TRUE(imageChain == nullptr);
+    system("rm -rf test.json");
 }
 } // namespace Rosen
 } // namespace OHOS
