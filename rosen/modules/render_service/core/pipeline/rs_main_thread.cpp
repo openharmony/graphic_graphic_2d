450e285dfb7394050b05ffdbd6d378c8d63db62e
chenlulu <chenlulu16@huawei.com>
2023-02-03 03:18:04 +0000

    dfx about requestNextVsync, OnVsync don't come
    Signed-off-by: chenlulu <chenlulu16@huawei.com>
    Change-Id: Ifa6fd8a353156e4f4ee6f1e04ed26826b7fef772

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -60,6 +60,7 @@ using namespace OHOS::AccessibilityConfig;
 namespace OHOS {
 namespace Rosen {
 namespace {
+constexpr uint32_t RUQUEST_VSYNC_NUMBER_LIMIT = 10;
 constexpr uint64_t REFRESH_PERIOD = 16666667;
 constexpr int32_t PERF_ANIMATION_REQUESTED_CODE = 10017;
 constexpr int32_t PERF_MULTI_WINDOW_REQUESTED_CODE = 10026;
@@ -803,6 +804,10 @@ void RSMainThread::RequestNextVSync()
         .callback_ = [this](uint64_t timestamp, void* data) { OnVsync(timestamp, data); },
     };
     if (receiver_ != nullptr) {
+        requestNextVsyncNum_++;
+        if (requestNextVsyncNum_ > RUQUEST_VSYNC_NUMBER_LIMIT) {
+            RS_LOGW("RSMainThread::RequestNextVSync too many times:%d", requestNextVsyncNum_);
+        }
         receiver_->RequestNextVSync(fcb);
     }
 }
@@ -811,6 +816,7 @@ void RSMainThread::OnVsync(uint64_t timestamp, void* data)
 {
     ROSEN_TRACE_BEGIN(HITRACE_TAG_GRAPHIC_AGP, "RSMainThread::OnVsync");
     timestamp_ = timestamp;
+    requestNextVsyncNum_ = 0;
     if (isUniRender_) {
         MergeToEffectiveTransactionDataMap(cachedTransactionDataMap_);
         RSUnmarshalThread::Instance().PostTask(unmarshalBarrierTask_);
