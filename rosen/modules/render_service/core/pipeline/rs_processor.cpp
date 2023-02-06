451db4d7f99bba2659d058f5c72dd6daf2dd6bc2
edwardcaoyue <caoyue24@huawei.com>
2023-02-06 20:09:12 +0800

    new frame_trace scene
    Signed-off-by: edwardcaoyue <caoyue24@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_processor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_processor.cpp
@@ -41,7 +41,8 @@ constexpr int64_t PERF_TIME_OUT = 950;
 constexpr uint32_t PERF_LEVEL_INTERVAL = 10;
 constexpr uint32_t PERF_LAYER_START_NUM = 7;
 #ifdef FRAME_AWARE_TRACE
-constexpr uint32_t FRAME_TRACE_LAYER_NUM = 11;
+constexpr uint32_t FRAME_TRACE_LAYER_NUM_1 = 11;
+constexpr uint32_t FRAME_TRACE_LAYER_NUM_2 = 13;
 constexpr int32_t FRAME_TRACE_PERF_REQUESTED_CODE = 10024;
 #endif
 
@@ -57,7 +58,7 @@ void PerfRequest(int32_t perfRequestCode, bool onOffTag)
 #ifdef FRAME_AWARE_TRACE
 bool RSProcessor::FrameAwareTraceBoost(size_t layerNum)
 {
-    if (layerNum != FRAME_TRACE_LAYER_NUM) {
+    if (layerNum != FRAME_TRACE_LAYER_NUM_1 && layerNum != FRAME_TRACE_LAYER_NUM_2) {
         if (FrameAwareTraceIsOpen()) {
             FrameAwareTraceClose();
             PerfRequest(FRAME_TRACE_PERF_REQUESTED_CODE, false);
