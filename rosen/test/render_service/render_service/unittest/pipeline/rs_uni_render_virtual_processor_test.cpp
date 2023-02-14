e90c99e9d1109b0586787c8ac53a93f823afd7d8
Klaus_q <wangqi175@huawei.com>
2023-02-14 09:48:28 +0800

    add RSUniRenderVirtualProcessorTest
    Signed-off-by: Klaus_q <wangqi175@huawei.com>
    Change-Id: I469efebb95b86d8d86aeb6476b0c16c8108de4f2

---
+++ rosen/test/render_service/render_service/unittest/pipeline/rs_uni_render_virtual_processor_test.cpp
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+#include "limit_number.h"
+#include "pipeline/rs_uni_render_virtual_processor.h"
+#include "pipeline/rs_processor_factory.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+class RSUniRenderVirtualProcessorTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void RSUniRenderVirtualProcessorTest::SetUpTestCase() {}
+void RSUniRenderVirtualProcessorTest::TearDownTestCase() {}
+void RSUniRenderVirtualProcessorTest::SetUp() {}
+void RSUniRenderVirtualProcessorTest::TearDown() {}
+
+/**
+ * @tc.name: CreateAndDestroy
+ * @tc.desc:
+ * @tc.type:
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RSUniRenderVirtualProcessorTest, CreateAndDestroy, TestSize.Level2)
+{
+    // The best way to create RSUniRenderVirtualProcessor.
+    auto type = RSDisplayRenderNode::CompositeType::UNI_RENDER_MIRROR_COMPOSITE;
+    auto processor = RSProcessorFactory::CreateProcessor(type);
+    auto rsUniRenderVirtualProcessor = std::static_pointer_cast<RSUniRenderVirtualProcessor>(processor);
+    ASSERT_NE(nullptr, rsUniRenderVirtualProcessor);
+}
+
+/**
+ * @tc.name: Init
+ * @tc.desc:
+ * @tc.type:
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RSUniRenderVirtualProcessorTest, Init, TestSize.Level2)
+{
+    RSDisplayNodeConfig config;
+    NodeId id = 0;
+    int32_t offsetX = 0;
+    int32_t offsetY = 0;
+    RSDisplayRenderNode rsDisplayRenderNode(id, config);
+    auto processor = RSProcessorFactory::CreateProcessor(RSDisplayRenderNode::CompositeType::
+        UNI_RENDER_MIRROR_COMPOSITE);
+    ASSERT_NE(nullptr, processor);
+    ASSERT_EQ(false, processor->Init(rsDisplayRenderNode, offsetX, offsetY, INVALID_SCREEN_ID));
+}
+
+/**
+ * @tc.name: ProcessSurface
+ * @tc.desc:
+ * @tc.type:
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RSUniRenderVirtualProcessorTest, ProcessSurface, TestSize.Level2)
+{
+    RSSurfaceRenderNodeConfig config;
+    RSSurfaceRenderNode rsSurfaceRenderNode(config);
+    auto processor = RSProcessorFactory::CreateProcessor(RSDisplayRenderNode::CompositeType::
+        UNI_RENDER_MIRROR_COMPOSITE);
+    processor->ProcessSurface(rsSurfaceRenderNode);
+}
+
+/**
+ * @tc.name: ProcessDisplaySurface
+ * @tc.desc:
+ * @tc.type:
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RSUniRenderVirtualProcessorTest, ProcessDisplaySurface, TestSize.Level2)
+{
+    RSDisplayNodeConfig config;
+    NodeId id = 0;
+    RSDisplayRenderNode rsDisplayRenderNode(id, config);
+    auto processor = RSProcessorFactory::CreateProcessor(RSDisplayRenderNode::CompositeType::
+        UNI_RENDER_MIRROR_COMPOSITE);
+    ASSERT_NE(nullptr, processor);
+    processor->ProcessDisplaySurface(rsDisplayRenderNode);
+}
+
+/**
+ * @tc.name: PostProcess
+ * @tc.desc:
+ * @tc.type:
+ * @tc.require:
+ * @tc.author:
+ */
+HWTEST_F(RSUniRenderVirtualProcessorTest, PostProcess, TestSize.Level2)
+{
+    auto processor = RSProcessorFactory::CreateProcessor(RSDisplayRenderNode::CompositeType::
+        UNI_RENDER_MIRROR_COMPOSITE);
+    processor->PostProcess();
+}
+} // namespace OHOS::Rosen
