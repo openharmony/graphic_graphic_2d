cd3d2b5fdcb2b3431bc5f1760a3f3cac9fb3a081
abbuu <liyu37@huawei.com>
2023-02-14 10:09:44 +0800

    adapt glfw_render_context
    Signed-off-by: abbuu <liyu37@huawei.com>

--- interfaces/inner_api/common/graphic_common.h
+++ interfaces/inner_api/common/graphic_common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -66,6 +66,10 @@ static inline std::string LowErrorStrSpecial(GSError err)
     }
     return "";
 }
+
+#ifdef _WIN32
+#define strerror_r(err, buf, len) strerror_s((buf), (len), (err))
+#endif
 
 static inline std::string LowErrorStr(GSError lowerr)
 {
