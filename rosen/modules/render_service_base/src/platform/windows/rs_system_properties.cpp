c47fc329ee5a1f5e6652b2fe6597c41fe9388615
lee <liwei576@huawei.com>
2023-02-28 17:49:21 +0800

    previewer: anim speed bugfix
    Signed-off-by: lee <liwei576@huawei.com>

--- rosen/modules/render_service_base/src/platform/windows/rs_system_properties.cpp
+++ rosen/modules/render_service_base/src/platform/windows/rs_system_properties.cpp
@@ -110,7 +110,7 @@ bool RSSystemProperties::FrameTraceEnabled()
 
 float RSSystemProperties::GetAnimationScale()
 {
-    return {};
+    return 1.f;
 }
 
 bool RSSystemProperties::GetBoolSystemProperty(const char* name, bool defaultValue)
