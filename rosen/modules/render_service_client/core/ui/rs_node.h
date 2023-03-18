6bd0075bf2e5d4342e4b1b69510f0959f97a1855
chuchengcheng <chuchengcheng4@huawei.com>
2023-03-18 16:49:25 +0800

    remove reference in modifier
    Signed-off-by: chuchengcheng <chuchengcheng4@huawei.com>
    Change-Id: Iceb68625ac9ff568ff5a904e710c71750efb47fd

--- rosen/modules/render_service_client/core/ui/rs_node.h
+++ rosen/modules/render_service_client/core/ui/rs_node.h
@@ -200,7 +200,7 @@ public:
     void MarkContentChanged(bool isChanged);
 
     void AddModifier(const std::shared_ptr<RSModifier> modifier);
-    void RemoveModifier(const std::shared_ptr<RSModifier>& modifier);
+    void RemoveModifier(const std::shared_ptr<RSModifier> modifier);
 
 protected:
     explicit RSNode(bool isRenderServiceNode);
