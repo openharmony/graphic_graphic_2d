d4bd9234207c624c3d5ffa3914f0ce8e3a3f0fa3
w200200 <wangyonglang@huawei.com>
2023-04-13 10:20:25 +0800

    Replace libtxt: add adapter 4
    Change-Id: I2fc88ed8c75906ec625cfada195a0aba0f56ee01
    Signed-off-by: w200200 <wangyonglang@huawei.com>

---
+++ rosen/modules/2d_engine/rosen_text/adapter/txt/font_collection.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ROSEN_TEXT_ADAPTER_TXT_FONT_COLLECTION_H
+#define ROSEN_TEXT_ADAPTER_TXT_FONT_COLLECTION_H
+
+#include "rosen_text/font_collection.h"
+
+#include "txt/font_collection.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace AdapterTxt {
+class FontCollection : public ::OHOS::Rosen::FontCollection {
+public:
+    FontCollection(std::shared_ptr<txt::FontCollection> fontCollection = nullptr);
+
+    std::shared_ptr<txt::FontCollection> Get();
+
+    void DisableFallback() override;
+    void DisableSystemFont() override;
+    void LoadFont(const std::string &familyName, const uint8_t *data, size_t datalen) override;
+
+private:
+    std::shared_ptr<txt::FontCollection> fontCollection_ = nullptr;
+    sk_sp<txt::DynamicFontManager> dfmanager_ = nullptr;
+    bool disableSystemFont_ = false;
+};
+} // namespace AdapterTxt
+} // namespace Rosen
+} // namespace OHOS
+
+#endif // ROSEN_TEXT_ADAPTER_TXT_FONT_COLLECTION_H
