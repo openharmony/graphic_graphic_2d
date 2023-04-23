98ea3fd82373ec8ed61404d4b1553fd4637b60d2
w200200 <wangyonglang@huawei.com>
2023-04-23 13:37:35 +0800

    Add the remaining part of char_groups.cpp
    Change-Id: I669eddd11671ba7b605bfda61f071a2f0630f36a
    Signed-off-by: w200200 <wangyonglang@huawei.com>

--- rosen/modules/texgine/src/char_groups.cpp
+++ rosen/modules/texgine/src/char_groups.cpp
@@ -158,6 +158,213 @@ CharGroupsPair CharGroups::GetSplitAll(const int &index) const
     retval[1].range_.start_ = index;
     return retval;
 }
+
+CharGroups CharGroups::GetSub(const int &start, const int &end) const
+{
+    if (!(0 <= start && start <= end)) {
+        throw TEXGINE_EXCEPTION(InvalidArgument);
+    }
+
+    return GetSubAll(start + range_.start_, end + range_.start_);
+}
+
+CharGroups CharGroups::GetSubAll(const int &start, const int &end) const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    if (!(0 <= start && start <= end && end <= GetSize())) {
+        throw TEXGINE_EXCEPTION(InvalidArgument);
+    }
+
+    CharGroups cgs = *this;
+    cgs.range_ = {start, end};
+    return cgs;
+}
+
+CharGroups CharGroups::GetSubFromU16RangeAll(const int &u16start, const int &u16end) const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    if (!(0 <= u16start && u16start <= u16end)) {
+        throw TEXGINE_EXCEPTION(InvalidArgument);
+    }
+
+    size_t sum = 0;
+    int start = 0;
+    int end = 1e9;
+    for (int i = -1; i <= static_cast<int>(pcgs_->size()); i++) {
+        if (sum <= u16start) {
+            start = std::max(start, i);
+        }
+
+        if (0 <= i && i < pcgs_->size()) {
+            sum += pcgs_->at(i).chars_.size();
+        }
+
+        if (sum >= u16end) {
+            end = std::min(end, i);
+        }
+    }
+
+    return GetSubAll(start, end + 1);
+}
+
+CharGroups CharGroups::GetIntersect(const CharGroups &right) const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    try {
+        if (!right.IsValid()) {
+            throw TEXGINE_EXCEPTION(InvalidArgument);
+        }
+    } catch(const TexgineException &err) {
+        throw TEXGINE_EXCEPTION(InvalidArgument);
+    }
+
+    if (!IsIntersect(right)) {
+        throw CustomException("these two cgs is not intersect");
+    }
+
+    CharGroups retval;
+    retval = *this;
+    retval.range_.start_ = std::max(range_.start_, right.range_.start_);
+    retval.range_.end_ = std::min(range_.end_, right.range_.end_);
+    return retval;
+}
+
+struct CharGroup &CharGroups::Get(const int32_t &index) const
+{
+    if (index < 0) {
+        throw TEXGINE_EXCEPTION(InvalidArgument);
+    }
+
+    return GetAll(index + range_.start_);
+}
+
+struct CharGroup &CharGroups::GetAll(const int32_t &index) const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    if (!(0 <= index && index < GetSize())) {
+        throw TEXGINE_EXCEPTION(OutOfRange);
+    }
+
+    return pcgs_->at(index);
+}
+
+std::vector<uint16_t> CharGroups::ToUTF16() const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    std::vector<uint16_t> u16;
+    for (auto i = range_.start_; i < range_.end_; i++) {
+        u16.insert(u16.end(), pcgs_->at(i).chars_.begin(), pcgs_->at(i).chars_.end());
+    }
+
+    return u16;
+}
+
+std::vector<uint16_t> CharGroups::ToUTF16All() const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    std::vector<uint16_t> u16;
+    for (const auto &cg : *pcgs_) {
+        u16.insert(u16.end(), cg.chars_.begin(), cg.chars_.end());
+    }
+
+    return u16;
+}
+
+std::vector<struct CharGroup>::iterator CharGroups::begin() const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    return pcgs_->begin() + range_.start_;
+}
+
+std::vector<struct CharGroup>::iterator CharGroups::end() const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    return pcgs_->begin() + range_.end_;
+}
+
+CharGroups CharGroups::Clone() const
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    auto ret = CharGroups::CreateEmpty();
+    *ret.pcgs_ = *pcgs_;
+    ret.range_ = range_;
+    return ret;
+}
+
+void CharGroups::Merge(const CharGroups &right)
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    try {
+        if (!right.IsValid()) {
+            throw TEXGINE_EXCEPTION(InvalidArgument);
+        }
+    } catch(const struct TexgineException &err) {
+        throw TEXGINE_EXCEPTION(InvalidArgument);
+    }
+
+    if (range_.end_ != right.range_.start_) {
+        throw CustomException("the right start not equal this end");
+    }
+
+    range_.end_ += right.range_.end_ - right.range_.start_;
+}
+
+void CharGroups::PushBack(const struct CharGroup &cg)
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    if (!(0 == range_.start_ && range_.end_ == pcgs_->size())) {
+        throw CustomException("incomplete CharGroups cannot revert");
+    }
+
+    pcgs_->push_back(cg);
+    range_.end_++;
+}
+
+void CharGroups::ReverseAll()
+{
+    if (!IsValid()) {
+        throw TEXGINE_EXCEPTION(InvalidCharGroups);
+    }
+
+    if (!(0 == range_.start_ && range_.end_ == pcgs_->size())) {
+        throw CustomException("incomplete CharGroups cannot revert");
+    }
+
+    std::reverse(pcgs_->begin(), pcgs_->end());
+}
 } // namespace TextEngine
 } // namespace Rosen
 } // namespace OHOS
