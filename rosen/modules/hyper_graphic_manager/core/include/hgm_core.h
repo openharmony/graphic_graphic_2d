da7d62500023133d59947586495e0fa6a9c4d0c7
liuzhiyuan <liuzhiyuan31@huawei.com>
2023-05-24 17:04:14 +0800

    add
    Signed-off-by: liuzhiyuan <liuzhiyuan31@huawei.com>

---
+++ rosen/modules/hyper_graphic_manager/core/include/hgm_core.h
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HGM_CORE_H
+#define HGM_CORE_H
+
+#include <functional>
+#include <inttypes.h>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include <event_handler.h>
+
+#include "hgm_screen.h"
+#include "xml_parser.h"
+
+namespace OHOS::Rosen {
+class HgmCore final {
+public:
+    static HgmCore& Instance();
+
+    std::vector<ScreenId> GetScreenIds() const
+    {
+        return screenIds_;
+    }
+
+    bool IsEnabled() const
+    {
+        return isEnabled_;
+    }
+
+    bool IsInit() const
+    {
+        return isInit_;
+    }
+
+    int32_t GetScreenListSize() const
+    {
+        return screenList_.size();
+    }
+
+    int32_t SetScreenRefreshRate(ScreenId id, int32_t sceneId, int32_t rate);
+    int32_t SetRateAndResolution(ScreenId id, int32_t sceneId, int32_t rate, int32_t width, int32_t height);
+    int32_t SetRefreshRateMode(RefreshRateMode refreshRateMode);
+    int32_t AddScreen(ScreenId id, int32_t defaultMode);
+    int32_t RemoveScreen(ScreenId id);
+    int32_t AddScreenInfo(ScreenId id, int32_t width, int32_t height, uint32_t rate, int32_t mode);
+    uint32_t GetScreenCurrentRefreshRate(ScreenId id);
+    sptr<HgmScreen> GetScreen(ScreenId id) const;
+    std::vector<uint32_t> GetScreenSupportedRefreshRates(ScreenId id);
+    std::unique_ptr<std::unordered_map<ScreenId, int32_t>> GetModesToApply();
+
+private:
+    HgmCore();
+    ~HgmCore();
+    HgmCore(const HgmCore&) = delete;
+    HgmCore(const HgmCore&&) = delete;
+    HgmCore& operator=(const HgmCore&) = delete;
+    HgmCore& operator=(const HgmCore&&) = delete;
+
+    bool Init();
+    int32_t InitXmlConfig();
+    int32_t SetCustomRateMode(RefreshRateMode mode);
+    int32_t SetModeViaChosenSetting();
+
+    bool isEnabled_ = true;
+    bool isInit_ = false;
+    std::unique_ptr<XMLParser> mParser_;
+    std::unique_ptr<ParsedConfigData> mParsedConfigData_ = nullptr;
+
+    RefreshRateMode customFrameRateMode_;
+    std::vector<ScreenId> screenIds_;
+    std::vector<sptr<HgmScreen>> screenList_;
+    mutable std::mutex listMutex_;
+
+    mutable std::mutex modeListMutex_;
+    std::unique_ptr<std::unordered_map<ScreenId, int32_t>> modeListToApply_ = nullptr;
+};
+} // namespace OHOS::Rosen
+#endif // HGM_CORE_H
---
+++ rosen/modules/hyper_graphic_manager/core/src/hgm_core.cpp
@@ -0,0 +1,307 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hgm_core.h"
+
+#include <string>
+#include <unistd.h>
+
+#include "hgm_log.h"
+
+namespace OHOS::Rosen {
+HgmCore& HgmCore::Instance()
+{
+    static HgmCore instance;
+    if (instance.IsInit()) {
+        return instance;
+    }
+
+    if (instance.Init() == false) {
+        HGM_LOGI("HgmCore initialization failed");
+    }
+
+    return instance;
+}
+
+HgmCore::HgmCore()
+{
+    HGM_LOGI("Construction of Hgmcore");
+}
+
+HgmCore::~HgmCore()
+{
+    std::lock_guard<std::mutex> lock(listMutex_);
+    HGM_LOGI("Destruction of HgmCore");
+    auto screen = screenList_.begin();
+    while (screen != screenList_.end()) {
+        screenList_.erase(screen++);
+    }
+    auto screenIdIter = screenIds_.begin();
+    while (screenIdIter != screenIds_.end()) {
+        screenIds_.erase(screenIdIter++);
+    }
+}
+
+bool HgmCore::Init()
+{
+    if (isInit_) {
+        return true;
+    }
+
+    if (!isEnabled_) {
+        HGM_LOGE("HgmCore Hgm is desactivated");
+        return false;
+    }
+
+    if (InitXmlConfig() != EXEC_SUCCESS) {
+        HGM_LOGE("HgmCore falied to parse");
+        return false;
+    }
+
+    isInit_ = true;
+    HGM_LOGI("HgmCore initialization success!!!");
+    return isInit_;
+}
+
+int32_t HgmCore::InitXmlConfig()
+{
+    // parse xml configuration to hgm
+    HGM_LOGD("HgmCore is parsing xml configuration");
+    if (!mParser_) {
+        mParser_ = std::make_unique<XMLParser>();
+    }
+    if (mParser_->LoadConfiguration() != EXEC_SUCCESS) {
+        HGM_LOGE("HgmCore failed to load xml configuration file");
+        return XML_FILE_LOAD_FAIL;
+    }
+    if (mParser_->Parse()) {
+        HGM_LOGE("HgmCore failed to parse xml configuration");
+        return XML_GET_ROOT_FAIL;
+    }
+    if (!mParsedConfigData_) {
+        mParsedConfigData_ = mParser_->GetParsedData();
+    }
+    return EXEC_SUCCESS;
+}
+
+int32_t HgmCore::SetCustomRateMode(RefreshRateMode mode)
+{
+    customFrameRateMode_ = mode;
+    return EXEC_SUCCESS;
+}
+
+int32_t HgmCore::SetModeBySettingConfig()
+{
+    if (!mParsedConfigData_) {
+        HGM_LOGW("HgmCore no parsed xml configuration found, failed to apply refreshrate mode");
+        return HGM_ERROR;
+    }
+
+    std::string settingTag = std::to_string(customFrameRateMode_);
+    auto mapIter = mParsedConfigData_->customerSettingConfig_.find(settingTag);
+    if (mapIter == mParsedConfigData_->customerSettingConfig_.end()) {
+        HGM_LOGW("HgmCore failed to find strategy for customer setting : %{public}d", customFrameRateMode_);
+        return HGM_ERROR;
+    }
+    
+    std::string strat = mParsedConfigData_->customerSettingConfig_[settingTag];
+    auto strategy = mParsedConfigData_->detailedStrategies_.find(strat);
+    if (strategy == mParsedConfigData_->detailedStrategies_.end()) {
+        HGM_LOGW("HgmCore failed to find strategy detail for strat : %{public}s", strat.c_str());
+        return HGM_ERROR;
+    }
+    int32_t rateToSwitch = std::stoi(mParsedConfigData_->detailedStrategies_[strat].max);
+
+    HGM_LOGW("HgmCore switching to rate : %{public}d via refreshrate mode", rateToSwitch);
+    if (rateToSwitch <= 0) {
+        HGM_LOGW("HgmCore get an illegal rate via parsed config data : %{public}d", rateToSwitch);
+        return HGM_ERROR;
+    }
+
+    for (auto &screen : screenList_) {
+        int32_t setThisScreen = SetScreenRefreshRate(screen->GetId(), 0, rateToSwitch);
+        if (setThisScreen) {
+            HGM_LOGW("HgmCore failed to apply refreshrate mode to screen : " PUBU64 "", screen->GetId());
+            return HGM_ERROR;
+        }
+    }
+
+    return EXEC_SUCCESS;
+}
+
+int32_t HgmCore::SetScreenRefreshRate(ScreenId id, int32_t sceneId, int32_t rate)
+{
+    // set the screen to the desired refreshrate
+    HGM_LOGD("HgmCore setting screen " PUBU64 " to the rate of %{public}d", id, rate);
+    auto screen = GetScreen(id);
+    if (!screen) {
+        HGM_LOGW("HgmCore failed to get screen of : " PUBU64 "", id);
+        return HGM_ERROR;
+    }
+
+    if (rate <= 0) {
+        HGM_LOGW("HgmCore refuse an illegal framerate: %{public}d", rate);
+        return HGM_ERROR;
+    }
+
+    int32_t modeToSwitch = screen->SetActiveRefreshRate(sceneId, static_cast<uint32_t>(rate));
+    if (modeToSwitch < 0) {
+        return modeToSwitch;
+    }
+
+    std::lock_guard<std::mutex> lock(modeListMutex_);
+
+    // the rate is accepted and passed to a list, will be applied by hardwarethread before sending the composition
+    HGM_LOGI("HgmCore the rate of %{public}d is accepted, the target mode is %{public}d", rate, modeToSwitch);
+    if (modeListToApply_ == nullptr) {
+        HGM_LOGD("HgmCore modeListToApply_ is invalid, buiding a new mode list");
+        modeListToApply_ = std::make_unique<std::unordered_map<ScreenId, int32_t>>();
+    }
+    auto modeList = modeListToApply_.get();
+    (*modeList)[id] = modeToSwitch;
+    return modeToSwitch;
+}
+
+int32_t HgmCore::SetRateAndResolution(ScreenId id, int32_t sceneId, int32_t rate, int32_t width, int32_t height)
+{
+    // reserved
+    return HGM_ERROR;
+}
+
+int32_t HgmCore::SetRefreshRateMode(RefreshRateMode refreshRateMode)
+{
+    HGM_LOGI("HgmCore set refreshrate mode to : %{public}d", refreshRateMode);
+    // change refreshrate mode by setting application
+    if (SetCustomRateMode(refreshRateMode) != EXEC_SUCCESS) {
+        return HGM_ERROR;
+    }
+
+    // apply the refreshrate mode to the screen
+    if (SetModeBySettingConfig() != EXEC_SUCCESS) {
+        return HGM_ERROR;
+    }
+
+    return EXEC_SUCCESS;
+}
+
+int32_t HgmCore::AddScreen(ScreenId id, int32_t defaultMode)
+{
+    // add a physical screen to hgm during hotplug event
+    HGM_LOGI("HgmCore adding screen : " PUBI64 "", id);
+    bool removeId = false;
+    for (auto screen : screenIds_) {
+        if (screen == id) {
+            removeId = true;
+            break;
+        }
+    }
+    if (removeId) {
+        if (RemoveScreen(id) != EXEC_SUCCESS) {
+            HGM_LOGW("HgmCore failed to remove the existing screen, not adding : " PUBI64 "", id);
+            return HGM_BASE_REMOVE_FAILED;
+        }
+    }
+
+    sptr<HgmScreen> newScreen = new HgmScreen(id, defaultMode);
+
+    std::lock_guard<std::mutex> lock(listMutex_);
+    screenList_.push_back(newScreen);
+    screenIds_.push_back(id);
+
+    int32_t screenNum = GetScreenListSize();
+    HGM_LOGI("HgmCore num of screen is %{public}d", screenNum);
+    return EXEC_SUCCESS;
+}
+
+int32_t HgmCore::RemoveScreen(ScreenId id)
+{
+    std::lock_guard<std::mutex> lock(listMutex_);
+    // delete a screen during a hotplug event
+    HGM_LOGD("HgmCore deleting the screen : " PUBU64 "", id);
+    for (auto screen = screenIds_.begin(); screen != screenIds_.end(); ++screen) {
+        if (*screen == id) {
+            screenIds_.erase(screen);
+            break;
+        }
+    }
+    for (auto screen = screenList_.begin(); screen != screenList_.end(); ++screen) {
+        if ((*screen)->GetId() == id) {
+            screenList_.erase(screen);
+            break;
+        }
+    }
+    return EXEC_SUCCESS;
+}
+
+int32_t HgmCore::AddScreenInfo(ScreenId id, int32_t width, int32_t height, uint32_t rate, int32_t mode)
+{
+    // add a supported screen mode to the screen
+    auto screen = GetScreen(id);
+    if (!screen) {
+        HGM_LOGW("HgmCore failed to get screen of : " PUBU64 "", id);
+        return HGM_NO_SCREEN;
+    }
+
+    if (screen->AddScreenModeInfo(width, height, rate, mode) == EXEC_SUCCESS) {
+        return EXEC_SUCCESS;
+    }
+
+    HGM_LOGW("HgmCore failed to add screen mode info of screen : " PUBU64 "", id);
+    return HGM_SCREEN_PARAM_ERROR;
+}
+
+uint32_t HgmCore::GetScreenCurrentRefreshRate(ScreenId id)
+{
+    auto screen = GetScreen(id);
+    if (!screen) {
+        HGM_LOGW("HgmCore failed to find screen " PUBU64 "", id);
+        return static_cast<uint32_t>(EXEC_SUCCESS);
+    }
+
+    return screen->GetActiveRefreshRate();
+}
+
+sptr<HgmScreen> HgmCore::GetScreen(ScreenId id) const
+{
+    std::lock_guard<std::mutex> lock(listMutex_);
+    for (auto screen = screenList_.begin(); screen != screenList_.end(); ++screen) {
+        if ((*screen)->GetId() == id) {
+            return *screen;
+        }
+    }
+
+    return nullptr;
+}
+
+std::vector<uint32_t> HgmCore::GetScreenSupportedRefreshRates(ScreenId id)
+{
+    auto screen = GetScreen(id);
+    if (!screen) {
+        HGM_LOGW("HgmCore failed to find screen " PUBU64 "", id);
+        return std::vector<uint32_t>(static_cast<uint32_t>(EXEC_SUCCESS));
+    }
+
+    auto supportedRates = screen->GetSupportedRates();
+    std::vector<uint32_t> retVec;
+    retVec.assign(supportedRates.begin(), supportedRates.end());
+    return retVec;
+}
+
+std::unique_ptr<std::unordered_map<ScreenId, int32_t>> HgmCore::GetModesToApply()
+{
+    std::lock_guard<std::mutex> lock(modeListMutex_);
+    return std::move(modeListToApply_);
+}
+} // namespace OHOS::Rosen
