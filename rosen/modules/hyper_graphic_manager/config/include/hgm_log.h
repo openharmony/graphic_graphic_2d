86a66509413e9cdc8a0017699194089af28f0a26
liuzhiyuan <liuzhiyuan31@huawei.com>
2023-05-25 10:15:34 +0800

    add HGM feature: screen
    Signed-off-by: liuzhiyuan <liuzhiyuan31@huawei.com>

---
+++ rosen/modules/hyper_graphic_manager/config/include/hgm_log.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HGM_CONFIG_LOG_H
+#define HGM_CONFIG_LOG_H
+
+#include <hilog/log.h>
+
+namespace OHOS {
+#ifdef __aarch64__
+#define PUBI64 "%{public}ld"
+#define PUBU64 "%{public}lu"
+#else
+#define PUBI64 "%{public}lld"
+#define PUBU64 "%{public}llu"
+#endif
+
+#define HGM_DFUNC OHOS::HiviewDFX::HiLog::Debug
+#define HGM_IFUNC OHOS::HiviewDFX::HiLog::Info
+#define HGM_WFUNC OHOS::HiviewDFX::HiLog::Warn
+#define HGM_EFUNC OHOS::HiviewDFX::HiLog::Error
+
+#define HGM_CPRINTF(func, fmt, ...) \
+    func( {LOG_CORE, 0xD001400, "graphic_2d_hgm"}, "<%{public}d>%{public}s: " fmt, \
+        __LINE__, __func__, ##__VA_ARGS__)
+
+#define HGM_LOGD(fmt, ...) HGM_CPRINTF(HGM_DFUNC, fmt, ##__VA_ARGS__)
+#define HGM_LOGI(fmt, ...) HGM_CPRINTF(HGM_IFUNC, fmt, ##__VA_ARGS__)
+#define HGM_LOGW(fmt, ...) HGM_CPRINTF(HGM_WFUNC, fmt, ##__VA_ARGS__)
+#define HGM_LOGE(fmt, ...) HGM_CPRINTF(HGM_EFUNC, fmt, ##__VA_ARGS__)
+} // namespace OHOS
+#endif // HGM_CONFIG_LOG_H
---
+++ rosen/modules/hyper_graphic_manager/core/include/hgm_screen.h
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HGM_SCREEN_H
+#define HGM_SCREEN_H
+
+#include <memory>
+#include <mutex>
+#include <unistd.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "refbase.h"
+#include "screen_manager/screen_types.h"
+#include "screen_manager/rs_screen_mode_info.h"
+
+#include "hgm_command.h"
+
+namespace OHOS::Rosen {
+class HgmScreen : public virtual RefBase {
+public:
+    HgmScreen();
+    HgmScreen(ScreenId id, int32_t mode);
+    virtual ~HgmScreen();
+
+    ScreenId GetId() const
+    {
+        return id_;
+    }
+
+    uint32_t GetActiveMode() const
+    {
+        return activeModeId_;
+    }
+
+    std::unordered_set<uint32_t> GetSupportedRates() const
+    {
+        return supportedRefreshRates_;
+    }
+
+    bool IsSupportRate(uint32_t rate) const
+    {
+        return supportedRefreshRates_.find(rate) != supportedRefreshRates_.end() ? true : false;
+    }
+
+    int32_t GetActiveRefreshRateMode() const
+    {
+        return customFrameRateMode_;
+    }
+
+    uint32_t GetActiveRefreshRate();
+    int32_t SetActiveRefreshRate(int32_t sceneId, uint32_t rate);
+    int32_t SetRateAndResolution(int32_t sceneId, uint32_t rate, int32_t width, int32_t height);
+    int32_t AddScreenModeInfo(int32_t width, int32_t height, uint32_t rate, int32_t modeId);
+
+private:
+    class ScreenProfile {
+    public:
+        ScreenProfile(int32_t width, int32_t height, uint32_t rate, int32_t modeId)
+            : width_(width), height_(height), refreshrate_(rate), modeId_(modeId) {}
+
+        ~ScreenProfile() = default;
+
+        bool operator==(const ScreenProfile& rValue) const
+        {
+            if (rValue.GetWidth() != width_ || rValue.GetHeight() != height_ ||
+                rValue.GetRate() != refreshrate_) {
+                return false;
+            }
+            return true;
+        }
+
+        int32_t GetWidth() const
+        {
+            return width_;
+        }
+
+        int32_t GetHeight() const
+        {
+            return height_;
+        }
+
+        uint32_t GetRate() const
+        {
+            return refreshrate_;
+        }
+
+        int32_t GetModeId() const
+        {
+            return modeId_;
+        }
+
+    private:
+        int32_t width_ = -1;
+        int32_t height_ = -1;
+        uint32_t refreshrate_ = 0;
+        int32_t modeId_ = -1;
+    };
+
+    ScreenId id_;
+    int32_t activeModeId_;
+    int32_t customFrameRateMode_ = -1;
+    std::unordered_set<uint32_t> supportedRefreshRates_;
+    std::unordered_set<int32_t> supportedModeIds_;
+    std::vector<std::shared_ptr<ScreenProfile>> screenModeInfos_;
+    std::mutex baseMutex_;
+
+    void VerifySupportedProfiles() const;
+    void SetActiveModeId(int32_t modeId);
+    std::shared_ptr<ScreenProfile> GetModeViaId(int32_t id) const;
+    bool IfSwitchToRate(int32_t screenId, uint32_t rate) const;
+    int32_t GetModeIdViaRate(uint32_t rate) const;
+    int32_t GetModeIdViaResolutionAndRate(int32_t width, int32_t height, uint32_t rate) const;
+    static constexpr uint32_t RATE_NOT_SUPPORTED = 0;
+};
+} // namespace Ohos::Rosen
+# endif // HGM_SCREEN_H
