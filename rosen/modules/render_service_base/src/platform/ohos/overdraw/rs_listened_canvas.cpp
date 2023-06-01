72974ca9e39bf16cff10ab368b5e74131c0c33a0
huangzejia1 <huangzejia1@huawei.com>
2023-06-01 13:18:57 +0800

    adapt to drawing for rsb/src/platform/ohos/overdraw -3
    Change-Id: I38f8af467d7988ac4bb7a1828572654b03fabadb
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/overdraw/rs_listened_canvas.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/overdraw/rs_listened_canvas.cpp
@@ -20,6 +20,7 @@
 
 namespace OHOS {
 namespace Rosen {
+#ifndef USE_ROSEN_DRAWING
 RSListenedCanvas::RSListenedCanvas(SkCanvas* canvas, float alpha)
     : RSPaintFilterCanvas(canvas, alpha)
 {
@@ -29,12 +30,24 @@ RSListenedCanvas::RSListenedCanvas(SkSurface* skSurface, float alpha)
     : RSPaintFilterCanvas(skSurface, alpha)
 {
 }
+#else
+RSListenedCanvas::RSListenedCanvas(Drawing::Canvas& canvas, float alpha)
+    : RSPaintFilterCanvas(&canvas, alpha)
+{
+}
+
+RSListenedCanvas::RSListenedCanvas(Drawing::Surface& surface, float alpha)
+    : RSPaintFilterCanvas(&surface, alpha)
+{
+}
+#endif
 
 void RSListenedCanvas::SetListener(const std::shared_ptr<RSCanvasListener> &listener)
 {
     listener_ = listener;
 }
 
+#ifndef USE_ROSEN_DRAWING
 void RSListenedCanvas::onDrawPaint(const SkPaint& paint)
 {
     RSPaintFilterCanvas::onDrawPaint(paint);
@@ -180,5 +193,210 @@ void RSListenedCanvas::onDrawImageRect(const SkImage* image, const SkRect* src, 
     }
 }
 #endif
+#else
+using namespace Drawing;
+
+void RSListenedCanvas::DrawPoint(const Point& point)
+{
+    RSPaintFilterCanvas::DrawPoint(point);
+    if (listener_ != nullptr) {
+        listener_->DrawPoint(point);
+    }
+}
+
+void RSListenedCanvas::DrawLine(const Point& startPt, const Point& endPt)
+{
+    RSPaintFilterCanvas::DrawLine(startPt, endPt);
+    if (listener_ != nullptr) {
+        listener_->DrawLine(startPt, endPt);
+    }
+}
+
+void RSListenedCanvas::DrawRect(const Rect& rect)
+{
+    RSPaintFilterCanvas::DrawRect(rect);
+    if (listener_ != nullptr) {
+        listener_->DrawRect(rect);
+    }
+}
+
+void RSListenedCanvas::DrawRoundRect(const RoundRect& roundRect)
+{
+    RSPaintFilterCanvas::DrawRoundRect(roundRect);
+    if (listener_ != nullptr) {
+        listener_->DrawRoundRect(roundRect);
+    }
+}
+
+void RSListenedCanvas::DrawNestedRoundRect(const RoundRect& outer, const RoundRect& inner)
+{
+    RSPaintFilterCanvas::DrawNestedRoundRect(outer, inner);
+    if (listener_ != nullptr) {
+        listener_->DrawNestedRoundRect(outer, inner);
+    }
+}
+
+void RSListenedCanvas::DrawArc(const Rect& oval, scalar startAngle, scalar sweepAngle)
+{
+    RSPaintFilterCanvas::DrawArc(oval, startAngle, sweepAngle);
+    if (listener_ != nullptr) {
+        listener_->DrawArc(oval, startAngle, sweepAngle);
+    }
+}
+
+void RSListenedCanvas::DrawPie(const Rect& oval, scalar startAngle, scalar sweepAngle)
+{
+    RSPaintFilterCanvas::DrawPie(oval, startAngle, sweepAngle);
+    if (listener_ != nullptr) {
+        listener_->DrawPie(oval, startAngle, sweepAngle);
+    }
+}
+
+void RSListenedCanvas::DrawOval(const Rect& oval)
+{
+    RSPaintFilterCanvas::DrawOval(oval);
+    if (listener_ != nullptr) {
+        listener_->DrawOval(oval);
+    }
+}
+
+void RSListenedCanvas::DrawCircle(const Point& centerPt, scalar radius)
+{
+    RSPaintFilterCanvas::DrawCircle(centerPt, radius);
+    if (listener_ != nullptr) {
+        listener_->DrawCircle(centerPt, radius);
+    }
+}
+
+void RSListenedCanvas::DrawPath(const Path& path)
+{
+    RSPaintFilterCanvas::DrawPath(path);
+    if (listener_ != nullptr) {
+        listener_->DrawPath(path);
+    }
+}
+
+void RSListenedCanvas::DrawBackground(const Brush& brush)
+{
+    RSPaintFilterCanvas::DrawBackground(brush);
+    if (listener_ != nullptr) {
+        listener_->DrawBackground(brush);
+    }
+}
+
+void RSListenedCanvas::DrawShadow(const Path& path, const Point3& planeParams, const Point3& devLightPos,
+    scalar lightRadius, Color ambientColor, Color spotColor, ShadowFlags flag)
+{
+    RSPaintFilterCanvas::DrawShadow(path, planeParams, devLightPos, lightRadius, ambientColor, spotColor, flag);
+    if (listener_ != nullptr) {
+        listener_->DrawShadow(path, planeParams, devLightPos, lightRadius, ambientColor, spotColor, flag);
+    }
+}
+
+void RSListenedCanvas::DrawRegion(const Drawing::Region& region)
+{
+    RSPaintFilterCanvas::DrawRegion(region);
+    if (listener_ != nullptr) {
+        listener_->DrawRegion(region);
+    }
+}
+
+void RSListenedCanvas::DrawBitmap(const Bitmap& bitmap, const scalar px, const scalar py)
+{
+    RSPaintFilterCanvas::DrawBitmap(bitmap, px, py);
+    if (listener_ != nullptr) {
+        listener_->DrawBitmap(bitmap, px, py);
+    }
+}
+
+void RSListenedCanvas::DrawBitmap(Media::PixelMap& pixelMap, const scalar px, const scalar py)
+{
+    RSPaintFilterCanvas::DrawBitmap(pixelMap, px, py);
+    if (listener_ != nullptr) {
+        listener_->DrawBitmap(pixelMap, px, py);
+    }
+}
+
+void RSListenedCanvas::DrawImage(const Image& image, const scalar px, const scalar py, const SamplingOptions& sampling)
+{
+    RSPaintFilterCanvas::DrawImage(image, px, py, sampling);
+    if (listener_ != nullptr) {
+        listener_->DrawImage(image, px, py, sampling);
+    }
+}
+
+void RSListenedCanvas::DrawImageRect(const Image& image, const Rect& src, const Rect& dst,
+    const SamplingOptions& sampling, SrcRectConstraint constraint)
+{
+    RSPaintFilterCanvas::DrawImageRect(image, src, dst, sampling, constraint);
+    if (listener_ != nullptr) {
+        listener_->DrawImageRect(image, src, dst, sampling, constraint);
+    }
+}
+
+void RSListenedCanvas::DrawImageRect(const Image& image, const Rect& dst, const SamplingOptions& sampling)
+{
+    RSPaintFilterCanvas::DrawImageRect(image, dst, sampling);
+    if (listener_ != nullptr) {
+        listener_->DrawImageRect(image, dst, sampling);
+    }
+}
+
+void RSListenedCanvas::DrawPicture(const Picture& picture)
+{
+    RSPaintFilterCanvas::DrawPicture(picture);
+    if (listener_ != nullptr) {
+        listener_->DrawPicture(picture);
+    }
+}
+
+void RSListenedCanvas::Clear(Drawing::ColorQuad color)
+{
+    RSPaintFilterCanvas::Clear(color);
+    if (listener_ != nullptr) {
+        listener_->Clear(color);
+    }
+}
+
+CoreCanvas& RSListenedCanvas::AttachPen(const Drawing::Pen& pen)
+{
+    RSPaintFilterCanvas::AttachPen(pen);
+    if (listener_ != nullptr) {
+        listener_->AttachPen(pen);
+    }
+
+    return *this;
+}
+
+CoreCanvas& RSListenedCanvas::AttachBrush(const Drawing::Brush& brush)
+{
+    RSPaintFilterCanvas::AttachBrush(brush);
+    if (listener_ != nullptr) {
+        listener_->AttachBrush(brush);
+    }
+
+    return *this;
+}
+
+CoreCanvas& RSListenedCanvas::DetachPen()
+{
+    RSPaintFilterCanvas::DetachPen();
+    if (listener_ != nullptr) {
+        listener_->DetachPen();
+    }
+
+    return *this;
+}
+
+CoreCanvas& RSListenedCanvas::DetachBrush()
+{
+    RSPaintFilterCanvas::DetachBrush();
+    if (listener_ != nullptr) {
+        listener_->DetachBrush();
+    }
+
+    return *this;
+}
+#endif // USE_ROSEN_DRAWING
 } // namespace Rosen
 } // namespace OHOS
