31678da703c1e2f00b81acedbc213248e5e4a599
lishijie <lishijie22@huawei.com>
2023-06-05 19:29:29 +0800

    add testcase
    Signed-off-by: lishijie <lishijie22@huawei.com>
    Change-Id: Id575933e6284bed1430108a81fb516e844b75725

---
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_map_test.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "pipeline/rs_render_node_map.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+class RSRenderNodeMapTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+    static inline NodeId id;
+    static inline std::weak_ptr<RSContext> context = {};
+};
+
+void RSRenderNodeMapTest::SetUpTestCase() {}
+void RSRenderNodeMapTest::TearDownTestCase() {}
+void RSRenderNodeMapTest::SetUp() {}
+void RSRenderNodeMapTest::TearDown() {}
+
+/**
+ * @tc.name: ObtainScreenLockWindowNodeIdTest
+ * @tc.desc: 
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeMapTest, ObtainScreenLockWindowNodeIdTest, TestSize.Level1)
+{
+    std::shared_ptr<RSSurfaceRenderNode> surfaceNode = nullptr;
+    RSRenderNodeMap rsRenderNodeMap;
+    rsRenderNodeMap.ObtainScreenLockWindowNodeId(surfaceNode);
+}
+
+/**
+ * @tc.name: AddDrivenRenderNodeTest
+ * @tc.desc: 
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeMapTest, AddDrivenRenderNodeTest, TestSize.Level1)
+{
+    std::shared_ptr<RSSurfaceRenderNode> nodePtr;
+    RSRenderNodeMap rsRenderNodeMap;
+    rsRenderNodeMap.ObtainLauncherNodeId(nodePtr);
+}
+/**
+ * @tc.name: AddDrivenRenderNodeTest
+ * @tc.desc: 
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeMapTest, GetAnimationFallbackNodeTest, TestSize.Level1)
+{
+    RSRenderNodeMap rsRenderNodeMap;
+    rsRenderNodeMap.GetAnimationFallbackNode();
+    ASSERT_NE(rsRenderNodeMap.GetAnimationFallbackNode(), nullptr);
+}
+} // namespace OHOS::Rosen
--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_test.cpp
@@ -17,6 +17,11 @@
 
 #include "pipeline/rs_render_node.h"
 #include "pipeline/rs_surface_render_node.h"
+#ifdef NEW_SKIA
+#include "include/gpu/GrDirectContext.h"
+#else
+#include "third_party/flutter/skia/include/gpu/GrContext.h"
+#endif
 
 using namespace testing;
 using namespace testing::ext;
@@ -31,10 +36,18 @@ public:
     void TearDown() override;
     static inline NodeId id;
     static inline std::weak_ptr<RSContext> context = {};
+    static inline RSPaintFilterCanvas* canvas_;
+    static inline SkCanvas skCanvas_;
 };
 
-void RSRenderNodeTest::SetUpTestCase() {}
-void RSRenderNodeTest::TearDownTestCase() {}
+void RSRenderNodeTest::SetUpTestCase()
+{
+    canvas_ = new RSPaintFilterCanvas(&skCanvas_);
+}
+void RSRenderNodeTest::TearDownTestCase()
+{
+    delete canvas_;
+}
 void RSRenderNodeTest::SetUp() {}
 void RSRenderNodeTest::TearDown() {}
 
@@ -52,5 +65,135 @@ HWTEST_F(RSRenderNodeTest, UpdateRenderStatus001, TestSize.Level1)
     node.UpdateRenderStatus(dirtyRegion, isPartialRenderEnabled);
 }
 
+/**
+ * @tc.name: UpdateParentChildrenRectTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, UpdateParentChildrenRectTest, TestSize.Level1)
+{
+    RSRenderNode node(id, context);
+    std::shared_ptr<RSBaseRenderNode> parentNode;
+    node.UpdateParentChildrenRect(parentNode);
+    RSBaseRenderNode rsBaseRenderNode(id, context);
+    ASSERT_FALSE(rsBaseRenderNode.HasChildrenOutOfRect());
+}
+
+/**
+ * @tc.name: ProcessTransitionBeforeChildrenTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, ProcessTransitionBeforeChildrenTest, TestSize.Level1)
+{
+    RSRenderNode node(id, context);
+    node.ProcessTransitionBeforeChildren(*canvas_);
+}
+
+/**
+ * @tc.name: ProcessTransitionBeforeChildrenTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, AddModifierTest, TestSize.Level1)
+{
+    std::shared_ptr<RSRenderModifier> modifier = nullptr;
+    RSRenderNode node(id, context);
+    node.AddModifier(modifier);
+    RSBaseRenderNode rsBaseRenderNode(id, context);
+    ASSERT_TRUE(rsBaseRenderNode.IsDirty());
+
+}
+
+/**
+ * @tc.name: SetSharedTransitionParamTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, SetSharedTransitionParamTest, TestSize.Level1)
+{
+    using SharedTransitionParam = std::pair<NodeId, std::weak_ptr<RSRenderNode>>;
+    const std::optional<SharedTransitionParam> sharedTransitionParam;
+    RSRenderNode node(id, context);
+    node.SetSharedTransitionParam(std::move(sharedTransitionParam));
+    RSBaseRenderNode rsBaseRenderNode(id, context);
+    ASSERT_TRUE(rsBaseRenderNode.IsDirty());
+}
+
+/**
+ * @tc.name: InitCacheSurfaceTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, InitCacheSurfaceTest, TestSize.Level1)
+{
+    RSRenderNode node(id, context);
+    CacheType type = CacheType::ANIMATE_PROPERTY;
+    node.SetCacheType(type);
+#ifdef NEW_SKIA
+    node.InitCacheSurface(canvas_->recordingContext());
+#else
+    node.InitCacheSurface(canvas_->getGrContext());
+#endif
+}
+
+/**
+ * @tc.name: DrawCacheSurfaceTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, DrawCacheSurfaceTest001, TestSize.Level1)
+{
+    RSRenderNode node(id, context);
+    bool isSubThreadNode = false;
+    node.DrawCacheSurface(*canvas_, isSubThreadNode);
+    if (node.GetCompletedCacheSurface() == nullptr){
+        ASSERT_EQ(NULL, node.GetCacheSurface());
+    }
+}
+
+/**
+ * @tc.name: DrawCacheSurfaceTest002
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, DrawCacheSurfaceTest002, TestSize.Level1)
+{
+    RSRenderNode node(id, context);
+    bool isSubThreadNode = false;
+    CacheType type = CacheType::ANIMATE_PROPERTY;
+    node.SetCacheType(type);
+    node.DrawCacheSurface(*canvas_, isSubThreadNode);
+    ASSERT_EQ(node.GetCompletedCacheSurface(), NULL);
+}
+
+/**
+ * @tc.name: MarkNodeGroupTest
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSRenderNodeTest, MarkNodeGroupTest, TestSize.Level1)
+{
+    RSRenderNode node(id, context);
+    bool isNodeGruop = true;
+    bool isNodeGruop_ = false;
+
+    auto nodeGruopType = node.GetNodeGroupType();
+    RSRenderNode::NodeGroupType type = RSRenderNode::NodeGroupType::GROUPED_BY_UI;
+    if (type >= nodeGruopType) {
+        node.MarkNodeGroup(type, isNodeGruop);
+        ASSERT_EQ(node.GetDrawingCacheType(), RSDrawingCacheType::TARGETED_CACHE);
+        node.MarkNodeGroup(type, isNodeGruop_);
+        ASSERT_EQ(node.GetDrawingCacheType(), RSDrawingCacheType::DISABLED_CACHE);
+    }
+}
 } // namespace Rosen
 } // namespace OHOS
