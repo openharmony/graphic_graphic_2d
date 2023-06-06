f0a6e53b826e41248b162c6a45da357776900648
huangzejia1 <huangzejia1@huawei.com>
2023-06-06 14:18:24 +0800

    adapt to drawing for rsb/src/platform/ohos/overdraw -1
    Change-Id: Id0de48b4d247435eb79eb6dc9f70276c3f8e2ecd
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/overdraw/rs_overdraw_controller.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/overdraw/rs_overdraw_controller.cpp
@@ -76,7 +76,11 @@ OverdrawColorArray RSOverdrawController::GetColorArray() const
     return colorArray_;
 }
 
+#ifndef USE_ROSEN_DRAWING
 std::map<int, SkColor> RSOverdrawController::GetColorMap() const
+#else
+std::map<int, Drawing::ColorQuad> RSOverdrawController::GetColorMap() const
+#endif
 {
     std::lock_guard lock(colorMutex_);
     return colorMap_;
@@ -130,7 +134,11 @@ void RSOverdrawController::OnColorChange(const char *key, const char *value, voi
         }
 
         // map
+#ifndef USE_ROSEN_DRAWING
         std::map<int, SkColor> colorMap;
+#else
+        std::map<int, Drawing::ColorQuad> colorMap;
+#endif
         for (size_t i = 0; i < colors.size(); i++) {
             colorMap[i + 1] = colors[i];
         }
