c4e3fffc15f81c90db27a1206e4b085b4a27b2db
zhuxiaoyu <zhuxiaoyu26@huawei.com>
2023-06-07 14:44:55 +0800

    Add RSCanvasDrawingNode
    Signed-off-by: zhuxiaoyu <zhuxiaoyu26@huawei.com>
    Change-Id: I0648ca3b7e699168727f308515bca651fc9318c8

---
+++ rosen/modules/render_service_base/include/command/rs_canvas_drawing_node_command.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ROSEN_RENDER_SERVICE_BASE_COMMAND_RS_CANVAS_DRAWING_NODE_COMMAND_H
+#define ROSEN_RENDER_SERVICE_BASE_COMMAND_RS_CANVAS_DRAWING_NODE_COMMAND_H
+
+#include "rs_command_templates.h"
+
+#include "common/rs_macros.h"
+
+namespace OHOS {
+namespace Rosen {
+
+enum RSCanvasDrawingNodeCommandType : uint16_t {
+    CANVAS_DRAWING_NODE_CREATE,
+};
+
+class RSB_EXPORT RSCanvasDrawingNodeCommandHelper {
+public:
+    static void Create(RSContext& context, NodeId id);
+};
+
+ADD_COMMAND(RSCanvasDrawingNodeCreate,
+    ARG(CANVAS_DRAWING_NODE, CANVAS_DRAWING_NODE_CREATE, RSCanvasDrawingNodeCommandHelper::Create, NodeId))
+
+} // namespace Rosen
+} // namespace OHOS
+
+#endif // ROSEN_RENDER_SERVICE_BASE_COMMAND_RS_CANVAS_DRAWING_NODE_COMMAND_H
