deba86f1263ae761df271d2be4a58180356dac24
周思远 <zhousiyuan8@huawei.com>
2023-08-18 09:40:42 +0800

    fixed a8b657a from https://gitee.com/siyuan_zhou/graphic_graphic_2d/pulls/5807 渲染服务code码访问权限校验与管控（part7回退）
    Signed-off-by: 周思远 <zhousiyuan8@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp
+++ rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp
@@ -104,11 +104,6 @@ void RSRenderServiceConnection::CleanAll(bool toDelete) noexcept
             mainThread_->UnRegisterOcclusionChangeCallback(remotePid_);
         }).wait();
 
-    {
-        std::lock_guard<std::mutex> lock(vsyncConnCounterMutex_);
-        vsyncConnCounter_ = 0;
-    }
-
     for (auto& conn : vsyncConnections_) {
         appVSyncDistributor_->RemoveConnection(conn);
     }
@@ -261,14 +256,6 @@ sptr<Surface> RSRenderServiceConnection::CreateNodeAndSurface(const RSSurfaceRen
 sptr<IVSyncConnection> RSRenderServiceConnection::CreateVSyncConnection(const std::string& name,
                                                                         const sptr<VSyncIConnectionToken>& token)
 {
-    {
-        std::lock_guard<std::mutex> lock(vsyncConnCounterMutex_);
-        if (vsyncConnCounter_ >= VSYNC_CONN_MAX) {
-            RS_LOGE("RSRenderServiceConnection::CreateVSyncConnection fail, maximum number of connections reached");
-            return nullptr;
-        }
-        vsyncConnCounter_++;
-    }
     sptr<VSyncConnection> conn = new VSyncConnection(appVSyncDistributor_, name, token->AsObject());
     appVSyncDistributor_->AddConnection(conn);
     {
