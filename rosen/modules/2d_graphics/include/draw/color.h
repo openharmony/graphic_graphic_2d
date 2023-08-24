b27874c782748e292e6827127732ddd29eebce92
huangzejia1 <huangzejia1@huawei.com>
2023-08-24 16:32:27 +0800

    fix rosen_text build issues
    Change-Id: Ia75f6131d2558ccb2a3478baa639b12ffda088ce
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

--- rosen/modules/2d_graphics/include/draw/color.h
+++ rosen/modules/2d_graphics/include/draw/color.h
@@ -19,6 +19,7 @@
 #include <cstdint>
 #include <stdint.h>
 
+#include "common/rs_macros.h"
 #include "effect/color_space.h"
 #include "utils/drawing_macros.h"
 #include "utils/scalar.h"
@@ -52,7 +53,7 @@ struct Color4f {
 };
 
 typedef uint32_t ColorQuad;
-class DRAWING_API Color {
+class RS_EXPORT Color {
 public:
     constexpr static ColorQuad COLOR_TRANSPARENT = 0;
     constexpr static ColorQuad COLOR_BLACK = 0xFF000000;
@@ -131,8 +132,8 @@ public:
     void SetColorQuad(uint32_t c);
     ColorQuad CastToColorQuad() const;
 
-    friend DRAWING_API bool operator==(const Color& c1, const Color& c2);
-    friend DRAWING_API bool operator!=(const Color& c1, const Color& c2);
+    RS_EXPORT friend bool operator==(const Color& c1, const Color& c2);
+    RS_EXPORT friend bool operator!=(const Color& c1, const Color& c2);
 
 private:
     uint32_t alpha_;
