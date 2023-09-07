9c1728eb2333e22e13baad1d38e0f1087716f5dc
yexiandong <yexiandong@huawei.com>
2023-09-07 11:44:55 +0800

    fixed e6a2632 from https://gitee.com/ye-xiandong/graphic_graphic_2d_1/pulls/6184 add fence tracker
    Signed-off-by: yexiandong <yexiandong@huawei.com>

--- frameworks/surface/include/buffer_queue.h
+++ frameworks/surface/include/buffer_queue.h
@@ -182,6 +182,7 @@ private:
     sptr<SurfaceTunnelHandle> tunnelHandle_ = nullptr;
     std::atomic_bool isValidStatus_ = true;
     std::atomic_bool producerCacheClean_ = false;
+    const bool isLocalRender_;
 };
 }; // namespace OHOS
 
