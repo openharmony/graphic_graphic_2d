f7d13d606ddec0618fb58c738655520f7cfb7c91
liuyufeng <liuyufeng52@huawei.com>
2023-09-18 16:03:20 +0800

    新增tdd用例
    Signed-off-by: liuyufeng <liuyufeng52@huawei.com>

--- rosen/test/render_service/render_service_base/unittest/overdraw/rs_gpu_overdraw_canvas_listener_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/overdraw/rs_gpu_overdraw_canvas_listener_test.cpp
@@ -18,6 +18,12 @@
 #include "platform/ohos/overdraw/rs_gpu_overdraw_canvas_listener.h"
 #include "platform/ohos/overdraw/rs_listened_canvas.h"
 
+#include "third_party/skia/include/core/SkRRect.h"
+#include "third_party/skia/include/core/SkPath.h"
+#include "third_party/skia/include/core/SkRegion.h"
+#include "third_party/skia/include/core/SkScalar.h"
+#include "third_party/skia/include/core/SkBlendMode.h"
+
 using namespace testing;
 using namespace testing::ext;
 
@@ -49,5 +55,205 @@ HWTEST_F(RSGPUOverdrawCanvasListenerTest, Create001, TestSize.Level1)
     delete listener;
 }
 
+/**
+ * @tc.name: onDrawRect
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawRect001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRect rect;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawRect(rect, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawRRect
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawRRect001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRRect rect;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawRRect(rect, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawDRRect
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawDRRect001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRRect outer;
+    SkRRect inner;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawDRRect(outer, inner, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawOval
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawOval001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRect rect;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawOval(rect, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawArc
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawArc001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRect rect;
+    SkScalar startAngle = 0.0;
+    SkScalar sweepAngle = 0.0;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawArc(rect, startAngle, sweepAngle, true, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawArc
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawArc002, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRect rect;
+    SkScalar startAngle = 0.0;
+    SkScalar sweepAngle = 0.0;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawArc(rect, startAngle, sweepAngle, false, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawPath
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawPath001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkPath path;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawPath(path, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawRegion
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawRegion001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRegion region;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawRegion(region, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawTextBlob
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawTextBlob001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkTextBlob *blob;
+    SkScalar x = 0.0;
+    SkScalar y = 0.0;
+    SkPaint paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawTextBlob(blob, x, y, paint);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawAnnotation
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawAnnotation001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkRect rect;
+    char key[10];
+    SkData* value = nullptr;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawAnnotation(rect, key, value);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawDrawable
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawDrawable001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkDrawable* drawable = nullptr;
+    SkMatrix* matrix;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawDrawable(drawable, matrix);
+    delete listener;
+}
+
+/**
+ * @tc.name: onDrawPicture
+ * @tc.desc: test
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSGPUOverdrawCanvasListenerTest, onDrawPicture001, TestSize.Level1)
+{
+    SkCanvas skCanvas;
+    SkPicture* picture;
+    SkMatrix* matrix;
+    SkPaint* paint;
+    auto listener = new RSGPUOverdrawCanvasListener(skCanvas);
+    listener->onDrawPicture(picture, matrix, paint);
+    delete listener;
+}
 } // namespace Rosen
 } // namespace OHOS
