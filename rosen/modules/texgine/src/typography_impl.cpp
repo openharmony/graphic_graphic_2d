09abfeb7a5dda6d6390964b9e91e4c0f056c4a12
huangzejia1 <huangzejia1@huawei.com>
2023-10-09 09:28:41 +0800

    修改review
    Change-Id: Ide72845245218cb6ea71a807c09e6d4f721bf57f
    Signed-off-by: huangzejia1 <huangzejia1@huawei.com>

--- rosen/modules/texgine/src/typography_impl.cpp
+++ rosen/modules/texgine/src/typography_impl.cpp
@@ -163,16 +163,13 @@ IndexAndAffinity TypographyImpl::GetGlyphIndexByCoordinate(double x, double y) c
 
     // process y < 0
     if (fabs(height_) < DBL_EPSILON) {
-        LOGEX_FUNC_LINE_DEBUG() << "special: y < 0";
         return {0, Affinity::NEXT};
     }
 
     // find targetLine
     int targetLine = static_cast<int>(FindGlyphTargetLine(y));
-    LOGEX_FUNC_LINE_DEBUG() << "targetLine: " << targetLine;
-
-    // process y more than typography
     if (targetLine == static_cast<int>(lineMetrics_.size())) {
+        // process y more than typography, (lineMetrics_.size() - 1) is the last line
         targetLine = static_cast<int>(lineMetrics_.size() - 1);
     }
 
@@ -183,24 +180,21 @@ IndexAndAffinity TypographyImpl::GetGlyphIndexByCoordinate(double x, double y) c
             count += span.GetNumberOfCharGroup();
         }
     }
-    LOGEX_FUNC_LINE_DEBUG() << "count: " << count;
 
     // find targetIndex
     double offsetX = 0;
     std::vector<double> widths;
     auto targetIndex = FindGlyphTargetIndex(targetLine, x, offsetX, widths);
     count += targetIndex;
-    LOGEX_FUNC_LINE_DEBUG() << "targetIndex: " << targetIndex;
 
     // process first line left part
     if (targetIndex == 0 && targetLine == 0) {
-        LOGEX_FUNC_LINE_DEBUG() << "special: first line left part";
         return {0, Affinity::NEXT};
     }
 
-    // process right part
     auto affinity = Affinity::PREV;
     if (targetIndex == widths.size()) {
+        // process right part, (count - 1) is the index of last chargroup
         return {count - 1, affinity};
     }
 
@@ -214,7 +208,6 @@ IndexAndAffinity TypographyImpl::GetGlyphIndexByCoordinate(double x, double y) c
             affinity = Affinity::PREV;
         }
     }
-    LOGEX_FUNC_LINE_DEBUG() << "affinity: " << (affinity == Affinity::PREV ? "upstream" : "downstream");
 
     return {count, affinity};
 }
