b262f435aeb994d407314d5a2d2e085a857f90d2
linghao-ji <jilinghao@huawei.com>
2023-10-27 14:23:49 +0800

    CheckSurfaceNeedProcess中做动画的surfacenode不应该参与计算
    Signed-off-by: linghao-ji <jilinghao@huawei.com>
    Change-Id: Ie898cce769b38978d91d1670954c168f221dcb5c

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -1308,6 +1308,7 @@ bool RSMainThread::CheckSurfaceNeedProcess(OcclusionRectISet& occlusionSurfaces,
     if (curSurface->IsFocusedNode(focusNodeId_)) {
         needProcess = true;
         if (!curSurface->HasContainerWindow() && !curSurface->IsTransparent() &&
+            !curSurface->GetAnimateState() && // when node animating (i.e. 3d animation), the region cannot be trusted
             curSurface->GetName().find("hisearch") == std::string::npos) {
             occlusionSurfaces.insert(curSurface->GetDstRect());
         }
@@ -1317,7 +1318,9 @@ bool RSMainThread::CheckSurfaceNeedProcess(OcclusionRectISet& occlusionSurfaces,
         bool insertSuccess = occlusionSurfaces.size() > beforeSize ? true : false;
         if (insertSuccess) {
             needProcess = true;
-            if (curSurface->IsTransparent() || curSurface->GetName().find("hisearch") != std::string::npos) {
+            if (curSurface->IsTransparent() ||
+                curSurface->GetAnimateState() || // when node animating(i.e. 3d animation), the region cannot be trusted
+                curSurface->GetName().find("hisearch") != std::string::npos) {
                 auto iter = std::find_if(occlusionSurfaces.begin(), occlusionSurfaces.end(),
                     [&curSurface](RectI r) -> bool {return r == curSurface->GetDstRect();});
                 if (iter != occlusionSurfaces.end()) {
