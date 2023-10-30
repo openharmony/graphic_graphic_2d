637bcc32e35b57c6b46d92256b12e181c386edbc
yxu63 <xuyue63@huawei.com>
2023-10-30 10:56:48 +0800

    change rotation to == 90
    Signed-off-by: yxu63 <xuyue63@huawei.com>
    Change-Id: I1e4d61229a22dee657b4cd5c1215b3b7fb0d8b0d

--- rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -3302,7 +3302,7 @@ void RSUniRenderVisitor::ProcessSurfaceRenderNode(RSSurfaceRenderNode& node)
             }
             // if this window is in freeze state, disable hardware composer for its child surfaceView
             if (IsHardwareComposerEnabled() && !node.IsHardwareForcedDisabled() &&
-                node.IsHardwareEnabledType() && rotation % ROTATION_90 != 0) {
+                node.IsHardwareEnabledType() && rotation % ROTATION_90 == 0) {
 #ifndef USE_ROSEN_DRAWING
                 if (!node.IsHardwareEnabledTopSurface()) {
                     canvas_->clear(SK_ColorTRANSPARENT);
