bfc372530eb472e914362abcf9b643482f50b07c
yxu63 <xuyue63@huawei.com>
2023-12-01 17:45:17 +0800

    addBaseUT
    Signed-off-by: yxu63 <xuyue63@huawei.com>
    Change-Id: I768b4ee6ea5572121759703f59564a4ddab8a890

---
+++ rosen/test/render_service/render_service_base/unittest/command/rs_effect_node_command_test.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "command/rs_effect_node_command.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+class EffectNodeCommandTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void EffectNodeCommandTest::SetUpTestCase() {}
+void EffectNodeCommandTest::TearDownTestCase() {}
+void EffectNodeCommandTest::SetUp() {}
+void EffectNodeCommandTest::TearDown() {}
+
+/**
+ * @tc.name: UpdateRange
+ * @tc.desc:
+ * @tc.type: FUNC
+ */
+HWTEST_F(EffectNodeCommandTest, Create, TestSize.Level1)
+{
+    RSContext context;
+    NodeId nodeId = static_cast<NodeId>(0);   
+    EffectNodeCommandHelper::Create(context, nodeId);
+}
+
+
+} // namespace Rosen
+} // namespace OHOS
---
+++ rosen/test/render_service/render_service_base/unittest/common/rs_upload_texture_thread_test.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+
+#include "common/rs_upload_texture_thread.h"
+
+using namespace testing;
+using namespace testing::ext;
+namespace OHOS::Rosen {
+class RSUploadTextureThreadTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void RSUploadTextureThreadTest::SetUpTestCase() {}
+void RSUploadTextureThreadTest::TearDownTestCase() {}
+void RSUploadTextureThreadTest::SetUp() {}
+void RSUploadTextureThreadTest::TearDown() {}
+
+/**
+ * @tc.name: testing
+ * @tc.desc: test results of instance
+ * @tc.type:FUNC
+ * @tc.require:issueI5NMHT
+ */
+HWTEST_F(RSUploadTextureThreadTest, testing, TestSize.Level1)
+{
+    RSUploadTextureThread::Instance().PostTask(nullptr);
+    RSUploadTextureThread::Instance().PostSyncTask(nullptr);
+    std::string name = "name";
+    RSUploadTextureThread::Instance().RemoveTask(name);
+}
+} // namespace OHOS::Rosen
