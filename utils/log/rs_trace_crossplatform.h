e4fde4f3414a202061cbb1d76726ef587ec3db35
liu_wei <liuwei742@huawei.com>
2023-12-15 14:01:39 +0800

    跨平台trace log 分类构建
    Signed-off-by: liu_wei <liuwei742@huawei.com>
    Change-Id: I71b9d2ece743f3f7790e97135b33f641152c7632

--- utils/log/rs_trace_crossplatform.h
+++ utils/log/rs_trace_crossplatform.h
@@ -16,16 +16,16 @@
 #include <string>
 
 
-#define ROSEN_TRACE_BEGIN(tag, name)
-#define RS_TRACE_BEGIN(name)
-#define ROSEN_TRACE_END(tag)
-#define RS_TRACE_END()
+#define ROSEN_TRACE_BEGIN(tag, name) OHOS::Rosen::RosenTraceBegin(name)
+#define RS_TRACE_BEGIN(name) OHOS::Rosen::RosenTraceBegin(name)
+#define ROSEN_TRACE_END(tag) OHOS::Rosen::RosenTraceEnd()
+#define RS_TRACE_END() OHOS::Rosen::RosenTraceEnd()
+#define RS_TRACE_NAME(name) OHOS::Rosen::ScopedTrace ___tracer(name)
 #define RS_TRACE_NAME_FMT(fmt, ...)
 #define RS_ASYNC_TRACE_BEGIN(name, value)
 #define RS_ASYNC_TRACE_END(name, value)
 #define RS_TRACE_INT(name, value)
-#define RS_TRACE_NAME(name)
-#define RS_TRACE_FUNC()
+#define RS_TRACE_FUNC() RS_TRACE_NAME(__FUNCTION__)
 
 namespace OHOS {
 namespace Rosen {
