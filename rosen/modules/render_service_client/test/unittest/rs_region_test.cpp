983222eb6d70eb777ed5bffb7114fa29b834a505
yxu63 <xuyue63@huawei.com>
2023-12-21 15:23:56 +0800

    change time
    Signed-off-by: yxu63 <xuyue63@huawei.com>
    Change-Id: I3d502fb95097334d23a458b962df801e9dca33a1

--- rosen/modules/render_service_client/test/unittest/rs_region_test.cpp
+++ rosen/modules/render_service_client/test/unittest/rs_region_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -83,17 +83,13 @@ HWTEST_F(RSRegionTest, OperatorOr, Function | SmallTest | Level2)
  */
 HWTEST_F(RSRegionTest, OperatorAnd, Function | SmallTest | Level2)
 {
-    int x = 0;
-    int left = 50;
-    int y = 50;
-    int height = 100;
     Occlusion::Rect rect1 { 0, 0, 100, 100 };
     Occlusion::Region resgion1 { rect1 };
     Occlusion::Rect rect2 { 50, 50, 150, 150 };
     Occlusion::Region resgion2 { rect2 };
     Occlusion::Region resAnd = resgion1.And(resgion2);
 
-    Occlusion::Rect res {x, y, left, height};
+    Occlusion::Rect res {0, 50, 100, 100};
     EXPECT_EQ(resAnd.GetSize(), 1);
     EXPECT_EQ(resAnd.GetBound(), res);
 }
