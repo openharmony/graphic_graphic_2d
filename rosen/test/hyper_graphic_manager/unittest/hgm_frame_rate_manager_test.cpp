4273e34772522686e7e753d53c09c14758f277bf
openharmony_ci <120357966@qq.com>
2023-12-22 09:35:51 +0000

    回退 'Pull Request !7822 : 删除失效的测试用例'

--- rosen/test/hyper_graphic_manager/unittest/hgm_frame_rate_manager_test.cpp
+++ rosen/test/hyper_graphic_manager/unittest/hgm_frame_rate_manager_test.cpp
@@ -95,6 +95,9 @@ HWTEST_F(HgmFrameRateMgrTest, UniProcessData, Function | SmallTest | Level1)
             ASSERT_NE(frameRateMgr, nullptr);
             frameRateMgr->Init(rsController, appController, vsyncGenerator);
         }
+        STEP("2. check the result of UniProcessData") {
+            frameRateMgr->UniProcessData(id, timestamp, rsFrameRateLinker, appFrameLinkers, flag);
+        }
     }
 }
 
