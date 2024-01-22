e9b325db38c180d33ad47fc9c9fa965fb6eab65e
chuchengcheng <chuchengcheng4@huawei.com>
2024-01-22 20:04:05 +0800

    fixed 9e4372c from https://gitee.com/chuchengcheng/graphic_graphic_2d/pulls/8418 修复分离渲染空间截图节点bounds为0的问题
    Signed-off-by: chuchengcheng <chuchengcheng4@huawei.com>
    Change-Id: I4d6590e0ab7f1c2d8d656e34be8d15f80dac20c1

--- rosen/modules/render_service_client/core/pipeline/rs_divided_ui_capture.cpp
+++ rosen/modules/render_service_client/core/pipeline/rs_divided_ui_capture.cpp
@@ -64,6 +64,8 @@ std::shared_ptr<Media::PixelMap> RSDividedUICapture::TakeLocalCapture()
 #else
     auto recordingCanvas = std::make_shared<ExtendRecordingCanvas>(FAKE_WIDTH, FAKE_HEIGHT);
 #endif
+    PostTaskToRTRecord(recordingCanvas, node, visitor);
+    auto drawCallList = recordingCanvas->GetDrawCmdList();
     std::shared_ptr<Media::PixelMap> pixelmap = CreatePixelMapByNode(node);
     if (pixelmap == nullptr) {
         ROSEN_LOGE("RSDividedUICapture::TakeLocalCapture: pixelmap == nullptr!");
@@ -83,8 +85,6 @@ std::shared_ptr<Media::PixelMap> RSDividedUICapture::TakeLocalCapture()
     }
     auto canvas = std::make_shared<RSPaintFilterCanvas>(drSurface.get());
 #endif
-    PostTaskToRTRecord(recordingCanvas, node, visitor);
-    auto drawCallList = recordingCanvas->GetDrawCmdList();
     drawCallList->Playback(*canvas);
     return pixelmap;
 }
