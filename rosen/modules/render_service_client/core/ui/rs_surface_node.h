88b207f70b93e4999b7cd28a57954ab1b5a753f7
yangfan479 <yangfan479@huawei.com>
2024-01-30 09:51:39 +0800

    fixed 3dc09ba from https://gitee.com/lucianyang/graphic_graphic_2d/pulls/8648 SurfaceNode默认同步注册&DumpTree按ZOrder排序
    Signed-off-by: yangfan479 <yangfan479@huawei.com>

--- rosen/modules/render_service_client/core/ui/rs_surface_node.h
+++ rosen/modules/render_service_client/core/ui/rs_surface_node.h
@@ -46,7 +46,7 @@ struct RSSurfaceNodeConfig {
     void* additionalData = nullptr;
     bool isTextureExportNode = false;
     SurfaceId surfaceId = 0;
-    bool isSync = false;
+    bool isSync = true;
 };
 
 class RSC_EXPORT RSSurfaceNode : public RSNode {
