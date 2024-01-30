c7f0baa9c458e97217bca799e790ab0fe0f23486
zzvscx <zhangsongwei2@huawei.com>
2024-01-30 10:10:25 +0800

    animAtrributes初始化不再抛到主线程执行
    Signed-off-by: zzvscx <zhangsongwei2@huawei.com>
    Change-Id: Idbe4e246dcc6884b460bf4f371830f682df2748d

--- rosen/modules/render_service_client/core/ui/rs_frame_rate_policy.h
+++ rosen/modules/render_service_client/core/ui/rs_frame_rate_policy.h
@@ -15,12 +15,19 @@
 #ifndef RENDER_SERVICE_CLIENT_CORE_UI_RS_FRAME_RATE_POLICY_H
 #define RENDER_SERVICE_CLIENT_CORE_UI_RS_FRAME_RATE_POLICY_H
 
+#include <mutex>
+#include <unordered_map>
 #include <memory>
 #include "transaction/rs_hgm_config_data.h"
 
 namespace OHOS {
 namespace Rosen {
 enum class RSPropertyUnit : int16_t;
+struct AnimDynamicAttribute {
+    int32_t minSpeed = 0;
+    int32_t maxSpeed = 0;
+    int32_t preferredFps = 0;
+};
 
 class RSFrameRatePolicy {
 public:
@@ -43,6 +50,8 @@ private:
     float xDpi_ = 1.0f;
     float yDpi_ = 1.0f;
     int32_t currentRefreshRateMode_ = 0;
+    std::unordered_map<std::string, std::unordered_map<std::string, AnimDynamicAttribute>> animAttributes_;
+    std::mutex mutex_;
 };
 } // namespace Rosen
 } // namespace OHOS
