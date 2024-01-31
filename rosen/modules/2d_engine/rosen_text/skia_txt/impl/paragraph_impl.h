ba5c40e21d991db439d93ee85c614906e09f5aa5
hashicop <yanhao64@huawei.com>
2024-01-31 17:17:00 +0800

    fix code check
    Signed-off-by: hashicop <yanhao64@huawei.com>
    Change-Id: Iceeb8bedfef842d39f2b3d0714fee0479a1a3255

--- rosen/modules/2d_engine/rosen_text/skia_txt/impl/paragraph_impl.h
+++ rosen/modules/2d_engine/rosen_text/skia_txt/impl/paragraph_impl.h
@@ -83,7 +83,7 @@ public:
     }
 
 private:
-    TextStyle SkStyleToTextStyle(const skia::textlayout::TextStyle& skia);
+    TextStyle SkStyleToTextStyle(const skia::textlayout::TextStyle& skStyle);
 
     std::unique_ptr<skia::textlayout::Paragraph> paragraph_;
     std::vector<PaintRecord> paints_;
