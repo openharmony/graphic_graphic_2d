a64cb54030acc72ee06443e7f5052042a315585d
Viktor <podlesny.viktor@huawei.com>
2024-02-01 15:07:12 +0300

    Fix code check
    Signed-off-by: Viktor <podlesny.viktor@huawei.com>

--- rosen/modules/render_service_profiler/rs_profiler_archive.h
+++ rosen/modules/render_service_profiler/rs_profiler_archive.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,9 +18,9 @@
 
 #include <cstring>
 #include <iostream>
+#include <securec.h>
 #include <string>
 #include <vector>
-#include <securec.h>
 
 namespace OHOS::Rosen {
 
@@ -65,9 +65,7 @@ public:
     virtual void Serialize(void* data, size_t size) = 0;
 
 protected:
-    explicit Archive(bool reader)
-        : isReading_(reader)
-    {}
+    explicit Archive(bool reader) : isReading_(reader) {}
 
     virtual ~Archive() = default;
 
@@ -82,23 +80,14 @@ protected:
     }
 
 private:
-
     bool isReading_ = true;
 };
 
 // Data archives
 
 class DataArchive : public Archive {
-public:
-
-    ~DataArchive() = default;
-
 protected:
-
-    explicit DataArchive(bool reader, size_t offset = 0)
-        : Archive(reader)
-        , offset_(offset)
-    {}
+    explicit DataArchive(bool reader, size_t offset = 0) : Archive(reader), offset_(offset) {}
 
 protected:
     size_t offset_ = 0;
@@ -106,10 +95,7 @@ protected:
 
 class DataReader final : public DataArchive {
 public:
-    DataReader(const std::vector<char>& data, size_t offset = 0)
-        : DataArchive(true, offset)
-        , data_(data)
-    {}
+    explicit DataReader(const std::vector<char>& data, size_t offset = 0) : DataArchive(true, offset), data_(data) {}
 
     void Serialize(void* data, size_t size) override
     {
@@ -125,16 +111,13 @@ protected:
 
 class DataWriter final : public DataArchive {
 public:
-    DataWriter(std::vector<char>& data, size_t offset = 0)
-        : DataArchive(false, offset)
-        , data_(data)
-    {}
+    explicit DataWriter(std::vector<char>& data, size_t offset = 0) : DataArchive(false, offset), data_(data) {}
 
     void Serialize(void* data, size_t size) override
     {
         if (data && (size > 0)) {
             data_.resize(data_.size() + size);
-            if(::memmove_s(data_.data() + offset_, data_.size(), data, size) == 0)
+            if (::memmove_s(data_.data() + offset_, data_.size(), data, size) == 0)
                 offset_ += size;
         }
     }
@@ -148,19 +131,13 @@ protected:
 template<bool reader = true>
 class FileArchive final : public Archive {
 public:
-    explicit FileArchive(FILE* file)
-        : Archive(reader) 
-        , file_(file)
-        , external_(true)
-    {}
+    explicit FileArchive(FILE* file) : Archive(reader), file_(file), external_(true) {}
 
     explicit FileArchive(const std::string& path)
-        : Archive(reader) 
-        , file_(fopen(path.data(), reader ? "rb" : "wb"))
-        , external_(false)
+        : Archive(reader), file_(fopen(path.data(), reader ? "rb" : "wb")), external_(false)
     {}
 
-    ~FileArchive()
+    ~FileArchive() override
     {
         if (file_ && !external_)
             fclose(file_);
