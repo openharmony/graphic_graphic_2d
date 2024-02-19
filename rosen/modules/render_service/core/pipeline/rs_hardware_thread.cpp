8d4d90504c15182ed82a4c0e9fb4853c6b7c1165
zhuxiaoyu <zhuxiaoyu26@huawei.com>
2024-02-19 17:53:10 +0800

    fixed 55e45cf from https://gitee.com/huaixu-y/graphic_graphic_2d/pulls/9004 修复buffer显示异常
    Signed-off-by: zhuxiaoyu <zhuxiaoyu26@huawei.com>
    Change-Id: I536ffde4297e33f9a41c051084bfa96404245a13

--- rosen/modules/render_service/core/pipeline/rs_hardware_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_hardware_thread.cpp
@@ -452,9 +452,7 @@ void RSHardwareThread::Redraw(const sptr<Surface>& surface, const std::vector<La
         }
 
 #ifndef USE_ROSEN_DRAWING
-        auto saveCount = canvas->getSaveCount();
-
-        canvas->save();
+        RSAutoCanvasRestore acr(canvas_);
         auto dstRect = layer->GetLayerSize();
         SkRect clipRect = SkRect::MakeXYWH(static_cast<float>(dstRect.x), static_cast<float>(dstRect.y),
             static_cast<float>(dstRect.w), static_cast<float>(dstRect.h));
@@ -464,7 +462,7 @@ void RSHardwareThread::Redraw(const sptr<Surface>& surface, const std::vector<La
         auto params = RSUniRenderUtil::CreateLayerBufferDrawParam(layer, forceCPU);
         canvas->concat(params.matrix);
 #else
-        auto saveCount = canvas->Save();
+        Drawing::AutoCanvasRestore acr(*canvas, true);
         auto dstRect = layer->GetLayerSize();
         Drawing::Rect clipRect = Drawing::Rect(static_cast<float>(dstRect.x), static_cast<float>(dstRect.y),
             static_cast<float>(dstRect.w) + static_cast<float>(dstRect.x),
@@ -701,13 +699,6 @@ void RSHardwareThread::Redraw(const sptr<Surface>& surface, const std::vector<La
         } else {
             uniRenderEngine_->DrawBuffer(*canvas, params);
         }
-#endif
-#ifndef USE_ROSEN_DRAWING
-        canvas->restore();
-        canvas->restoreToCount(saveCount);
-#else
-        canvas->Restore();
-        canvas->RestoreToCount(saveCount);
 #endif
     }
 
