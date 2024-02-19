6a8f5d1dbe0d1a4c30ea2759af812f6b708f1d2c
sunshipan <sunshipan@huawei.com>
2024-02-19 09:16:08 +0800

    修复打开应用从广告到主界面AI Bar不及时更新的问题
    Signed-off-by: sunshipan <sunshipan@huawei.com>

--- rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
@@ -1171,8 +1171,8 @@ void RSRenderNode::UpdateFilterCacheWithDirty(RSDirtyRegionManager& dirtyManager
         return;
     }
     auto& cachedImageRect = manager->GetCachedImageRegion();
-    if (dirtyManager.HasIntersectionWithVisitedDirtyRect(cachedImageRect)) {
-        manager->UpdateCacheStateWithDirtyRegion();
+    if (manager->UpdateCacheStateWithDirtyRegion(dirtyManager)) {
+        dirtyManager.MergeDirtyRect(cachedImageRect);
     }
 #endif
 }
