02e6a99c031eef0616413cc3b382ce3f3d4ac6a4
denispavlov <pavlov.denis@huawei.com>
2024-02-22 06:11:56 -0800

    Add RsProfilerCaptureRecorder and replay
    Signed-off-by: denispavlov <pavlov.denis@huawei.com>

---
+++ rosen/modules/render_service_profiler/rs_profiler_capture_recorder.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rs_profiler_capture_recorder.h"
+
+#ifndef REPLAY_TOOL_CLIENT
+#include "rs_profiler_base.h"
+#include "platform/common/rs_log.h"
+#else
+#include "../rs_log.h"
+#endif
+
+namespace OHOS::Rosen {
+
+    Drawing::Canvas* RSCaptureRecorder::TryInstantCapture(float width, float height){
+        if(RSSystemProperties::GetSaveRDC()){
+            // for saving .drawing file
+            return TryInstantCaptureDrawing(width, height);
+        }
+        // for saving .skp file
+        return TryInstantCaptureSKP(width, height); 
+    }
+
+    void RSCaptureRecorder::EndInstantCapture(){
+        if(RSSystemProperties::GetSaveRDC()){
+            // for saving .drawing file
+            EndInstantCaptureDrawing();
+            return;
+        }
+        // for saving .skp file
+        EndInstantCaptureSKP();
+    }
+
+    ExtendRecordingCanvas* RSCaptureRecorder::TryInstantCaptureDrawing(float width, float height)
+    {
+        if (!RSSystemProperties::GetInstantRecording()) {
+            return nullptr;
+        }
+        
+        recordingCanvas_ = std::make_unique<ExtendRecordingCanvas>(width, height);
+        
+        return recordingCanvas_.get();
+    }
+
+    void RSCaptureRecorder::EndInstantCaptureDrawing() 
+    {
+        if (!RSSystemProperties::GetInstantRecording()) {
+            return;
+        }
+        auto drawCmdList = recordingCanvas_->GetDrawCmdList();
+
+        std::shared_ptr<MessageParcel> messageParcel = std::make_shared<MessageParcel>();
+        messageParcel->SetMaxCapacity(RECORDING_PARCEL_CAPCITY);
+        RSMarshallingHelper::BeginNoSharedMem(std::this_thread::get_id());
+        RSMarshallingHelper::Marshalling(*messageParcel, drawCmdList);
+        RSMarshallingHelper::EndNoSharedMem();
+
+        size_t parcelSize = messageParcel->GetDataSize();
+        uintptr_t parcelBuf = messageParcel->GetData();
+
+        // Create file and write the parcel
+        FILE *f = fopen(drawCmdListFilename_.c_str(), "wbe");
+        if (f == NULL) {
+            RS_LOGE("Error while writing to file");
+        } else {
+            Utils::FileWrite(reinterpret_cast<uint8_t *>(parcelBuf), sizeof(uint8_t), parcelSize, f);
+            fclose(f);
+        }
+        
+        const size_t minPacketSize = 2; 
+        char* buf = new char[minPacketSize];
+        buf[0] = (char)OHOS::Rosen::PackageID::RS_PROFILER_DCL_BINARY;
+        buf[1] = 't';
+        OHOS::Rosen::Network::SendBinary(buf, minPacketSize);
+        
+        OHOS::Rosen::Network::SendMessage("Saved locally");
+        RSSystemProperties::SetInstantRecording(false);
+    }
+
+    Drawing::Canvas* RSCaptureRecorder::TryInstantCaptureSKP(float width, float height)
+    {
+        if (!RSSystemProperties::GetInstantRecording()) {
+            return nullptr;
+        }
+        OHOS::Rosen::Network::SendMessage("Starting .skp capturing.");
+
+        recordingSkpCanvas_ = std::make_shared<Drawing::Canvas>(width, height);
+        skRecorder_.reset();
+        skRecorder_ = std::make_unique<SkPictureRecorder>();
+        skiaCanvas_ = skRecorder_->beginRecording(width, height);
+
+        recordingSkpCanvas_->GetImpl<Drawing::SkiaCanvas>()->ImportSkCanvas(skiaCanvas_);
+        return recordingSkpCanvas_.get();
+    }
+
+    void RSCaptureRecorder::EndInstantCaptureSKP() 
+    {   
+        if (!RSSystemProperties::GetInstantRecording()){
+            return;
+        }
+        OHOS::Rosen::Network::SendMessage("Finishing .skp capturing.");
+
+        sk_sp<SkPicture> picture = skRecorder_->finishRecordingAsPicture();
+        
+        if(picture->approximateOpCount() > 0) {
+            SkSerialProcs procs;
+            procs.fTypefaceProc = [](SkTypeface* tf, void* ctx) {
+                return tf->serialize(SkTypeface::SerializeBehavior::kDoIncludeData);
+            };
+            auto data = picture->serialize(&procs);
+
+            // to send to client as binary
+            const int size = 1 + data->size();
+            char* buf = new char[size];
+            buf[0] = (char)OHOS::Rosen::PackageID::RS_PROFILER_SKP_BINARY;
+            std::memcpy(&buf[1], data->data(), data->size());
+            
+            OHOS::Rosen::Network::SendMessage("SKP file sending");
+            OHOS::Rosen::Network::SendBinary(buf, size);
+            OHOS::Rosen::Network::SendMessage("SKP file sent");
+            delete [] buf;
+        }
+        RSSystemProperties::SetInstantRecording(false);
+    }
+ } // namespace OHOS::Rosen
---
+++ rosen/samples/2d_graphics/drawing_sample_replayer.cpp
@@ -0,0 +1,242 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+#include <iostream>
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "window.h"
+
+#include "drawing_sample_replayer.h"
+
+
+namespace OHOS::Rosen {
+
+DrawingSampleReplayer::~DrawingSampleReplayer(){
+    DestoryNativeWindow(nativeWindow_);
+    surfaceNode_->DetachToDisplay(DEFAULT_DISPLAY_ID);
+}
+
+int DrawingSampleReplayer::ReadCmds(const std::string path){
+    int32_t fd = open(path.c_str(), O_RDONLY);
+    if (fd <= 0) {
+        return -1;
+    }
+    struct stat statbuf;
+    if (fstat(fd, &statbuf) < 0) {
+        return -1;
+    }
+    auto mapFile = static_cast<uint8_t*>(mmap(NULL, statbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0));
+    if (mapFile == MAP_FAILED) {
+        close(fd);
+        return -1;
+    }
+    MessageParcel messageParcel(new EmptyAllocator());
+    if (!messageParcel.ParseFrom(reinterpret_cast<uintptr_t>(mapFile), statbuf.st_size)) {
+        munmap(mapFile, statbuf.st_size);
+        close(fd);
+        return -1;
+    }
+    RSMarshallingHelper::BeginNoSharedMem(std::this_thread::get_id());
+    RSMarshallingHelper::Unmarshalling(messageParcel, dcl_);
+    RSMarshallingHelper::EndNoSharedMem();
+    if (dcl_ == nullptr) {
+        std::cout << "dcl is nullptr" << std::endl;
+        munmap(mapFile, statbuf.st_size);
+        return -1;
+    }
+    //std::cout << dcl_->GetOpsWithDesc() << std::endl;
+    munmap(mapFile, statbuf.st_size);
+    return 0;
+}
+
+void DrawingSampleReplayer::PrepareFrame(Drawing::Canvas* canvas){
+    if (canvas == nullptr) {
+        return;
+    }
+
+#if (defined RS_ENABLE_GL) || (defined RS_ENABLE_VK)
+    if(captureMode_ == CaptureMode::SKP){
+        // skia recording canvas adding to nwaycanvas
+        std::cout << "DrawingDCL::Test skia" << std::endl;
+        orgSkiaCanvas_ = canvas->GetImpl<Drawing::SkiaCanvas>()->ExportSkCanvas();
+        
+        std::cout << "DrawingDCL::Test skia begin capture+" << std::endl;
+        //skiaCanvas_ = skiaRecording.BeginCapture(orgSkiaCanvas_, width, height);
+        recorder_ = std::make_unique<SkPictureRecorder>();
+        pictureCanvas_ = recorder_->beginRecording(drawingWidth_, drawingHeight_);
+        // Setting up an nway canvas is common to any kind of capture.
+        if (nwayCanvas_ == nullptr) {
+            nwayCanvas_ = std::make_unique<SkNWayCanvas>(drawingWidth_, drawingHeight_);
+        }
+        nwayCanvas_->addCanvas(orgSkiaCanvas_);
+        nwayCanvas_->addCanvas(pictureCanvas_);
+
+        canvas->GetImpl<Drawing::SkiaCanvas>()->ImportSkCanvas(nwayCanvas_.get());
+    }
+#endif
+
+    if (ReadCmds("/data/default.drawing") < 0) {
+        std::cout << "Failed to load .drawing cmds" << std::endl;
+        return;
+    }
+    dcl_->Playback(*canvas);
+
+#if (defined RS_ENABLE_GL) || (defined RS_ENABLE_VK)
+    if(captureMode_ == CaptureMode::SKP){
+        // finishing capturing and saving skp
+        nwayCanvas_->removeAll();
+        sk_sp<SkPicture> picture = recorder_->finishRecordingAsPicture();
+
+        if(picture->approximateOpCount() > 0) {
+            SkSerialProcs procs;
+            procs.fTypefaceProc = [](SkTypeface* tf, void* ctx) {
+                return tf->serialize(SkTypeface::SerializeBehavior::kDoIncludeData);
+            };
+            auto data = picture->serialize(&procs);
+
+            // to save locally
+            std::string outputFile = "/data/default.skp";
+            SkFILEWStream stream(outputFile.c_str());
+            if (stream.isValid()) {
+                stream.write(data->data(), data->size());
+                stream.flush();
+                std::cout << "Written and flushed" << std::endl;
+            } else {
+                std::cout << "Failed to create stream" << std::endl;
+            }
+        }
+        canvas->GetImpl<Drawing::SkiaCanvas>()->ImportSkCanvas(orgSkiaCanvas_);
+    }
+#endif
+}
+
+void DrawingSampleReplayer::RenderLoop(){
+    PrepareNativeEGLSetup();
+    int maxIter = (captureMode_ == CaptureMode::RDC) ? MAX_RENDERED_FRAMES : 1; 
+
+    for(int i = 0; i < maxIter; ++i){
+        auto surface = renderContext_->AcquireSurface(drawingWidth_, drawingHeight_);
+        Drawing::Canvas* canvas = surface->GetCanvas().get();
+
+        std::cout << i << "\t >> new iteration" << std::endl;
+
+        PrepareFrame(canvas);
+
+        renderContext_->RenderFrame(); // which involves surface->flush()
+        renderContext_->SwapBuffers(eglSurface_);
+
+        int rdcnum = 0;
+        std::string path = "/data/autocaps";
+        // Checks if the folder exists
+        if(fs::exists(path)){
+            for (const auto & entry : fs::directory_iterator(path)) {
+                std::string filestr = entry.path();                
+                if(filestr.substr( filestr.length() - 3 ) == "rdc"){
+                    rdcnum++;
+                }
+            }
+            if(rdcnum > 0){
+                return;
+            }
+        }
+
+    }
+}
+
+void DrawingSampleReplayer::PrepareNativeEGLSetup(){
+    renderContext_ = std::make_shared<RenderContext>();
+    renderContext_->InitializeEglContext();
+
+    auto defaultDisplay = DisplayManager::GetInstance().GetDefaultDisplay();
+    drawingWidth_ = defaultDisplay->GetWidth();
+    drawingHeight_ = defaultDisplay->GetHeight();
+
+    std::cout << "Width: " << drawingWidth_ << "  -  Height: " << drawingHeight_ << std::endl;
+
+
+    RSSurfaceNodeConfig surfaceNodeConfig = {"DrawingEngineSample"};
+    RSSurfaceNodeType surfaceNodeType = RSSurfaceNodeType::SELF_DRAWING_WINDOW_NODE;
+    surfaceNode_ = RSSurfaceNode::Create(surfaceNodeConfig, surfaceNodeType);
+    if (!surfaceNode_) {
+        std::cout << "surfaceNode is nullptr" << std::endl;
+        return;
+    }
+
+    surfaceNode_->SetFrameGravity(Gravity::RESIZE_ASPECT_FILL);
+    surfaceNode_->SetPositionZ(RSSurfaceNode::POINTER_WINDOW_POSITION_Z);
+    surfaceNode_->SetBounds(0, 0, drawingWidth_, drawingHeight_);
+    surfaceNode_->AttachToDisplay(DEFAULT_DISPLAY_ID);
+    RSTransaction::FlushImplicitTransaction();
+
+    sptr<Surface> ohosSurface = surfaceNode_->GetSurface();
+    nativeWindow_ = CreateNativeWindowFromSurface(&ohosSurface);
+    if (nativeWindow_ == nullptr) {
+        std::cout << "CreateNativeWindowFromSurface Failed" << std::endl;
+        return;
+    }
+
+    eglSurface_ = renderContext_->CreateEGLSurface((EGLNativeWindowType)nativeWindow_);
+    if (eglSurface_ == EGL_NO_SURFACE) {
+        std::cout << "Invalid eglSurface, return" << std::endl;
+        DestoryNativeWindow(nativeWindow_);
+        return;
+    }
+
+    renderContext_->MakeCurrent(eglSurface_);
+    EGLint initRes = eglGetError();
+    if (initRes != EGL_SUCCESS) {
+        std::cout << "Fail to init egl" << std::endl;
+        DestoryNativeWindow(nativeWindow_);
+        return;
+    }
+    NativeWindowHandleOpt(nativeWindow_, SET_BUFFER_GEOMETRY, drawingWidth_, drawingHeight_);
+
+    //renderContext->SwapBuffers(eglSurface); 
+}
+
+void DrawingSampleReplayer::SetCaptureMode(CaptureMode mode){
+    captureMode_ = mode;
+}
+
+} // namespace OHOS::Rosen
+
+int32_t main(int32_t argc, char *argv[])
+{
+    OHOS::Rosen::DrawingSampleReplayer replayer;
+    
+    std::string replayType = "default";
+    if(argc > 1){
+        replayType = std::string(argv[1]);
+        if(replayType == "skp"){
+            replayer.SetCaptureMode(OHOS::Rosen::CaptureMode::SKP);
+        }
+        else if(replayType == "rdc"){
+            replayer.SetCaptureMode(OHOS::Rosen::CaptureMode::RDC);
+        }
+    }
+    std::cout << "Mode: " << replayType << std::endl;
+
+    replayer.RenderLoop();
+    //replayer.ReadCmds("/data/default.drawing");
+
+    return 0;
+}
