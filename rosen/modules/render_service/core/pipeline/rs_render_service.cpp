46823588aca4e1bb1915f62a82a6ffac09f1385f
yexiandong <yexiandong@huawei.com>
2024-02-27 14:24:26 +0800

    enable tcache
    Signed-off-by: yexiandong <yexiandong@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_render_service.cpp
+++ rosen/modules/render_service/core/pipeline/rs_render_service.cpp
@@ -27,6 +27,7 @@
 #include "touch_screen/touch_screen.h"
 #endif
 
+#include <malloc.h>
 #include <string>
 #include <unistd.h>
 
@@ -46,6 +47,12 @@ RSRenderService::~RSRenderService() noexcept {}
 
 bool RSRenderService::Init()
 {
+    // enable cache
+    mallopt(M_OHOS_CONFIG, M_TCACHE_NORMAL_MODE);
+    mallopt(M_OHOS_CONFIG, M_ENABLE_OPT_TCACHE);
+    mallopt(M_SET_THREAD_CACHE, M_THREAD_CACHE_ENABLE);
+    mallopt(M_DELAYED_FREE, M_DELAYED_FREE_ENABLE);
+
     RSMainThread::Instance();
     RSUniRenderJudgement::InitUniRenderConfig();
 #ifdef TP_FEATURE_ENABLE
