b60685cfabd59ca0b79eb17b027fc7597c011788
hehao <hehao16@huawei.com>
2024-04-12 19:10:52 +0800

    存在共享元素时关闭交互优先

--- rosen/modules/render_service_base/include/pipeline/rs_render_node.h
+++ rosen/modules/render_service_base/include/pipeline/rs_render_node.h
@@ -531,7 +531,7 @@ public:
 
     virtual bool GetUifirstSupportFlag()
     {
-        return !GetSharedTransitionParam() && isChildSupportUifirst_;
+        return !GetRenderProperties().GetSandBox() && isChildSupportUifirst_;
     }
 
     virtual void MergeOldDirtyRect()
