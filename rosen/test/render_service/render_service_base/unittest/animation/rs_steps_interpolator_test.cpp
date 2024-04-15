2d4b29660e367a6096fabf429485a5b50ce39e69
13556317231 <liwei576@huawei.com>
2024-04-15 17:25:24 +0800

    add_TDD_animation01
    Signed-off-by: 13556317231 <liwei576@huawei.com>
    Change-Id: Id5390f5d781ac7e13f72c41be8b88ed196501993

--- rosen/test/render_service/render_service_base/unittest/animation/rs_steps_interpolator_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/animation/rs_steps_interpolator_test.cpp
@@ -35,8 +35,8 @@ void RSStepsInterpolatorTest::TearDown() {}
 
 /**
  * @tc.name: StepsCurveTest001
- * @tc.desc:
- * @tc.type:FUNC
+ * @tc.desc: test results of StepsCurve
+ * @tc.type: FUNC
  */
 HWTEST_F(RSStepsInterpolatorTest, StepsCurveTest001, TestSize.Level1)
 {
@@ -68,8 +68,8 @@ HWTEST_F(RSStepsInterpolatorTest, StepsCurveTest001, TestSize.Level1)
 
 /**
  * @tc.name: StepsCurveTest002
- * @tc.desc:
- * @tc.type:FUNC
+ * @tc.desc: test results of StepsCurve
+ * @tc.type: FUNC
  */
 HWTEST_F(RSStepsInterpolatorTest, StepsCurveTest002, TestSize.Level1)
 {
@@ -101,8 +101,8 @@ HWTEST_F(RSStepsInterpolatorTest, StepsCurveTest002, TestSize.Level1)
 
 /**
  * @tc.name: StepsCurveTest003
- * @tc.desc:
- * @tc.type:FUNC
+ * @tc.desc: test results of Interpolate
+ * @tc.type: FUNC
  */
 HWTEST_F(RSStepsInterpolatorTest, StepsCurveTest003, TestSize.Level1)
 {
@@ -119,5 +119,57 @@ HWTEST_F(RSStepsInterpolatorTest, StepsCurveTest003, TestSize.Level1)
      */
     ASSERT_EQ(1.0f, stepsCurveStart.Interpolate(testValueFirst));
     ASSERT_EQ(1.0f, stepsCurveStart.Interpolate(testValueSecond));
+}
+
+/**
+ * @tc.name: InterpolateImplTest
+ * @tc.desc: test results of InterpolateImpl
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSStepsInterpolatorTest, InterpolateImplTest, TestSize.Level1)
+{
+    RSStepsInterpolator stepsCurveStart(0, 0);
+    RSStepsInterpolator stepsCurveStart2(0, 1);
+    float fraction = -1.0f;
+    float res = stepsCurveStart.InterpolateImpl(fraction);
+    EXPECT_EQ(res, 1.0f);
+
+    fraction = 3.0f;
+    res = stepsCurveStart.InterpolateImpl(fraction);
+    EXPECT_EQ(res, 1.0f);
+
+    fraction = 0.0f;
+    res = stepsCurveStart.InterpolateImpl(fraction);
+    EXPECT_EQ(res, 1.0f);
+
+    res = stepsCurveStart2.InterpolateImpl(fraction);
+    EXPECT_EQ(res, 1.0f);
+}
+
+/**
+ * @tc.name: MarshallingTest
+ * @tc.desc: test results of Marshalling
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSStepsInterpolatorTest, MarshallingTest, TestSize.Level1)
+{
+    RSStepsInterpolator stepsCurveStart(0);
+    Parcel parcel;
+    bool res = stepsCurveStart.Marshalling(parcel);
+    ASSERT_EQ(res, true);
+}
+
+/**
+ * @tc.name: UnmarshallingTest
+ * @tc.desc: test results of Unmarshalling
+ * @tc.type: FUNC
+ */
+HWTEST_F(RSStepsInterpolatorTest, UnmarshallingTest, TestSize.Level1)
+{
+    RSStepsInterpolator stepsCurveStart(0);
+    Parcel parcel;
+    RSStepsInterpolator* step;
+    step = stepsCurveStart.Unmarshalling(parcel);
+    ASSERT_EQ(step, nullptr);
 }
 } // namespace OHOS::Rosen
