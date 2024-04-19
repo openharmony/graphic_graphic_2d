e51fefb842e780ca3de001bb7bfa1321a6d97ca8
李蔚 <liwei576@huawei.com>
2024-04-19 11:18:35 +0800

    04192nd
    Signed-off-by: 李蔚 <liwei576@huawei.com>
    Change-Id: I5b086215a418fa6eb2c800e2ba2615e8f91a68f8

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_proxy_render_node_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_proxy_render_node_test.cpp
@@ -41,7 +41,7 @@ void RSProxyRenderNodeTest::TearDown() {}
  * @tc.name: LifeCycel001
  * @tc.desc: test
  * @tc.type:FUNC
- * @tc.require:
+ * @tc.require: issueI9H4AD
  */
 HWTEST_F(RSProxyRenderNodeTest, LifeCycel001, TestSize.Level1)
 {
@@ -62,6 +62,140 @@ HWTEST_F(RSProxyRenderNodeTest, LifeCycel001, TestSize.Level1)
     node->SetContextAlpha(0.0f);
     clipRegion = Drawing::Rect(0.f, 0.f, 10.f, 10.f);
     node->SetContextClipRegion(clipRegion);
+}
+
+/**
+ * @tc.name: Process
+ * @tc.desc: test results of Process
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, Process, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    RSSurfaceRenderNodeConfig config;
+    auto target = std::make_shared<RSSurfaceRenderNode>(config);
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+    std::shared_ptr<RSNodeVisitor> visitor;
+    node->Process(visitor);
+    ASSERT_TRUE(true);
+}
+
+/**
+ * @tc.name: Prepare
+ * @tc.desc: test results of Prepare
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, Prepare, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    RSSurfaceRenderNodeConfig config;
+    auto target = std::make_shared<RSSurfaceRenderNode>(config);
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+    std::shared_ptr<RSNodeVisitor> visitor;
+    node->Prepare(visitor);
+    ASSERT_TRUE(true);
+}
+
+/**
+ * @tc.name: SetContextMatrix
+ * @tc.desc: test results of SetContextMatrix
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, SetContextMatrix, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    std::weak_ptr<RSSurfaceRenderNode> target;
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+    node->SetContextMatrix(node->contextMatrix_);
+    auto matrix = Drawing::Matrix();
+    node->SetContextMatrix(matrix);
+    ASSERT_EQ(node->contextMatrix_, matrix);
+}
+
+/**
+ * @tc.name: SetContextAlpha
+ * @tc.desc: test results of clipRegion
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, SetContextAlpha, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    RSSurfaceRenderNodeConfig config;
+    auto target = std::make_shared<RSSurfaceRenderNode>(config);
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+
+    node->SetContextAlpha(1.0f);
+    node->target_.reset();
+    node->SetContextAlpha(1.0f);
+    ASSERT_EQ(node->contextAlpha_, 1.0f);
+}
+
+/**
+ * @tc.name: SetContextClipRegion
+ * @tc.desc: test results of SetContextClipRegion
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, SetContextClipRegion, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    RSSurfaceRenderNodeConfig config;
+    auto target = std::make_shared<RSSurfaceRenderNode>(config);
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+    auto clipRegion = Drawing::Rect();
+    node->SetContextClipRegion(node->contextClipRect_);
+    node->target_.reset();
+    node->SetContextClipRegion(clipRegion);
+    ASSERT_EQ(node->contextClipRect_, clipRegion);
+}
+
+/**
+ * @tc.name: OnTreeStateChanged
+ * @tc.desc: test results of OnTreeStateChanged
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, OnTreeStateChanged, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    RSSurfaceRenderNodeConfig config;
+    auto target = std::make_shared<RSSurfaceRenderNode>(config);
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+    auto clipRegion = Drawing::Rect();
+    node->OnTreeStateChanged();
+    node->isOnTheTree_ = true;
+    node->OnTreeStateChanged();
+    ASSERT_EQ(node->contextAlpha_, -1.0f);
+}
+
+/**
+ * @tc.name: CleanUp
+ * @tc.desc: test results of CleanUp
+ * @tc.type:FUNC
+ * @tc.require: issueI9H4AD
+ */
+HWTEST_F(RSProxyRenderNodeTest, CleanUp, TestSize.Level1)
+{
+    NodeId id = 10;
+    NodeId targetId = 11;
+    RSSurfaceRenderNodeConfig config;
+    auto target = std::make_shared<RSSurfaceRenderNode>(config);
+    auto node = std::make_shared<RSProxyRenderNode>(id, target, targetId);
+    bool removeModifiers = true;
+    node->CleanUp(removeModifiers);
+    removeModifiers = false;
+    node->CleanUp(removeModifiers);
+    ASSERT_EQ(node->target_.lock()->contextAlpha_, 1.0f);
 }
 } // namespace Rosen
 } // namespace OHOS
