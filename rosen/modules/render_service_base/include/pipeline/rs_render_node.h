06fc54680c622f5072e3bebaff8579e643d28822
chenlushen <chenlushen1@huawei.com>
2024-04-19 11:58:27 +0800

    fix shadow dirty bug & fix self drawing node size change bug
    Signed-off-by: chenlushen <chenlushen1@huawei.com>
    Change-Id: I4da13166c58e1108bf7acaa7d282b9c201618f8b

--- rosen/modules/render_service_base/include/pipeline/rs_render_node.h
+++ rosen/modules/render_service_base/include/pipeline/rs_render_node.h
@@ -728,7 +728,7 @@ private:
     void FallbackAnimationsToRoot();
     void FilterModifiersByPid(pid_t pid);
 
-    void UpdateBufferDirtyRegion(RectI& dirtyRect, const RectI& drawRegion);
+    bool UpdateBufferDirtyRegion(RectI& dirtyRect, const RectI& drawRegion);
     void CollectAndUpdateLocalShadowRect();
     void CollectAndUpdateLocalOutlineRect();
     void CollectAndUpdateLocalPixelStretchRect();
@@ -824,6 +824,13 @@ private:
     float boundsHeight_ = 0.0f;
     bool hasCacheableAnim_ = false;
     bool geometryChangeNotPerceived_ = false;
+    // node region, only used in selfdrawing node dirty
+    bool isSelfDrawingNode_ = false;
+    RectF selfDrawingNodeDirtyRect_;
+    RectI selfDrawingNodeAbsDirtyRect_;
+    RectI oldAbsDrawRect_;
+    // used in old pipline
+    RectI oldRectFromRenderProperties_;
     // including enlarged draw region
     RectF selfDrawRect_;
     RectI localShadowRect_;
