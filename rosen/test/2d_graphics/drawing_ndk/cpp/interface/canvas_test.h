e725f4915303deb131e91f10de4e9738b755bc8c
秦莉文 <qinliwen3@huawei.com>
2024-04-30 17:08:27 +0800

    drawing ndk interface 2 head file Signed-off-by: qinliwen <qinliwen3@huawei.com>

---
+++ rosen/test/2d_graphics/drawing_ndk/cpp/interface/canvas_test.h
@@ -0,0 +1,421 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CANVAS_TEST_H
+#define CANVAS_TEST_H
+#include <bits/alltypes.h>
+#include <multimedia/player_framework/native_avscreen_capture_base.h>
+#include <native_drawing/drawing_canvas.h>
+#include <native_drawing/drawing_rect.h>
+
+#include "../test_base.h"
+#include "test_common.h"
+class CanvasDrawRect : public TestBase {
+public:
+    explicit CanvasDrawRect(int type) : TestBase(type) {};
+    ~CanvasDrawRect() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+// 林江涛用例
+class CanvasDrawImageRect : public TestBase {
+public:
+    explicit CanvasDrawImageRect(int type) : TestBase(type) {};
+    ~CanvasDrawImageRect() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+};
+
+/*
+ * 测试void OH_Drawing_CanvasRotate
+ */
+class CanvasRotate : public TestBase {
+public:
+    explicit CanvasRotate(int type) : TestBase(type) {};
+    ~CanvasRotate() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+// 测试void OH_Drawing_CanvasTranslate
+class CanvasTranslate : public TestBase {
+public:
+    explicit CanvasTranslate(int type) : TestBase(type) {};
+    ~CanvasTranslate() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+// void OH_Drawing_CanvasSkew
+class CanvasScale : public TestBase {
+public:
+    explicit CanvasScale(int type) : TestBase(type) {};
+    ~CanvasScale() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+class CanvasConcatMatrix : public TestBase {
+public:
+    explicit CanvasConcatMatrix(int type) : TestBase(type) {};
+    ~CanvasConcatMatrix() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+// 清理画布
+class CanvasClear : public TestBase {
+public:
+    explicit CanvasClear(int type) : TestBase(type) {};
+    ~CanvasClear() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+// 设置画布的矩阵状态。
+class CanvasSetMatrix : public TestBase {
+public:
+    explicit CanvasSetMatrix(int type) : TestBase(type) {};
+    ~CanvasSetMatrix() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasResetMatrix : public TestBase {
+public:
+    explicit CanvasResetMatrix(int type) : TestBase(type) {};
+    ~CanvasResetMatrix() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawImageRectWithSrc : public TestBase {
+public:
+    explicit CanvasDrawImageRectWithSrc(int type) : TestBase(type) {};
+    ~CanvasDrawImageRectWithSrc() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+// 3
+class CanvasReadPixels : public TestBase {
+public:
+    explicit CanvasReadPixels(int type) : TestBase(type) {};
+    ~CanvasReadPixels() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+// 4
+class CanvasReadPixelsToBitmap : public TestBase {
+public:
+    explicit CanvasReadPixelsToBitmap(int type) : TestBase(type) {};
+    ~CanvasReadPixelsToBitmap() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawShadow : public TestBase {
+public:
+    explicit CanvasDrawShadow(int type) : TestBase(type) {};
+    ~CanvasDrawShadow() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+class CanvasSkew : public TestBase {
+public:
+    explicit CanvasSkew(int type) : TestBase(type) {};
+    ~CanvasSkew() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+class CanvasDrawVertices : public TestBase {
+public:
+    explicit CanvasDrawVertices(int type) : TestBase(type) {};
+    ~CanvasDrawVertices() = default;
+
+protected:
+    int rectWidth_ = 0;  // 0 矩形宽度
+    int rectHeight_ = 0; // 0 矩形高度
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+
+private:
+    TestRend renderer;
+};
+
+class CanvasDrawPath : public TestBase {
+public:
+    explicit CanvasDrawPath(int type) : TestBase(type) {};
+    ~CanvasDrawPath() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasRestoreToCount : public TestBase {
+public:
+    explicit CanvasRestoreToCount(int type) : TestBase(type) {};
+    ~CanvasRestoreToCount() = default;
+
+protected:
+    uint32_t fSave_Count = 0; // 0 保存的数量
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawPoints : public TestBase {
+public:
+    explicit CanvasDrawPoints(int type) : TestBase(type) {};
+    ~CanvasDrawPoints() = default;
+
+protected:
+    const int n = 30; // 30 可以同时存储的点的数量
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawLine : public TestBase {
+public:
+    explicit CanvasDrawLine(int type) : TestBase(type) {};
+    ~CanvasDrawLine() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawLayer : public TestBase {
+public:
+    explicit CanvasDrawLayer(int type) : TestBase(type) {};
+    ~CanvasDrawLayer() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawBitmap : public TestBase {
+public:
+    explicit CanvasDrawBitmap(int type) : TestBase(type) {};
+    ~CanvasDrawBitmap() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawBackground : public TestBase {
+public:
+    explicit CanvasDrawBackground(int type) : TestBase(type) {};
+    ~CanvasDrawBackground() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawRegion : public TestBase {
+public:
+    explicit CanvasDrawRegion(int type) : TestBase(type) {};
+    ~CanvasDrawRegion() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawPixelMapRect : public TestBase {
+public:
+    explicit CanvasDrawPixelMapRect(int type) : TestBase(type) {};
+    ~CanvasDrawPixelMapRect() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasSave : public TestBase {
+public:
+    explicit CanvasSave(int type) : TestBase(type) {};
+    ~CanvasSave() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawBitmapRect : public TestBase {
+public:
+    explicit CanvasDrawBitmapRect(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasDrawBitmapRect";
+    };
+    ~CanvasDrawBitmapRect() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawCircle : public TestBase {
+public:
+    explicit CanvasDrawCircle(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasDrawCircle";
+    };
+    ~CanvasDrawCircle() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawOval : public TestBase {
+public:
+    explicit CanvasDrawOval(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasDrawOval";
+    };
+    ~CanvasDrawOval() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawArc : public TestBase {
+public:
+    explicit CanvasDrawArc(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasDrawArc";
+    };
+    ~CanvasDrawArc() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawRoundRect : public TestBase {
+public:
+    explicit CanvasDrawRoundRect(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasDrawRoundRect";
+    };
+    ~CanvasDrawRoundRect() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasDrawTextBlob : public TestBase {
+public:
+    explicit CanvasDrawTextBlob(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasDrawTextBlob";
+    };
+    ~CanvasDrawTextBlob() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasClipRect : public TestBase {
+public:
+    explicit CanvasClipRect(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasClipRect";
+    };
+    ~CanvasClipRect() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasClipRoundRect : public TestBase {
+public:
+    explicit CanvasClipRoundRect(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasClipRoundRect";
+    };
+    ~CanvasClipRoundRect() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+
+class CanvasClipPath : public TestBase {
+public:
+    explicit CanvasClipPath(int type) : TestBase(type)
+    {
+        fileName_ = "CanvasClipPath";
+    };
+    ~CanvasClipPath() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override;
+};
+#endif // CANVAS_TEST_H
---
+++ rosen/test/2d_graphics/drawing_ndk/cpp/interface/text_blob_test.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TEXT_BLOB_TEST_H
+#define TEXT_BLOB_TEST_H
+#include <bits/alltypes.h>
+#include <native_drawing/drawing_canvas.h>
+#include <native_drawing/drawing_rect.h>
+
+#include "../test_base.h"
+
+class TextBlobBuilderCreate : public TestBase {
+public:
+    explicit TextBlobBuilderCreate(int type) : TestBase(type)
+    {
+        fileName_ = "TextBlobBuilderCreate";
+    };
+    ~TextBlobBuilderCreate() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override; // 614 ms 1000次
+};
+
+class TextBlobCreateFromText : public TestBase {
+public:
+    explicit TextBlobCreateFromText(int type) : TestBase(type)
+    {
+        fileName_ = "TextBlobCreateFromText";
+    };
+    ~TextBlobCreateFromText() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override; // 440 ms 100次
+};
+
+class TextBlobCreateFromPosText : public TestBase {
+public:
+    explicit TextBlobCreateFromPosText(int type) : TestBase(type)
+    {
+        fileName_ = "TextBlobCreateFromPosText";
+    };
+    ~TextBlobCreateFromPosText() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override; // 230 ms 100次
+};
+
+class TextBlobCreateFromString : public TestBase {
+public:
+    explicit TextBlobCreateFromString(int type) : TestBase(type)
+    {
+        fileName_ = "TextBlobCreateFromString";
+    };
+    ~TextBlobCreateFromString() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override; // 486 ms 100次
+};
+
+class TextBlobGetBounds : public TestBase {
+public:
+    explicit TextBlobGetBounds(int type) : TestBase(type)
+    {
+        fileName_ = "TextBlobGetBounds";
+    };
+    ~TextBlobGetBounds() = default;
+
+protected:
+    void OnTestPerformance(OH_Drawing_Canvas* canvas) override; // 365 ms 100次
+};
+#endif // TEXT_BLOB_CREATE_FROM_TEXT_H
