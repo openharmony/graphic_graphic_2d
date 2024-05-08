2f923dc000dd5199e7ea8299d4a8ecf99193e0b7
changleipeng <changleipeng4@huawei.com>
2024-05-08 11:36:24 +0800

    overdraw new 0508
    Signed-off-by: changleipeng <changleipeng4@huawei.com>
    Change-Id: I5dc133dc3fdf0316ef968a1627b71df9247acb67

--- rosen/test/render_service/render_service_base/unittest/overdraw/rs_listened_canvas_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/overdraw/rs_listened_canvas_test.cpp
@@ -35,6 +35,13 @@ using namespace testing::ext;
 
 namespace OHOS {
 namespace Rosen {
+constexpr float SET_XORY1 = 1.0f;
+constexpr float SET_XORY2 = 2.0f;
+constexpr float SET_RADIUS = 1.0f;
+constexpr float SET_START_ANGLE = 1.0f;
+constexpr float SET_SWEEP_ANGLE = 1.0f;
+constexpr uint32_t SET_TOP = 100;
+constexpr uint32_t SET_BOTTOM = 100;
 class RSListenedCanvasTest : public testing::Test {
 public:
     static void SetUpTestCase();
@@ -63,6 +70,34 @@ class MockRSCanvasListener : public RSCanvasListener {
 public:
     explicit MockRSCanvasListener(Drawing::Canvas& canvas) : RSCanvasListener(canvas) {}
     MOCK_METHOD1(DrawRect, void(const Drawing::Rect& rect));
+};
+
+class TextBlobImplTest : public Drawing::TextBlobImpl {
+public:
+    TextBlobImplTest() {}
+    // Corrected constructor
+    std::shared_ptr<Drawing::Data> Serialize(void* ctx) const override
+    {
+        std::shared_ptr<Drawing::Data> data = std::make_shared<Drawing::Data>();
+        EXPECT_TRUE(data);
+        return data;
+    }
+
+    std::shared_ptr<Drawing::Rect> Bounds() const override
+    {
+        std::shared_ptr<Drawing::Rect> rect = std::make_shared<Drawing::Rect>();
+        EXPECT_TRUE(rect);
+        return rect;
+    }
+    Drawing::AdapterType GetType() const override
+    {
+        return Drawing::AdapterType::SKIA_ADAPTER;
+    }
+    uint32_t UniqueID() const override
+    {
+        static uint32_t idCounter = 0;
+        return idCounter++;
+    }
 };
 
 /*
@@ -206,19 +241,25 @@ HWTEST_F(RSListenedCanvasTest, onDrawPath001, TestSize.Level1)
 }
 
 /**
- * @tc.name: onDrawRegion001
+ * @tc.name: DrawRegion001
  * @tc.desc: test
  * @tc.type:FUNC
- * @tc.require:
+ * @tc.require:issueI9NDED
  */
-HWTEST_F(RSListenedCanvasTest, onDrawRegion001, TestSize.Level1)
+HWTEST_F(RSListenedCanvasTest, DrawRegion001, TestSize.Level1)
 {
-    MockDrawingCanvas canvas;
+    Drawing::Canvas canvasTest1;
+    RSListenedCanvas listenedCanvas(canvasTest1);
+    EXPECT_FALSE(listenedCanvas.listener_);
+
+    Drawing::Canvas canvasTest2;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest2);
+    EXPECT_TRUE(canvasListener);
+
     Drawing::Region region;
-    RSListenedCanvas listenedCanvas(canvas);
-    auto listener = std::make_shared<MockRSCanvasListener>(canvas);
     listenedCanvas.DrawRegion(region);
-    listenedCanvas.SetListener(listener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
     listenedCanvas.DrawRegion(region);
 }
 
@@ -239,6 +280,249 @@ HWTEST_F(RSListenedCanvasTest, onDrawPoints001, TestSize.Level1)
     auto listener = std::make_shared<MockRSCanvasListener>(canvas);
     listenedCanvas.SetListener(listener);
     listenedCanvas.DrawPoint(point);
+}
+
+/**
+ * @tc.name: ListenerIsNullptrTest001
+ * @tc.desc: ListenerIsNullptr Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, ListenerIsNullptrTest001, TestSize.Level1)
+{
+    // When listenedCanvas.listener_ is null, no operation will be performed on the RSListenedCanvas object, so writing
+    // the relevant functions together will not have any impact. If there is an operation on listenedCanvas.listener_
+    // later, pay attention to modifying the test case
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    EXPECT_FALSE(listenedCanvas.listener_);
+
+    listenedCanvas.DrawLine(Drawing::Point(SET_XORY1, SET_XORY1), Drawing::Point(SET_XORY2, SET_XORY1));
+
+    Drawing::Rect rect = Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM);
+    listenedCanvas.DrawRect(rect);
+
+    Drawing::RoundRect roundRect2;
+    listenedCanvas.DrawRoundRect(roundRect2);
+
+    Drawing::RoundRect roundRect21;
+    Drawing::RoundRect roundRect22;
+    listenedCanvas.DrawNestedRoundRect(roundRect21, roundRect22);
+
+    listenedCanvas.DrawPie(Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM), SET_START_ANGLE, SET_SWEEP_ANGLE);
+
+    listenedCanvas.DrawCircle(Drawing::Point(SET_XORY1, SET_XORY1), SET_RADIUS);
+
+    Drawing::Brush brush2;
+    listenedCanvas.DrawBackground(brush2);
+
+    Drawing::Path path;
+    Drawing::Point3 planeParams;
+    Drawing::Point3 devLightPos;
+    Drawing::Color ambientColor;
+    Drawing::Color spotColor;
+    listenedCanvas.DrawShadow(
+        path, planeParams, devLightPos, SET_XORY1, ambientColor, spotColor, Drawing::ShadowFlags::NONE);
+
+    Drawing::Picture picture;
+    listenedCanvas.DrawPicture(picture);
+    listenedCanvas.Clear(Drawing::Color::COLOR_BLACK);
+
+    Drawing::Bitmap bitmap;
+    listenedCanvas.DrawBitmap(bitmap, SET_XORY1, SET_XORY1);
+
+    std::shared_ptr<TextBlobImplTest> textBlobImplTest = std::make_shared<TextBlobImplTest>();
+    EXPECT_TRUE(textBlobImplTest);
+    Drawing::TextBlob blob(textBlobImplTest);
+    listenedCanvas.DrawTextBlob(&blob, SET_XORY1, SET_XORY1);
+
+    Drawing::Image imageTest1;
+    Drawing::SamplingOptions samplingTest1;
+    listenedCanvas.DrawImage(imageTest1, SET_XORY1, SET_XORY1, samplingTest1);
+
+    Drawing::Image imageTest2;
+    Drawing::SamplingOptions samplingTest2;
+    listenedCanvas.DrawImageRect(imageTest2, Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM),
+        Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM), samplingTest2,
+        Drawing::SrcRectConstraint::STRICT_SRC_RECT_CONSTRAINT);
+
+    Drawing::Image imageTest3;
+    Drawing::SamplingOptions samplingTest3;
+    listenedCanvas.DrawImageRect(imageTest3, Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM), samplingTest3);
+
+    Drawing::Pen pen;
+    listenedCanvas.AttachPen(pen);
+
+    Drawing::Brush brush;
+    listenedCanvas.AttachBrush(brush);
+
+    listenedCanvas.DetachPen();
+    listenedCanvas.DetachBrush();
+}
+
+/**
+ * @tc.name: DrawingFunctionsTest002
+ * @tc.desc: DrawingFunctions Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, DrawingFunctionsTest002, TestSize.Level1)
+{
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    Drawing::Canvas canvasTest;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest);
+    EXPECT_TRUE(canvasListener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
+
+    Drawing::RoundRect roundRectTest1;
+    listenedCanvas.DrawRoundRect(roundRectTest1);
+
+    Drawing::RoundRect roundRectTest2;
+    Drawing::RoundRect roundRectTest3;
+    listenedCanvas.DrawNestedRoundRect(roundRectTest2, roundRectTest3);
+
+    listenedCanvas.DrawPie(Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM), SET_START_ANGLE, SET_SWEEP_ANGLE);
+
+    listenedCanvas.DrawCircle(Drawing::Point(SET_XORY1, SET_XORY1), SET_RADIUS);
+
+    Drawing::Brush brush;
+    listenedCanvas.DrawBackground(brush);
+
+    Drawing::Path path;
+    Drawing::Point3 planeParams;
+    Drawing::Point3 devLightPos;
+    Drawing::Color ambientColor;
+    Drawing::Color spotColor;
+    listenedCanvas.DrawShadow(
+        path, planeParams, devLightPos, SET_XORY1, ambientColor, spotColor, Drawing::ShadowFlags::NONE);
+
+    Drawing::Picture picture;
+    listenedCanvas.DrawPicture(picture);
+
+    listenedCanvas.Clear(Drawing::Color::COLOR_BLACK);
+}
+
+/**
+ * @tc.name: DrawingFunctionsTest003
+ * @tc.desc: DrawingFunctions Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, DrawingFunctionsTest003, TestSize.Level1)
+{
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    Drawing::Canvas canvasTest;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest);
+    EXPECT_TRUE(canvasListener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
+
+    listenedCanvas.DrawLine(Drawing::Point(SET_XORY1, SET_XORY1), Drawing::Point(SET_XORY2, SET_XORY1));
+
+    Drawing::Rect rect = Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM);
+    listenedCanvas.DrawRect(rect);
+}
+
+/**
+ * @tc.name: BitmapFunctionsTest004
+ * @tc.desc: BitmapFunctions Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, BitmapFunctionsTest004, TestSize.Level1)
+{
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    Drawing::Canvas canvasTest;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest);
+    EXPECT_TRUE(canvasListener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
+
+    Drawing::Bitmap bitmap;
+    listenedCanvas.DrawBitmap(bitmap, SET_XORY1, SET_XORY1);
+}
+
+/**
+ * @tc.name: TextBlobFunctionsTest005
+ * @tc.desc: TextBlobFunctions Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, TextBlobFunctionsTest005, TestSize.Level1)
+{
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    Drawing::Canvas canvasTest;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest);
+    EXPECT_TRUE(canvasListener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
+
+    std::shared_ptr<TextBlobImplTest> textBlobImplTest = std::make_shared<TextBlobImplTest>();
+    EXPECT_TRUE(textBlobImplTest);
+    Drawing::TextBlob blob(textBlobImplTest);
+    listenedCanvas.DrawTextBlob(&blob, SET_XORY1, SET_XORY1);
+}
+
+/**
+ * @tc.name: ImageFunctionsTestTest005
+ * @tc.desc: ImageFunctionsTest Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, ImageFunctionsTest006, TestSize.Level1)
+{
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    Drawing::Canvas canvasTest;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest);
+    EXPECT_TRUE(canvasListener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
+
+    Drawing::Image imageTest1;
+    Drawing::SamplingOptions samplingTest1;
+    listenedCanvas.DrawImage(imageTest1, SET_XORY1, SET_XORY1, samplingTest1);
+
+    Drawing::Image imageTest2;
+    Drawing::SamplingOptions samplingTest2;
+    listenedCanvas.DrawImageRect(imageTest2, Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM),
+        Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM), samplingTest2,
+        Drawing::SrcRectConstraint::STRICT_SRC_RECT_CONSTRAINT);
+
+    Drawing::Image imageTest3;
+    Drawing::SamplingOptions samplingTest3;
+    listenedCanvas.DrawImageRect(imageTest3, Drawing::Rect(0, 0, SET_TOP, SET_BOTTOM), samplingTest3);
+}
+
+/**
+ * @tc.name: PenAndBrushFunctionsTest007
+ * @tc.desc: PenAndBrushFunctions Test
+ * @tc.type:FUNC
+ * @tc.require:issueI9NDED
+ */
+HWTEST_F(RSListenedCanvasTest, PenAndBrushFunctionsTest007, TestSize.Level1)
+{
+    Drawing::Canvas canvas;
+    RSListenedCanvas listenedCanvas(canvas);
+    Drawing::Canvas canvasTest;
+    std::shared_ptr<RSCanvasListener> canvasListener = std::make_shared<RSCanvasListener>(canvasTest);
+    EXPECT_TRUE(canvasListener);
+    listenedCanvas.SetListener(canvasListener);
+    EXPECT_TRUE(listenedCanvas.listener_);
+
+    Drawing::Pen pen;
+    listenedCanvas.AttachPen(pen);
+
+    Drawing::Brush brush;
+    listenedCanvas.AttachBrush(brush);
+
+    listenedCanvas.DetachPen();
+    listenedCanvas.DetachBrush();
 }
 } // namespace Rosen
 } // namespace OHOS
