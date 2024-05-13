a7e45258389f8c61e732b53e4358cd020c62ff59
liuwei793 <liuwei793@h-partners.com>
2024-05-13 17:03:03 +0800

    add_TDD_0513_01
    Signed-off-by: liuwei793 <liuwei793@h-partners.com>
    Change-Id: Ib955972ed9b0f8cfb69036de0449c56f9112f09a

--- rosen/test/render_service/render_service_base/unittest/common/rs_thread_looper_impl_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/common/rs_thread_looper_impl_test.cpp
@@ -15,6 +15,7 @@
 
 #include "gtest/gtest.h"
 #include "rs_thread_looper_impl.h"
+#include <thread>
 
 using namespace testing;
 using namespace testing::ext;
@@ -36,6 +37,15 @@ void RSThreadLooperImplTest::SetUp()
     threadlooperimpl_ = std::make_shared<ThreadLooperImpl> ();
 }
 void RSThreadLooperImplTest::TearDown() {}
+
+class ThreadLooperMessageTest final : public ThreadLooperMessage {
+public:
+    explicit ThreadLooperMessageTest() {};
+
+    ~ThreadLooperMessageTest() override = default;
+
+    void Process(int param) const override {}
+};
 
 /**
  * @tc.name: CreateThreadInstance001
@@ -108,5 +118,107 @@ HWTEST_F(RSThreadLooperImplTest, ProcessAllMessagesTest, TestSize.Level1)
 {
     int timeoutMillis = 0;
     threadlooperimpl_->ProcessAllMessages(timeoutMillis);
+}
+
+/**
+ * @tc.name: RemoveMessagesTest
+ * @tc.desc: Verify function RemoveMessages
+ * @tc.type: FUNC
+ * @tc.require: issuesI9OX7J
+ */
+HWTEST_F(RSThreadLooperImplTest, RemoveMessagesTest, TestSize.Level1)
+{
+    auto message = std::make_shared<ThreadLooperMessageTest>();
+    auto message2 = std::make_shared<ThreadLooperMessageTest>();
+    threadlooperimpl_->PostMessage(message, 0);
+    threadlooperimpl_->PostMessage(0, message, 0);
+    threadlooperimpl_->RemoveMessages(message2);
+    threadlooperimpl_->RemoveMessages(message);
+    EXPECT_TRUE(threadlooperimpl_->queue_.empty());
+}
+
+/**
+ * @tc.name: HaveDelayedMessageToProcessTest
+ * @tc.desc: Verify function HaveDelayedMessageToProcess
+ * @tc.type: FUNC
+ * @tc.require: issuesI9OX7J
+ */
+HWTEST_F(RSThreadLooperImplTest, HaveDelayedMessageToProcessTest, TestSize.Level1)
+{
+    auto message = std::make_shared<ThreadLooperMessageTest>();
+    EXPECT_FALSE(threadlooperimpl_->HaveDelayedMessageToProcess());
+    threadlooperimpl_->PostMessage(0, message, 0);
+    EXPECT_TRUE(threadlooperimpl_->HaveDelayedMessageToProcess());
+}
+
+/**
+ * @tc.name: WaitForMessageTest
+ * @tc.desc: Verify function WaitForMessage
+ * @tc.type: FUNC
+ * @tc.require: issuesI9OX7J
+ */
+HWTEST_F(RSThreadLooperImplTest, WaitForMessageTest, TestSize.Level1)
+{
+    auto message = std::make_shared<ThreadLooperMessageTest>();
+    std::thread wakeUpThread([&]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        threadlooperimpl_->wakeup_ = true;
+        threadlooperimpl_->cv_.notify_one();
+    });
+    threadlooperimpl_->WaitForMessage(-1);
+    wakeUpThread.join();
+    threadlooperimpl_->WaitForMessage(1);
+    threadlooperimpl_->PostMessage(message, 0);
+    threadlooperimpl_->WaitForMessage(1);
+    threadlooperimpl_->queue_.clear();
+    threadlooperimpl_->PostMessage(0, message, 0);
+    threadlooperimpl_->WaitForMessage(1);
+    EXPECT_TRUE(threadlooperimpl_->queue_.empty());
+}
+
+/**
+ * @tc.name: ProcessOneMessageInternalTest
+ * @tc.desc: Verify function ProcessOneMessageInternal
+ * @tc.type: FUNC
+ * @tc.require: issuesI9OX7J
+ */
+HWTEST_F(RSThreadLooperImplTest, ProcessOneMessageInternalTest, TestSize.Level1)
+{
+    auto message = std::make_shared<ThreadLooperMessageTest>();
+    threadlooperimpl_->PostMessage(0, message, 0);
+    EXPECT_TRUE(threadlooperimpl_->ProcessOneMessageInternal());
+    threadlooperimpl_->PostMessage(message, 0);
+    EXPECT_TRUE(threadlooperimpl_->ProcessOneMessageInternal());
+}
+
+/**
+ * @tc.name: ProcessAllMessagesTest002
+ * @tc.desc: Verify function ProcessAllMessages
+ * @tc.type: FUNC
+ * @tc.require: issuesI9OX7J
+ */
+HWTEST_F(RSThreadLooperImplTest, ProcessAllMessagesTest002, TestSize.Level1)
+{
+    threadlooperimpl_->ProcessAllMessages(1);
+    std::thread wakeUpThread([&]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        threadlooperimpl_->wakeup_ = true;
+        threadlooperimpl_->cv_.notify_one();
+    });
+    threadlooperimpl_->ProcessAllMessages(-1);
+    wakeUpThread.join();
+    EXPECT_TRUE(threadlooperimpl_->wakeup_);
+}
+
+/**
+ * @tc.name: WakeUpTest
+ * @tc.desc: Verify function WakeUp
+ * @tc.type: FUNC
+ * @tc.require: issuesI9OX7J
+ */
+HWTEST_F(RSThreadLooperImplTest, WakeUpTest, TestSize.Level1)
+{
+    threadlooperimpl_->WakeUp();
+    EXPECT_TRUE(threadlooperimpl_->wakeup_);
 }
 } // namespace OHOS::Rosen
