bfa5723edae9206a1f28b222aecbd2358bce9854
liuwei793 <liuwei793@h-partners.com>
2024-05-14 19:43:10 +0800

    render051405
    Signed-off-by: liuwei793 <liuwei793@h-partners.com>
    Change-Id: I5311bec5bd3023df6f81e6ca6158c8e4829023ef

---
+++ rosen/test/render_service/render_service_base/fuzztest/rsshader_fuzzer/rsshader_fuzzer.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rsshader_fuzzer.h"
+
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
+#include <hilog/log.h>
+#include <securec.h>
+#include <unistd.h>
+
+#include "render/rs_shader.h"
+
+namespace OHOS {
+namespace Rosen {
+using namespace Drawing;
+namespace {
+const uint8_t* g_data = nullptr;
+size_t g_size = 0;
+size_t g_pos;
+} // namespace
+
+template<class T>
+T GetData()
+{
+    T object {};
+    size_t objectSize = sizeof(object);
+    if (g_data == nullptr || objectSize > g_size - g_pos) {
+        return object;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, g_data + g_pos, objectSize);
+    if (ret != EOK) {
+        return {};
+    }
+    g_pos += objectSize;
+    return object;
+}
+bool DoCreateRSShader(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    auto rsShader = std::make_shared<RSShader>();
+    auto drShader = std::make_shared<ShaderEffect>();
+    rsShader->CreateRSShader();
+    rsShader->CreateRSShader(drShader);
+    return true;
+}
+bool DoSetDrawingShader(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    auto rsShader = std::make_shared<RSShader>();
+    auto drShader = std::make_shared<ShaderEffect>();
+    rsShader->SetDrawingShader(drShader);
+    return true;
+}
+bool DoGetDrawingShader(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    auto rsShader = std::make_shared<RSShader>();
+    rsShader->GetDrawingShader();
+    return true;
+}
+} // namespace Rosen
+} // namespace OHOS
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::DoCreateRSShader(data, size);
+    OHOS::Rosen::DoSetDrawingShader(data, size);
+    OHOS::Rosen::DoGetDrawingShader(data, size);
+    return 0;
+}
