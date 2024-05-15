109130b3d8a123d9cda5bd512be55ce0669d891e
denispavlov <pavlov.denis@huawei.com>
2024-05-15 16:16:04 +0300

    Adding files for beta-recording
    Signed-off-by: denispavlov <pavlov.denis@huawei.com>

--- rosen/modules/render_service_base/include/platform/common/rs_system_properties.h
+++ rosen/modules/render_service_base/include/platform/common/rs_system_properties.h
@@ -124,6 +124,8 @@ public:
     static bool GetProfilerEnabled();
     static bool GetInstantRecording();
     static void SetInstantRecording(bool flag);
+    static uint32_t GetBetaRecordingMode();
+    static void SetBetaRecordingMode(uint32_t param);
     static bool GetSaveRDC();
     static void SetSaveRDC(bool flag);
 
