4c0b8dbac7386678e5ad0a221694265fcc01939b
liujianghu <liujianghu1@huawei.com>
2024-05-16 09:35:31 +0800

    add rs_surface_capture_task_parallel tdd test case
    Signed-off-by: liujianghu <liujianghu1@huawei.com>

--- rosen/test/render_service/render_service/unittest/pipeline/rs_surface_capture_task_parallel_test.cpp
+++ rosen/test/render_service/render_service/unittest/pipeline/rs_surface_capture_task_parallel_test.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <gtest/gtest.h>
 
 #include "pipeline/rs_surface_capture_task_parallel.h"
@@ -27,7 +42,7 @@ void RSSurfaceCaptureTaskParallelTest::TearDown() {}
  * @tc.name: FindSecurityOrSkipOrProtectedLayer
  * @tc.desc: function test
  * @tc.type: FUNC
- * @tc.require: 
+ * @tc.require:
 */
 HWTEST_F(RSSurfaceCaptureTaskParallelTest, FindSecurityOrSkipOrProtectedLayer, TestSize.Level2)
 {
@@ -70,7 +85,7 @@ HWTEST_F(RSSurfaceCaptureTaskParallelTest, FindSecurityOrSkipOrProtectedLayer, T
  * @tc.name: ScreenCorrection
  * @tc.desc: function test
  * @tc.type: FUNC
- * @tc.require: 
+ * @tc.require:
 */
 HWTEST_F(RSSurfaceCaptureTaskParallelTest, ScreenCorrection, TestSize.Level2)
 {
