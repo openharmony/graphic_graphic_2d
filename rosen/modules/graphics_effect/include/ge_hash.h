34f30e88588066a92735c3c15bafea2c47034500
Jet Ding <jet.ding@huawei.com>
2024-05-21 18:43:43 +0800

    Update hash mechanism which uses the same algorithms like Skia
    Signed-off-by: jetd<jet.ding@huawei.com>

--- rosen/modules/graphics_effect/include/ge_hash.h
+++ rosen/modules/graphics_effect/include/ge_hash.h
@@ -12,38 +12,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#ifndef GRAPHICS_EFFECT_GE_CACHE_H
-#define GRAPHICS_EFFECT_GE_CACHE_H
-
+#ifndef GRAPHICS_EFFECT_GE_HASH_H
+#define GRAPHICS_EFFECT_GE_HASH_H
+#include <cstdint>
 #include <memory>
-#include <mutex>
-#include <unordered_map>
 
-#include "image/image.h"
-
+#include "ge_hash_refer.h"
 namespace OHOS {
 namespace GraphicsEffectEngine {
-
-using namespace Rosen;
-
-class GECache {
-public:
-    static GECache& GetInstance();
-    void Set(size_t key, const std::shared_ptr<Drawing::Image>& value);
-    const std::shared_ptr<Drawing::Image>& Get(size_t key) const;
-
-private:
-    GECache() = default;
-    ~GECache() = default;
-    GECache(const GECache&) = delete;
-    GECache& operator=(const GECache&) = delete;
-
-    std::unordered_map<size_t, std::shared_ptr<Drawing::Image>> cache_;
-    mutable std::mutex mutex_;
-};
-
+uint32_t Hash32Next(uint32_t previousHash, const void* data, size_t bytes, uint32_t seed = 0);
+uint64_t Hash64Next(uint64_t previousHash, const void* data, size_t bytes, uint64_t seed = 0);
 } // namespace GraphicsEffectEngine
 } // namespace OHOS
-
-#endif // GRAPHICS_EFFECT_GE_CACHE_H
+#endif // GRAPHICS_EFFECT_GE_HASH_H
--- rosen/modules/graphics_effect/src/ge_hash.cpp
+++ rosen/modules/graphics_effect/src/ge_hash.cpp
@@ -12,38 +12,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "ge_hash.h"
 
-#ifndef GRAPHICS_EFFECT_GE_CACHE_H
-#define GRAPHICS_EFFECT_GE_CACHE_H
+#include <cstring>
 
-#include <memory>
-#include <mutex>
-#include <unordered_map>
-
-#include "image/image.h"
+#include "ge_hash_refer.h"
 
 namespace OHOS {
 namespace GraphicsEffectEngine {
-
-using namespace Rosen;
-
-class GECache {
-public:
-    static GECache& GetInstance();
-    void Set(size_t key, const std::shared_ptr<Drawing::Image>& value);
-    const std::shared_ptr<Drawing::Image>& Get(size_t key) const;
-
-private:
-    GECache() = default;
-    ~GECache() = default;
-    GECache(const GECache&) = delete;
-    GECache& operator=(const GECache&) = delete;
-
-    std::unordered_map<size_t, std::shared_ptr<Drawing::Image>> cache_;
-    mutable std::mutex mutex_;
-};
-
+uint32_t Hash32Next(uint32_t hash, const void* data, size_t bytes, uint32_t seed)
+{
+    return static_cast<uint32_t>(GEHash(&hash, sizeof(hash), GEHash(data, bytes, seed, SECRET), SECRET));
+}
+uint64_t Hash64Next(uint64_t hash, const void* data, size_t bytes, uint64_t seed)
+{
+    return GEHash(&hash, sizeof(hash), GEHash(data, bytes, seed, SECRET), SECRET);
+}
 } // namespace GraphicsEffectEngine
 } // namespace OHOS
-
-#endif // GRAPHICS_EFFECT_GE_CACHE_H
