1900c0b9208e97c62ea1a8cf72e732efec60e155
lw19901203 <liuwei793@h-partners.com>
2024-06-05 14:28:13 +0800

    drawing fuzz upload
    Signed-off-by: lw19901203 <liuwei793@h-partners.com>

---
+++ rosen/test/2d_graphics/fuzztest/ndk/ndkpixelmap_fuzzer/pixelmap_fuzzer.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "pixelmap_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "get_object.h"
+
+#include "drawing_pixel_map.h"
+#include "drawing_types.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+constexpr size_t DATA_MIN_SIZE = 2;
+} // namespace
+
+namespace Drawing {
+void NativeDrawingPixelMapTest001(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return;
+    }
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    OH_Drawing_PixelMap* pixelMap = OH_Drawing_PixelMapGetFromNativePixelMap(nullptr);
+    OH_Drawing_PixelMap* pixelMap2 = OH_Drawing_PixelMapGetFromOhPixelMapNative(nullptr);
+
+    OH_Drawing_PixelMapDissolve(pixelMap);
+    OH_Drawing_PixelMapDissolve(pixelMap2);
+}
+} // namespace Drawing
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::Drawing::NativeDrawingPixelMapTest001(data, size);
+
+    return 0;
+}
---
+++ rosen/test/2d_graphics/fuzztest/ndk/ndksamplingoptions_fuzzer/samplingoptions_fuzzer.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "samplingoptions_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "get_object.h"
+
+#include "drawing_sampling_options.h"
+#include "drawing_types.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+constexpr size_t DATA_MIN_SIZE = 2;
+constexpr size_t SAMPLING_OPTIONS_FILTER_MODE_ENUM_SIZE = 2;
+constexpr size_t SAMPLING_OPTIONS_MIPMAP_MODE_ENUM_SIZE = 3;
+} // namespace
+
+namespace Drawing {
+void NativeDrawingSamplingOptionsTest001(const uint8_t* data, size_t size)
+{
+    if (data == nullptr || size < DATA_MIN_SIZE) {
+        return;
+    }
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    uint32_t filterMode = GetObject<uint32_t>();
+    uint32_t mipMapMode = GetObject<uint32_t>();
+
+    OH_Drawing_SamplingOptions* samplingOptions = OH_Drawing_SamplingOptionsCreate(
+        static_cast<OH_Drawing_FilterMode>(filterMode % SAMPLING_OPTIONS_FILTER_MODE_ENUM_SIZE),
+        static_cast<OH_Drawing_MipmapMode>(mipMapMode % SAMPLING_OPTIONS_MIPMAP_MODE_ENUM_SIZE));
+
+    OH_Drawing_SamplingOptionsDestroy(samplingOptions);
+}
+} // namespace Drawing
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::Drawing::NativeDrawingSamplingOptionsTest001(data, size);
+
+    return 0;
+}
