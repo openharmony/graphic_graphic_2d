ad75b8414d336936f9f461a9a00891dbb426c511
changleipeng <changleipeng4@huawei.com>
2024-06-06 21:21:33 +0800

    finish_pipeline_0606
    Signed-off-by: changleipeng <changleipeng4@huawei.com>
    Change-Id: I5f670a440e82bcbc5560a5de2205d116c558d07c

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_content_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_content_test.cpp
@@ -27,10 +27,6 @@ public:
     static void TearDownTestCase();
     void SetUp() override;
     void TearDown() override;
-    class MyPropertyDrawable : public RSPropertyDrawable {
-    public:
-        void Draw(const RSRenderContent& content, RSPaintFilterCanvas& canvas) const override {}
-    };
 };
 
 void RSRenderContentTest::SetUpTestCase() {}
@@ -38,48 +34,86 @@ void RSRenderContentTest::TearDownTestCase() {}
 void RSRenderContentTest::SetUp() {}
 void RSRenderContentTest::TearDown() {}
 
+class MyPropertyDrawable : public RSPropertyDrawable {
+public:
+    void Draw(const RSRenderContent& content, RSPaintFilterCanvas& canvas) const override
+    {
+        return;
+    }
+};
+
 /**
  * @tc.name: RegisterTaskDispatchTest001
  * @tc.desc: RegisterTaskDispatchFunc test.
  * @tc.type: FUNC
- * @tc.require: issuesI9J766
+ * @tc.require: issueI9VT6E
  */
 HWTEST_F(RSRenderContentTest, RegisterTaskDispatchTest001, TestSize.Level1)
 {
-    RSRenderContent renderContent;
+    std::shared_ptr<RSRenderContent> renderContent = std::make_shared<RSRenderContent>();
     auto drawingCanvas = std::make_unique<Drawing::Canvas>();
     RSPaintFilterCanvas canvas(drawingCanvas.get());
-
     // propertyDrawablesVec_[0] is nullptr operation
-    renderContent.propertyDrawablesVec_[0] = nullptr;
-    renderContent.DrawPropertyDrawable(RSPropertyDrawableSlot::INVALID, canvas);
+    renderContent->propertyDrawablesVec_[0] = nullptr;
+    renderContent->DrawPropertyDrawable(RSPropertyDrawableSlot::INVALID, canvas);
 
     // not recording canvas, draw directly
+    canvas.SetRecordingState(false);
     canvas.SetRecordDrawable(false);
+    std::unique_ptr<MyPropertyDrawable> notRecordingCanvasTest1 = std::make_unique<MyPropertyDrawable>();
+    renderContent->propertyDrawablesVec_[1] = std::move(notRecordingCanvasTest1);
+    renderContent->DrawPropertyDrawable(RSPropertyDrawableSlot::SAVE_ALL, canvas);
+
     canvas.SetRecordingState(true);
-    std::unique_ptr<MyPropertyDrawable> notRecordingCanvas = std::make_unique<MyPropertyDrawable>();
-    renderContent.propertyDrawablesVec_[1] = std::move(notRecordingCanvas);
-    renderContent.DrawPropertyDrawable(RSPropertyDrawableSlot::SAVE_ALL, canvas);
+    canvas.SetRecordDrawable(false);
+    std::unique_ptr<MyPropertyDrawable> notRecordingCanvasTest2 = std::make_unique<MyPropertyDrawable>();
+    renderContent->propertyDrawablesVec_[2] = std::move(notRecordingCanvasTest2);
+    renderContent->DrawPropertyDrawable(RSPropertyDrawableSlot::BOUNDS_MATRIX, canvas);
+
+    canvas.SetRecordingState(true);
+    canvas.SetRecordDrawable(true);
+    std::unique_ptr<MyPropertyDrawable> notRecordingCanvasTest3 = std::make_unique<MyPropertyDrawable>();
+    renderContent->propertyDrawablesVec_[3] = std::move(notRecordingCanvasTest3);
+    ExtendRecordingCanvas extendRecordingCanvas(0, 1);
+    extendRecordingCanvas.addDrawOpImmediate_ = false;
+    canvas.canvas_ = &extendRecordingCanvas;
+    renderContent->DrawPropertyDrawable(RSPropertyDrawableSlot::ALPHA, canvas);
 }
 
 /**
  * @tc.name: DrawPropertyDrawableRangeTest002
  * @tc.desc: DrawPropertyDrawableRange test.
  * @tc.type: FUNC
- * @tc.require: issuesI9J766
+ * @tc.require: issueI9VT6E
  */
 HWTEST_F(RSRenderContentTest, DrawPropertyDrawableRangeTest002, TestSize.Level1)
 {
-    RSRenderContent renderContent;
+    std::shared_ptr<RSRenderContent> renderContent = std::make_shared<RSRenderContent>();
     auto drawingCanvas = std::make_unique<Drawing::Canvas>();
     RSPaintFilterCanvas canvas(drawingCanvas.get());
 
     std::unique_ptr<MyPropertyDrawable> recordingCanvas = std::make_unique<MyPropertyDrawable>();
-    renderContent.propertyDrawablesVec_[0] = std::move(recordingCanvas);
+    renderContent->propertyDrawablesVec_[0] = std::move(recordingCanvas);
 
     // not recording canvas, draw directly
+    canvas.SetRecordingState(false);
     canvas.SetRecordDrawable(false);
+    renderContent->DrawPropertyDrawableRange(RSPropertyDrawableSlot::INVALID, RSPropertyDrawableSlot::INVALID, canvas);
+
     canvas.SetRecordingState(true);
-    renderContent.DrawPropertyDrawableRange(RSPropertyDrawableSlot::INVALID, RSPropertyDrawableSlot::INVALID, canvas);
+    canvas.SetRecordDrawable(false);
+    std::unique_ptr<MyPropertyDrawable> notRecordingCanvasTest2 = std::make_unique<MyPropertyDrawable>();
+    renderContent->propertyDrawablesVec_[2] = std::move(notRecordingCanvasTest2);
+    renderContent->DrawPropertyDrawableRange(
+        RSPropertyDrawableSlot::BOUNDS_MATRIX, RSPropertyDrawableSlot::BOUNDS_MATRIX, canvas);
+
+    canvas.SetRecordDrawable(true);
+    canvas.SetRecordingState(true);
+    std::unique_ptr<MyPropertyDrawable> notRecordingCanvasTest3 = std::make_unique<MyPropertyDrawable>();
+    renderContent->propertyDrawablesVec_[3] = std::move(notRecordingCanvasTest3);
+    ExtendRecordingCanvas extendRecordingCanvas(0, 1);
+    extendRecordingCanvas.addDrawOpImmediate_ = false;
+    canvas.canvas_ = &extendRecordingCanvas;
+    renderContent->DrawPropertyDrawableRange(RSPropertyDrawableSlot::ALPHA, RSPropertyDrawableSlot::ALPHA, canvas);
 }
 } // namespace OHOS::Rosen
--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_uni_render_judgement_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_uni_render_judgement_test.cpp
@@ -62,7 +62,7 @@ HWTEST_F(RSUniRenderJudgementTest, TestUniRenderJudgement002, TestSize.Level1)
     if (uniType == UniRenderEnabledType::UNI_RENDER_DISABLED) {
         ASSERT_EQ(false, isUni);
     } else {
-        ASSERT_NE(true, isUni);
+        ASSERT_EQ(true, isUni);
     }
 }
 
@@ -96,5 +96,35 @@ HWTEST_F(RSUniRenderJudgementTest, TestUniRenderJudgement003, TestSize.Level1)
     // safely remove file
     rsCmd = "rm " + configFilePath;
     (void)system(rsCmd.c_str());
+}
+
+/**
+ * @tc.name: SafeGetLineTest002
+ * @tc.desc: SafeGetLine test.
+ * @tc.type: FUNC
+ * @tc.require: issueI9VT6E
+ */
+HWTEST_F(RSUniRenderJudgementTest, SafeGetLineTest002, TestSize.Level1)
+{
+    std::ofstream tempFile("test_file.txt");
+    tempFile << "Line 1\r\n";
+    tempFile << "Line 2\n";
+    tempFile << "Line 3\r";
+    tempFile.close();
+    std::ifstream configFile;
+    configFile.open("test_file.txt");
+    std::string line;
+
+    (void)RSUniRenderJudgement::SafeGetLine(configFile, line);
+    EXPECT_EQ(line, "Line 1");
+
+    (void)RSUniRenderJudgement::SafeGetLine(configFile, line);
+    EXPECT_EQ(line, "Line 2");
+
+    (void)RSUniRenderJudgement::SafeGetLine(configFile, line);
+    EXPECT_EQ(line, "Line 3");
+
+    configFile.close();
+    std::remove("test_file.txt");
 }
 } // namespace OHOS::Rosen
