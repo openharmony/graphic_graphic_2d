44dec8678eaccf1401dcaefc2abc6d97c8adeb25
lw19901203 <liuwei793@h-partners.com>
2024-06-24 15:26:46 +0800

    add c++ fuzz
    Signed-off-by: lw19901203 <liuwei793@h-partners.com>

---
+++ rosen/test/2d_graphics/fuzztest/utils/point_fuzzer/point_fuzzer.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "point_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <securec.h>
+
+#include "get_object.h"
+#include "utils/point.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace Drawing {
+void PointFuzzTest000(const uint8_t* data, size_t size)
+{
+    // initialize
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    scalar x = GetObject<scalar>();
+    scalar y = GetObject<scalar>();
+    scalar scale = GetObject<scalar>();
+    scalar divisor = GetObject<scalar>();
+
+    PointF pointf;
+    PointF pointf1 = PointF(x, y);
+
+    pointf.SetX(x);
+    pointf.GetX();
+    pointf.SetY(y);
+    pointf.GetY();
+    pointf.Set(x, y);
+
+    pointf.IsZero();
+    pointf.Offset(x, y);
+
+    pointf1 += pointf;
+    pointf1 -= pointf;
+    pointf1 *= scale;
+    pointf1 /= divisor;
+}
+
+void PointFuzzTest001(const uint8_t* data, size_t size)
+{
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    scalar x = GetObject<scalar>();
+    scalar y = GetObject<scalar>();
+    scalar scale = GetObject<scalar>();
+    scalar divisor = GetObject<scalar>();
+
+    PointF pointf;
+    PointF pointf1 = PointF(x, y);
+    PointF pointf2 = PointF(pointf);
+
+    pointf = pointf1 + pointf2;
+    pointf = pointf1 - pointf2;
+    pointf = scale * pointf2;
+    pointf = pointf1 * scale;
+    pointf = pointf1 / divisor;
+
+    if (pointf == pointf1) {}
+    if (pointf != pointf1) {}
+}
+
+void PointFuzzTest002(const uint8_t* data, size_t size)
+{
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    int x = GetObject<int>();
+    int y = GetObject<int>();
+    scalar scale = GetObject<scalar>();
+    scalar divisor = GetObject<scalar>();
+
+    PointI pointi;
+    PointI pointi1 = PointI(x, y);
+
+    pointi.SetX(x);
+    pointi.GetX();
+    pointi.SetY(y);
+    pointi.GetY();
+    pointi.Set(x, y);
+
+    pointi1 += pointi;
+    pointi1 -= pointi;
+    pointi1 *= scale;
+    pointi1 /= divisor;
+}
+
+void PointFuzzTest003(const uint8_t* data, size_t size)
+{
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    int x = GetObject<int>();
+    int y = GetObject<int>();
+    scalar scale = GetObject<scalar>();
+    scalar divisor = GetObject<scalar>();
+
+    PointI pointi;
+    PointI pointi1 = PointI(x, y);
+    PointI pointi2 = PointI(pointi1);
+
+    pointi = pointi1 + pointi2;
+    pointi = pointi1 - pointi2;
+    pointi = scale * pointi2;
+    pointi = pointi1 * scale;
+    pointi = pointi1 / divisor;
+
+    if (pointi == pointi1) {}
+    if (pointi != pointi1) {}
+}
+} // namespace Drawing
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::Drawing::PointFuzzTest000(data, size);
+    OHOS::Rosen::Drawing::PointFuzzTest001(data, size);
+    OHOS::Rosen::Drawing::PointFuzzTest002(data, size);
+    OHOS::Rosen::Drawing::PointFuzzTest003(data, size);
+    return 0;
+}
---
+++ rosen/test/2d_graphics/fuzztest/utils/rect_fuzzer/rect_fuzzer.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rect_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <securec.h>
+
+#include "get_object.h"
+#include "utils/rect.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace Drawing {
+void RectFuzzTest000(const uint8_t* data, size_t size)
+{
+    // initialize
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    int pos = GetObject<int>();
+
+    RectI rect;
+
+    rect.SetLeft(pos);
+    rect.GetLeft();
+    rect.SetTop(pos);
+    rect.GetTop();
+    rect.SetRight(pos);
+    rect.GetRight();
+    rect.SetBottom(pos);
+    rect.GetBottom();
+    rect.GetHeight();
+    rect.GetWidth();
+
+    rect.IsEmpty();
+    rect.IsValid();
+}
+
+void RectFuzzTest001(const uint8_t* data, size_t size)
+{
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    int dx = GetObject<int>();
+    int dy = GetObject<int>();
+    int l = GetObject<int>();
+    int t = GetObject<int>();
+    int r = GetObject<int>();
+    int b = GetObject<int>();
+
+    RectI rect1 = RectI(l, t, r, b);
+    RectI rect2 = RectI(rect1);
+
+    rect1.Offset(dx, dy);
+    rect1.MakeOutset(dx, dy);
+
+    rect1.Contains(rect2);
+    rect1.Join(rect2);
+    rect1.ToString();
+    if (rect1 == rect2) {}
+    if (rect1 != rect2) {}
+}
+
+void RectFuzzTest002(const uint8_t* data, size_t size)
+{
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    scalar pos = GetObject<scalar>();
+    scalar l = GetObject<scalar>();
+    scalar t = GetObject<scalar>();
+    scalar r = GetObject<scalar>();
+    scalar b = GetObject<scalar>();
+
+    RectF rectf;
+    RectI recti;
+    RectF rectf1 = RectF(l, t, r, b);
+    RectF rectf2 = RectF(rectf1);
+    RectF rectf3 = RectF(recti);
+
+    rectf.SetLeft(pos);
+    rectf.GetLeft();
+    rectf.SetTop(pos);
+    rectf.GetTop();
+    rectf.SetRight(pos);
+    rectf.GetRight();
+    rectf.SetBottom(pos);
+    rectf.GetBottom();
+    rectf.GetWidth();
+    rectf.GetHeight();
+    rectf.IsValid();
+    rectf.IsEmpty();
+
+    rectf.Intersect(rectf1);
+    rectf.Join(rectf1);
+
+    if (rectf == rectf1) {}
+    if (rectf != rectf1) {}
+}
+
+void RectFuzzTest003(const uint8_t* data, size_t size)
+{
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    scalar dx = GetObject<scalar>();
+    scalar dy = GetObject<scalar>();
+    scalar l = GetObject<scalar>();
+    scalar t = GetObject<scalar>();
+    scalar r = GetObject<scalar>();
+    scalar b = GetObject<scalar>();
+
+    RectF rectf = RectF(l, t, r, b);
+    rectf.Offset(dx, dy);
+    rectf.MakeOutset(dx, dy);
+    rectf.Round();
+    rectf.RoundOut();
+    rectf.ToString();
+}
+} // namespace Drawing
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::Drawing::RectFuzzTest000(data, size);
+    OHOS::Rosen::Drawing::RectFuzzTest001(data, size);
+    OHOS::Rosen::Drawing::RectFuzzTest002(data, size);
+    OHOS::Rosen::Drawing::RectFuzzTest003(data, size);
+    
+    return 0;
+}
