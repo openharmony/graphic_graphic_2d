175d1d15a87667bdabc3c9e2e8c71f287dfb16cc
Klein-CK <chenke76@huawei.com>
2024-07-01 14:16:46 +0800

    Add abort discriptions of Tdd for displaySoloist NDK interfaces. Signed-off-by: Klein-CK<chenke76@huawei.com>
    Change-Id: Ibf0ddd8900d8745b1f6a44af36cbc33e3fa6d624

--- rosen/test/hyper_graphic_manager/native_display_soloist_test/native_display_soloist_test.cpp
+++ rosen/test/hyper_graphic_manager/native_display_soloist_test/native_display_soloist_test.cpp
@@ -26,6 +26,8 @@ namespace {
     constexpr uint32_t FRAME_RATE_90_HZ = 90;
     constexpr uint32_t FRAME_RATE_120_HZ = 120;
     constexpr uint32_t SLEEP_TIME_US = 100000;
+    constexpr int32_t EXEC_SUCCESS = 0;
+    constexpr int32_t SOLOIST_ERROR = -1;
 }
 class NativeDisplaySoloistTest : public testing::Test {
 public:
@@ -83,7 +85,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Create001, Function | Mediu
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Start001, Function | MediumTest | Level2)
 {
     OH_DisplaySoloist_FrameCallback callback = OnVSync;
-    OH_DisplaySoloist_Start(nullptr, callback, nullptr);
+    int32_t result = OH_DisplaySoloist_Start(nullptr, callback, nullptr);
+    EXPECT_EQ(SOLOIST_ERROR, result);
 }
 
 /*
@@ -96,7 +99,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Start001, Function | Medium
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Start002, Function | MediumTest | Level2)
 {
     OH_DisplaySoloist_FrameCallback callback = OnVSync;
-    OH_DisplaySoloist_Start(nativeDisplaySoloist, callback, nullptr);
+    int32_t result = OH_DisplaySoloist_Start(nativeDisplaySoloist, callback, nullptr);
+    EXPECT_EQ(EXEC_SUCCESS, result);
 }
 
 /*
@@ -109,8 +113,10 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Start002, Function | Medium
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_SetExpectedFrameRateRange001, Function | MediumTest | Level2)
 {
     DisplaySoloist_ExpectedRateRange validRange = { FRAME_RATE_30_HZ, FRAME_RATE_120_HZ, FRAME_RATE_60_HZ };
-    OH_DisplaySoloist_SetExpectedFrameRateRange(nullptr, &validRange);
-    OH_DisplaySoloist_SetExpectedFrameRateRange(nativeDisplaySoloist, nullptr);
+    int32_t result1 = OH_DisplaySoloist_SetExpectedFrameRateRange(nullptr, &validRange);
+    EXPECT_EQ(SOLOIST_ERROR, result1);
+    int32_t result2 = OH_DisplaySoloist_SetExpectedFrameRateRange(nativeDisplaySoloist, nullptr);
+    EXPECT_EQ(SOLOIST_ERROR, result2);
 }
 
 /*
@@ -123,7 +129,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_SetExpectedFrameRateRange00
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_SetExpectedFrameRateRange002, Function | MediumTest | Level2)
 {
     DisplaySoloist_ExpectedRateRange invalidRange = { FRAME_RATE_30_HZ, FRAME_RATE_90_HZ, FRAME_RATE_120_HZ };
-    OH_DisplaySoloist_SetExpectedFrameRateRange(nullptr, &invalidRange);
+    int32_t result = OH_DisplaySoloist_SetExpectedFrameRateRange(nullptr, &invalidRange);
+    EXPECT_EQ(SOLOIST_ERROR, result);
 }
 
 /*
@@ -136,7 +143,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_SetExpectedFrameRateRange00
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_SetExpectedFrameRateRange003, Function | MediumTest | Level2)
 {
     DisplaySoloist_ExpectedRateRange validRange = { FRAME_RATE_30_HZ, FRAME_RATE_120_HZ, FRAME_RATE_60_HZ };
-    OH_DisplaySoloist_SetExpectedFrameRateRange(nullptr, &validRange);
+    int32_t result = OH_DisplaySoloist_SetExpectedFrameRateRange(nativeDisplaySoloist, &validRange);
+    EXPECT_EQ(EXEC_SUCCESS, result);
 }
 
 /*
@@ -148,7 +156,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_SetExpectedFrameRateRange00
  */
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Stop001, Function | MediumTest | Level2)
 {
-    OH_DisplaySoloist_Stop(nullptr);
+    int32_t result = OH_DisplaySoloist_Stop(nullptr);
+    EXPECT_EQ(SOLOIST_ERROR, result);
 }
 
 /*
@@ -160,7 +169,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Stop001, Function | MediumT
  */
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Stop002, Function | MediumTest | Level2)
 {
-    OH_DisplaySoloist_Stop(nativeDisplaySoloist);
+    int32_t result = OH_DisplaySoloist_Stop(nativeDisplaySoloist);
+    EXPECT_EQ(EXEC_SUCCESS, result);
 }
 
 /*
@@ -172,7 +182,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Stop002, Function | MediumT
  */
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Destroy001, Function | MediumTest | Level2)
 {
-    OH_DisplaySoloist_Destroy(nullptr);
+    int32_t result = OH_DisplaySoloist_Destroy(nullptr);
+    EXPECT_EQ(SOLOIST_ERROR, result);
 }
 
 /*
@@ -184,7 +195,8 @@ HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Destroy001, Function | Medi
  */
 HWTEST_F(NativeDisplaySoloistTest, OH_DisplaySoloist_Destroy002, Function | MediumTest | Level2)
 {
-    OH_DisplaySoloist_Destroy(nativeDisplaySoloist);
+    int32_t result = OH_DisplaySoloist_Destroy(nativeDisplaySoloist);
+    EXPECT_EQ(EXEC_SUCCESS, result);
 }
 
 } // namespace
