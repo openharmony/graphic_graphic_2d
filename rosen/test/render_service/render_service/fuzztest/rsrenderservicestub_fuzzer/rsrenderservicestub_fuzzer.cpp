3c1de6a075514edfdd1f978576ab20d59fa9499f
wangwei <changleipeng4@huawei.com>
2024-07-01 18:06:43 +0800

    ww
    Signed-off-by: wangwei <changleipeng4@huawei.com>

---
+++ rosen/test/render_service/render_service/fuzztest/rsrenderservicestub_fuzzer/rsrenderservicestub_fuzzer.cpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rsrenderservicestub_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <iremote_stub.h>
+#include <message_option.h>
+#include <message_parcel.h>
+#include <securec.h>
+
+#include "platform/ohos/rs_irender_service.h"
+#include "platform/ohos/rs_irender_service_ipc_interface_code_access_verifier.h"
+#include "pipeline/rs_render_service.h"
+#include "transaction/rs_render_service_stub.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace {
+const uint8_t* DATA = nullptr;
+size_t g_size = 0;
+size_t g_pos;
+} // namespace
+
+template<class T>
+T GetData()
+{
+    T object {};
+    size_t objectSize = sizeof(object);
+    if (DATA == nullptr || objectSize > g_size - g_pos) {
+        return object;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, DATA + g_pos, objectSize);
+    if (ret != EOK) {
+        return {};
+    }
+    g_pos += objectSize;
+    return object;
+}
+
+bool RSRenderServiceStubFuzztest001(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+    
+    // get data
+    uint32_t code = GetData<uint32_t>();
+    sptr<RSRenderServiceStub> stub = new RSRenderService();
+    MessageParcel data1;
+    MessageParcel reply;
+    MessageOption option;
+    stub->OnRemoteRequest(code, data1, reply, option);
+    return true;
+}
+
+bool RSRenderServiceStubFuzztest002(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+    
+    // get data
+    uint32_t code = static_cast<uint32_t>(RSIRenderServiceInterfaceCode::CREATE_CONNECTION);
+    sptr<RSRenderServiceStub> stub = new RSRenderService();
+    MessageParcel data1;
+    MessageParcel reply;
+    MessageOption option;
+    stub->OnRemoteRequest(code, data1, reply, option);
+    return true;
+}
+
+bool RSRenderServiceStubFuzztest003(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+    
+    // get data
+    uint32_t code = static_cast<uint32_t>(RSIRenderServiceInterfaceCode::CREATE_CONNECTION);
+    sptr<RSRenderServiceStub> stub = new RSRenderService();
+    MessageParcel data1;
+    MessageParcel reply;
+    MessageOption option;
+    data1.WriteInterfaceToken(RSIRenderService::GetDescriptor());
+    stub->OnRemoteRequest(code, data1, reply, option);
+    return true;
+}
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::RSRenderServiceStubFuzztest001(data, size);
+    OHOS::Rosen::RSRenderServiceStubFuzztest002(data, size);
+    OHOS::Rosen::RSRenderServiceStubFuzztest003(data, size);
+    return 0;
+}
