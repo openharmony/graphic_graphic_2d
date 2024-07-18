6dc4b03fc21423713cdec39be7b9d6a2cbec149c
wulei <19072970815@163.com>
2024-07-18 16:22:20 +0800

    将surface保存在map里，不用每次都新创建一个，提高图形的性能。
    Signed-off-by: wulei <19072970815@163.com>

--- rosen/modules/render_service/core/pipeline/rs_base_render_engine.cpp
+++ rosen/modules/render_service/core/pipeline/rs_base_render_engine.cpp
@@ -52,7 +52,6 @@ RSBaseRenderEngine::~RSBaseRenderEngine() noexcept
     std::lock_guard<std::mutex> lockGuard(rsSurfacesMutex_);
     // clear map when deinit
     uint32_t mapSize = rsSurfaces_.size();
-    RS_LOGI("RSBaseRenderEngine deinit enter, mapSize:%{public}u", mapSize);
     uint32_t itemIndex = 0;
     while (!rsSurfaces_.empty()) {
         auto it = rsSurfaces_.begin();
