e54484788109171722334ab3fbb6e791db8b50d0
zhoubin <zhoubin113@huawei.com>
2024-07-18 10:18:25 +0000

    update rosen/samples/webgl/entry/src/main/js/MainAbility/pages/index/sample2/shadow2_demo.js.
    Signed-off-by: zhoubin <zhoubin113@huawei.com>

--- rosen/samples/webgl/entry/src/main/js/MainAbility/pages/index/sample2/shadow2_demo.js
+++ rosen/samples/webgl/entry/src/main/js/MainAbility/pages/index/sample2/shadow2_demo.js
@@ -315,7 +315,7 @@ function initFramebuffer(gl) {
         return error();
     }
     texture = gl.createTexture();
-    if (!texture){
+    if (!texture) {
         return error();
     }
     gl.bindTexture(gl.TEXTURE_2D, texture);
