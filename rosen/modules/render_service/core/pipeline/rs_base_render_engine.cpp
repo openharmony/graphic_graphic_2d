c639a988c989587e0075d23cfdcb4ddb4cc2bcdd
wulei <19072970815@163.com>
2024-07-22 11:47:49 +0800

    修复日志打印数据类型错误
    Signed-off-by: wulei <19072970815@163.com>

--- rosen/modules/render_service/core/pipeline/rs_base_render_engine.cpp
+++ rosen/modules/render_service/core/pipeline/rs_base_render_engine.cpp
@@ -446,7 +446,7 @@ std::unique_ptr<RSRenderFrame> RSBaseRenderEngine::RequestFrame(const sptr<Surfa
     }
     auto surfaceId = targetSurface->GetUniqueId();
     uint32_t mapSize = rsSurfaces_.size();
-    RS_LOGD("RequestFrame: surfaceId=%{public}llu, mapSize=%{public}u", surfaceId, mapSize);
+    RS_LOGD("RequestFrame: surfaceId=%{public}lu, mapSize=%{public}u", surfaceId, mapSize);
     if (rsSurfaces_.count(surfaceId) != 0) {
         RS_OPTIONAL_TRACE_END();
         return RequestFrame(rsSurfaces_.at(surfaceId), config, forceCPU, useAFBC);
