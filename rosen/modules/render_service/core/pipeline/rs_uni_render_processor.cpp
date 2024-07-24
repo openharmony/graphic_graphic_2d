b09203e71b4f420ebbd66cecf891cdf826bd995e
fseeeye <liyifan57@huawei.com>
2024-07-24 15:55:25 +0800

    fix redraw memory leaking
    Signed-off-by: fseeeye <liyifan57@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_processor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_processor.cpp
@@ -244,6 +244,7 @@ LayerInfoPtr RSUniRenderProcessor::GetLayerInfo(RSSurfaceRenderParams& params, s
     layer->SetMatrix(matrix);
     layer->SetScalingMode(params.GetPreScalingMode());
     layer->SetLayerSourceTuning(params.GetLayerSourceTuning());
+    layer->SetClearCacheSet(params.GetBufferClearCacheSet());
     return layer;
 }
 
