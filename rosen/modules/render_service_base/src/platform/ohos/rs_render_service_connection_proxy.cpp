475e9635ee614ea37cd0cc248e4897051dcf2c72
钱常德 <qianchangde@huawei.com>
2024-07-31 12:00:37 +0800

    Code security rectification
    Signed-off-by: 钱常德 <qianchangde@huawei.com>
    Change-Id: I6952c8007d23a396875cfe14ed416df13a388273

--- rosen/modules/render_service_base/src/platform/ohos/rs_render_service_connection_proxy.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/rs_render_service_connection_proxy.cpp
@@ -461,6 +461,7 @@ int32_t RSRenderServiceConnectionProxy::SetVirtualScreenBlackList(ScreenId id, s
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetVirtualScreenBlackList: Send Request err.");
+        return RS_CONNECTION_ERROR;
     }
 
     int32_t status = reply.ReadInt32();
@@ -484,6 +485,7 @@ int32_t RSRenderServiceConnectionProxy::SetCastScreenEnableSkipWindow(ScreenId i
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetCastScreenEnableSkipWindow: Send Request err.");
+        return RS_CONNECTION_ERROR;
     }
     int32_t result = reply.ReadInt32();
     return result;
@@ -512,6 +514,7 @@ int32_t RSRenderServiceConnectionProxy::SetVirtualScreenSurface(ScreenId id, spt
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetVirtualScreenSurface: Send Request err.");
+        return RS_CONNECTION_ERROR;
     }
 
     int32_t status = reply.ReadInt32();
@@ -557,6 +560,7 @@ void RSRenderServiceConnectionProxy::RemoveVirtualScreen(ScreenId id)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::RemoveVirtualScreen: Send Request err.");
+        return;
     }
 }
 
@@ -718,6 +722,7 @@ void RSRenderServiceConnectionProxy::SetScreenRefreshRate(ScreenId id, int32_t s
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceProxy sendrequest error : %{public}d", err);
+        return;
     }
 }
 
@@ -737,6 +742,7 @@ void RSRenderServiceConnectionProxy::SetRefreshRateMode(int32_t refreshRateMode)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceProxy sendrequest error : %{public}d", err);
+        return;
     }
 }
 
@@ -762,6 +768,7 @@ void RSRenderServiceConnectionProxy::SyncFrameRateRange(FrameRateLinkerId id, co
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceProxy sendrequest error : %{public}d", err);
+        return;
     }
 }
 
@@ -878,6 +885,7 @@ void RSRenderServiceConnectionProxy::SetShowRefreshRateEnabled(bool enable)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceProxy sendrequest error : %{public}d", err);
+        return;
     }
 }
 
@@ -897,6 +905,7 @@ std::string RSRenderServiceConnectionProxy::GetRefreshInfo(pid_t pid)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceProxy sendrequest error : %{public}d", err);
+        return "";
     }
     std::string enable = reply.ReadString();
     return enable;
@@ -1334,6 +1343,7 @@ void RSRenderServiceConnectionProxy::RegisterBufferClearListener(
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::RegisterBufferClearListener: Send Request err.");
+        return;
     }
 }
 
@@ -1361,6 +1371,7 @@ void RSRenderServiceConnectionProxy::RegisterBufferAvailableListener(
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::RegisterBufferAvailableListener: Send Request err.");
+        return;
     }
 }
 
@@ -2061,6 +2072,7 @@ void RSRenderServiceConnectionProxy::SetAppWindowNum(uint32_t num)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetAppWindowNum: Send Request err.");
+        return;
     }
 }
 
@@ -2103,6 +2115,7 @@ void RSRenderServiceConnectionProxy::ShowWatermark(const std::shared_ptr<Media::
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::ShowWatermark: Send Request err.");
+        return;
     }
 }
 
@@ -2143,6 +2156,7 @@ void RSRenderServiceConnectionProxy::ReportJankStats()
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::ReportJankStats: Send Request err.");
+        return;
     }
 }
 
@@ -2159,6 +2173,7 @@ void RSRenderServiceConnectionProxy::ReportEventResponse(DataBaseRs info)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::ReportEventResponse: Send Request err.");
+        return;
     }
 }
 
@@ -2175,6 +2190,7 @@ void RSRenderServiceConnectionProxy::ReportEventComplete(DataBaseRs info)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::ReportEventComplete: Send Request err.");
+        return;
     }
 }
 
@@ -2191,6 +2207,7 @@ void RSRenderServiceConnectionProxy::ReportEventJankFrame(DataBaseRs info)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::ReportEventJankFrame: Send Request err.");
+        return;
     }
 }
 
@@ -2240,6 +2257,7 @@ void RSRenderServiceConnectionProxy::ReportGameStateData(GameStateData info)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::ReportGameStateData: Send Request err.");
+        return;
     }
 }
 
@@ -2265,6 +2283,7 @@ void RSRenderServiceConnectionProxy::SetHardwareEnabled(NodeId id, bool isEnable
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetHardwareEnabled: Send Request err.");
+        return;
     }
 }
 
@@ -2284,6 +2303,7 @@ void RSRenderServiceConnectionProxy::NotifyLightFactorStatus(bool isSafe)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::NotifyLightFactorStatus: Send Request err.");
+        return;
     }
 }
 
@@ -2308,6 +2328,7 @@ void RSRenderServiceConnectionProxy::NotifyPackageEvent(uint32_t listSize, const
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::NotifyPackageEvent: Send Request err.");
+        return;
     }
 }
 
@@ -2339,6 +2360,7 @@ void RSRenderServiceConnectionProxy::NotifyRefreshRateEvent(const EventInfo& eve
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::NotifyRefreshRateEvent: Send Request err.");
+        return;
     }
 }
 
@@ -2361,6 +2383,7 @@ void RSRenderServiceConnectionProxy::NotifyTouchEvent(int32_t touchStatus, int32
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::NotifyTouchEvent: Send Request err.");
+        return;
     }
 }
 
@@ -2380,6 +2403,7 @@ void RSRenderServiceConnectionProxy::NotifyDynamicModeEvent(bool enableDynamicMo
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::NotifyDynamicModeEvent: Send Request err.");
+        return;
     }
 }
 
@@ -2399,6 +2423,7 @@ void RSRenderServiceConnectionProxy::SetCacheEnabledForRotation(bool isEnabled)
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetCacheEnabledForRotation: Send Request err.");
+        return;
     }
 }
 
@@ -2557,6 +2582,7 @@ void RSRenderServiceConnectionProxy::SetVirtualScreenUsingStatus(bool isVirtualS
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetVirtualScreenUsingStatus: Send Request err.");
+        return;
     }
 }
 
@@ -2576,6 +2602,7 @@ void RSRenderServiceConnectionProxy::SetCurtainScreenUsingStatus(bool isCurtainS
     int32_t err = Remote()->SendRequest(code, data, reply, option);
     if (err != NO_ERROR) {
         ROSEN_LOGE("RSRenderServiceConnectionProxy::SetCurtainScreenUsingStatus: Send Request err.");
+        return;
     }
 }
 
