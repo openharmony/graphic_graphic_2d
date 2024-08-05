4c02339601a9a09e10cedd4e41d92b5a2b8f91af
王德海 <wangdehai2@huawei.com>
2024-08-05 11:33:20 +0800

    fix tdd
    Signed-off-by: 王德海 <wangdehai2@huawei.com>
    Change-Id: I238e3f2ce00028876401408114d01b2090285af1

--- rosen/modules/render_service_base/src/pipeline/rs_dirty_region_manager.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_dirty_region_manager.cpp
@@ -273,8 +273,8 @@ void RSDirtyRegionManager::UpdateDirtyByAligned(int32_t alignedBits)
 void RSDirtyRegionManager::UpdateDirtyRegionInfoForDfx(NodeId id, RSRenderNodeType nodeType,
     DirtyRegionType dirtyType, const RectI& rect)
 {
-    if ((dirtyType >= dirtyCanvasNodeInfo_.size()) ||
-        (dirtyType >= DirtyRegionType::TYPE_AMOUNT) || (dirtyType < 0)) {
+    if ((dirtyType >= dirtyCanvasNodeInfo_.size()) || (dirtyType >= DirtyRegionType::TYPE_AMOUNT) ||
+        (dirtyType < 0) || (rect.IsEmpty())) {
         return;
     }
     if (nodeType == RSRenderNodeType::CANVAS_NODE) {
@@ -288,8 +288,8 @@ void RSDirtyRegionManager::GetDirtyRegionInfo(std::map<NodeId, RectI>& target,
     RSRenderNodeType nodeType, DirtyRegionType dirtyType) const
 {
     target.clear();
-    if ((dirtyType >= dirtyCanvasNodeInfo_.size()) ||
-        (dirtyType >= DirtyRegionType::TYPE_AMOUNT) || (dirtyType < 0)) {
+    if ((dirtyType >= dirtyCanvasNodeInfo_.size()) || (dirtyType >= DirtyRegionType::TYPE_AMOUNT) ||
+        (dirtyType < 0)) {
         return;
     }
     if (nodeType == RSRenderNodeType::CANVAS_NODE) {
