0255560022337fc8dcf0f0593d55a286cbf6e99c
003 <zhanglitao11@huawei.com>
2024-08-12 20:28:45 +0800

    Add TDD
    Signed-off-by: 003 <zhanglitao11@huawei.com>

--- rosen/modules/render_service_base/src/property/rs_color_picker_cache_task.cpp
+++ rosen/modules/render_service_base/src/property/rs_color_picker_cache_task.cpp
@@ -129,6 +129,10 @@ bool RSColorPickerCacheTask::GpuScaleImage(std::shared_ptr<RSPaintFilterCanvas> 
         *threadImage, Drawing::TileMode::CLAMP, Drawing::TileMode::CLAMP, linear, matrix));
     std::shared_ptr<Drawing::Image> tmpColorImg = effectBulider->MakeImage(
         cacheCanvas->GetGPUContext().get(), nullptr, pcInfo, false);
+    if (tmpColorImg == nullptr) {
+        ROSEN_LOGE("RSColorPickerCacheTask tmpColorImg is null");
+        return false;
+    }
 
     const int buffLen = tmpColorImg->GetWidth() * tmpColorImg->GetHeight();
     if (pixelPtr_ != nullptr) {
@@ -169,8 +173,8 @@ bool RSColorPickerCacheTask::Render()
             return false;
         }
         SharedTextureContext* sharedContext = new SharedTextureContext(imageSnapshotCache_);
-        if (!threadImage->BuildFromTexture(*cacheCanvas->GetGPUContext(), cacheBackendTexture_.GetTextureInfo(),
-            Drawing::TextureOrigin::BOTTOM_LEFT, cacheBitmapFormat_, nullptr,
+        if (cacheCanvas->GetGPUContext() == nullptr || !threadImage->BuildFromTexture(*cacheCanvas->GetGPUContext(),
+            cacheBackendTexture_.GetTextureInfo(), Drawing::TextureOrigin::BOTTOM_LEFT, cacheBitmapFormat_, nullptr,
             SKResourceManager::DeleteSharedTextureContext, sharedContext)) {
             SetStatus(CacheProcessStatus::WAITING);
             ROSEN_LOGE("RSColorPickerCacheTask::Render BuildFromTexture failed");
@@ -385,6 +389,10 @@ void RSColorPickerCacheTask::ReleaseColorPicker()
 #ifdef IS_OHOS
     waitRelease_->store(true);
     cacheProcessStatus_.store(CacheProcessStatus::WAITING);
+    if (RSColorPickerCacheTask::saveImgAndSurToRelease == nullptr) {
+        ROSEN_LOGD("RSColorPickerCacheTask::saveImgAndSurToRelease null: %{public}p", this);
+        return;
+    }
     if (imageSnapshotCache_ || cacheSurface_ || initHandler_ || handler_) {
         ROSEN_LOGD("RSColorPickerCacheTask::ReleaseColorPicker:%{public}p", this);
         RSColorPickerCacheTask::saveImgAndSurToRelease(std::move(imageSnapshotCache_), std::move(cacheSurface_),
