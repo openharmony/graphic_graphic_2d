826c415677ded12b39ac4e1f5de6ef5e4ac956a4
li-kiao <lx.96@foxmail.com>
2024-08-12 12:00:46 +0000

    注册字体适配watchdog超时检测机制
    Signed-off-by: li-kiao <lixiang284@huawei.com>
    Change-Id: I4ceccd5bf59ebb65f0d9f934b40be050e901f351

---
+++ rosen/modules/render_service_base/src/common/rs_xcollie.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "common/rs_xcollie.h"
+#include "platform/common/rs_log.h"
+#ifdef ROSEN_OHOS
+#include "xcollie/xcollie.h"
+#endif
+namespace OHOS {
+namespace Rosen {
+RSXCollie::RSXCollie(const std::string& tag, uint32_t timeoutSeconds,
+    std::function<void(void*)> func, void* arg, uint32_t flag)
+{
+    tag_ = "render_service:" + tag;
+#ifdef ROSEN_OHOS
+    id_ = HiviewDFX::XCollie::GetInstance().SetTimer(tag_, timeoutSeconds, func, arg, flag);
+#else
+    id_ = -1;
+#endif
+    RS_LOGD("start RSXCollie, tag: %{public}s, timeout: %{public}u, flag: %{public}u, id: %{public}d",
+        tag.c_str(), timeoutSeconds, flag, id_);
+}
+
+void RSXCollie::CancelRSXCollie()
+{
+    if (!isCanceled_) {
+        isCanceled_ = true;
+#ifdef ROSEN_OHOS
+        HiviewDFX::XCollie::GetInstance().CancelTimer(id_);
+#endif
+        RS_LOGD("cancel RSXCollie, tag: %{public}s, id: %{public}d", tag_.c_str(), id_);
+    }
+}
+
+RSXCollie::~RSXCollie()
+{
+    CancelRSXCollie();
+}
+} // namespace Rosen
+} // namespace OHOS
---
+++ rosen/test/render_service/render_service_base/unittest/common/rs_xcollie_test.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+#include "common/rs_xcollie.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+class RSXCollieTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+void RSXCollieTest::SetUpTestCase() {}
+void RSXCollieTest::TearDownTestCase() {}
+void RSXCollieTest::SetUp() {}
+void RSXCollieTest::TearDown() {}
+
+/**
+ * @tc.name: RSXCollieTest001
+ * @tc.desc: test of RSXCollie
+ * @tc.type:FUNC
+ * @tc.require:
+ */
+HWTEST_F(RSXCollieTest, RSXCollieTest001, TestSize.Level1)
+{
+    // no except
+    RSXCollie test("RSXCollieTest");
+}
+} // namespace OHOS::Rosen
