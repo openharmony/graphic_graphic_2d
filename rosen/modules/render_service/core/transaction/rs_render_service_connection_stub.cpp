ac74354d6b5e34cc0549d72ce149da4e902207f9
li-kiao <lx.96@foxmail.com>
2024-08-19 02:31:39 +0000

    drawing IPC接口安全校验
    Signed-off-by: li-kiao <lixiang284@huawei.com>
    Change-Id: If67e63b52eec64432fb48362f7c7263ad7029fa3

--- rosen/modules/render_service/core/transaction/rs_render_service_connection_stub.cpp
+++ rosen/modules/render_service/core/transaction/rs_render_service_connection_stub.cpp
@@ -1045,19 +1045,31 @@ int RSRenderServiceConnectionStub::OnRemoteRequest(
             break;
         }
         case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::REGISTER_TYPEFACE): {
-            uint64_t uniqueId = data.ReadUint64();
-            std::shared_ptr<Drawing::Typeface> typeface;
             bool result = false;
-            result = RSMarshallingHelper::Unmarshalling(data, typeface);
-            if (result) {
-                RegisterTypeface(uniqueId, typeface);
+            uint64_t uniqueId = data.ReadUint64();
+            // safe check
+            if (ExtractPid(uniqueId) == callingPid) {
+                std::shared_ptr<Drawing::Typeface> typeface;
+                result = RSMarshallingHelper::Unmarshalling(data, typeface);
+                if (result) {
+                    RegisterTypeface(uniqueId, typeface);
+                }
+            } else {
+                RS_LOGE("RSRenderServiceConnectionStub::OnRemoteRequest callingPid[%{public}d] "
+                    "no permission REGISTER_TYPEFACE", callingPid);
             }
             reply.WriteBool(result);
             break;
         }
         case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::UNREGISTER_TYPEFACE): {
             uint64_t uniqueId = data.ReadUint64();
-            UnRegisterTypeface(uniqueId);
+            // safe check
+            if (ExtractPid(uniqueId) == callingPid) {
+                UnRegisterTypeface(uniqueId);
+            } else {
+                RS_LOGE("RSRenderServiceConnectionStub::OnRemoteRequest callingPid[%{public}d] "
+                    "no permission UNREGISTER_TYPEFACE", callingPid);
+            }
             break;
         }
         case static_cast<uint32_t>(RSIRenderServiceConnectionInterfaceCode::SET_SCREEN_SKIP_FRAME_INTERVAL): {
