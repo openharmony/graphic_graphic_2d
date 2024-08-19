ee879ea8b8c661e2df7104f4c10f5fbd4b7e311f
PengKejie <pengkejie@h-partners.com>
2024-08-19 19:41:44 +0800

    新增接口OH_Drawing_TypographyGetLongestLineWithIndent，获取带有缩进的最长行宽度。
    Signed-off-by: PengKejie <pengkejie@h-partners.com>
    (cherry picked from commit cc014c0b3ff024a0ed839289586056de0e352ab9)

--- rosen/modules/2d_graphics/drawing_ndk/src/drawing_text_typography.cpp
+++ rosen/modules/2d_graphics/drawing_ndk/src/drawing_text_typography.cpp
@@ -558,6 +558,14 @@ double OH_Drawing_TypographyGetLongestLine(OH_Drawing_Typography* typography)
 #endif
 }
 
+double OH_Drawing_TypographyGetLongestLineWithIndent(OH_Drawing_Typography* typography)
+{
+    if (typography == nullptr) {
+        return 0.0;
+    }
+    return ConvertToOriginalText<Typography>(typography)->GetLongestLineWithIndent();
+}
+
 double OH_Drawing_TypographyGetMinIntrinsicWidth(OH_Drawing_Typography* typography)
 {
     return ConvertToOriginalText<Typography>(typography)->GetMinIntrinsicWidth();
