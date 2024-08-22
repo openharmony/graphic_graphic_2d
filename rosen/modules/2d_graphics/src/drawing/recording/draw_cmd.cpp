73893d07f7baf36f65af16304bc630bab5d9f764
s30043564 <sxl_gryx@126.com>
2024-08-22 06:54:00 +0000

    GetImageSnapshot空指针防护排查
    Signed-off-by: s30043564 <shenxiaoliang1@huawei.com>

--- rosen/modules/2d_graphics/src/drawing/recording/draw_cmd.cpp
+++ rosen/modules/2d_graphics/src/drawing/recording/draw_cmd.cpp
@@ -1505,13 +1505,17 @@ void DrawTextBlobOpItem::DrawHighContrastEnabled(Canvas* canvas) const
         if (GetOffScreenSurfaceAndCanvas(*canvas, offScreenSurface, offScreenCanvas)) {
             DrawHighContrast(offScreenCanvas.get(), true);
             offScreenCanvas->Flush();
+            auto image = offScreenSurface->GetImageSnapshot();
+            if (image == nullptr) {
+                return;
+            }
             Drawing::Brush paint;
             paint.SetAntiAlias(true);
             canvas->AttachBrush(paint);
             Drawing::SamplingOptions sampling =
                 Drawing::SamplingOptions(Drawing::FilterMode::NEAREST, Drawing::MipmapMode::NEAREST);
-            canvas->DrawImage(*offScreenSurface->GetImageSnapshot().get(),
-                x_ + textBlob_->Bounds()->GetLeft(), y_ + textBlob_->Bounds()->GetTop(), sampling);
+            canvas->DrawImage(*image, x_ + textBlob_->Bounds()->GetLeft(),
+                y_ + textBlob_->Bounds()->GetTop(), sampling);
             canvas->DetachBrush();
             return;
         }
@@ -1580,6 +1584,9 @@ bool DrawTextBlobOpItem::ConstructorHandle::GenerateCachedOpItem(
     offscreenCanvas->DrawTextBlob(textBlob, x, y);
 
     std::shared_ptr<Image> image = offscreenSurface->GetImageSnapshot();
+    if (image == nullptr) {
+        return false;
+    }
     Drawing::Rect src(0, 0, image->GetWidth(), image->GetHeight());
     Drawing::Rect dst(bounds->GetLeft(), bounds->GetTop(),
         bounds->GetLeft() + image->GetWidth(), bounds->GetTop() + image->GetHeight());
@@ -1635,6 +1642,9 @@ bool DrawTextBlobOpItem::ConstructorHandle::GenerateCachedOpItem(DrawCmdList& cm
     offscreenCanvas->DrawTextBlob(textBlob_.get(), x, y);
 
     std::shared_ptr<Image> image = offscreenSurface->GetImageSnapshot();
+    if (image == nullptr) {
+        return false;
+    }
     Drawing::Rect src(0, 0, image->GetWidth(), image->GetHeight());
     Drawing::Rect dst(bounds->GetLeft(), bounds->GetTop(),
         bounds->GetLeft() + image->GetWidth(), bounds->GetTop() + image->GetHeight());
@@ -1686,6 +1696,9 @@ std::shared_ptr<DrawImageRectOpItem> DrawTextBlobOpItem::GenerateCachedOpItem(Ca
     Playback(offscreenCanvas, nullptr);
 
     std::shared_ptr<Image> image = offscreenSurface->GetImageSnapshot();
+    if (image == nullptr) {
+        return nullptr;
+    }
     Drawing::Rect src(0, 0, image->GetWidth(), image->GetHeight());
     Drawing::Rect dst(bounds->GetLeft(), bounds->GetTop(), bounds->GetRight(), bounds->GetBottom());
     Paint fakePaint;
