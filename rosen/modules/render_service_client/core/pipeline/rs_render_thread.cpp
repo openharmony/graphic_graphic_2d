384dd9de5c4323cbe7a8ec62f4cde48f8dd3208d
chuchengcheng <chuchengcheng4@huawei.com>
2024-09-05 21:38:17 +0800

    cherry pick 261b98e from https://gitee.com/chuchengcheng/graphic_graphic_2d/pulls/14796 为RenderThread提高线程优先级
    Signed-off-by: chuchengcheng <chuchengcheng4@huawei.com>
    Change-Id: I80f03e677a63e656e9febbc92c5b3217dbe87454

--- rosen/modules/render_service_client/core/pipeline/rs_render_thread.cpp
+++ rosen/modules/render_service_client/core/pipeline/rs_render_thread.cpp
@@ -47,6 +47,10 @@
 #include "res_type.h"
 #endif
 
+#ifdef RES_CLINET_SCHED_ENABLE
+#include "qos.h"
+#endif
+
 #ifdef ROSEN_PREVIEW
 #include "glfw_render_context.h"
 #endif
@@ -312,6 +316,11 @@ void RSRenderThread::RenderLoop()
     auto delegate = RSFunctionalDelegate::Create();
     delegate->SetRepaintCallback([this]() { this->RequestNextVSync(); });
     RSOverdrawController::GetInstance().SetDelegate(delegate);
+#endif
+
+#ifdef RES_CLINET_SCHED_ENABLE
+    auto ret = OHOS::QOS::SetThreadQos(OHOS::QOS::QosLevel::QOS_USER_INTERACTIVE);
+    RS_LOGI("RSRenderThread: SetThreadQos retcode = %{public}d", ret);
 #endif
 
     if (runner_) {
