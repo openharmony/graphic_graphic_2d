300abc5a5ffe83f59231e5d245c43117009e1529
zhanglitao <zhanglitao11@huawei.com>
2024-09-05 14:37:15 +0800

    代码告警处理
    Signed-off-by: zhanglitao <zhanglitao11@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_canvas_drawing_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_canvas_drawing_render_node_drawable.cpp
@@ -239,6 +239,10 @@ void RSCanvasDrawingRenderNodeDrawable::FlushForGL(float width, float height, st
                 RS_LOGE("RSCanvasDrawingRenderNodeDrawable::Flush backendTexture_ is nullptr");
                 return;
             }
+            if (rscanvas.GetGPUContext() == nullptr) {
+                RS_LOGE("RSCanvasDrawingRenderNodeDrawable::Flush GPU context is nullptr");
+                return;
+            }
             Drawing::TextureOrigin origin = Drawing::TextureOrigin::BOTTOM_LEFT;
             Drawing::BitmapFormat info = Drawing::BitmapFormat{ image_->GetColorType(), image_->GetAlphaType() };
             SharedTextureContext* sharedContext = new SharedTextureContext(image_); // last image
@@ -704,6 +708,11 @@ bool RSCanvasDrawingRenderNodeDrawable::ResetSurfaceWithTexture(int width, int h
     }
     if (!backendTexture_.IsValid()) {
         RS_LOGE("RSCanvasDrawingRenderNodeDrawable::ResetSurfaceWithTexture backendTexture_ is nullptr");
+        ClearPreSurface(preSurface);
+        return false;
+    }
+    if (canvas.GetGPUContext() == nullptr) {
+        RS_LOGE("RSCanvasDrawingRenderNodeDrawable::ResetSurfaceWithTexture GPU context is nullptr");
         ClearPreSurface(preSurface);
         return false;
     }
