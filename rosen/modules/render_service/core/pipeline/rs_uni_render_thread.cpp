a6a733d63117781c36862d76a2ff0e58ff37aab1
玲珑 <gengchen1@huawei.com>
2024-09-06 02:37:06 +0000

    撤销帧间预分配
    Signed-off-by: 玲珑 <gengchen1@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_thread.cpp
@@ -65,7 +65,6 @@ namespace {
 constexpr const char* CLEAR_GPU_CACHE = "ClearGpuCache";
 constexpr const char* DEFAULT_CLEAR_GPU_CACHE = "DefaultClearGpuCache";
 constexpr const char* PURGE_CACHE_BETWEEN_FRAMES = "PurgeCacheBetweenFrames";
-constexpr const char* PRE_ALLOCATE_TEXTURE_BETWEEN_FRAMES = "PreAllocateTextureBetweenFrames";
 constexpr const char* ASYNC_FREE_VMAMEMORY_BETWEEN_FRAMES = "AsyncFreeVMAMemoryBetweenFrames";
 const std::string PERF_FOR_BLUR_IF_NEEDED_TASK_NAME = "PerfForBlurIfNeeded";
 constexpr uint32_t TIME_OF_EIGHT_FRAMES = 8000;
@@ -770,22 +769,6 @@ void RSUniRenderThread::PurgeCacheBetweenFrames()
         PURGE_CACHE_BETWEEN_FRAMES, 0, AppExecFwk::EventQueue::Priority::LOW);
 }
 
-void RSUniRenderThread::PreAllocateTextureBetweenFrames()
-{
-    if (!RSSystemProperties::IsPhoneType()) {
-        return;
-    }
-    RemoveTask(PRE_ALLOCATE_TEXTURE_BETWEEN_FRAMES);
-    PostTask(
-        [this]() {
-            RS_TRACE_NAME_FMT("PreAllocateTextureBetweenFrames");
-            GrDirectContext::preAllocateTextureBetweenFrames();
-        },
-        PRE_ALLOCATE_TEXTURE_BETWEEN_FRAMES,
-        0,
-        AppExecFwk::EventQueue::Priority::LOW);
-}
-
 void RSUniRenderThread::AsyncFreeVMAMemoryBetweenFrames()
 {
     RemoveTask(ASYNC_FREE_VMAMEMORY_BETWEEN_FRAMES);
@@ -801,9 +784,6 @@ void RSUniRenderThread::AsyncFreeVMAMemoryBetweenFrames()
 
 void RSUniRenderThread::MemoryManagementBetweenFrames()
 {
-    if (RSSystemProperties::GetPreAllocateTextureBetweenFramesEnabled()) {
-        PreAllocateTextureBetweenFrames();
-    }
     if (RSSystemProperties::GetAsyncFreeVMAMemoryBetweenFramesEnabled()) {
         AsyncFreeVMAMemoryBetweenFrames();
     }
