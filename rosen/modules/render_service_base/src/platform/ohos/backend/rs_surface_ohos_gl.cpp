06331dd317bd5ea8a69792d504226d098da50f8c
xiaoguandao <guanzhao1@huawei.com>
2024-09-09 19:11:45 +0800

    帧内释放时长控制，帧间压低水线，异步连续释放
    Signed-off-by: xiaoguandao <guanzhao1@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/backend/rs_surface_ohos_gl.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/backend/rs_surface_ohos_gl.cpp
@@ -88,6 +88,7 @@ std::unique_ptr<RSSurfaceFrame> RSSurfaceOhosGl::RequestFrame(int32_t width, int
     NativeWindowHandleOpt(mWindow, SET_UI_TIMESTAMP, uiTimestamp);
 
     context->MakeCurrent(mEglSurface);
+    
     ROSEN_LOGD("RSSurfaceOhosGl:RequestFrame, width is %d, height is %d",
         mWidth, mHeight);
 
