a97bd44d5181e768d7a787e149cf221677253bd5
PengKejie <pengkejie@h-partners.com>
2024-09-10 12:01:49 +0800

    修改version
    Signed-off-by: PengKejie <pengkejie@h-partners.com>

--- rosen/modules/2d_graphics/drawing_ndk/include/drawing_text_typography.h
+++ rosen/modules/2d_graphics/drawing_ndk/include/drawing_text_typography.h
@@ -1031,7 +1031,7 @@ double OH_Drawing_TypographyGetLongestLine(OH_Drawing_Typography*);
  * @syscap SystemCapability.Graphic.Graphic2D.NativeDrawing
  * @param OH_Drawing_Typography Indicates the pointer to an <b>OH_Drawing_Typography</b> object.
  * @return Returns the length of the longest line with indent.
- * @since 12
+ * @since 13
  * @version 1.1
  */
 double OH_Drawing_TypographyGetLongestLineWithIndent(OH_Drawing_Typography*);
