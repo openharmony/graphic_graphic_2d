f4cfebce2067ce2fb9e1a6a5eece5ca127949437
lili(@lili-lili-lili) <lili304@h-partners.com>
2024-09-11 16:57:25 +0800

    修复节点skipsync与节点下发子线程状态不一致问题
    Signed-off-by: lili(@lili-lili-lili) <lili304@h-partners.com>

--- rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
@@ -1022,9 +1022,11 @@ void RSSurfaceRenderNode::OnSkipSync()
         }
         auto context = GetContext().lock();
         if (context && !surfaceParams->GetHardwareEnabled()) {
-            context->GetMutableSkipSyncBuffer().push_back(
-                { preBuffer, GetRSSurfaceHandler()->GetConsumer(), surfaceParams->GetLastFrameHardwareEnabled() });
+            context->GetMutableSkipSyncBuffer().push_back({ GetFirstLevelNodeId(), preBuffer,
+                GetRSSurfaceHandler()->GetConsumer(), surfaceParams->GetLastFrameHardwareEnabled() });
             surfaceParams->SetPreBuffer(nullptr);
+            RS_LOGD("CollectSkipSyncBuffer fId[%" PRIu64"], sId[%" PRIu64"]",
+                GetFirstLevelNodeId(), surfaceParams->GetId());
         }
     }
 #endif
