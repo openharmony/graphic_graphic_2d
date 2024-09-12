53c6bf00aff0bea0d18386b2b44fe0b54ae57076
h30026439 <huangzhizhong6@huawei.com>
2024-09-12 18:50:20 +0800

    Signed-off-by: h30026439 <huangzhizhong6@huawei.com>

--- rosen/modules/render_service_client/core/ui/rs_node.h
+++ rosen/modules/render_service_client/core/ui/rs_node.h
@@ -48,6 +48,7 @@ namespace OHOS {
 namespace Rosen {
 using DrawFunc = std::function<void(std::shared_ptr<Drawing::Canvas>)>;
 using PropertyCallback = std::function<void()>;
+using BoundsChangedCallback = std::function<void (const Rosen::Vector4f&)>;
 class RSAnimation;
 class RSCommand;
 class RSImplicitAnimParam;
@@ -440,7 +441,6 @@ public:
 
     std::string GetFrameNodeTag();
 
-    using BoundsChangedCallback = std::function<void (const Rosen::Vector4f&)>;
     virtual void SetBoundsChangedCallback(BoundsChangedCallback callback){};
     bool IsTextureExportNode() const
     {
