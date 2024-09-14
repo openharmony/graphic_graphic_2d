a8c3ff90f1d14fbdeacd70f563bbb7b98c8bb315
chuchengcheng <chuchengcheng4@huawei.com>
2024-09-14 10:51:39 +0800

    格式修改cherry-pick
    Signed-off-by: chuchengcheng <chuchengcheng4@huawei.com>
    Change-Id: I0739118d2b42ecb33d2a88f37d0bf6beead440b0

--- rosen/modules/render_service_client/core/pipeline/rs_divided_ui_capture.cpp
+++ rosen/modules/render_service_client/core/pipeline/rs_divided_ui_capture.cpp
@@ -41,6 +41,7 @@ namespace Rosen {
 
 const int FAKE_WIDTH = 10; // When the width and height of the node are not set, use the fake width
 const int FAKE_HEIGHT = 10; // When the width and height of the node are not set, use the fake height
+const int MAX_WAIT_TIME = 2000;
 
 std::shared_ptr<Media::PixelMap> RSDividedUICapture::TakeLocalCapture()
 {
@@ -266,7 +267,7 @@ void RSDividedUICapture::RSDividedUICaptureVisitor::ProcessSurfaceRenderNode(RSS
     captureConfig.scaleY = scaleY_;
     captureConfig.captureType = SurfaceCaptureType::UICAPTURE;
     renderServiceClient->TakeSurfaceCapture(node.GetId(), callback, captureConfig);
-    std::shared_ptr<Media::PixelMap> pixelMap = callback->GetResult(2000);
+    std::shared_ptr<Media::PixelMap> pixelMap = callback->GetResult(MAX_WAIT_TIME);
     if (pixelMap == nullptr) {
         ROSEN_LOGE("RSDividedUICaptureVisitor::TakeLocalCapture failed to get pixelmap, return nullptr!");
         return;
