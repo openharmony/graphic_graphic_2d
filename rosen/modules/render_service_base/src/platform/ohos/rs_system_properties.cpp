10371441e5165a134d6c4755737cd01d4ac338ee
l00844999 <lizhan12@huawei.com>
2024-09-26 09:43:43 +0800

    ddos1.0 （cherry picked commit from <gitee.com//openharmony/graphic_graphic_2d/commit/85a46329054937d4e945220c5514cf136fddf24e> Signed-off-by: l00844999 <lizhan12@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/rs_system_properties.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/rs_system_properties.cpp
@@ -1062,11 +1062,10 @@ bool RSSystemProperties::GetSkipDisplayIfScreenOffEnabled()
     return ConvertToInt(num, 1) != 0;
 }
 
-bool RSSystemProperties::GetMemoryOverTreminateEnabled()
+std::string RSSystemProperties::GetVersionType()
 {
-    static bool flag =
-        std::atoi((system::GetParameter("persist.sys.graphic.memoryOverTreminateEnabled", "0")).c_str()) != 0;
-    return flag;
+    static std::string versionType = system::GetParameter("const.logsystem.versiontype", "");
+    return versionType;
 }
 
 bool RSSystemProperties::GetDrmMarkedFilterEnabled()
--- rosen/modules/render_service_base/src/platform/windows/rs_system_properties.cpp
+++ rosen/modules/render_service_base/src/platform/windows/rs_system_properties.cpp
@@ -483,9 +483,9 @@ bool RSSystemProperties::GetSkipDisplayIfScreenOffEnabled()
     return false;
 }
 
-bool RSSystemProperties::GetMemoryOverTreminateEnabled()
+std::string RSSystemProperties::GetVersionType()
 {
-    return false;
+    return "";
 }
 
 bool RSSystemProperties::GetLayerCursorEnable()
