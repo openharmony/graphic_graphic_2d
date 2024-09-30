a154100e06286d02acb39e1c0150540eb50a011b
沁心 <chenqinxin1@huawei.com>
2024-09-30 13:16:20 +0000

    回退 'Pull Request !15632 : pixelmap序列化和反序列化增加维测校验'

--- rosen/modules/render_service_base/include/transaction/rs_marshalling_helper.h
+++ rosen/modules/render_service_base/include/transaction/rs_marshalling_helper.h
@@ -411,7 +411,6 @@ public:
     static void BeginNoSharedMem(std::thread::id tid);
     static void EndNoSharedMem();
     static bool GetUseSharedMem(std::thread::id tid);
-    static bool CheckReadPosition(Parcel& parcel);
 private:
     static bool WriteToParcel(Parcel& parcel, const void* data, size_t size);
     static const void* ReadFromParcel(Parcel& parcel, size_t size, bool& isMalloc);
