89db27f0f73e82eeaeb967cf788ef9c5fb69ec90
chenlushen <chenlushen1@huawei.com>
2024-10-03 10:39:10 +0800

    fix cast bug caused by confilcts of skipframe and dirty
    Signed-off-by: chenlushen <chenlushen1@huawei.com>
    Change-Id: If8ba8aab74b4f8129695a89224eb6a067cff1547

--- rosen/modules/render_service/core/drawable/rs_display_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_display_render_node_drawable.cpp
@@ -558,6 +558,9 @@ void RSDisplayRenderNodeDrawable::OnDraw(Drawing::Canvas& canvas)
         screenManager->ForceRefreshOneFrameIfNoRNV();
         return;
     }
+    if (curScreenInfo.skipFrameInterval > 1) {
+        virtualDirtyRefresh_ = true;
+    }
 
     auto screenInfo = params->GetScreenInfo();
     auto processor = RSProcessorFactory::CreateProcessor(params->GetCompositeType());
@@ -881,6 +884,9 @@ std::vector<RectI> RSDisplayRenderNodeDrawable::CalculateVirtualDirty(
     }
     ScreenInfo mainScreenInfo = screenManager->QueryScreenInfo(mirrorParams->GetScreenId());
     ScreenInfo curScreenInfo = screenManager->QueryScreenInfo(params.GetScreenId());
+    if (curScreenInfo.skipFrameInterval > 1) {
+        return mappedDamageRegionRects;
+    }
 
     int32_t bufferAge = virtualProcesser->GetBufferAge();
     int32_t actualAge = curScreenInfo.skipFrameInterval ?
