e6ab53fcc9608bd90d18f7d96a33b6580b3df655
key-destiny <luojianqi@huawei.com>
2024-10-08 21:55:52 +0800

    Signed-off-by:key-destiny<luojianqi@huawei.com>
    Signed-off-by: key-destiny <luojianqi@huawei.com>

--- rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h
+++ rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h
@@ -53,7 +53,8 @@ public:
     static std::shared_ptr<Drawing::ShaderEffect> MakeBinarizationShader(float low, float high, float thresholdLow,
         float thresholdHigh, std::shared_ptr<Drawing::ShaderEffect> imageShader);
     static std::shared_ptr<Drawing::RuntimeBlenderBuilder> MakeDynamicBrightnessBuilder();
-    static std::shared_ptr<Drawing::Blender> MakeDynamicBrightnessBlender(const RSDynamicBrightnessPara& params);
+    static std::shared_ptr<Drawing::Blender> MakeDynamicBrightnessBlender(const RSDynamicBrightnessPara& params,
+        float ratio = 1.0f);
     static void DrawBinarization(Drawing::Canvas* canvas, const std::optional<Vector4f>& aiInvert);
     static void DrawPixelStretch(Drawing::Canvas* canvas, const std::optional<Vector4f>& pixelStretch,
         const RectF& boundsRect, const bool boundsGeoValid, const Drawing::TileMode pixelStretchTileMode);
