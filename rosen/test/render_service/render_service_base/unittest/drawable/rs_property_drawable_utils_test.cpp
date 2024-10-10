701ef750e4d5dae78dfe912e7e082d863b5891f8
porridge783 <chenbaoshan_mr@163.com>
2024-10-10 11:12:39 +0000

    修改ut用例
    Signed-off-by: porridge783 <chenbaoshan_mr@163.com>

--- rosen/test/render_service/render_service_base/unittest/drawable/rs_property_drawable_utils_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/drawable/rs_property_drawable_utils_test.cpp
@@ -236,8 +236,6 @@ HWTEST_F(RSPropertyDrawableUtilsTest, RSPropertyDrawableUtilsTest008, testing::e
     paintFilterCanvas.canvas_ = &canvasTest2;
     rsPropertyDrawableUtils->DrawBackgroundEffect(&paintFilterCanvas, rsFilter, cacheManager, false, bounds);
     paintFilterCanvas.SetDisableFilterCache(true);
-    auto surfaceTwo = Drawing::Surface::MakeRasterN32Premul(10, 10);
-    paintFilterCanvas.surface_ = surfaceTwo.get();
     rsPropertyDrawableUtils->DrawBackgroundEffect(&paintFilterCanvas, rsFilter, cacheManager, false, bounds);
     int lastBlurCnt = rsPropertyDrawableUtils->g_blurCnt;
     ASSERT_NE(lastBlurCnt, 0);
