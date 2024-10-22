ff0f183f811f6e85cf70e2a8722c76424bf35a5c
liumingxiang1 <liumingxiang1@huawei.com>
2024-10-22 19:22:04 +0800

    修复TS AddText接口非法UTF-8转UTF-16崩溃问题
    Signed-off-by: liumingxiang1 <liumingxiang1@huawei.com>

--- interfaces/kits/napi/graphic/text/paragraph_builder_napi/js_paragraph_builder.cpp
+++ interfaces/kits/napi/graphic/text/paragraph_builder_napi/js_paragraph_builder.cpp
@@ -16,6 +16,7 @@
 #include "js_paragraph_builder.h"
 #include "napi_common.h"
 #include "paragraph_napi/js_paragraph.h"
+#include "utils/string_util.h"
 #include "utils/text_log.h"
 
 namespace OHOS::Rosen {
@@ -166,6 +167,10 @@ napi_value JsParagraphBuilder::OnAddText(napi_env env, napi_callback_info info)
     }
     std::string text = "";
     if (ConvertFromJsValue(env, argv[0], text)) {
+        if (!IsUtf8(text.c_str(), text.size())) {
+            TEXT_LOGE("Invalid utf-8 text");
+            return NapiThrowError(env, TextErrorCode::ERROR_INVALID_PARAM, "Invalid params.");
+        }
         typographyCreate_->AppendText(Str8ToStr16(text));
     }
     return NapiGetUndefined(env);
