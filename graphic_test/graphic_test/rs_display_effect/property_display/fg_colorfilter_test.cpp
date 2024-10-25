aac946fe5fde8436d8a4495876bbdb847daff950
王德海 <wangdehai2@huawei.com>
2024-10-25 10:34:37 +0800

    graphictest
    Signed-off-by: 王德海 <wangdehai2@huawei.com>
    Change-Id: I5d0fb5925248967e62c2005d2b307db8b4acc72a

---
+++ graphic_test/graphic_test/rs_display_effect/property_display/fg_colorfilter_test.cpp
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rs_graphic_test.h"
+#include "rs_graphic_test_img.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+
+class ForegroundTest01 : public RSGraphicTest {
+private:
+    const int screenWidth = 1260;
+    const int screenHeight = 2720;
+
+public:
+    // called before each tests
+    void BeforeEach() override
+    {
+        SetScreenSurfaceBounds({ 0, 0, screenWidth, screenHeight });
+        SetSurfaceColor(RSColor(0xff000000));
+    }
+};
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, GrayScale_Test_1)
+{
+    float grayScaleList[] = { 0.0, 0.5, 1.0, 2.0 };
+    for (int i = 0; i < 4; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetGrayScale(grayScaleList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, Brightness_Test_1)
+{
+    float brightnessList[] = { 0.0, 0.5, 1.0, 2.0, 5.0 };
+    for (int i = 0; i < 5; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetBrightness(brightnessList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, Contrast_Test_1)
+{
+    float contrastList[] = { 0.0, 0.5, 1.0, 5.0, 20.0 };
+    for (int i = 0; i < 5; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetContrast(contrastList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, Saturate_Test_1)
+{
+    float saturateList[] = { 0.0, 0.5, 1.0, 10.0, 100.0 };
+    for (int i = 0; i < 5; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetSaturate(saturateList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, Sepia_Test_1)
+{
+    float sepiaList[] = { 0.0, 0.5, 1.0, 2.0 };
+    for (int i = 0; i < 4; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetSepia(sepiaList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, Color_Invert_Test_1)
+{
+    float invertList[] = { 0.0, 0.5, 1.0, 2.0 };
+    for (int i = 0; i < 4; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetInvert(invertList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, HueRotate_Test_1)
+{
+    float hueRotateList[] = { 0.0, 90.0, 180.0, 270.0, 360.0 };
+    for (int i = 0; i < 5; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetHueRotate(hueRotateList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, ColorBlend_Test_1)
+{
+    uint32_t colorList[] = { 0xffffffff, 0xff000000, 0xffff0000, 0xff00ff00, 0xff0000ff, 0x00000000 };
+    for (int i = 0; i < 6; i++) {
+        int x = (i % 2) * 510;
+        int y = (i / 2) * 510;
+        auto testNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { x, y, 500, 500 });
+        testNode->SetColorBlend(colorList[i]);
+        GetRootNode()->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+GRAPHIC_TEST(ForegroundTest01, CONTENT_DISPLAY_TEST, Color_AiInvert_Test_1)
+{
+    auto testFaNode = SetUpNodeBgImage("/data/local/tmp/fg_test.jpg", { 0, 0, 1000, 1000 });
+    GetRootNode()->AddChild(testFaNode);
+    RegisterNode(testFaNode);
+    Vector4f param_list[] = { { 0.0, 1.0, 0.5, 0.2 }, { 0.2, 0.5, 0.2, 0.2 }, { 0.0, 1.0, 0.5, 0.0 } };
+    for (int i = 0; i < 3; i++) {
+        int y = i * 310;
+        auto testNode = RSCanvasNode::Create();
+        testNode->SetBounds({ 0.0, y, 1000.0, 310.0 });
+        testNode->SetAiInvert(param_list[i]);
+        testFaNode->AddChild(testNode);
+        RegisterNode(testNode);
+    }
+}
+
+} // namespace OHOS::Rosen
---
+++ graphic_test/graphic_test_framework/include/rs_graphic_test.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef RS_GRAPHIC_TEST_H
+#define RS_GRAPHIC_TEST_H
+
+#include "common/rs_color.h"
+#include "common/rs_rect.h"
+#include "rs_graphic_rootnode.h"
+#include "rs_graphic_test_ext.h"
+#include "ui/rs_canvas_node.h"
+#include "ui/rs_surface_node.h"
+
+namespace OHOS {
+namespace Rosen {
+class RSGraphicRootNode;
+class RSGraphicTest : public testing::Test {
+public:
+    std::shared_ptr<RSGraphicRootNode> GetRootNode() const;
+    Vector2f GetScreenSize() const;
+    void SetSurfaceBounds(const Vector4f& bounds);
+    void SetScreenSurfaceBounds(const Vector4f& bounds);
+    void SetSurfaceColor(const RSColor& color);
+    void RegisterNode(std::shared_ptr<RSNode> node);
+
+    // overrides gtest functions
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override final;
+    void TearDown() override final;
+
+    // user functions
+    virtual void BeforeEach() {};
+    virtual void AfterEach() {};
+
+private:
+    std::string GetImageSavePath(const std::string path);
+
+    bool shouldRunTest_ = true;
+    Vector4f surfaceBounds_;
+    std::vector<std::shared_ptr<RSNode>> nodes_;
+    static uint32_t imageWriteId_;
+    std::string imageSavePath_ = "";
+};
+} // namespace Rosen
+} // namespace OHOS
+
+#endif // RS_GRAPHIC_TEST_H
