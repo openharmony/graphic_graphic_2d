09a24da439aedada23b61125d6704031d376dae2
wangxiuxiu96 <wangxiuxiu9@huawei.com>
2024-11-01 09:43:37 +0800

    navigation节点缓存优化性能
    Signed-off-by: wangxiuxiu96 <wangxiuxiu9@huawei.com>
    Change-Id: I3acf51464ad43acc481406e7ff6181ede8216a3a

--- rosen/modules/render_service_client/core/ui/rs_node.h
+++ rosen/modules/render_service_client/core/ui/rs_node.h
@@ -459,6 +459,11 @@ public:
         return isTextureExportNode_;
     }
 
+    size_t GetAnimationsCount() const
+    {
+        return animations_.size();
+    }
+
     bool IsGeometryDirty() const;
     bool IsAppearanceDirty() const;
     void MarkDirty(NodeDirtyType type, bool isDirty);
