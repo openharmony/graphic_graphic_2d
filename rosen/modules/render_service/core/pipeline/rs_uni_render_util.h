81bc6b7d30141c50dd63ba75aaa216acef3b76cb
g30058336 <guqiuhan@huawei.com>
2024-11-01 18:08:30 +0800

    addPostTaskName
    Signed-off-by: g30058336 <guqiuhan@huawei.com>
    Change-Id: I29a1efc0d37bda290cbd4cfdde3621975e584b9f

--- rosen/modules/render_service/core/pipeline/rs_uni_render_util.h
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_util.h
@@ -164,6 +164,7 @@ private:
     static GraphicTransformType GetRotateTransformForRotationFixed(RSSurfaceRenderNode& node,
         sptr<IConsumerSurface> consumer);
     static inline int currentUIExtensionIndex_ = -1;
+    static char* RELEASE_SURFACE_TASK = "releaseSurface";
 };
 }
 }
