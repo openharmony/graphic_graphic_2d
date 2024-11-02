24cc3320fc1cd47fdf5f44776e3fdca60beb07b7
lishijie <lishijie22@huawei.com>
2024-11-02 09:16:53 +0000

    update
    Signed-off-by: lishijie <lishijie22@huawei.com>
    Change-Id: Ib6d4694ea9e887d6912aefcf56988b357c80dda6

--- rosen/test/render_service/render_service_client/fuzztest/ui/rscanvasnode_fuzzer/rscanvasnode_fuzzer.cpp
+++ rosen/test/render_service/render_service_client/fuzztest/ui/rscanvasnode_fuzzer/rscanvasnode_fuzzer.cpp
@@ -13,33 +13,26 @@
  * limitations under the License.
  */
 
-#include "rscanvasdrawingnode_fuzzer.h"
+#include "rscanvasnode_fuzzer.h"
 
 #include <cstddef>
 #include <cstdint>
 #include <securec.h>
 
-#include "ui/rs_canvas_drawing_node.h"
-#include "command/rs_canvas_drawing_node_command.h"
-#include "common/rs_obj_geometry.h"
-#include "pipeline/rs_canvas_drawing_render_node.h"
-#include "pipeline/rs_node_map.h"
-#include "pipeline/rs_render_thread.h"
-#include "platform/common/rs_log.h"
-#include "transaction/rs_render_service_client.h"
+#include "ui/rs_canvas_node.h"
 #include "transaction/rs_transaction_proxy.h"
-#include "pipeline/rs_canvas_render_node.h"
+#include "transaction/rs_transaction_data.h"
 
 namespace OHOS {
 namespace Rosen {
 namespace {
-const uint8_t* g_data = nullptr;
+const uint8_t* DATA = nullptr;
 size_t g_size = 0;
 size_t g_pos;
 } // namespace
 
 /*
- * describe: get data from outside untrusted data(g_data) which size is according to sizeof(T)
+ * describe: get data from outside untrusted data(DATA) which size is according to sizeof(T)
  * tips: only support basic type
  */
 template<class T>
@@ -47,33 +40,15 @@ T GetData()
 {
     T object {};
     size_t objectSize = sizeof(object);
-    if (g_data == nullptr || objectSize > g_size - g_pos) {
+    if (DATA == nullptr || objectSize > g_size - g_pos) {
         return object;
     }
-    errno_t ret = memcpy_s(&object, objectSize, g_data + g_pos, objectSize);
+    errno_t ret = memcpy_s(&object, objectSize, DATA + g_pos, objectSize);
     if (ret != EOK) {
         return {};
     }
     g_pos += objectSize;
     return object;
-}
-
-bool DoRSCanvasDrawingNode(const uint8_t* data, size_t size)
-{
-    if (data == nullptr) {
-        return false;
-    }
-
-    // initialize
-    g_data = data;
-    g_size = size;
-    g_pos = 0;
-
-    // test
-    bool isRenderServiceNode = GetData<bool>();
-    bool isTextureExportNode = GetData<bool>();
-    RSCanvasDrawingNode canvasDrawingNode(isRenderServiceNode, isTextureExportNode);
-    return true;
 }
 
 bool DoCreate(const uint8_t* data, size_t size)
@@ -83,91 +58,41 @@ bool DoCreate(const uint8_t* data, size_t size)
     }
 
     // initialize
-    g_data = data;
+    DATA = data;
     g_size = size;
     g_pos = 0;
 
     // test
     bool isRenderServiceNode = GetData<bool>();
     bool isTextureExportNode = GetData<bool>();
-    RSCanvasDrawingNode::SharedPtr node = RSCanvasDrawingNode::Create(isRenderServiceNode, isTextureExportNode);
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
     return true;
 }
 
-bool DoGetBitmap(const uint8_t* data, size_t size)
+bool DoBeginRecordingAndFinishRecording(const uint8_t* data, size_t size)
 {
     if (data == nullptr) {
         return false;
     }
 
     // initialize
-    g_data = data;
+    DATA = data;
     g_size = size;
     g_pos = 0;
 
     // test
     bool isRenderServiceNode = GetData<bool>();
     bool isTextureExportNode = GetData<bool>();
-    RSCanvasDrawingNode canvasDrawingNode(isRenderServiceNode, isTextureExportNode);
-    Drawing::Bitmap bitmap;
-    int w = GetData<int>();
-    int h = GetData<int>();
-    auto list = std::make_shared<Drawing::DrawCmdList>(w, h);
-    float fLeft = GetData<float>();
-    float fTop = GetData<float>();
-    float fRight = GetData<float>();
-    float fBottom = GetData<float>();
-    Drawing::Rect rect { fLeft, fTop, fRight, fBottom };
-    canvasDrawingNode.GetBitmap(bitmap, list, &rect);
-    return true;
-}
-
-bool DoGetPixelmap(const uint8_t* data, size_t size)
-{
-    if (data == nullptr) {
-        return false;
-    }
-
-    // initialize
-    g_data = data;
-    g_size = size;
-    g_pos = 0;
-
-    // test
-    bool isRenderServiceNode = GetData<bool>();
-    bool isTextureExportNode = GetData<bool>();
-    RSCanvasDrawingNode canvasDrawingNode(isRenderServiceNode, isTextureExportNode);
-    std::shared_ptr<Media::PixelMap> pixelmap = std::make_shared<Media::PixelMap>();
-    int w = GetData<int>();
-    int h = GetData<int>();
-    auto list = std::make_shared<Drawing::DrawCmdList>(w, h);
-    float fLeft = GetData<float>();
-    float fTop = GetData<float>();
-    float fRight = GetData<float>();
-    float fBottom = GetData<float>();
-    Drawing::Rect rect { fLeft, fTop, fRight, fBottom };
-    canvasDrawingNode.GetPixelmap(pixelmap, list, &rect);
-    return true;
-}
-
-bool DoResetSurface(const uint8_t* data, size_t size)
-{
-    if (data == nullptr) {
-        return false;
-    }
-
-    // initialize
-    g_data = data;
-    g_size = size;
-    g_pos = 0;
-
-    // test
-    bool isRenderServiceNode = GetData<bool>();
-    bool isTextureExportNode = GetData<bool>();
-    RSCanvasDrawingNode canvasDrawingNode(isRenderServiceNode, isTextureExportNode);
-    int w = GetData<int>();
-    int h = GetData<int>();
-    canvasDrawingNode.ResetSurface(w, h);
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    int width = GetData<int>();
+    int height = GetData<int>();
+    canvasNode->BeginRecording(width, height);
+    canvasNode->IsRecording();
+    canvasNode->FinishRecording();
     return true;
 }
 
@@ -178,15 +103,149 @@ bool DoCreateTextureExportRenderNodeInRT(const uint8_t* data, size_t size)
     }
 
     // initialize
-    g_data = data;
+    DATA = data;
     g_size = size;
     g_pos = 0;
 
     // test
     bool isRenderServiceNode = GetData<bool>();
     bool isTextureExportNode = GetData<bool>();
-    RSCanvasDrawingNode canvasDrawingNode(isRenderServiceNode, isTextureExportNode);
-    canvasDrawingNode.CreateTextureExportRenderNodeInRT();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    canvasNode->CreateTextureExportRenderNodeInRT();
+    return true;
+}
+
+bool DoDrawOnNode(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+
+    // test
+    bool isRenderServiceNode = GetData<bool>();
+    bool isTextureExportNode = GetData<bool>();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    RSModifierType type = RSModifierType::INVALID;
+    DrawFunc func = [&](std::shared_ptr<Drawing::Canvas>) {};
+    canvasNode->DrawOnNode(type, func);
+    return true;
+}
+
+bool DoGetPaintWidthAndGetPaintHeight(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+
+    // test
+    bool isRenderServiceNode = GetData<bool>();
+    bool isTextureExportNode = GetData<bool>();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    canvasNode->GetPaintWidth();
+    canvasNode->GetPaintHeight();
+    return true;
+}
+
+bool DoSetFreeze(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+
+    // test
+    bool isRenderServiceNode = GetData<bool>();
+    bool isTextureExportNode = GetData<bool>();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    bool isFreeze = GetData<bool>();
+    canvasNode->SetFreeze(isFreeze);
+    return true;
+}
+
+bool DoSetHDRPresent(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+
+    // test
+    bool isRenderServiceNode = GetData<bool>();
+    bool isTextureExportNode = GetData<bool>();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    bool hdrPresent = GetData<bool>();
+    canvasNode->SetHDRPresent(hdrPresent);
+    return true;
+}
+
+bool DoOnBoundsSizeChanged(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+
+    // test
+    bool isRenderServiceNode = GetData<bool>();
+    bool isTextureExportNode = GetData<bool>();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    canvasNode->OnBoundsSizeChanged();
+    return true;
+}
+
+bool DoSetBoundsChangedCallback(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    DATA = data;
+    g_size = size;
+    g_pos = 0;
+
+    // test
+    bool isRenderServiceNode = GetData<bool>();
+    bool isTextureExportNode = GetData<bool>();
+    delete RSTransactionProxy::instance_;
+    RSTransactionProxy::instance_ = nullptr;
+    RSCanvasNode::SharedPtr canvasNode = RSCanvasNode::Create(isRenderServiceNode, isTextureExportNode);
+    BoundsChangedCallback callback = [](const Rosen::Vector4f& vector4f) {};
+    canvasNode->SetBoundsChangedCallback(callback);
     return true;
 }
 } // namespace Rosen
@@ -196,12 +255,15 @@ bool DoCreateTextureExportRenderNodeInRT(const uint8_t* data, size_t size)
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
-    OHOS::Rosen::DoRSCanvasDrawingNode(data, size);
     OHOS::Rosen::DoCreate(data, size);
-    OHOS::Rosen::DoGetBitmap(data, size);
-    OHOS::Rosen::DoGetPixelmap(data, size);
-    OHOS::Rosen::DoResetSurface(data, size);
+    OHOS::Rosen::DoBeginRecordingAndFinishRecording(data, size);
     OHOS::Rosen::DoCreateTextureExportRenderNodeInRT(data, size);
+    OHOS::Rosen::DoDrawOnNode(data, size);
+    OHOS::Rosen::DoGetPaintWidthAndGetPaintHeight(data, size);
+    OHOS::Rosen::DoSetFreeze(data, size);
+    OHOS::Rosen::DoSetHDRPresent(data, size);
+    OHOS::Rosen::DoOnBoundsSizeChanged(data, size);
+    OHOS::Rosen::DoSetBoundsChangedCallback(data, size);
+
     return 0;
 }
-
