1813e70898f186e32191155f3b8c60e4cac85cbe
xcxcxcx2019 <liujiaming35@huawei.com>
2024-11-05 23:08:13 +0800

    添加TDD
    Signed-off-by: xcxcxcx2019 <liujiaming35@huawei.com>

--- rosen/test/render_service/render_service_base/unittest/render/rs_image_cache_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/render/rs_image_cache_test.cpp
@@ -251,4 +251,263 @@ HWTEST_F(RSImageCacheTest, ReleaseUniqueIdListTest, TestSize.Level1)
     EXPECT_TRUE(RSImageCache::Instance().uniqueIdList_.empty());
     RSImageCache::Instance().uniqueIdList_.clear();
 }
+
+/**
+ * @tc.name: GetDrawingImageCacheTest
+ * @tc.desc: Verify function GetDrawingImageCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, GetDrawingImageCacheTest, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto img = std::make_shared<Drawing::Image>();
+    imageCache.CacheDrawingImage(1, img);
+    EXPECT_EQ(imageCache.GetDrawingImageCache(1), img);
+    imageCache.drawingImageCache_.clear(); 
+}
+
+/**
+ * @tc.name: GetPixelMapCacheTest001
+ * @tc.desc: Verify function GetPixelMapCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, GetPixelMapCacheTest001, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.CacheDrawingImage(1, pixelMap);
+    EXPECT_EQ(imageCache.GetPixelMapCache(1), pixelMap);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: IncreasePixelMapCacheRefCountTest001
+ * @tc.desc: Verify function IncreasePixelMapCacheRefCount
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, IncreasePixelMapCacheRefCountTest001, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.CacheDrawingImage(1, pixelMap);
+    imageCache.IncreasePixelMapCacheRefCount(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: ReleasePixelMapCacheTest001
+ * @tc.desc: Verify function ReleasePixelMapCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, ReleasePixelMapCacheTest001, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.CacheDrawingImage(1, pixelMap);
+    imageCache.ReleasePixelMapCache(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: ReleasePixelMapCacheTest002
+ * @tc.desc: Verify function ReleasePixelMapCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, ReleasePixelMapCacheTest002, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.pixelMapCache_.emplace(1, std::make_pair(nullptr, 0));
+    pixelMap->allocatorType_ = Media::AllocatorType::DMA_ALLOC;
+    imageCache.ReleasePixelMapCache(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: ReleasePixelMapCacheTest003
+ * @tc.desc: Verify function ReleasePixelMapCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, ReleasePixelMapCacheTest003, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.pixelMapCache_.emplace(1, std::make_pair(nullptr, 1));
+    pixelMap->allocatorType_ = Media::AllocatorType::DMA_ALLOC;
+    imageCache.ReleasePixelMapCache(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: ReleasePixelMapCacheTest004
+ * @tc.desc: Verify function ReleasePixelMapCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, ReleasePixelMapCacheTest004, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.pixelMapCache_.emplace(1, std::make_pair(pixelMap, 0));
+    pixelMap->allocatorType_ = Media::AllocatorType::HEAP_ALLOC;
+    imageCache.ReleasePixelMapCache(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: ReleasePixelMapCacheTest005
+ * @tc.desc: Verify function ReleasePixelMapCache
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, ReleasePixelMapCacheTest005, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    imageCache.pixelMapCache_.emplace(1, std::make_pair(pixelMap, 1));
+    pixelMap->allocatorType_ = Media::AllocatorType::HEAP_ALLOC;
+    imageCache.ReleasePixelMapCache(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: GetRenderDrawingImageCacheByPixelMapIdTest001
+ * @tc.desc: Verify function GetRenderDrawingImageCacheByPixelMapId
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, GetRenderDrawingImageCacheByPixelMapIdTest001, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto img = std::make_shared<Drawing::Image>();
+    imageCache.CacheRenderDrawingImageByPixelMapId(1, img, 0);
+    EXPECT_EQ(imageCache.GetRenderDrawingImageCacheByPixelMapId(1, 0), img);
+    imageCache.pixelMapIdRelatedDrawingImageCache_.clear();
+}
+
+/**
+ * @tc.name: ReleaseDrawingImageCacheByPixelMapIdTest001
+ * @tc.desc: Verify function ReleaseDrawingImageCacheByPixelMapId
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, ReleaseDrawingImageCacheByPixelMapIdTest001, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto img = std::make_shared<Drawing::Image>();
+    imageCache.CacheRenderDrawingImageByPixelMapId(1, img, 0);
+    imageCache.ReleaseDrawingImageCacheByPixelMapId(1);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapIdRelatedDrawingImageCache_.clear();
+}
+
+/**
+ * @tc.name: CachePixelMapTest
+ * @tc.desc: Verify function CachePixelMap
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, CachePixelMapTest, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    uint64_t uniqueId = 1;
+    imageCache.CachePixelMap(uniqueId, nullptr);
+    EXPECT_FALSE(imageCache.pixelMapCache_.empty());
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: CachePixelMapTest001
+ * @tc.desc: Verify function CachePixelMap
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, CachePixelMapTest001, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    uint64_t uniqueId = 0;
+    imageCache.CachePixelMap(uniqueId, nullptr);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: CachePixelMapTest002
+ * @tc.desc: Verify function CachePixelMap
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, CachePixelMapTest002, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    uint64_t uniqueId = 1;
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    pixelMap->allocatorType_ = Media::AllocatorType::DMA_ALLOC;
+    imageCache.CachePixelMap(uniqueId, pixelMap);
+    EXPECT_FALSE(imageCache.pixelMapCache_.empty());
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: CachePixelMapTest003
+ * @tc.desc: Verify function CachePixelMap
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, CachePixelMapTest003, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    uint64_t uniqueId = 0;
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    pixelMap->allocatorType_ = Media::AllocatorType::DMA_ALLOC;
+    imageCache.CachePixelMap(uniqueId, pixelMap);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: CachePixelMapTest004
+ * @tc.desc: Verify function CachePixelMap
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, CachePixelMapTest004, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    uint64_t uniqueId = 1;
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    pixelMap->allocatorType_ = Media::AllocatorType::HEAP_ALLOC;
+    imageCache.CachePixelMap(uniqueId, pixelMap);
+    EXPECT_FALSE(imageCache.pixelMapCache_.empty());
+    imageCache.pixelMapCache_.clear();
+}
+
+/**
+ * @tc.name: CachePixelMapTest005
+ * @tc.desc: Verify function CachePixelMap
+ * @tc.type:FUNC
+ * @tc.require: issue#IB2B3G
+ */
+HWTEST_F(RSImageCacheTest, CachePixelMapTest005, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    uint64_t uniqueId = 0;
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    pixelMap->allocatorType_ = Media::AllocatorType::HEAP_ALLOC;
+    imageCache.CachePixelMap(uniqueId, pixelMap);
+    EXPECT_TRUE(true);
+    imageCache.pixelMapCache_.clear();
+}
 } // namespace OHOS::Rosen
