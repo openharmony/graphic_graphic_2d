6e007e71b8ceb379bc06bb6c63ca81185f6ace49
g30058336 <guqiuhan@huawei.com>
2024-11-05 19:35:09 +0800

    addPostTaskName
    Signed-off-by: g30058336 <guqiuhan@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_util.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_util.cpp
@@ -1165,12 +1165,12 @@ void RSUniRenderUtil::PostReleaseSurfaceTask(std::shared_ptr<Drawing::Surface>&&
             auto instance = &(RSUniRenderThread::Instance());
             instance->AddToReleaseQueue(std::move(surface));
             instance->PostTask(([instance] () { instance->ReleaseSurface(); }),
-                0, AppExecFwk::EventQueue::Priority::IMMEDIATE);
+                RELEASE_SURFACE_TASK, 0, AppExecFwk::EventQueue::Priority::IMMEDIATE);
         } else {
             auto instance = RSMainThread::Instance();
             instance->AddToReleaseQueue(std::move(surface));
             instance->PostTask(([instance] () { instance->ReleaseSurface(); }),
-                0, AppExecFwk::EventQueue::Priority::IMMEDIATE);
+                RELEASE_SURFACE_TASK, 0, AppExecFwk::EventQueue::Priority::IMMEDIATE);
         }
     } else {
 #if defined(RS_ENABLE_GL) || defined(RS_ENABLE_VK)
