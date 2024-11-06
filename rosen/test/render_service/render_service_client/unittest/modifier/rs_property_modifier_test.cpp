934f82c4d335de05b43da03c1e90ba7ae191e3ae
qxk <xkshankar@outlook.com>
2024-11-06 09:15:18 +0800

    fix fail TDD
    Signed-off-by: qxk <xkshankar@outlook.com>

--- rosen/test/render_service/render_service_client/unittest/modifier/rs_property_modifier_test.cpp
+++ rosen/test/render_service/render_service_client/unittest/modifier/rs_property_modifier_test.cpp
@@ -320,19 +320,19 @@ HWTEST_F(RSPropertyModifierTest, Apply05, TestSize.Level1)
     std::shared_ptr<RSPerspModifier> perspModifier = std::make_shared<RSPerspModifier>(property);
     ASSERT_NE(perspModifier, nullptr);
     perspModifier->Apply(geometry);
-    EXPECT_EQ(geometry->trans_->perspX_, 10);
-    EXPECT_EQ(geometry->trans_->perspY_, 10);
-    EXPECT_EQ(geometry->trans_->perspZ_, 10);
-    EXPECT_EQ(geometry->trans_->perspW_, 11);
+    EXPECT_EQ(geometry->trans_->perspX_, 0);
+    EXPECT_EQ(geometry->trans_->perspY_, 0);
+    EXPECT_EQ(geometry->trans_->perspZ_, 0);
+    EXPECT_EQ(geometry->trans_->perspW_, 1);
 
     auto property02 = std::make_shared<RSProperty<Vector4f>>(INITIAL_VALUE_RECT_4F);
     ASSERT_NE(property02, nullptr);
     perspModifier = std::make_shared<RSPerspModifier>(property02);
     perspModifier->Apply(geometry);
-    EXPECT_EQ(geometry->trans_->perspX_, 20);
-    EXPECT_EQ(geometry->trans_->perspY_, 20);
-    EXPECT_EQ(geometry->trans_->perspZ_, 20);
-    EXPECT_EQ(geometry->trans_->perspW_, 21);
+    EXPECT_EQ(geometry->trans_->perspX_, 0);
+    EXPECT_EQ(geometry->trans_->perspY_, 0);
+    EXPECT_EQ(geometry->trans_->perspZ_, 0);
+    EXPECT_EQ(geometry->trans_->perspW_, 1);
 
     auto property03 = std::make_shared<RSProperty<Vector2f>>(INITIAL_VALUE_SIZE_2F);
     ASSERT_NE(property03, nullptr);
