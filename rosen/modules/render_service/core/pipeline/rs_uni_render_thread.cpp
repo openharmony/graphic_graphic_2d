d32aaf78c4ae3e48413e603ff9404ade5f8a2009
zed <maozijie@huawei.com>
2024-11-07 09:44:41 +0800

    Purge unlocked resources when clear memory
    Signed-off-by: zed <maozijie@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_thread.cpp
@@ -805,6 +805,10 @@ void RSUniRenderThread::PostClearMemoryTask(ClearMemoryMoment moment, bool deepl
             this->clearMemoryFinished_ = true;
         } else {
             this->isDefaultCleanTaskFinished_ = true;
+            if (RSSystemProperties::GetRenderNodePurgeEnabled()) {
+                RS_TRACE_NAME_FMT("Purge unlocked resources when clear memory");
+                grContext->PurgeUnlockedResources(false);
+            }
         }
         RSUifirstManager::Instance().TryReleaseTextureForIdleThread();
         this->clearMemDeeply_ = false;
