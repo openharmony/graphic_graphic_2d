b3b3a8237d37b9b09ecf29c48ab0c92b58daefa8
chuchengcheng <chuchengcheng4@huawei.com>
2024-11-09 15:03:01 +0800

    修复同层渲染canvas getPixelmap失效
    Signed-off-by: chuchengcheng <chuchengcheng4@huawei.com>
    Change-Id: Ia873c1631622c2c38eeff37984c78c3d186eeef6

--- rosen/modules/render_service_client/core/ui/rs_canvas_drawing_node.cpp
+++ rosen/modules/render_service_client/core/ui/rs_canvas_drawing_node.cpp
@@ -83,7 +83,7 @@ void RSCanvasDrawingNode::CreateRenderNodeForTextureExportSwitch()
 bool RSCanvasDrawingNode::GetBitmap(Drawing::Bitmap& bitmap,
     std::shared_ptr<Drawing::DrawCmdList> drawCmdList, const Drawing::Rect* rect)
 {
-    if (IsUniRenderEnabled()) {
+    if (IsRenderServiceNode()) {
         auto renderServiceClient =
             std::static_pointer_cast<RSRenderServiceClient>(RSIRenderClient::CreateRenderServiceClient());
         if (renderServiceClient == nullptr) {
@@ -129,7 +129,7 @@ bool RSCanvasDrawingNode::GetPixelmap(std::shared_ptr<Media::PixelMap> pixelmap,
         RS_LOGE("RSCanvasDrawingNode::GetPixelmap: pixelmap is nullptr");
         return false;
     }
-    if (IsUniRenderEnabled()) {
+    if (IsRenderServiceNode()) {
         auto renderServiceClient =
             std::static_pointer_cast<RSRenderServiceClient>(RSIRenderClient::CreateRenderServiceClient());
         if (renderServiceClient == nullptr) {
