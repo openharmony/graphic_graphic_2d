5693a1880b8458b599d38ba94a8e7d38c11d45ef
yxu63 <xuyue63@huawei.com>
2024-11-15 01:41:15 +0000

    update rosen/modules/render_service_client/core/ui/rs_surface_node.cpp.
    Signed-off-by: yxu63 <xuyue63@huawei.com>

--- rosen/modules/render_service_client/core/ui/rs_surface_node.cpp
+++ rosen/modules/render_service_client/core/ui/rs_surface_node.cpp
@@ -77,7 +77,7 @@ RSSurfaceNode::SharedPtr RSSurfaceNode::Create(const RSSurfaceNodeConfig& surfac
         .name = node->name_,
         .additionalData = surfaceNodeConfig.additionalData,
         .isTextureExportNode = surfaceNodeConfig.isTextureExportNode,
-        .isSync = isWindow || surfaceNodeConfig.isSync,
+        .isSync = surfaceNodeConfig.isSync,
         .surfaceWindowType = surfaceNodeConfig.surfaceWindowType,
     };
     config.nodeType = type;
