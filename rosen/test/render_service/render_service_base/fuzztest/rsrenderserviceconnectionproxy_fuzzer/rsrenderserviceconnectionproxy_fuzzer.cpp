98db5d26b7ebdd843afca91ec3e48f5a9295374f
zhongpengzhi <zhongpengzhi@huawei.com>
2024-11-18 08:28:17 +0000

    update fuzztest/rsrenderserviceconnectionproxy_fuzzer/rsrenderserviceconnectionproxy_fuzzer.cpp.
    Signed-off-by: zhongpengzhi <zhongpengzhi@huawei.com>

--- rosen/test/render_service/render_service_base/fuzztest/rsrenderserviceconnectionproxy_fuzzer/rsrenderserviceconnectionproxy_fuzzer.cpp
+++ rosen/test/render_service/render_service_base/fuzztest/rsrenderserviceconnectionproxy_fuzzer/rsrenderserviceconnectionproxy_fuzzer.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+zpz
 #include "rsrenderserviceconnectionproxy_fuzzer.h"
 
 #include <climits>
