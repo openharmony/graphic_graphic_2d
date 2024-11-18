a46852d80e4c956e9f58f1c4b9fa5cf9f1393dca
zhongpengzhi <zhongpengzhi@huawei.com>
2024-11-18 08:16:07 +0000

    update rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp.
    Signed-off-by: zhongpengzhi <zhongpengzhi@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp
+++ rosen/modules/render_service/core/pipeline/rs_render_service_connection.cpp
@@ -6,7 +6,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing4, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -15,7 +15,7 @@
 
 #include "rs_render_service_connection.h"
 #include <string>
-
+zpz
 #include "frame_report.h"
 #include "hgm_command.h"
 #include "hgm_core.h"
