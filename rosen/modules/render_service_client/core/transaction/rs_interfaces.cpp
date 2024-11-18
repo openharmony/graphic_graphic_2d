4ac896a472e8fdeecbb591932a9da169d192fa0a
zhongpengzhi <zhongpengzhi@huawei.com>
2024-11-18 08:26:32 +0000

    update rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp.
    Signed-off-by: zhongpengzhi <zhongpengzhi@huawei.com>

--- rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp
+++ rosen/modules/render_service_client/core/transaction/rs_interfaces.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+zpz
 #include <cstdint>
 #include <functional>
 #include "rs_interfaces.h"
