2cfaa1dfd51e89f671cf0cbbb5f846356706c844
zhongpengzhi <zhongpengzhi@huawei.com>
2024-11-18 08:29:57 +0000

    update unittest/platform/ohos/rs_render_service_connection_proxy_test.cpp.
    Signed-off-by: zhongpengzhi <zhongpengzhi@huawei.com>

--- rosen/test/render_service/render_service_base/unittest/platform/ohos/rs_render_service_connection_proxy_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/platform/ohos/rs_render_service_connection_proxy_test.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+zpz
 #include <gtest/gtest.h>
 
 #include <if_system_ability_manager.h>
