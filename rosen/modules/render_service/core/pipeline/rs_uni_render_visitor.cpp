6584e1549e97f81e68b1ad915a58237448294aa6
zhongpengzhi <zhongpengzhi@huawei.com>
2024-11-19 03:29:44 +0000

    update rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp.
    Signed-off-by: zhongpengzhi <zhongpengzhi@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -1883,7 +1883,7 @@ void RSUniRenderVisitor::UpdateHwcNodeDirtyRegionAndCreateLayer(std::shared_ptr<
             continue;
         }
         if (hasUniRenderHdrSurface_) {
-            RS_OPTIONAL_TRACE_NAME_FMT("hwc debug: name:%s id:%" PRIu64 " disabled by render hdr surface",
+            RS_OPTIONAL_TRACE_NAME_FMT("hwc debug: name:%s id:%" PRIu64 " disabled by hdr surface",
                 node->GetName().c_str(), node->GetId());
             hwcNodePtr->SetHardwareForcedDisabledState(true);
             hwcDisabledReasonCollection_.UpdateHwcDisabledReasonForDFX(node->GetId(),
