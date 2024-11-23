daf32bb5721542a6bdc227e2f4a27833e9848fd3
diaochenggong <diaochenggong2@huawei.com>
2024-11-23 15:32:32 +0800

    修改上限值
    Signed-off-by: diaochenggong <diaochenggong2@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/rs_marshalling_helper.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/rs_marshalling_helper.cpp
@@ -844,7 +844,7 @@ bool RSMarshallingHelper::Unmarshalling(Parcel& parcel, std::vector<std::shared_
     uint32_t size = parcel.ReadUint32();
     bool success = true;
     std::vector<std::shared_ptr<EmitterUpdater>> emitterUpdaters;
-    if (size > PARTICLE_UPPER_LIMIT) {
+    if (size > PARTICLE_EMMITER_UPPER_LIMIT) {
         return false;
     }
     for (size_t i = 0; i < size; i++) {
@@ -928,7 +928,7 @@ bool RSMarshallingHelper::Unmarshalling(Parcel& parcel, std::shared_ptr<Particle
     }
     uint32_t size = parcel.ReadUint32();
     bool success = true;
-    if (size > PARTICLE_UPPER_LIMIT) {
+    if (size > PARTICLE_EMMITER_UPPER_LIMIT) {
         return false;
     }
     std::shared_ptr<ParticleNoiseFields> noiseFields = std::make_shared<ParticleNoiseFields>();
@@ -1232,7 +1232,7 @@ bool RSMarshallingHelper::Unmarshalling(Parcel& parcel, std::vector<std::shared_
     uint32_t size = parcel.ReadUint32();
     bool success = true;
     std::vector<std::shared_ptr<ParticleRenderParams>> particlesRenderParams;
-    if (size > PARTICLE_UPPER_LIMIT) {
+    if (size > PARTICLE_EMMITER_UPPER_LIMIT) {
         return false;
     }
     for (size_t i = 0; i < size; i++) {
