6d88fc56f083fface2750431f2a5fe14245ea970
hsc <husichao1@huawei.com>
2024-11-29 15:09:15 +0800

    fix tdd
    Signed-off-by: hsc <husichao1@huawei.com>

--- rosen/modules/render_service_client/test/unittest/rs_interfaces_test.cpp
+++ rosen/modules/render_service_client/test/unittest/rs_interfaces_test.cpp
@@ -1403,7 +1403,7 @@ HWTEST_F(RSInterfacesTest, RegisterFrameRateLinkerExpectedFpsUpdateCallbackTest,
     FrameRateLinkerExpectedFpsUpdateCallback cb = [](uint32_t pid, int32_t fps){};
     int32_t ret = rsInterfaces->RegisterFrameRateLinkerExpectedFpsUpdateCallback(1, cb);
     ASSERT_EQ(ret, 0);
-    ret = rsInterfaces->RegisterFrameRateLinkerExpectedFpsUpdateCallback(1, nullptr);
+    ret = rsInterfaces->UnRegisterFrameRateLinkerExpectedFpsUpdateCallback(1);
     ASSERT_EQ(ret, 0);
 }
 
