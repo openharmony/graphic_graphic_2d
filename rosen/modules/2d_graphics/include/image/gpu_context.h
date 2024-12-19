0f0983e6a15c95c3e065eccb1e2aa73d82a55831
xiaoguandao <guanzhao1@huawei.com>
2024-12-19 10:50:59 +0800

    调整纹理释放线程优先级同步到5.0.2
    Signed-off-by: xiaoguandao <guanzhao1@huawei.com>

--- rosen/modules/2d_graphics/include/image/gpu_context.h
+++ rosen/modules/2d_graphics/include/image/gpu_context.h
@@ -274,7 +274,7 @@ public:
 
     void SetGpuCacheSuppressWindowSwitch(bool enabled);
 
-    void SetGpuMemoryAsyncReclaimerSwitch(bool enabled);
+    void SetGpuMemoryAsyncReclaimerSwitch(bool enabled, const std::function<void()>& setThreadPriority);
 
     void FlushGpuMemoryInWaitQueue();
     
