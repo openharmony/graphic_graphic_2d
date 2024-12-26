628421baff3ba5e4e68a31fa5a5c83738301c7d4
Felix <fangxiang19@huawei.com>
2024-12-26 17:19:55 +0800

    repair diff Signed-off-by: Felix <fangxiang19@huawei.com> Change-Id: I9597072dda16e5c7b261ad555204c25676bb1ed0
    Change-Id: I78ef6b965630049dc4e97da3bb33bf88b727fda8

--- rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
@@ -1755,7 +1755,7 @@ void RSRenderNode::UpdateRenderStatus(RectI& dirtyRegion, bool isPartialRenderEn
 
 void RSRenderNode::MapAndUpdateChildrenRect()
 {
-    auto& geoPtr = GetRenderProperties().GetBoundsGeometry();
+    auto geoPtr = GetRenderProperties().GetBoundsGeometry();
     if (!shouldPaint_ || geoPtr == nullptr) {
         return;
     }
