b1704f998236886551e2be761efe606148f0f7f8
杜廷佳java <dutingjia@huawei.com>
2024-12-27 11:42:11 +0800

    黄蓝同步
    Signed-off-by: 杜廷佳java <dutingjia@huawei.com>

--- frameworks/opengl_wrapper/src/EGL/egl_wrapper_layer.h
+++ frameworks/opengl_wrapper/src/EGL/egl_wrapper_layer.h
@@ -31,10 +31,10 @@ public:
     static EglWrapperLayer& GetInstance();
     ~EglWrapperLayer();
     bool Init(EglWrapperDispatchTable *table);
+    bool InitBundleInfo();
 #if USE_IGRAPHICS_EXTENDS_HOOKS
     bool GetIGraphicsLogicStatus() { return iGraphicsLogic; }
 #endif
-    bool InitBundleInfo();
 
 private:
     EglWrapperLayer() : initialized_(false), dlhandle_(nullptr) {};
@@ -48,6 +48,7 @@ private:
     std::vector<FunctionTable> layerFuncTbl_;
     bool initialized_;
     void *dlhandle_;
+    
     EglSystemLayersManager eglSystemLayersManager_;
 #if USE_IGRAPHICS_EXTENDS_HOOKS
     bool iGraphicsLogic = false;
