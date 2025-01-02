0d29603418178ba475ff663c00727bfab85ea132
zzhang <zhangze47@huawei.com>
2025-01-02 21:49:06 +0800

    bugfix
    Signed-off-by: zzhang <zhangze47@huawei.com>

--- rosen/modules/render_service_base/include/pipeline/rs_surface_render_node.h
+++ rosen/modules/render_service_base/include/pipeline/rs_surface_render_node.h
@@ -1241,6 +1241,15 @@ public:
     {
         return intersectedRoundCornerAABBs_.size();
     }
+    bool GetIsHwcPendingDisabled() const
+    {
+        return isHwcPendingDisabled_;
+    }
+
+    void SetIsHwcPendingDisabled(bool isHwcPendingDisabled)
+    {
+        isHwcPendingDisabled_ = isHwcPendingDisabled;
+    }
 
 protected:
     void OnSync() override;
@@ -1351,6 +1360,9 @@ private:
     uint8_t abilityBgAlpha_ = 0;
     bool alphaChanged_ = false;
     bool isUIHidden_ = false;
+
+    // is hwc node disabled by filter rect
+    bool isHwcPendingDisabled_ = false;
 
     // dirtyRegion caused by surfaceNode visible region after alignment
     Occlusion::Region extraDirtyRegionAfterAlignment_;
