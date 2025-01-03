3b5aa8237864b0c245d6ce902533fc9050385a4b
wanggang <wanggang292@huawei.com>
2025-01-03 15:44:48 +0800

    根据分辨率设置gpu cache limit
    Signed-off-by: wanggang<wanggang292@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -711,7 +711,8 @@ void RSMainThread::UpdateGpuContextCacheSize()
         * MEMUNIT_RATE * MEMUNIT_RATE; // adjust by actual Resolution
     cacheLimitsResourceSize = cacheLimitsResourceSize > MAX_GPU_CONTEXT_CACHE_SIZE ?
         MAX_GPU_CONTEXT_CACHE_SIZE : cacheLimitsResourceSize;
-    if (cacheLimitsResourceSize > maxResourcesSize) {
+    if (cacheLimitsResourceSize > maxResourcesSize ||
+        system::GetParameter("const.product.devicetype", "pc") == "wearable") {
         gpuContext->SetResourceCacheLimits(maxResources, cacheLimitsResourceSize);
     }
 #endif
