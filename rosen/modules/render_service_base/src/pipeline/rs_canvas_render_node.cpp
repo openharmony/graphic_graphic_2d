c51d1fdd71b00f09b82190f186eb45144bda1b2d
sunzy <sunzhenyang1@huawei.com>
2025-01-05 15:25:23 +0800

    custom clip support rk
    Signed-off-by: sunzy <sunzhenyang1@huawei.com>

--- rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_canvas_render_node.cpp
@@ -196,9 +196,10 @@ void RSCanvasRenderNode::ProcessRenderBeforeChildren(RSPaintFilterCanvas& canvas
             RSPropertyDrawableSlot::SAVE_ALL, RSPropertyDrawableSlot::ENV_FOREGROUND_COLOR, canvas);
         // Just need to skip RSPropertyDrawableSlot::SHADOW
         DrawPropertyDrawableRange(
-            RSPropertyDrawableSlot::FOREGROUND_FILTER, RSPropertyDrawableSlot::CLIP_TO_FRAME, canvas);
+            RSPropertyDrawableSlot::FOREGROUND_FILTER, RSPropertyDrawableSlot::CUSTOM_CLIP_TO_FRAME, canvas);
     } else {
-        DrawPropertyDrawableRange(RSPropertyDrawableSlot::SAVE_ALL, RSPropertyDrawableSlot::CLIP_TO_FRAME, canvas);
+        DrawPropertyDrawableRange(
+            RSPropertyDrawableSlot::SAVE_ALL, RSPropertyDrawableSlot::CUSTOM_CLIP_TO_FRAME, canvas);
     }
 }
 
