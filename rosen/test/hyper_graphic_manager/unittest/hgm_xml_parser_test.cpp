e963de13705ef43645036a27e26ed611b7622b88
鲁保良 <lubaoliang1@h-partners.com>
2025-01-11 06:10:57 +0000

    1
    Signed-off-by: 鲁保良 <lubaoliang1@h-partners.com>

--- rosen/test/hyper_graphic_manager/unittest/hgm_xml_parser_test.cpp
+++ rosen/test/hyper_graphic_manager/unittest/hgm_xml_parser_test.cpp
@@ -69,7 +69,7 @@ HWTEST_F(HgmXmlParserTest, Parse, Function | SmallTest | Level1)
 {
     std::unique_ptr<XMLParser> parser = std::make_unique<XMLParser>();
     int32_t load = parser->LoadConfiguration(CONFIG);
-    EXEPECT_GE(load, EXEC_SUCCESS);
+    EXPECT_GE(load, EXEC_SUCCESS);
     parser->Parse();
     parser->GetParsedData();
 }
