333d85d8713c62b9eb9b290ad4fd259faee688c8
francis <liujianxin20@huawei.com>
2025-01-12 11:08:12 +0800

    add assert
    Signed-off-by: francis <liujianxin20@huawei.com>

--- rosen/test/render_service/render_service/unittest/screen_manager/rs_screen_test.cpp
+++ rosen/test/render_service/render_service/unittest/screen_manager/rs_screen_test.cpp
@@ -95,6 +95,26 @@ HWTEST_F(RSScreenTest, SetResolution_001, testing::ext::TestSize.Level1)
 }
 
 /*
+ * @tc.name: SetRogResolution_001
+ * @tc.desc: SetRogResolution Test
+ * @tc.type: FUNC
+ * @tc.require: issueI8JJXW
+ */
+HWTEST_F(RSScreenTest, SetRogResolution_001, testing::ext::TestSize.Level1)
+{
+    ScreenId id = INVALID_SCREEN_ID;
+    auto rsScreen = std::make_unique<impl::RSScreen>(id, false, HdiOutput::CreateHdiOutput(id), nullptr);
+    ASSERT_NE(rsScreen, nullptr);
+    auto width = rsScreen->Width();
+    auto height = rsScreen->Height();
+    uint32_t newWidth = 100;
+    uint32_t newHeight = 100;
+    rsScreen->SetRogResolution(newWidth, newHeight);
+    ASSERT_EQ(rsScreen->Width(), width);
+    ASSERT_EQ(rsScreen->Height(), height);
+}
+
+/*
  * @tc.name: SetPowerStatus_001
  * @tc.desc: SetPowerStatus Test
  * @tc.type: FUNC
