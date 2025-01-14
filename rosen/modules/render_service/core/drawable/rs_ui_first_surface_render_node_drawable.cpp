87c22baf6cd1c40f695477dac7aa43f48b4562f1
tttaaaooo <yangtao141@huawei.com>
2025-01-14 17:26:46 +0800

    Fixed starting transparent.
    Signed-off-by: tttaaaooo <yangtao141@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_ui_first_surface_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_ui_first_surface_render_node_drawable.cpp
@@ -476,7 +476,9 @@ void RSSurfaceRenderNodeDrawable::SubDraw(Drawing::Canvas& canvas)
     auto parentSurfaceMatrix = RSRenderParams::GetParentSurfaceMatrix();
     RSRenderParams::SetParentSurfaceMatrix(IDENTITY_MATRIX);
 
+    ClearTotalProcessedSurfaceCount();
     RSRenderNodeDrawable::DrawUifirstContentChildren(*rscanvas, bounds);
+    RS_TRACE_NAME_FMT("SubDraw the number of total ProcessedSurface: %d", GetTotalProcessedSurfaceCount());
     RSRenderParams::SetParentSurfaceMatrix(parentSurfaceMatrix);
 }
 
@@ -517,22 +519,29 @@ bool RSSurfaceRenderNodeDrawable::DrawUIFirstCache(RSPaintFilterCanvas& rscanvas
 bool RSSurfaceRenderNodeDrawable::DrawUIFirstCacheWithStarting(RSPaintFilterCanvas& rscanvas, NodeId id)
 {
     RS_TRACE_NAME_FMT("DrawUIFirstCacheWithStarting %d, nodeID:%" PRIu64 "", HasCachedTexture(), id);
+    bool ret = true;
+    auto drawable = RSRenderNodeDrawableAdapter::GetDrawableById(id);
+    if (drawable) {
+        const auto& startingParams = drawable->GetRenderParams();
+        if (!HasCachedTexture() && startingParams && !ROSEN_EQ(startingParams->GetAlpha(), 1.0f)) {
+            ret = DrawUIFirstCache(rscanvas, false);
+            RS_TRACE_NAME_FMT("wait and drawStarting, GetAlpha:%f, GetGlobalAlpha:%f",
+                startingParams->GetAlpha(), startingParams->GetGlobalAlpha());
+            drawable->Draw(rscanvas);
+            return ret;
+        }
+    }
     const auto& params = GetRenderParams();
     if (!params) {
         RS_LOGE("RSUniRenderUtil::HandleSubThreadNodeDrawable params is nullptr");
         return false;
     }
-    bool ret = true;
     // draw surface content&&childrensss
     if (HasCachedTexture()) {
         ret = DrawCacheSurface(rscanvas, params->GetCacheSize(), UNI_MAIN_THREAD_INDEX, true);
     }
     // draw starting window
-    {
-        auto drawable = RSRenderNodeDrawableAdapter::GetDrawableById(id);
-        if (!drawable) {
-            return false;
-        }
+    if (drawable) {
         RS_TRACE_NAME_FMT("drawStarting");
         drawable->Draw(rscanvas);
     }
@@ -542,5 +551,22 @@ bool RSSurfaceRenderNodeDrawable::DrawUIFirstCacheWithStarting(RSPaintFilterCanv
 void RSSurfaceRenderNodeDrawable::SetSubThreadSkip(bool isSubThreadSkip)
 {
     isSubThreadSkip_ = isSubThreadSkip;
+}
+
+int RSSurfaceRenderNodeDrawable::GetTotalProcessedSurfaceCount() const
+{
+    return totalProcessedSurfaceCount_;
+}
+
+void RSSurfaceRenderNodeDrawable::TotalProcessedSurfaceCountInc(RSPaintFilterCanvas& canvas)
+{
+    if (canvas.GetIsParallelCanvas()) {
+        ++totalProcessedSurfaceCount_;
+    }
+}
+
+void RSSurfaceRenderNodeDrawable::ClearTotalProcessedSurfaceCount()
+{
+    totalProcessedSurfaceCount_ = 0;
 }
 } // namespace OHOS::Rosen
