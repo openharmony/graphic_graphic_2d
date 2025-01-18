bac7323dc00ea94abc7835a233512609110f39d4
zhanglitao <zhanglitao11@huawei.com>
2025-01-18 22:18:28 +0800

    区分GL与VK的坐标原点位置
    Signed-off-by: zhanglitao <zhanglitao11@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_canvas_drawing_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_canvas_drawing_render_node_drawable.cpp
@@ -1083,10 +1083,15 @@ bool RSCanvasDrawingRenderNodeDrawable::ResetSurfaceWithTexture(int width, int h
 
 Drawing::TextureOrigin RSCanvasDrawingRenderNodeDrawable::GetTextureOrigin()
 {
-#if defined(RS_ENABLE_GL)
-    return Drawing::TextureOrigin::BOTTOM_LEFT;
+#if (defined(RS_ENABLE_GL) || defined(RS_ENABLE_VK))
+    if (RSSystemProperties::GetGpuApiType() == GpuApiType::OPENGL) {
+        return Drawing::TextureOrigin::BOTTOM_LEFT;
+    } else {
 #endif
-    return Drawing::TextureOrigin::TOP_LEFT;
+        return Drawing::TextureOrigin::TOP_LEFT;
+#if (defined(RS_ENABLE_GL) || defined(RS_ENABLE_VK))
+    }
+#endif
 }
 
 void RSCanvasDrawingRenderNodeDrawable::DrawRegionForDfx(Drawing::Canvas& canvas, const Drawing::Rect& bounds)
