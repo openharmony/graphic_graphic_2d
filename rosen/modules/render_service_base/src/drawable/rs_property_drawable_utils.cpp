cb13de405532cc274568dd1e84ae0b46cac2ea49
wanshuo4@h-partners.com <wanshuo4@h-partners.com>
2025-01-23 17:36:18 +0800

    fix DrawPixelStretch
    Signed-off-by: wanshuo4@h-partners.com <wanshuo4@h-partners.com>

--- rosen/modules/render_service_base/src/drawable/rs_property_drawable_utils.cpp
+++ rosen/modules/render_service_base/src/drawable/rs_property_drawable_utils.cpp
@@ -849,27 +849,22 @@ void RSPropertyDrawableUtils::DrawPixelStretch(Drawing::Canvas* canvas, const st
         ROSEN_LOGE("RSPropertyDrawableUtils::DrawPixelStretch get invert matrix failed.");
         return;
     }
-
-    // convert local coord to world coord
-    Drawing::Rect tmpBounds;
-    auto bounds = Rect2DrawingRect(boundsRect);
-    if (!canvas->GetTotalMatrix().MapRect(tmpBounds, bounds)) {
-        ROSEN_LOGE("RSPropertyDrawableUtils::DrawPixelStretch map rect failed.");
-        return;
-    }
+    Drawing::Rect localClipBounds;
+    canvas->Save();
+    canvas->ClipRect(Rect2DrawingRect(boundsRect),Drawing::ClipOp::INTERSECT,false);
+    auto tmpBounds = canvas->GetDeviceClipBounds();
     RS_OPTIONAL_TRACE_NAME_FMT_LEVEL(TRACE_LEVEL_TWO,
         "RSPropertyDrawableUtils::DrawPixelStretch, tmpBounds: %s", tmpBounds.ToString().c_str());
-
+    canvas->Restore();
     Drawing::Rect clipBounds(
         tmpBounds.GetLeft(), tmpBounds.GetTop(), tmpBounds.GetRight() - 1, tmpBounds.GetBottom() - 1);
     Drawing::Rect fClipBounds(clipBounds.GetLeft(), clipBounds.GetTop(), clipBounds.GetRight(), clipBounds.GetBottom());
 
-    Drawing::Rect localClipBounds;
     if (!worldToLocalMat.MapRect(localClipBounds, fClipBounds)) {
         ROSEN_LOGE("RSPropertyDrawableUtils::DrawPixelStretch map rect failed.");
         return;
     }
-
+    auto bounds = Rect2DrawingRect(boundsRect);
     if (!bounds.Intersect(localClipBounds)) {
         ROSEN_LOGE("RSPropertyDrawableUtils::DrawPixelStretch intersect clipbounds failed");
         return;
