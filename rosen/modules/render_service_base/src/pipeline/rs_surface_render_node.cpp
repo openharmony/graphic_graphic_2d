5c8ea6e7957ae23618ad6916000fb9c0d081d277
yls <yanglusheng@h-partners.com>
2025-01-26 11:57:21 +0800

    修复交互优先下发
    Signed-off-by: yls <yanglusheng@h-partners.com>
    Change-Id: Icb277fc6ea6d476e93c3ba15544ca4e58e620fb5

--- rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_surface_render_node.cpp
@@ -1702,13 +1702,27 @@ void RSSurfaceRenderNode::UpdateOccludedByFilterCache(bool val)
 void RSSurfaceRenderNode::UpdateSurfaceCacheContentStaticFlag()
 {
     auto contentStatic = false;
+    uifirstContentDirty_ = false;
     if (IsLeashWindow()) {
+        for (auto& child : *GetSortedChildren()) {
+            if (!child) {
+                continue;
+            }
+            auto childSurface = RSBaseRenderNode::ReinterpretCast<RSSurfaceRenderNode>(child);
+            if (childSurface) {
+                continue;
+            }
+            if (child->IsDirty() || child->IsSubTreeDirty()) {
+                uifirstContentDirty_ = true;
+            }
+        }
         contentStatic = (!IsSubTreeDirty() || GetForceUpdateByUifirst()) && !HasRemovedChild();
     } else if (IsAbilityComponent()) {
         contentStatic = (!IsSubTreeDirty() || GetForceUpdateByUifirst()) && !IsContentDirty();
     } else {
         contentStatic = surfaceCacheContentStatic_;
     }
+    uifirstContentDirty_ = uifirstContentDirty_ || HasRemovedChild();
     auto stagingSurfaceParams = static_cast<RSSurfaceRenderParams*>(stagingRenderParams_.get());
     if (stagingSurfaceParams) {
         stagingSurfaceParams->SetSurfaceCacheContentStatic(contentStatic, lastFrameSynced_);
@@ -1717,8 +1731,10 @@ void RSSurfaceRenderNode::UpdateSurfaceCacheContentStaticFlag()
         AddToPendingSyncList();
     }
     RS_OPTIONAL_TRACE_NAME_FMT("RSSurfaceRenderNode::UpdateSurfaceCacheContentStaticFlag: "
-        "[%d] name:[%s] Id:[%" PRIu64 "] subDirty:[%d] forceUpdate:[%d]",
-        contentStatic, GetName().c_str(), GetId(), IsSubTreeDirty(), GetForceUpdateByUifirst());
+        "name[%s] Id[%" PRIu64 "], contentStatic[%d] subTreeDirty[%d] contentDirty[%d] forceUpdate[%d] "
+        "hasRemovedChild[%d], GetChildrenCount[%d], uifirstContentDirty:%d",
+        GetName().c_str(), GetId(), contentStatic, IsSubTreeDirty(), IsContentDirty(), GetForceUpdateByUifirst(),
+        HasRemovedChild(), GetChildrenCount(), uifirstContentDirty_);
 }
 
 bool RSSurfaceRenderNode::IsOccludedByFilterCache() const
