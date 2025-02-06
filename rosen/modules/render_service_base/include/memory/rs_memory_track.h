ae2e7e0904fd283a8240c7a9f18b7538d94d70c3
wangzhebufangqi <wangzhe231@huawei.com>
2025-02-06 15:31:57 +0800

    pixelmap dfx add pixelmapformat
    Signed-off-by: wangzhebufangqi <wangzhe231@huawei.com>

--- rosen/modules/render_service_base/include/memory/rs_memory_track.h
+++ rosen/modules/render_service_base/include/memory/rs_memory_track.h
@@ -81,6 +81,7 @@ private:
     const char* MemoryType2String(MEMORY_TYPE type);
     const std::string PixelMapInfo2String(MemoryInfo info);
     const std::string AllocatorType2String(OHOS::Media::AllocatorType);
+    const std::string PixelFormat2String(OHOS::Media::PixelFormat);
     std::string GenerateDumpTitle();
     std::string GenerateDetail(MemoryInfo info, uint64_t windowId, std::string& windowName, RectI& nodeFrameRect);
     void DumpMemoryNodeStatistics(DfxString& log);
