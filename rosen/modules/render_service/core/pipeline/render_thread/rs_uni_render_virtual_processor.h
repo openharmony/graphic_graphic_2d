128c09a38e311080b0d24aef11d161568b72caab
xcxcxcx2019 <liujiaming35@huawei.com>
2025-02-07 17:39:01 +0800

    fix map
    Signed-off-by: xcxcxcx2019 <liujiaming35@huawei.com>

--- rosen/modules/render_service/core/pipeline/render_thread/rs_uni_render_virtual_processor.h
+++ rosen/modules/render_service/core/pipeline/render_thread/rs_uni_render_virtual_processor.h
@@ -96,9 +96,10 @@ private:
     sptr<Surface> producerSurface_;
     std::unique_ptr<RSRenderFrame> renderFrame_;
     std::shared_ptr<RSPaintFilterCanvas> canvas_;
-    static const std::map<GraphicColorGamut, HDI::Display::Graphic::Common::V1_0::CM_ColorSpaceType> COLORSPACETYPE {
-        { GRAPHIC_COLOR_GAMUT_SRGB, Media::VideoProcessingEngine::CM_SRGB_LIMIT },
-        { GRAPHIC_COLOR_GAMUT_DISPLAY_P3, Media::VideoProcessingEngine::CM_P3_LIMIT }
+    inline static const std::map<GraphicColorGamut, HDI::Display::Graphic::Common::V1_0::CM_ColorSpaceType>
+        COLORSPACETYPE {
+            { GRAPHIC_COLOR_GAMUT_SRGB, Media::VideoProcessingEngine::CM_SRGB_LIMIT },
+            { GRAPHIC_COLOR_GAMUT_DISPLAY_P3, Media::VideoProcessingEngine::CM_P3_LIMIT }
     };
     bool forceCPU_ = false;
     bool isExpand_ = false;
