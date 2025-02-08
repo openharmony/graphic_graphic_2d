1f8a74e7f4c570bcf2a5f5a8a7910985000a0e48
guanweixiang <guanweixiang1@huawei.com>
2025-02-08 01:08:17 +0000

    fix mirror screen draw with method RSNodeRenderDrawable.OnCapture
    Signed-off-by: guanweixiang <guanweixiang1@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_display_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_display_render_node_drawable.cpp
@@ -1222,7 +1222,6 @@ void RSDisplayRenderNodeDrawable::DrawWiredMirrorOnDraw(
     auto screenInfo = mirroredParams->GetScreenInfo();
     uniParam->SetScreenInfo(screenInfo);
     Drawing::Rect rect(0, 0, screenInfo.width, screenInfo.height);
-    RSUniRenderThread::SetCaptureParam(CaptureParam(false, false, true, 1.0f, 1.0f));
     curCanvas_->ClipRect(rect, Drawing::ClipOp::INTERSECT, false);
     curCanvas_->ConcatMatrix(mirroredParams->GetMatrix());
     RSRenderParams::SetParentSurfaceMatrix(curCanvas_->GetTotalMatrix());
@@ -1230,7 +1229,6 @@ void RSDisplayRenderNodeDrawable::DrawWiredMirrorOnDraw(
     DrawCurtainScreen();
     DrawWatermarkIfNeed(*mirroredParams, *curCanvas_);
     SwitchColorFilter(*curCanvas_, 1.f); // 1.f: wired screen not use hdr, use default value 1.f
-    RSUniRenderThread::ResetCaptureParam();
 
     uniParam->SetOpDropped(isOpDropped);
 }
