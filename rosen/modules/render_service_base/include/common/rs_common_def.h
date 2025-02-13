d835d6b6dd205a22f428acd8eaba387f0557379c
zhangshixiong7 <zhangshixiong7@huawei.com>
2025-02-13 09:51:12 +0800

    add window min scene
    Signed-off-by: zhangshixiong7 <zhangshixiong7@huawei.com>

--- rosen/modules/render_service_base/include/common/rs_common_def.h
+++ rosen/modules/render_service_base/include/common/rs_common_def.h
@@ -265,6 +265,8 @@ enum class SystemAnimatedScenes : uint32_t {
     ENTER_RECENTS, // Enter recents only for phone, end with EXIT_RECENTS instead of OTHERS
     EXIT_RECENTS, // Exit recents only for phone
     LOCKSCREEN_TO_LAUNCHER, // Enter unlock screen for pc scene
+    ENTER_MIN_WINDOW, // Enter the window minimization state
+    RECOVER_MIN_WINDOW, // Recover minimized window
     OTHERS, // 1.Default state 2.The state in which the animation ends
 };
 
