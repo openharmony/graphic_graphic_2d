e1245bcd4d112966359e5f88719ab58344798d11
hsc <husichao1@huawei.com>
2025-02-22 17:26:21 +0800

    post screenManager interface call to HardwareTh
    Signed-off-by: hsc <husichao1@huawei.com>

--- rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/rs_main_thread.cpp
@@ -3958,12 +3958,14 @@ void RSMainThread::UpdateRogSizeIfNeeded()
         if (displayNode == nullptr) {
             return;
         }
-        auto screenManager_ = CreateOrGetScreenManager();
-        if (screenManager_ == nullptr) {
-            return;
-        }
-        screenManager_->SetRogScreenResolution(
-            displayNode->GetScreenId(), displayNode->GetRogWidth(), displayNode->GetRogHeight());
+        RSHardwareThread::Instance().PostTask([displayNode]() {
+            auto screenManager = CreateOrGetScreenManager();
+            if (screenManager == nullptr) {
+                return;
+            }
+            screenManager->SetRogScreenResolution(
+                displayNode->GetScreenId(), displayNode->GetRogWidth(), displayNode->GetRogHeight());
+        });
     }
 }
 
