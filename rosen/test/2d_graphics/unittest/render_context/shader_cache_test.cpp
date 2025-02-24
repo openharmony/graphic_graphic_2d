9d2ac29762c2f2162fe0f0b7a7ebe0b4d3faec2e
chaifu11 <chaifu1@huawei.com>
2025-02-24 22:28:16 +0800

    cwx1252618
    Signed-off-by: chaifu11 <chaifu1@huawei.com>

--- rosen/test/2d_graphics/unittest/render_context/shader_cache_test.cpp
+++ rosen/test/2d_graphics/unittest/render_context/shader_cache_test.cpp
@@ -295,12 +295,15 @@ HWTEST_F(ShaderCacheTest, load_test_001, TestSize.Level1)
 HWTEST_F(ShaderCacheTest, cleanAllShaders_test_001, TestSize.Level1)
 {
     auto &cache = ShaderCache::Instance();
-    EXPECT_EQ(nullptr, cache.cacheData_);
     cache.CleanAllShaders();
     const char* identity = nullptr;
     cache.InitShaderCache(identity, 0, false);
-    EXPECT_NE(nullptr, cache.cacheData_);
+    const char* testKey1 = "testKey1";
+    const char* testValue1 = "testValue1";
+    cache.cacheData_->Rewrite(testKey1, 8, testValue1, 8);
+    EXPECT_NE(0, cache.cacheData_->GetShaderNum());
     cache.CleanAllShaders();
+    EXPECT_EQ(0, cache.cacheData_->GetShaderNum());
 }
 
 /**
