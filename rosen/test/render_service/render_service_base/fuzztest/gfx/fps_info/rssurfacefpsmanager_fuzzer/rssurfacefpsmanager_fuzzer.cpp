a43c1e90a088a6fbc2703ed4bbcb580f1000ba8d
m00899184 <maochicong@huawei.com>
2025-02-28 17:04:47 +0800

    增加fuzz测试用例
    Signed-off-by: m00899184 <maochicong@huawei.com>
    Change-Id: I04ccf70831fd74595ed43f411a52344ed4ae1f8e

---
+++ rosen/test/render_service/render_service_base/fuzztest/gfx/fps_info/rssurfacefpsmanager_fuzzer/rssurfacefpsmanager_fuzzer.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "rssurfacefpsmanager_fuzzer.h"
+#include "gfx/fps_info/rs_surface_fps_manager.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <securec.h>
+#include <memory>
+
+namespace OHOS {
+namespace Rosen {                                            
+
+namespace {
+constexpr size_t STR_LEN = 10;
+const uint8_t* g_data = nullptr;
+size_t g_size = 0;
+size_t g_pos;
+} // namespace
+
+/*
+ * describe: get data from outside untrusted data(g_data) which size is according to sizeof(T)
+ * tips: only support basic type
+ */
+template<class T>
+T GetData()
+{
+    T object {};
+    size_t objectSize = sizeof(object);
+    if (g_data == nullptr || objectSize > g_size - g_pos) {
+        return object;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, g_data + g_pos, objectSize);
+    if (ret != EOK) {
+        return {};
+    }
+    g_pos += objectSize;
+    return object;
+}
+
+/*
+ * get a string from g_data
+ */
+std::string GetStringFromData(int strlen)
+{
+    if (strlen <= 0) {
+        return "fuzz";
+    }
+    char cstr[strlen];
+    cstr[strlen - 1] = '\0';
+    for (int i = 0; i < strlen - 1; i++) {
+        char tmp = GetData<char>();
+        if (tmp == '\0') {
+            tmp = '1';
+        }
+        cstr[i] = tmp;
+    }
+    std::string str(cstr);
+    return str;
+}
+
+std::shared_ptr<RSSurfaceFps> CreateSurfaceFps(const std::string& name)
+{
+    std::shared_ptr<RSSurfaceFps> surfaceFpsPtr = std::make_shared<RSSurfaceFps>(name);
+    uint8_t num = GetData<uint8_t>();
+    for (uint8_t i = 0; i < num; i++) {
+        uint64_t presentTime = GetData<uint64_t>();
+        uint32_t seqNum = GetData<uint32_t>();
+        surfaceFpsPtr->RecordPresentTime(presentTime, seqNum);
+    }
+    return surfaceFpsPtr;
+}
+
+bool RSSurfaceFpsManagerFuzzTest(const uint8_t* data, size_t size)
+{
+    if (data == nullptr) {
+        return false;
+    }
+
+    // initialize
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
+
+    // getdata
+    std::string result = GetStringFromData(STR_LEN);
+    std::string name = GetStringFromData(STR_LEN);
+    std::string uname = GetStringFromData(STR_LEN);
+    NodeId id = GetData<NodeId>();
+    NodeId uid = GetData<NodeId>();
+    uint64_t timestamp = GetData<uint64_t>();
+    uint32_t seqNum = GetData<uint32_t>();
+
+    RSSurfaceFpsManager& surfaceFpsManager = RSSurfaceFpsManager::GetInstance();
+    surfaceFpsManager.RegisterSurfaceFps(id, name);
+    surfaceFpsManager.RegisterSurfaceFps(id, name);
+    surfaceFpsManager.RecordPresentTime(id, timestamp, seqNum);
+    surfaceFpsManager.RecordPresentTime(uid, timestamp, seqNum);
+    surfaceFpsManager.Dump(result, name);
+    surfaceFpsManager.Dump(result, uname);
+    surfaceFpsManager.ClearDump(result, name);
+    surfaceFpsManager.ClearDump(result, uname);
+    surfaceFpsManager.GetSurfaceFps(id);
+    surfaceFpsManager.GetSurfaceFps(uid);
+    surfaceFpsManager.GetSurfaceFps(name);
+    surfaceFpsManager.GetSurfaceFps(uname);
+    surfaceFpsManager.UnregisterSurfaceFps(id);
+    surfaceFpsManager.UnregisterSurfaceFps(uid);
+    
+    return true;
+}
+
+} // namespace Rosen
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::Rosen::RSSurfaceFpsManagerFuzzTest(data, size);
+    return 0;
+}
