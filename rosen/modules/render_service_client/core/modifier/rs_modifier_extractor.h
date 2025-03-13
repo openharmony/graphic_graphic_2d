06da4690f8a1a9f314766569369e40fdbe6a55ce
luhan <luhan22@huawei.com>
2025-03-13 21:38:39 +0800

    add BlendMode dfx
    Signed-off-by: luhan <luhan22@huawei.com>
    Change-Id: I17004c7f0248ce575544140258b821e35b5b40dd

--- rosen/modules/render_service_client/core/modifier/rs_modifier_extractor.h
+++ rosen/modules/render_service_client/core/modifier/rs_modifier_extractor.h
@@ -127,6 +127,8 @@ public:
     float GetIlluminatedBorderWidth() const;
     int GetIlluminatedType() const;
     float GetBloom() const;
+    int GetColorBlendMode() const;
+    int GetColorBlendApplyType() const;
 
     std::string Dump() const;
 private:
