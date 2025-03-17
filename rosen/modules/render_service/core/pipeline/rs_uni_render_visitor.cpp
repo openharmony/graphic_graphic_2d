574872cfbcc76c649b3cab0844cea45eba1d6f4b
porridge783 <chenbaoshan_mr@163.com>
2025-03-17 19:40:19 +0800

    修复useEffect不可见问题
    Signed-off-by: porridge783 <chenbaoshan_mr@163.com>

--- rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/rs_uni_render_visitor.cpp
@@ -2493,7 +2493,7 @@ void RSUniRenderVisitor::CollectEffectInfo(RSRenderNode& node)
         nodeParent->SetChildHasVisibleFilter(true);
         nodeParent->UpdateVisibleFilterChild(node);
     }
-    if (node.GetRenderProperties().GetUseEffect() || node.ChildHasVisibleEffect()) {
+    if ((node.GetRenderProperties().GetUseEffect() && node.ShouldPaint()) || node.ChildHasVisibleEffect()) {
         nodeParent->SetChildHasVisibleEffect(true);
         nodeParent->UpdateVisibleEffectChild(node);
     }
