c99c5465038ce3f7856ba7325b6ccd3671964d34
zhoujie <zhoujie241@huawei.com>
2025-03-21 11:25:15 +0800

    新增strutStyle场景TDD防护
    Signed-off-by: zhoujie <zhoujie241@huawei.com>
    Change-Id: I9e5e0054043f654124087bbbbfdc4419685d3eb3

--- rosen/test/2d_graphics/unittest/ndk/drawing_text_typography_test.cpp
+++ rosen/test/2d_graphics/unittest/ndk/drawing_text_typography_test.cpp
@@ -2926,6 +2926,119 @@ HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest112, TestSize.Level
 }
 
 /*
+ * @tc.name: OH_Drawing_TypographyTest113
+ * @tc.desc: test for setting strut style and getting strut style
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest113, TestSize.Level1)
+{
+    OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
+    OH_Drawing_SetTypographyTextWordBreakType(typoStyle, WORD_BREAK_TYPE_BREAK_ALL);
+    OH_Drawing_SetTypographyTextMaxLines(typoStyle, 10);
+
+    OH_Drawing_StrutStyle* strutstyle = new OH_Drawing_StrutStyle();
+    strutstyle->weight = FONT_WEIGHT_400;
+    strutstyle->style = FONT_STYLE_ITALIC;
+    // 17.0 For size
+    strutstyle->size = 17.0;
+    // 2.0 For heightScale
+    strutstyle->heightScale = 2;
+    strutstyle->heightOverride = true;
+    strutstyle->halfLeading = true;
+    // 3.0 For leading
+    strutstyle->leading = 3.0;
+    strutstyle->forceStrutHeight = true;
+    // 4 For families size
+    strutstyle->familiesSize = 4;
+    strutstyle->families = (char**)malloc(strutstyle->familiesSize * sizeof(char*));
+    const char* temp[] = { "1", "2", "3", "4" };
+    for (int i = 0; i < strutstyle->familiesSize; i++) {
+        // 2 For families member size
+        strutstyle->families[i] = (char*)malloc(2 * sizeof(char));
+        strcpy_s(strutstyle->families[i], 2, temp[i]);
+    }
+    OH_Drawing_SetTypographyStyleTextStrutStyle(typoStyle, strutstyle);
+    OH_Drawing_SetTypographyTextUseLineStyle(typoStyle, true);
+    ASSERT_NE(OH_Drawing_TypographyStyleGetStrutStyle(typoStyle), nullptr);
+    
+    OH_Drawing_TypographyCreate *handler =
+        OH_Drawing_CreateTypographyHandler(typoStyle, OH_Drawing_CreateSharedFontCollection());
+    OH_Drawing_TextStyle *textStyle = OH_Drawing_CreateTextStyle();
+    OH_Drawing_SetTextStyleFontSize(textStyle, 52);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, textStyle);
+
+    const char *text = "HelloWorldHelloWorldHelloWorldHelloWorld";
+    OH_Drawing_TypographyHandlerAddText(handler, text);
+    OH_Drawing_TypographyHandlerPopTextStyle(handler);
+
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 1200);
+
+    OH_Drawing_LineMetrics lineMerics;
+    OH_Drawing_TypographyGetLineMetricsAt(typography, 0, &lineMerics);
+    EXPECT_EQ(lineMerics.height, 85);
+
+    // branch coverage
+    OH_Drawing_SetTypographyStyleTextStrutStyle(typoStyle, nullptr);
+    
+    // branch coverage
+    OH_Drawing_StrutStyle* strutstyle1 = new OH_Drawing_StrutStyle();
+    strutstyle1->familiesSize = 0;
+    strutstyle1->families = (char**)malloc(1 * sizeof(char*));
+    OH_Drawing_SetTypographyStyleTextStrutStyle(typoStyle, strutstyle1);
+    OH_Drawing_TypographyStyleGetStrutStyle(typoStyle);
+    OH_Drawing_TypographyStyleDestroyStrutStyle(nullptr);
+
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTextStyle(textStyle);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_TypographyStyleDestroyStrutStyle(strutstyle);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+}
+
+ /*
+ * @tc.name: OH_Drawing_TypographyTest114
+ * @tc.desc: test for strutstyle not equal
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest114, TestSize.Level1)
+{
+    OH_Drawing_StrutStyle* from = new OH_Drawing_StrutStyle();
+    // 4 For families size
+    from->familiesSize = 4;
+    from->families = (char**)malloc(from->familiesSize * sizeof(char*));
+    const char* temp[] = { "1", "2", "3", "4" };
+    for (int i = 0; i < from->familiesSize; i++) {
+        // 2 For families member size
+        from->families[i] = (char*)malloc(2 * sizeof(char));
+        strcpy_s(from->families[i], 2, temp[i]);
+    }
+    OH_Drawing_StrutStyle* to = new OH_Drawing_StrutStyle();
+    // 3 For families size
+    to->familiesSize = 4;
+    to->families = (char**)malloc(to->familiesSize * sizeof(char*));
+    const char* temp1[] = { "3", "2", "1"};
+    for (int i = 0; i < to->familiesSize; i++) {
+        // 2 For families member size
+        to->families[i] = (char*)malloc(2 * sizeof(char));
+        strcpy_s(to->families[i], 2, temp1[i]);
+    }
+    bool result = OH_Drawing_TypographyStyleStrutStyleEquals(from, to);
+    EXPECT_FALSE(result);
+    
+    OH_Drawing_StrutStyle* from1 = new OH_Drawing_StrutStyle();
+    from1->size = 98;
+    OH_Drawing_StrutStyle* to1 = new OH_Drawing_StrutStyle();
+    to1->size = 99;
+    bool result1 = OH_Drawing_TypographyStyleStrutStyleEquals(from1, to1);
+    EXPECT_FALSE(result1);
+    OH_Drawing_TypographyStyleDestroyStrutStyle(from);
+    OH_Drawing_TypographyStyleDestroyStrutStyle(to);
+    OH_Drawing_TypographyStyleDestroyStrutStyle(from1);
+    OH_Drawing_TypographyStyleDestroyStrutStyle(to1);
+}
+
+/*
  * @tc.name: OH_Drawing_FontParserGetSystemFontListTest001
  * @tc.desc: test for the OH_Drawing_FontParserGetSystemFontList.
  * @tc.type: FUNC
