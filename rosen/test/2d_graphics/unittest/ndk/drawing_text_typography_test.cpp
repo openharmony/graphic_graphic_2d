a19f2061f1ad662470eac216dd6c7e1420a4a1ca
zhoujie <zhoujie241@huawei.com>
2025-03-25 11:48:29 +0800

    添加textTab防护TDD
    Signed-off-by: zhoujie <zhoujie241@huawei.com>
    Change-Id: If2fe5ecdefac3174860aa1c3cd9052861f2160bf

--- rosen/test/2d_graphics/unittest/ndk/drawing_text_typography_test.cpp
+++ rosen/test/2d_graphics/unittest/ndk/drawing_text_typography_test.cpp
@@ -3072,6 +3072,209 @@ HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest116, TestSize.Level
 }
 
 /*
+ * @tc.name: OH_Drawing_TypographyTest117
+ * @tc.desc: test for text tab with left alignment
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest117, TestSize.Level1)
+{
+    OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
+    OH_Drawing_SetTypographyTextWordBreakType(typoStyle, WORD_BREAK_TYPE_BREAK_ALL);
+    OH_Drawing_SetTypographyTextMaxLines(typoStyle, 10);
+  
+    OH_Drawing_TextTab* tab = OH_Drawing_CreateTextTab(TEXT_ALIGN_LEFT, 100);
+    OH_Drawing_SetTypographyTextTab(typoStyle, tab);
+      
+    OH_Drawing_TypographyCreate *handler =
+        OH_Drawing_CreateTypographyHandler(typoStyle, OH_Drawing_CreateSharedFontCollection());
+    OH_Drawing_TextStyle *textStyle = OH_Drawing_CreateTextStyle();
+    OH_Drawing_SetTextStyleFontSize(textStyle, 52);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, textStyle);
+  
+    const char *text = "Hello\tWorld\tHello";
+    OH_Drawing_TypographyHandlerAddText(handler, text);
+    OH_Drawing_TypographyHandlerPopTextStyle(handler);
+  
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 1200);
+  
+    double width = OH_Drawing_TypographyGetLongestLine(typography);
+    EXPECT_NEAR(width, 522.772095, FLOAT_DATA_EPSILON);
+  
+    // branch coverage
+    OH_Drawing_SetTypographyTextTab(typoStyle, nullptr);
+  
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTextStyle(textStyle);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_DestroyTextTab(tab);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+}
+
+/*
+ * @tc.name: OH_Drawing_TypographyTest118
+ * @tc.desc: test for text tab with left alignment
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest118, TestSize.Level1)
+{
+    OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
+    OH_Drawing_SetTypographyTextWordBreakType(typoStyle, WORD_BREAK_TYPE_BREAK_ALL);
+    OH_Drawing_SetTypographyTextMaxLines(typoStyle, 10);
+   
+    OH_Drawing_TextTab* tab = OH_Drawing_CreateTextTab(TEXT_ALIGN_RIGHT, 100);
+    OH_Drawing_SetTypographyTextTab(typoStyle, tab);
+       
+    OH_Drawing_TypographyCreate *handler =
+        OH_Drawing_CreateTypographyHandler(typoStyle, OH_Drawing_CreateSharedFontCollection());
+    OH_Drawing_TextStyle *textStyle = OH_Drawing_CreateTextStyle();
+    OH_Drawing_SetTextStyleFontSize(textStyle, 52);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, textStyle);
+   
+    const char *text = "Hello\tWorld\tHello";
+    OH_Drawing_TypographyHandlerAddText(handler, text);
+    OH_Drawing_TypographyHandlerPopTextStyle(handler);
+   
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 1200);
+   
+    double width = OH_Drawing_TypographyGetLongestLine(typography);
+    EXPECT_NEAR(width, 386.828308, FLOAT_DATA_EPSILON);
+   
+    // branch coverage
+    OH_Drawing_SetTypographyTextTab(typoStyle, nullptr);
+   
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTextStyle(textStyle);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_DestroyTextTab(tab);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+}
+
+/*
+ * @tc.name: OH_Drawing_TypographyTest119
+ * @tc.desc: test for text tab with center alignment
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest119, TestSize.Level1)
+{
+    OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
+    OH_Drawing_SetTypographyTextWordBreakType(typoStyle, WORD_BREAK_TYPE_BREAK_ALL);
+    OH_Drawing_SetTypographyTextMaxLines(typoStyle, 10);
+    
+    OH_Drawing_TextTab* tab = OH_Drawing_CreateTextTab(TEXT_ALIGN_CENTER, 100);
+    OH_Drawing_SetTypographyTextTab(typoStyle, tab);
+        
+    OH_Drawing_TypographyCreate *handler =
+        OH_Drawing_CreateTypographyHandler(typoStyle, OH_Drawing_CreateSharedFontCollection());
+    OH_Drawing_TextStyle *textStyle = OH_Drawing_CreateTextStyle();
+    OH_Drawing_SetTextStyleFontSize(textStyle, 52);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, textStyle);
+    
+    const char *text = "Hello\tWorld\tHello";
+    OH_Drawing_TypographyHandlerAddText(handler, text);
+    OH_Drawing_TypographyHandlerPopTextStyle(handler);
+    
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 1200);
+    
+    double width = OH_Drawing_TypographyGetLongestLine(typography);
+    EXPECT_NEAR(width, 393.414185, FLOAT_DATA_EPSILON);
+    
+    // branch coverage
+    OH_Drawing_SetTypographyTextTab(typoStyle, nullptr);
+    
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTextStyle(textStyle);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_DestroyTextTab(tab);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+}
+
+/*
+ * @tc.name: OH_Drawing_TypographyTest120
+ * @tc.desc: test for setting both the texttab and text layout direction
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest120, TestSize.Level1)
+{
+    OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
+    OH_Drawing_SetTypographyTextWordBreakType(typoStyle, WORD_BREAK_TYPE_BREAK_ALL);
+    OH_Drawing_SetTypographyTextMaxLines(typoStyle, 10);
+    OH_Drawing_SetTypographyTextAlign(typoStyle, TEXT_ALIGN_CENTER);
+     
+    OH_Drawing_TextTab* tab = OH_Drawing_CreateTextTab(TEXT_ALIGN_CENTER, 100);
+    OH_Drawing_SetTypographyTextTab(typoStyle, tab);
+         
+    OH_Drawing_TypographyCreate *handler =
+        OH_Drawing_CreateTypographyHandler(typoStyle, OH_Drawing_CreateSharedFontCollection());
+    OH_Drawing_TextStyle *textStyle = OH_Drawing_CreateTextStyle();
+    OH_Drawing_SetTextStyleFontSize(textStyle, 52);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, textStyle);
+     
+    const char *text = "Hello\tWorld\tHello";
+    OH_Drawing_TypographyHandlerAddText(handler, text);
+    OH_Drawing_TypographyHandlerPopTextStyle(handler);
+     
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 1200);
+     
+    double width = OH_Drawing_TypographyGetLongestLine(typography);
+    EXPECT_NEAR(width, 414.388336, FLOAT_DATA_EPSILON);
+     
+    // branch coverage
+    OH_Drawing_SetTypographyTextTab(typoStyle, nullptr);
+     
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTextStyle(textStyle);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_DestroyTextTab(tab);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+}
+
+/*
+ * @tc.name: OH_Drawing_TypographyTest121
+ * @tc.desc: test for setting both the texttab and ellipsis
+ * @tc.type: FUNC
+ */
+HWTEST_F(OH_Drawing_TypographyTest, OH_Drawing_TypographyTest121, TestSize.Level1)
+{
+    OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
+    OH_Drawing_SetTypographyTextWordBreakType(typoStyle, WORD_BREAK_TYPE_BREAK_ALL);
+    OH_Drawing_SetTypographyTextMaxLines(typoStyle, 10);
+    OH_Drawing_SetTypographyTextEllipsis(typoStyle, "...");
+    OH_Drawing_SetTypographyTextEllipsisModal(typoStyle, ELLIPSIS_MODAL_TAIL);
+      
+    OH_Drawing_TextTab* tab = OH_Drawing_CreateTextTab(TEXT_ALIGN_CENTER, 100);
+    OH_Drawing_SetTypographyTextTab(typoStyle, tab);
+          
+    OH_Drawing_TypographyCreate *handler =
+        OH_Drawing_CreateTypographyHandler(typoStyle, OH_Drawing_CreateSharedFontCollection());
+    OH_Drawing_TextStyle *textStyle = OH_Drawing_CreateTextStyle();
+    OH_Drawing_SetTextStyleFontSize(textStyle, 52);
+    OH_Drawing_TypographyHandlerPushTextStyle(handler, textStyle);
+      
+    const char *text = "Hello\tWorld\tHello";
+    OH_Drawing_TypographyHandlerAddText(handler, text);
+    OH_Drawing_TypographyHandlerPopTextStyle(handler);
+      
+    OH_Drawing_Typography *typography = OH_Drawing_CreateTypography(handler);
+    OH_Drawing_TypographyLayout(typography, 1200);
+      
+    double width = OH_Drawing_TypographyGetLongestLine(typography);
+    EXPECT_NEAR(width, 414.388336, FLOAT_DATA_EPSILON);
+      
+    // branch coverage
+    OH_Drawing_SetTypographyTextTab(typoStyle, nullptr);
+      
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTextStyle(textStyle);
+    OH_Drawing_DestroyTypographyHandler(handler);
+    OH_Drawing_DestroyTextTab(tab);
+    OH_Drawing_DestroyTypographyStyle(typoStyle);
+}
+
+/*
  * @tc.name: OH_Drawing_TextStyleGetTest001
  * @tc.desc: test for getting basic attributes from textstyle
  * @tc.type: FUNC
