a3be2b64c0043e7b6c54a9d17a9528c1803b3c85
Niko-Yuan <yuanfangqi7@huawei.com>
2025-03-25 17:52:35 +0800

    fix save layer
    Signed-off-by: Niko-Yuan <yuanfangqi7@huawei.com>

--- rosen/modules/render_service_base/include/property/rs_properties.h
+++ rosen/modules/render_service_base/include/property/rs_properties.h
@@ -542,7 +542,6 @@ public:
 
     void SetColorBlendMode(int colorBlendMode);
     int GetColorBlendMode() const;
-    bool IsColorBlendModeValid() const;
     void SetColorBlendApplyType(int colorBlendApplyType);
     int GetColorBlendApplyType() const;
 
