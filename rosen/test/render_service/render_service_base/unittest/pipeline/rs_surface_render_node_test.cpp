2ab96ba0be086b00f179faa826efd0755f961b05
王德海 <wangdehai2@huawei.com>
2025-03-26 11:40:18 +0800

    xiugai
    Signed-off-by: 王德海 <wangdehai2@huawei.com>
    Change-Id: I382433c58e3169a2a078eddfc1973be38fa0eb44

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_render_node_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_render_node_test.cpp
@@ -515,7 +515,7 @@ HWTEST_F(RSSurfaceRenderNodeTest, UpdateInfoForClonedNode, TestSize.Level1)
 
     surfaceRenderNode.UpdateInfoForClonedNode(surfaceRenderNode.clonedSourceNodeId_);
     auto surfaceParams = static_cast<RSSurfaceRenderParams*>(surfaceRenderNode.stagingRenderParams_.get());
-    ASSERT_FALSE(surfaceParams->GetNeedCacheSurface() == surfaceRenderNode.IsMainWindowType());
+    ASSERT_TRUE(surfaceParams->GetNeedCacheSurface() == surfaceRenderNode.IsMainWindowType());
 }
 
 /**
