9d6787cd007c4b68208fbdfe0e52cd6bcdd5b58f
WebsterSuperior <wangyibo34@huawei.com>
2025-04-12 09:37:45 +0800

    Update sched interface
    Signed-off-by: WebsterSuperior <wangyibo34@huawei.com>

--- rosen/modules/frame_report/include/rs_frame_report.h
+++ rosen/modules/frame_report/include/rs_frame_report.h
@@ -13,84 +13,83 @@
  * limitations under the License.
  */
 
- #ifndef ROSEN_MODULE_RS_FRAME_REPORT_H
- #define ROSEN_MODULE_RS_FRAME_REPORT_H
- 
- #include <string>
- #include <unordered_map>
- 
- namespace OHOS {
- namespace Rosen {
- 
- enum class FrameSchedEvent {
-     SCHED_EVENT_BASE = 0,
-     INIT = 1,
-     RS_RENDER_START = 10001,
-     RS_RENDER_END = 10002,
-     RS_UNI_RENDER_START = 10003,
-     RS_UNI_RENDER_END = 10004,
-     RS_HARDWARE_START = 10005,
-     RS_HARDWARE_END = 10006,
-     RS_HARDWARE_INFO = 10007,
-     RS_BUFFER_COUNT = 10008,
-     RS_FRAME_DEADLINE = 10009,
-     RS_UNBLOCK_MAINTHREAD = 10010,
-     RS_POST_AND_WAIT = 10011,
-     RS_BEGIN_FLUSH = 10012,
-     RS_BLUR_PREDICT = 10013,
-     RS_MODIFIER_INFO = 10014,
-     RS_DDGR_TASK = 10017,
- };
- 
- using InitFunc = void (*)();
- using FrameGetEnableFunc = int (*)();
- using ReportSchedEventFunc = void (*)(FrameSchedEvent, const std::unordered_map<std::string, std::string>&);
- using SendCommandsStartFunc = void(*)();
- using SetFrameParamFunc = void(*)(int, int, int, int);
- class RsFrameReport final {
- public:
-     static RsFrameReport& GetInstance();
-     void Init();
-     int GetEnable();
-     void ReportSchedEvent(FrameSchedEvent event, const std::unordered_map<std::string, std::string> &payload);
- #ifdef RS_ENABLE_VK
-     void ModifierReportSchedEvent(FrameSchedEvent event, const std::unordered_map<std::string, std::string>& payload);
- #endif
-     void SendCommandsStart();
-     void SetFrameParam(int requestId, int load, int schedFrameNum, int value);
-     void RenderStart(uint64_t timestamp);
-     void RenderEnd();
-     void DirectRenderEnd();
-     void UniRenderStart();
-     void UniRenderEnd();
-     void UnblockMainThread();
-     void PostAndWait();
-     void BeginFlush();
-     void ReportBufferCount(int count);
-     void ReportHardwareInfo(int tid);
-     void ReportFrameDeadline(int deadline);
-     void ReportDDGRTaskInfo();
- 
- private:
-     RsFrameReport();
-     ~RsFrameReport();
-     bool LoadLibrary();
-     void CloseLibrary();
-     void *LoadSymbol(const char *symName);
- 
-     void *frameSchedHandle_ = nullptr;
-     bool frameSchedSoLoaded_ = false;
- 
-     InitFunc initFunc_ = nullptr;
-     FrameGetEnableFunc frameGetEnableFunc_ = nullptr;
-     ReportSchedEventFunc reportSchedEventFunc_ = nullptr;
-     SendCommandsStartFunc sendCommandsStartFunc_ = nullptr;
-     SetFrameParamFunc setFrameParamFunc_ = nullptr;
- 
-     int bufferCount_ = 0;
-     int hardwareTid_ = 0;
- };
- } // namespace Rosen
- } // namespace OHOS
- #endif // ROSEN_MODULE_RS_FRAME_REPORT_H
- 
+#ifndef ROSEN_MODULE_RS_FRAME_REPORT_H
+#define ROSEN_MODULE_RS_FRAME_REPORT_H
+
+#include <string>
+#include <unordered_map>
+
+namespace OHOS {
+namespace Rosen {
+
+enum class FrameSchedEvent {
+    SCHED_EVENT_BASE = 0,
+    INIT = 1,
+    RS_RENDER_START = 10001,
+    RS_RENDER_END = 10002,
+    RS_UNI_RENDER_START = 10003,
+    RS_UNI_RENDER_END = 10004,
+    RS_HARDWARE_START = 10005,
+    RS_HARDWARE_END = 10006,
+    RS_HARDWARE_INFO = 10007,
+    RS_BUFFER_COUNT = 10008,
+    RS_FRAME_DEADLINE = 10009,
+    RS_UNBLOCK_MAINTHREAD = 10010,
+    RS_POST_AND_WAIT = 10011,
+    RS_BEGIN_FLUSH = 10012,
+    RS_BLUR_PREDICT = 10013,
+    RS_MODIFIER_INFO = 10014,
+    RS_DDGR_TASK = 10017,
+};
+
+using InitFunc = void (*)();
+using FrameGetEnableFunc = int (*)();
+using ReportSchedEventFunc = void (*)(FrameSchedEvent, const std::unordered_map<std::string, std::string>&);
+using SendCommandsStartFunc = void(*)();
+using SetFrameParamFunc = void(*)(int, int, int, int);
+class RsFrameReport final {
+public:
+    static RsFrameReport& GetInstance();
+    void Init();
+    int GetEnable();
+    void ReportSchedEvent(FrameSchedEvent event, const std::unordered_map<std::string, std::string> &payload);
+#ifdef RS_ENABLE_VK
+    void ModifierReportSchedEvent(FrameSchedEvent event, const std::unordered_map<std::string, std::string>& payload);
+#endif
+    void SendCommandsStart();
+    void SetFrameParam(int requestId, int load, int schedFrameNum, int value);
+    void RenderStart(uint64_t timestamp);
+    void RenderEnd();
+    void DirectRenderEnd();
+    void UniRenderStart();
+    void UniRenderEnd();
+    void UnblockMainThread();
+    void PostAndWait();
+    void BeginFlush();
+    void ReportBufferCount(int count);
+    void ReportHardwareInfo(int tid);
+    void ReportFrameDeadline(int deadline);
+    void ReportDDGRTaskInfo();
+
+private:
+    RsFrameReport();
+    ~RsFrameReport();
+    bool LoadLibrary();
+    void CloseLibrary();
+    void *LoadSymbol(const char *symName);
+
+    void *frameSchedHandle_ = nullptr;
+    bool frameSchedSoLoaded_ = false;
+
+    InitFunc initFunc_ = nullptr;
+    FrameGetEnableFunc frameGetEnableFunc_ = nullptr;
+    ReportSchedEventFunc reportSchedEventFunc_ = nullptr;
+    SendCommandsStartFunc sendCommandsStartFunc_ = nullptr;
+    SetFrameParamFunc setFrameParamFunc_ = nullptr;
+
+    int bufferCount_ = 0;
+    int hardwareTid_ = 0;
+};
+} // namespace Rosen
+} // namespace OHOS
+#endif // ROSEN_MODULE_RS_FRAME_REPORT_H
--- rosen/modules/frame_report/src/rs_frame_report.cpp
+++ rosen/modules/frame_report/src/rs_frame_report.cpp
@@ -13,236 +13,234 @@
  * limitations under the License.
  */
 
+#include "rs_frame_report.h"
 
- #include "rs_frame_report.h"
+#include <dlfcn.h>
+#include <cstdio>
+#include <unistd.h>
 
- #include <dlfcn.h>
- #include <cstdio>
- #include <unistd.h>
- 
- #include "hilog/log.h"
- 
- namespace OHOS {
- namespace Rosen {
- #undef LOG_DOMAIN
- #define LOG_DOMAIN 0xD001400
- 
- #undef LOG_TAG
- #define LOG_TAG "OHOS::RS"
- #define LOGI(fmt, ...) HILOG_INFO(LOG_CORE, fmt, ##__VA_ARGS__)
- #define LOGE(fmt, ...) HILOG_ERROR(LOG_CORE, fmt, ##__VA_ARGS__)
- namespace {
- const std::string FRAME_AWARE_SO_PATH = "libframe_ui_intf.z.so";
- }
- RsFrameReport& RsFrameReport::GetInstance()
- {
-     static RsFrameReport instance;
-     return instance;
- }
- 
- RsFrameReport::RsFrameReport()
- {
-     int ret = LoadLibrary();
-     if (!ret) {
-         LOGE("RsFrameReport:[Init] dlopen libframe_ui_intf.so failed!");
-         return;
-     }
-     LOGI("RsFrameReport:[Init] dlopen libframe_ui_intf.so success!");
-     initFunc_ = (InitFunc)LoadSymbol("Init");
-     if (initFunc_ != nullptr) {
-         initFunc_();
-     }
- }
- 
- RsFrameReport::~RsFrameReport()
- {
-     CloseLibrary();
- }
- 
- void RsFrameReport::Init()
- {
-     ReportSchedEvent(FrameSchedEvent::INIT, {});
- }
- 
- bool RsFrameReport::LoadLibrary()
- {
-     if (!frameSchedSoLoaded_) {
-         frameSchedHandle_ = dlopen(FRAME_AWARE_SO_PATH.c_str(), RTLD_LAZY);
-         if (frameSchedHandle_ == nullptr) {
-             LOGE("RsFrameReport:[LoadLibrary]dlopen libframe_ui_intf.so failed!"
-                 " error = %{public}s\n", dlerror());
-             return false;
-         }
-         frameSchedSoLoaded_ = true;
-     }
-     LOGI("RsFrameReport:[LoadLibrary] load library success!");
-     return true;
- }
- 
- void RsFrameReport::CloseLibrary()
- {
-     if (dlclose(frameSchedHandle_) != 0) {
-         LOGE("RsFrameReport:[CloseLibrary]libframe_ui_intf.so failed!\n");
-         return;
-     }
-     frameSchedHandle_ = nullptr;
-     frameSchedSoLoaded_ = false;
-     LOGI("RsFrameReport:[CloseLibrary]libframe_ui_intf.so close success!\n");
- }
+#include "hilog/log.h"
 
- void *RsFrameReport::LoadSymbol(const char *symName)
- {
-     if (!frameSchedSoLoaded_) {
-         LOGE("RsFrameReport:[loadSymbol]libframe_ui_intf.so not loaded.\n");
-         return nullptr;
-     }
-     void *funcSym = dlsym(frameSchedHandle_, symName);
-     if (funcSym == nullptr) {
-         LOGE("RsFrameReport:[loadSymbol]Get %{public}s symbol failed: %{public}s\n", symName, dlerror());
-         return nullptr;
-     }
-     return funcSym;
- }
- 
- int RsFrameReport::GetEnable()
- {
-     if (!frameSchedSoLoaded_) {
-         return 0;
-     }
-     if (frameGetEnableFunc_ == nullptr) {
-         frameGetEnableFunc_ = (FrameGetEnableFunc)LoadSymbol("GetSenseSchedEnable");
-     }
-     if (frameGetEnableFunc_ != nullptr) {
-         return frameGetEnableFunc_();
-     } else {
-         LOGE("RsFrameReport:[GetEnable]load GetSenseSchedEnable function failed!");
-         return 0;
-     }
- }
- 
- void RsFrameReport::ReportSchedEvent(FrameSchedEvent event, const std::unordered_map<std::string, std::string>& payload)
- {
-     if (reportSchedEventFunc_ == nullptr) {
-         reportSchedEventFunc_ = (ReportSchedEventFunc)LoadSymbol("ReportSchedEvent");
-     }
-     if (reportSchedEventFunc_ != nullptr) {
-         reportSchedEventFunc_(event, payload);
-     } else {
-         LOGE("RsFrameReport load ReportSchedEvent function failed!");
-     }
- }
- 
- #ifdef RS_ENABLE_VK
- void RsFrameReport::ModifierReportSchedEvent(
-     FrameSchedEvent event, const std::unordered_map<std::string, std::string> &payload)
- {
-     if (!frameSchedSoLoaded_) {
-         LoadLibrary();
-     }
-     ReportSchedEvent(event, payload);
- }
- #endif
- 
- void RsFrameReport::SetFrameParam(int requestId, int load, int schedFrameNum, int value)
- {
-     if (setFrameParamFunc_ == nullptr) {
-         setFrameParamFunc_ = (SetFrameParamFunc)LoadSymbol("SetFrameParam");
-     }
- 
-     if (setFrameParamFunc_ != nullptr) {
-         setFrameParamFunc_(requestId, load, schedFrameNum, value);
-     } else {
-         LOGE("RsFrameReport:[SetFrameParam]load SetFrameParam function failed");
-     }
- }
- 
- void RsFrameReport::SendCommandsStart()
- {
-     if (sendCommandsStartFunc_ == nullptr) {
-         sendCommandsStartFunc_ = (SendCommandsStartFunc)LoadSymbol("SendCommandsStart");
-     }
-     if (sendCommandsStartFunc_ != nullptr) {
-         sendCommandsStartFunc_();
-     } else {
-         LOGE("RsFrameReport:[SendCommandsStart]load SendCommandsStart function failed!");
-     }
- }
- 
- void RsFrameReport::RenderStart(uint64_t timestamp)
- {
-     std::unordered_map<std::string, std::string> payload = {};
-     payload["vsyncTime"] = std::to_string(timestamp);
-     ReportSchedEvent(FrameSchedEvent::RS_RENDER_START, payload);
- }
- 
- void RsFrameReport::RenderEnd()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_RENDER_END, {});
- }
- 
- void RsFrameReport::DirectRenderEnd()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_UNI_RENDER_END, {});
- }
- 
- void RsFrameReport::UniRenderStart()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_UNI_RENDER_START, {});
- }
- 
- void RsFrameReport::UniRenderEnd()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_UNI_RENDER_END, {});
- }
- 
- void RsFrameReport::UnblockMainThread()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_UNBLOCK_MAINTHREAD, {});
- }
- 
- void RsFrameReport::PostAndWait()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_POST_AND_WAIT, {});
- }
- 
- void RsFrameReport::BeginFlush()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_BEGIN_FLUSH, {});
- }
- 
- void RsFrameReport::ReportBufferCount(int count)
- {
-     if (bufferCount_ == count) {
-         return;
-     }
-     bufferCount_ = count;
-     std::unordered_map<std::string, std::string> payload = {};
-     payload["bufferCount"] = std::to_string(count);
-     ReportSchedEvent(FrameSchedEvent::RS_BUFFER_COUNT, payload);
- }
- 
- void RsFrameReport::ReportHardwareInfo(int tid)
- {
-     if (hardwareTid_ == tid) {
-         return;
-     }
-     hardwareTid_ = tid;
-     std::unordered_map<std::string, std::string> payload = {};
-     payload["hardwareTid"] = std::to_string(tid);
-     ReportSchedEvent(FrameSchedEvent::RS_HARDWARE_INFO, payload);
- }
- 
- void RsFrameReport::ReportFrameDeadline(int deadline)
- {
-     std::unordered_map<std::string, std::string> payload = {};
-     payload["rsFrameDeadline"] = std::to_string(deadline);
-     ReportSchedEvent(FrameSchedEvent::RS_FRAME_DEADLINE, payload);
- }
- 
- void RsFrameReport::ReportDDGRTaskInfo()
- {
-     ReportSchedEvent(FrameSchedEvent::RS_DDGR_TASK, {});
- }
- } // namespace Rosen
- } // namespace OHOS
- 
+namespace OHOS {
+namespace Rosen {
+#undef LOG_DOMAIN
+#define LOG_DOMAIN 0xD001400
+
+#undef LOG_TAG
+#define LOG_TAG "OHOS::RS"
+#define LOGI(fmt, ...) HILOG_INFO(LOG_CORE, fmt, ##__VA_ARGS__)
+#define LOGE(fmt, ...) HILOG_ERROR(LOG_CORE, fmt, ##__VA_ARGS__)
+namespace {
+const std::string FRAME_AWARE_SO_PATH = "libframe_ui_intf.z.so";
+}
+RsFrameReport& RsFrameReport::GetInstance()
+{
+    static RsFrameReport instance;
+    return instance;
+}
+
+RsFrameReport::RsFrameReport()
+{
+    int ret = LoadLibrary();
+    if (!ret) {
+        LOGE("RsFrameReport:[Init] dlopen libframe_ui_intf.so failed!");
+        return;
+    }
+    LOGI("RsFrameReport:[Init] dlopen libframe_ui_intf.so success!");
+    initFunc_ = (InitFunc)LoadSymbol("Init");
+    if (initFunc_ != nullptr) {
+        initFunc_();
+    }
+}
+
+RsFrameReport::~RsFrameReport()
+{
+    CloseLibrary();
+}
+
+void RsFrameReport::Init()
+{
+    ReportSchedEvent(FrameSchedEvent::INIT, {});
+}
+
+bool RsFrameReport::LoadLibrary()
+{
+    if (!frameSchedSoLoaded_) {
+        frameSchedHandle_ = dlopen(FRAME_AWARE_SO_PATH.c_str(), RTLD_LAZY);
+        if (frameSchedHandle_ == nullptr) {
+            LOGE("RsFrameReport:[LoadLibrary]dlopen libframe_ui_intf.so failed!"
+                " error = %{public}s\n", dlerror());
+            return false;
+        }
+        frameSchedSoLoaded_ = true;
+    }
+    LOGI("RsFrameReport:[LoadLibrary] load library success!");
+    return true;
+}
+
+void RsFrameReport::CloseLibrary()
+{
+    if (dlclose(frameSchedHandle_) != 0) {
+        LOGE("RsFrameReport:[CloseLibrary]libframe_ui_intf.so failed!\n");
+        return;
+    }
+    frameSchedHandle_ = nullptr;
+    frameSchedSoLoaded_ = false;
+    LOGI("RsFrameReport:[CloseLibrary]libframe_ui_intf.so close success!\n");
+}
+
+void *RsFrameReport::LoadSymbol(const char *symName)
+{
+    if (!frameSchedSoLoaded_) {
+        LOGE("RsFrameReport:[loadSymbol]libframe_ui_intf.so not loaded.\n");
+        return nullptr;
+    }
+    void *funcSym = dlsym(frameSchedHandle_, symName);
+    if (funcSym == nullptr) {
+        LOGE("RsFrameReport:[loadSymbol]Get %{public}s symbol failed: %{public}s\n", symName, dlerror());
+        return nullptr;
+    }
+    return funcSym;
+}
+
+int RsFrameReport::GetEnable()
+{
+    if (!frameSchedSoLoaded_) {
+        return 0;
+    }
+    if (frameGetEnableFunc_ == nullptr) {
+        frameGetEnableFunc_ = (FrameGetEnableFunc)LoadSymbol("GetSenseSchedEnable");
+    }
+    if (frameGetEnableFunc_ != nullptr) {
+        return frameGetEnableFunc_();
+    } else {
+        LOGE("RsFrameReport:[GetEnable]load GetSenseSchedEnable function failed!");
+        return 0;
+    }
+}
+
+void RsFrameReport::ReportSchedEvent(FrameSchedEvent event, const std::unordered_map<std::string, std::string>& payload)
+{
+    if (reportSchedEventFunc_ == nullptr) {
+        reportSchedEventFunc_ = (ReportSchedEventFunc)LoadSymbol("ReportSchedEvent");
+    }
+    if (reportSchedEventFunc_ != nullptr) {
+        reportSchedEventFunc_(event, payload);
+    } else {
+        LOGE("RsFrameReport load ReportSchedEvent function failed!");
+    }
+}
+
+#ifdef RS_ENABLE_VK
+void RsFrameReport::ModifierReportSchedEvent(
+    FrameSchedEvent event, const std::unordered_map<std::string, std::string> &payload)
+{
+    if (!frameSchedSoLoaded_) {
+        LoadLibrary();
+    }
+    ReportSchedEvent(event, payload);
+}
+#endif
+
+void RsFrameReport::SetFrameParam(int requestId, int load, int schedFrameNum, int value)
+{
+    if (setFrameParamFunc_ == nullptr) {
+        setFrameParamFunc_ = (SetFrameParamFunc)LoadSymbol("SetFrameParam");
+    }
+
+    if (setFrameParamFunc_ != nullptr) {
+        setFrameParamFunc_(requestId, load, schedFrameNum, value);
+    } else {
+        LOGE("RsFrameReport:[SetFrameParam]load SetFrameParam function failed");
+    }
+}
+
+void RsFrameReport::SendCommandsStart()
+{
+    if (sendCommandsStartFunc_ == nullptr) {
+        sendCommandsStartFunc_ = (SendCommandsStartFunc)LoadSymbol("SendCommandsStart");
+    }
+    if (sendCommandsStartFunc_ != nullptr) {
+        sendCommandsStartFunc_();
+    } else {
+        LOGE("RsFrameReport:[SendCommandsStart]load SendCommandsStart function failed!");
+    }
+}
+
+void RsFrameReport::RenderStart(uint64_t timestamp)
+{
+    std::unordered_map<std::string, std::string> payload = {};
+    payload["vsyncTime"] = std::to_string(timestamp);
+    ReportSchedEvent(FrameSchedEvent::RS_RENDER_START, payload);
+}
+
+void RsFrameReport::RenderEnd()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_RENDER_END, {});
+}
+
+void RsFrameReport::DirectRenderEnd()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_UNI_RENDER_END, {});
+}
+
+void RsFrameReport::UniRenderStart()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_UNI_RENDER_START, {});
+}
+
+void RsFrameReport::UniRenderEnd()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_UNI_RENDER_END, {});
+}
+
+void RsFrameReport::UnblockMainThread()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_UNBLOCK_MAINTHREAD, {});
+}
+
+void RsFrameReport::PostAndWait()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_POST_AND_WAIT, {});
+}
+
+void RsFrameReport::BeginFlush()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_BEGIN_FLUSH, {});
+}
+
+void RsFrameReport::ReportBufferCount(int count)
+{
+    if (bufferCount_ == count) {
+        return;
+    }
+    bufferCount_ = count;
+    std::unordered_map<std::string, std::string> payload = {};
+    payload["bufferCount"] = std::to_string(count);
+    ReportSchedEvent(FrameSchedEvent::RS_BUFFER_COUNT, payload);
+}
+
+void RsFrameReport::ReportHardwareInfo(int tid)
+{
+    if (hardwareTid_ == tid) {
+        return;
+    }
+    hardwareTid_ = tid;
+    std::unordered_map<std::string, std::string> payload = {};
+    payload["hardwareTid"] = std::to_string(tid);
+    ReportSchedEvent(FrameSchedEvent::RS_HARDWARE_INFO, payload);
+}
+
+void RsFrameReport::ReportFrameDeadline(int deadline)
+{
+    std::unordered_map<std::string, std::string> payload = {};
+    payload["rsFrameDeadline"] = std::to_string(deadline);
+    ReportSchedEvent(FrameSchedEvent::RS_FRAME_DEADLINE, payload);
+}
+
+void RsFrameReport::ReportDDGRTaskInfo()
+{
+    ReportSchedEvent(FrameSchedEvent::RS_DDGR_TASK, {});
+}
+} // namespace Rosen
+} // namespace OHOS
