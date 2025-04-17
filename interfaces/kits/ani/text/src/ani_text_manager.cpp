761afc259b1065c335a45d20fb10b594a876594f
liufei <liufei225@h-partners.com>
2025-04-17 21:20:20 +0800

    add fontcollection method, use macro init ani, format gn
    Signed-off-by: liufei <liufei225@h-partners.com>
    Change-Id: I475a25f7c1341f83fd9da70d9f848eae1c339482

--- interfaces/kits/ani/text/src/ani_text_manager.cpp
+++ interfaces/kits/ani/text/src/ani_text_manager.cpp
@@ -17,20 +17,41 @@
 #include "ani_font_collection.h"
 #include "ani_paragraph.h"
 
+namespace {
+using namespace OHOS::Rosen;
+
+#define STRUCT_LIST(...) using AniTypes = std::tuple<__VA_ARGS__>
+
+// add new struct in this macro
+STRUCT_LIST(AniFontCollection, AniParagraph);
+
+template<typename T>
+static ani_status InitOneStruct(ani_vm* vm, uint32_t* result)
+{
+    return T::AniInit(vm, result);
+}
+
+template<typename Tuple, size_t... Is>
+static ani_status InitAllStruct(ani_vm* vm, uint32_t* result, std::index_sequence<Is...>)
+{
+    ani_status ret;
+    [[maybe_unused]] bool _ = (((ret = InitOneStruct<std::tuple_element_t<Is, Tuple>>(vm, result)) == ANI_OK) && ...);
+    return ret;
+}
+
+static ani_status Init(ani_vm* vm, uint32_t* result)
+{
+    return InitAllStruct<AniTypes>(vm, result, std::make_index_sequence<std::tuple_size_v<AniTypes>>());
+}
+} // namespace
+
 extern "C" {
 ANI_EXPORT ani_status ANI_Constructor(ani_vm* vm, uint32_t* result)
 {
-    using namespace OHOS::Rosen;
-    ani_status status;
-    status = AniFontCollection::AniInit(vm, result);
-    if (status != ANI_OK) {
-        return status;
+    ani_status status = Init(vm, result);
+    if (status == ANI_OK) {
+        *result = ANI_VERSION_1;
     }
-    status = AniParagraph::AniInit(vm, result);
-    if (status != ANI_OK) {
-        return status;
-    }
-    *result = ANI_VERSION_1;
-    return ANI_OK;
+    return status;
 }
 }
