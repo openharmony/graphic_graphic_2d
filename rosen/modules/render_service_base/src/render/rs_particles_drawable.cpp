a05cf0b1b17fae78f972df9603f62bae0ca58e69
zyan <zhangyan407@huawei.com>
2025-04-18 11:46:42 +0800

    门禁问题修改
    Signed-off-by: zyan <zhangyan407@huawei.com>

--- rosen/modules/render_service_base/src/render/rs_particles_drawable.cpp
+++ rosen/modules/render_service_base/src/render/rs_particles_drawable.cpp
@@ -228,7 +228,8 @@ void RSParticlesDrawable::DrawCircle(Drawing::Canvas& canvas)
     canvas.DetachBrush();
 }
 
-bool RSParticlesDrawable::CheckImageNull(std::shared_ptr<Drawing::Image>& image, const std::shared_ptr<Drawing::Image>& drawImage)
+bool RSParticlesDrawable::CheckImageNull(std::shared_ptr<Drawing::Image>& image,
+    const std::shared_ptr<Drawing::Image>& drawImage)
 {
     if (image) {
         return false;
