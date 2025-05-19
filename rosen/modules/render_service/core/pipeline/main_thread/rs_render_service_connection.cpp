f1c230ffe3459ccb8da7e7290def14655c8fb278
YouZijun97 <yemingwei1@huawei.com>
2025-05-19 11:42:19 +0800

    save scene info
    Signed-off-by: YouZijun97 <yemingwei1@huawei.com>

--- rosen/modules/render_service/core/pipeline/main_thread/rs_render_service_connection.cpp
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_render_service_connection.cpp
@@ -21,6 +21,7 @@
 #include "hgm_core.h"
 #include "hgm_frame_rate_manager.h"
 #include "offscreen_render/rs_offscreen_render_thread.h"
+#include "rs_frame_report.h"
 #include "rs_main_thread.h"
 #include "rs_trace.h"
 //blur predict
@@ -2632,6 +2633,10 @@ void RSRenderServiceConnection::NotifyRefreshRateEvent(const EventInfo& eventInf
         return;
     }
 
+    if (VOTER_SCENE_GPU == eventInfo.eventName) {
+        RsFrameReport::GetInstance().ReportScbSceneInfo(eventInfo.description, eventInfo.eventStatus);
+        return;
+    }
     HgmTaskHandleThread::Instance().PostTask([pid = remotePid_, eventInfo]() {
         auto frameRateMgr = HgmCore::Instance().GetFrameRateMgr();
         if (frameRateMgr != nullptr) {
