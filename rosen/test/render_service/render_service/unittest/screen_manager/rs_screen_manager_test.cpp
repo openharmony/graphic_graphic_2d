2cf8bc0eb251b9fb0e13676b443b6af15cb96e14
guanweixiang <guanweixiang1@huawei.com>
2025-05-24 09:41:34 +0800

    add rsscreenmanagertest tdd
    Signed-off-by: guanweixiang <guanweixiang1@huawei.com>

--- rosen/test/render_service/render_service/unittest/screen_manager/rs_screen_manager_test.cpp
+++ rosen/test/render_service/render_service/unittest/screen_manager/rs_screen_manager_test.cpp
@@ -3614,6 +3614,9 @@ HWTEST_F(RSScreenManagerTest, RemoveScreenChangeCallback, TestSize.Level1)
     EXPECT_NE(nullptr, callback2);
 
     screenManagerImpl->screenChangeCallbacks_.push_back(callback1);
+    screenManagerImpl->RSScreenManager::RemoveScreenChangeCallback(callback2);
+    EXPECT_EQ(1, screenManagerImpl->screenChangeCallbacks_.size());
+    
     screenManagerImpl->screenChangeCallbacks_.push_back(callback2);
     EXPECT_EQ(2, screenManagerImpl->screenChangeCallbacks_.size());
 
