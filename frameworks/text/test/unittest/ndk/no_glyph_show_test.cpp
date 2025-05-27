ae285a3b353c6993d4adafce7f697a7e3b74b077
liufei <liufei225@h-partners.com>
2025-05-27 15:05:29 +0800

    test(text): update undefined glyph display test cases
    - Update test cases to use correct enum values for glyph display options
    - Adjust expected results for Noto Sans font to match new rendering behavior
    - Refactor test setup and teardown for better isolation between test cases
    
    Signed-off-by: liufei <liufei225@h-partners.com>
    Change-Id: Ib5364c020eef1e29fa8bde7edad12c98fe210cb9

--- frameworks/text/test/unittest/ndk/no_glyph_show_test.cpp
+++ frameworks/text/test/unittest/ndk/no_glyph_show_test.cpp
@@ -40,8 +40,8 @@ private:
         { 0, 8.0, 319.4397, 42.0 } };
     static constexpr float tofuResult_[][4] = { { 2.0, 2.0, 228.63979, 29.0 }, { 1.0, 5.0, 388.10962, 37.0 },
         { 8.0, 8.0, 341.4397, 42.0 } };
-    static constexpr float notoResult_[][4] = { { 2.0, 2.0, 206.63979, 29.0 }, { 1.0, 5.0, 388.10962, 37.0 },
-        { 0, 8.0, 319.4397, 42.0 } };
+    static constexpr float notoResult_[][4] = { { 2.0, 2.0, 232.20976, 29.0 }, { 2.0, 5.0, 388.43961, 33.0 },
+        { 0, 8.0, 347.76962, 42.0 } };
     static constexpr float noGlyphTofuResult_[][4] = { { 8.0, 0, 22.0, 22.0 } };
     static constexpr float noGlyphDefaultResult_[][4] = { { 0, 0, 0, 0 } };
     static constexpr float noGlyphNotoResult_[][4] = { { 0, 0, 0, 0 } };
@@ -100,7 +100,7 @@ OH_Drawing_Typography* NdkUndefinedGlyphDisplayTest::PrepareCreateTypography(
  */
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest001, TestSize.Level1)
 {
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_TOFU);
     OH_Drawing_Typography* typography = PrepareCreateTypography(text_);
     EXPECT_NE(typography, nullptr);
     BoundsResult(typography, tofuResult_, 3);
@@ -114,7 +114,7 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest001, TestSize
  */
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest002, TestSize.Level1)
 {
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_DEFAULT);
     OH_Drawing_Typography* typography = PrepareCreateTypography(text_);
     EXPECT_NE(typography, nullptr);
     BoundsResult(typography, defaultResult_, 3);
@@ -128,12 +128,12 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest002, TestSize
  */
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest003, TestSize.Level1)
 {
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_DEFAULT);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
     OH_Drawing_Typography* defaultTypography = PrepareCreateTypography(text_);
     EXPECT_NE(defaultTypography, nullptr);
     BoundsResult(defaultTypography, defaultResult_, 3);
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_TOFU);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
     OH_Drawing_Typography* tofuTypography = PrepareCreateTypography(text_);
     EXPECT_NE(tofuTypography, nullptr);
@@ -149,12 +149,12 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest003, TestSize
  */
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest004, TestSize.Level1)
 {
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_DEFAULT);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
     OH_Drawing_Typography* defaultTypography = PrepareCreateTypography(noGlyphText_);
     EXPECT_NE(defaultTypography, nullptr);
     BoundsResult(defaultTypography, noGlyphDefaultResult_, 1);
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_TOFU);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
     OH_Drawing_Typography* tofuTypography = PrepareCreateTypography(noGlyphText_);
     EXPECT_NE(tofuTypography, nullptr);
@@ -170,13 +170,13 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest004, TestSize
  */
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest005, TestSize.Level1)
 {
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_TOFU);
     const char* fontFamilies[] = { "Noto Sans" };
     OH_Drawing_Typography* typography = PrepareCreateTypography(text_, fontFamilies, 1);
     BoundsResult(typography, tofuResult_, 3);
     OH_Drawing_Typography* onlyTypography = PrepareCreateTypography(noGlyphText_, fontFamilies, 1);
     BoundsResult(onlyTypography, noGlyphTofuResult_, 1);
-    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_DEFAULT);
     OH_Drawing_DestroyTypography(typography);
     OH_Drawing_DestroyTypography(onlyTypography);
 }
