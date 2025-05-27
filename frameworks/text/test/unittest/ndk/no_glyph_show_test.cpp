40af2d2b2a81fbdbacf2a7a9cc9e5197e74962a7
liufei <liufei225@h-partners.com>
2025-05-27 14:35:07 +0800

    test(text): improve undefined glyph display tests with font family support
    - Add font family parameter to PrepareCreateTypography function
    - Update test cases to use new typography creation method
    - Include tests for Noto Sans font family
    - Refactor test structure for better readability and maintainability
    
    Signed-off-by: liufei <liufei225@h-partners.com>
    Change-Id: I88f2741885b0ff55f08c086459a0b2d623107a4b

--- frameworks/text/test/unittest/ndk/no_glyph_show_test.cpp
+++ frameworks/text/test/unittest/ndk/no_glyph_show_test.cpp
@@ -29,27 +29,33 @@ namespace OHOS {
 namespace Rosen {
 class NdkUndefinedGlyphDisplayTest : public testing::Test {
 public:
-    OH_Drawing_Typography* PrepareCreateTextLine(const std::string& text);
+    OH_Drawing_Typography* PrepareCreateTypography(
+        const std::string& text, const char** fontFamilies = nullptr, int fontCount = 0);
 
 private:
     static constexpr const char* text_ = "Hello 测 World \uffff\n!@#$%^&*~(){\uffff\uffff}[]90 - = ,.\n\uffff"
                                          "testlp\uffff试\uffff Drawing\uffff";
-    static constexpr const char* onlyNoGlyph_ = "\uffff";
+    static constexpr const char* noGlyphText_ = "\uffff";
     static constexpr float defaultResult_[][4] = { { 2.0, 2.0, 206.63979, 29.0 }, { 1.0, 5.0, 388.10962, 37.0 },
         { 0, 8.0, 319.4397, 42.0 } };
     static constexpr float tofuResult_[][4] = { { 2.0, 2.0, 228.63979, 29.0 }, { 1.0, 5.0, 388.10962, 37.0 },
         { 8.0, 8.0, 341.4397, 42.0 } };
-    static constexpr float onlyTofuResult_[][4] = { { 8.0, 0, 22.0, 22.0 } };
-    static constexpr float onlyDefaultResult_[][4] = { { 0, 0, 0, 0 } };
+    static constexpr float notoResult_[][4] = { { 2.0, 2.0, 206.63979, 29.0 }, { 1.0, 5.0, 388.10962, 37.0 },
+        { 0, 8.0, 319.4397, 42.0 } };
+    static constexpr float noGlyphTofuResult_[][4] = { { 8.0, 0, 22.0, 22.0 } };
+    static constexpr float noGlyphDefaultResult_[][4] = { { 0, 0, 0, 0 } };
+    static constexpr float noGlyphNotoResult_[][4] = { { 0, 0, 0, 0 } };
 };
 
-OH_Drawing_Typography* NdkUndefinedGlyphDisplayTest::PrepareCreateTextLine(const std::string& text)
+OH_Drawing_Typography* NdkUndefinedGlyphDisplayTest::PrepareCreateTypography(
+    const std::string& text, const char** fontFamilies, int fontCount)
 {
     double maxWidth = 500.0;
     OH_Drawing_TypographyStyle* typoStyle = OH_Drawing_CreateTypographyStyle();
     EXPECT_NE(typoStyle, nullptr);
     OH_Drawing_TextStyle* txtStyle = OH_Drawing_CreateTextStyle();
     EXPECT_NE(txtStyle, nullptr);
+    OH_Drawing_SetTextStyleFontFamilies(txtStyle, fontCount, fontFamilies);
     OH_Drawing_FontCollection* fontCollection = OH_Drawing_GetFontCollectionGlobalInstance();
     EXPECT_NE(fontCollection, nullptr);
     OH_Drawing_TypographyCreate* handler = OH_Drawing_CreateTypographyHandler(typoStyle, fontCollection);
@@ -69,9 +75,8 @@ OH_Drawing_Typography* NdkUndefinedGlyphDisplayTest::PrepareCreateTextLine(const
     return typography;
 }
 
-#define BoundsResult(rectResult, size, text)                                                 \
+#define BoundsResult(typography, rectResult, size)                                           \
     do {                                                                                     \
-        OH_Drawing_Typography* typography = PrepareCreateTextLine(text);                     \
         OH_Drawing_Array* textLines = OH_Drawing_TypographyGetTextLines(typography);         \
         size_t arraySize = OH_Drawing_GetDrawingArraySize(textLines);                        \
         EXPECT_EQ(size, arraySize);                                                          \
@@ -86,7 +91,6 @@ OH_Drawing_Typography* NdkUndefinedGlyphDisplayTest::PrepareCreateTextLine(const
             OH_Drawing_DestroyTextLine(textLine);                                            \
         }                                                                                    \
         OH_Drawing_DestroyTextLines(textLines);                                              \
-        OH_Drawing_DestroyTypography(typography);                                            \
     } while (0)
 
 /**
@@ -97,7 +101,10 @@ OH_Drawing_Typography* NdkUndefinedGlyphDisplayTest::PrepareCreateTextLine(const
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest001, TestSize.Level1)
 {
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
-    BoundsResult(tofuResult_, 3, text_);
+    OH_Drawing_Typography* typography = PrepareCreateTypography(text_);
+    EXPECT_NE(typography, nullptr);
+    BoundsResult(typography, tofuResult_, 3);
+    OH_Drawing_DestroyTypography(typography);
 }
 
 /**
@@ -108,7 +115,10 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest001, TestSize
 HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest002, TestSize.Level1)
 {
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
-    BoundsResult(defaultResult_, 3, text_);
+    OH_Drawing_Typography* typography = PrepareCreateTypography(text_);
+    EXPECT_NE(typography, nullptr);
+    BoundsResult(typography, defaultResult_, 3);
+    OH_Drawing_DestroyTypography(typography);
 }
 
 /**
@@ -120,10 +130,16 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest003, TestSize
 {
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
-    BoundsResult(defaultResult_, 3, text_);
+    OH_Drawing_Typography* defaultTypography = PrepareCreateTypography(text_);
+    EXPECT_NE(defaultTypography, nullptr);
+    BoundsResult(defaultTypography, defaultResult_, 3);
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
-    BoundsResult(tofuResult_, 3, text_);
+    OH_Drawing_Typography* tofuTypography = PrepareCreateTypography(text_);
+    EXPECT_NE(tofuTypography, nullptr);
+    BoundsResult(tofuTypography, tofuResult_, 3);
+    OH_Drawing_DestroyTypography(defaultTypography);
+    OH_Drawing_DestroyTypography(tofuTypography);
 }
 
 /**
@@ -135,10 +151,34 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest004, TestSize
 {
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
-    BoundsResult(onlyDefaultResult_, 1, onlyNoGlyph_);
+    OH_Drawing_Typography* defaultTypography = PrepareCreateTypography(noGlyphText_);
+    EXPECT_NE(defaultTypography, nullptr);
+    BoundsResult(defaultTypography, noGlyphDefaultResult_, 1);
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
     OH_Drawing_SetTextUndefinedGlyphDisplay(static_cast<OH_Drawing_TextUndefinedGlyphDisplay>(100));
-    BoundsResult(onlyTofuResult_, 1, onlyNoGlyph_);
+    OH_Drawing_Typography* tofuTypography = PrepareCreateTypography(noGlyphText_);
+    EXPECT_NE(tofuTypography, nullptr);
+    BoundsResult(tofuTypography, noGlyphTofuResult_, 1);
+    OH_Drawing_DestroyTypography(defaultTypography);
+    OH_Drawing_DestroyTypography(tofuTypography);
+}
+
+/**
+ * @tc.name: NdkUndefinedGlyphDisplayTest005
+ * @tc.desc: Test set family name, but still force tofu
+ * @tc.type: FUNC
+ */
+HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest005, TestSize.Level1)
+{
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_TOFU);
+    const char* fontFamilies[] = { "Noto Sans" };
+    OH_Drawing_Typography* typography = PrepareCreateTypography(text_, fontFamilies, 1);
+    BoundsResult(typography, tofuResult_, 3);
+    OH_Drawing_Typography* onlyTypography = PrepareCreateTypography(noGlyphText_, fontFamilies, 1);
+    BoundsResult(onlyTypography, noGlyphTofuResult_, 1);
+    OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_NO_GLYPH_USE_DEFAULT);
+    OH_Drawing_DestroyTypography(typography);
+    OH_Drawing_DestroyTypography(onlyTypography);
 }
 } // namespace Rosen
 } // namespace OHOS
