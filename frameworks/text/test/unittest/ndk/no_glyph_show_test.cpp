6ee6a1319ba301fdcd8f84d1bb5a13d7911561a8
liufei <liufei225@h-partners.com>
2025-05-27 15:12:53 +0800

    test(text): correct the bounds result for Noto Sans font
    - Update the expected bounds result for Noto Sans font in the undefined glyph display test
    - Fix the incorrect data in the notoResult array to match the expected behavior
    - Adjust the test case to use the corrected result array
    
    Signed-off-by: liufei <liufei225@h-partners.com>
    Change-Id: I8215ea92eb8cf3bed5bd47e8e140ac42d1433c63

--- frameworks/text/test/unittest/ndk/no_glyph_show_test.cpp
+++ frameworks/text/test/unittest/ndk/no_glyph_show_test.cpp
@@ -41,7 +41,7 @@ private:
     static constexpr float tofuResult_[][4] = { { 2.0, 2.0, 228.63979, 29.0 }, { 1.0, 5.0, 388.10962, 37.0 },
         { 8.0, 8.0, 341.4397, 42.0 } };
     static constexpr float notoResult_[][4] = { { 2.0, 2.0, 232.20976, 29.0 }, { 2.0, 5.0, 388.43961, 33.0 },
-        { 0, 8.0, 347.76962, 42.0 } };
+        { 8.0, 8.0, 347.76962, 42.0 } };
     static constexpr float noGlyphTofuResult_[][4] = { { 8.0, 0, 22.0, 22.0 } };
     static constexpr float noGlyphDefaultResult_[][4] = { { 0, 0, 0, 0 } };
     static constexpr float noGlyphNotoResult_[][4] = { { 0, 0, 0, 0 } };
@@ -173,7 +173,7 @@ HWTEST_F(NdkUndefinedGlyphDisplayTest, NdkUndefinedGlyphDisplayTest005, TestSize
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_TOFU);
     const char* fontFamilies[] = { "Noto Sans" };
     OH_Drawing_Typography* typography = PrepareCreateTypography(text_, fontFamilies, 1);
-    BoundsResult(typography, tofuResult_, 3);
+    BoundsResult(typography, notoResult_, 3);
     OH_Drawing_Typography* onlyTypography = PrepareCreateTypography(noGlyphText_, fontFamilies, 1);
     BoundsResult(onlyTypography, noGlyphTofuResult_, 1);
     OH_Drawing_SetTextUndefinedGlyphDisplay(TEXT_UNDEFINED_GLYPH_USE_DEFAULT);
