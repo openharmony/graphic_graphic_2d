82b52c7a73e4ce5be7fc8d01aed5b87aaf7504e7
jianghongxi <jianghongxi3@h-partners.com>
2025-06-03 03:23:03 +0000

    update rosen/modules/composer/hdi_backend/include/hdi_layer_info.h.
    Signed-off-by: jianghongxi <jianghongxi3@h-partners.com>

--- rosen/modules/composer/hdi_backend/include/hdi_layer_info.h
+++ rosen/modules/composer/hdi_backend/include/hdi_layer_info.h
@@ -650,17 +650,22 @@ public:
     {
         nodeId_ = nodeId;
     }
-
+    enum class AncoFlags : uint32_t {
+        IS_ANCO_NODE = 0x0001,
+        ANCO_SFV_NODE = 0x0011,
+        ANCO_NDK_NODE = 0X0111,
+        FORCE_REFRESH = 0x1000
+    };
     void SetAncoFlags(const uint32_t ancoFlags) { ancoFlags_ = ancoFlags; }
     uint32_t GetAncoFlags() const { return ancoFlags_; }
     bool IsAncoSfv() const
     {
-        constexpr uint32_t ANCO_SFV_NODE_FLAG = 0x0011;
+        constexpr uint32_t ANCO_SFV_NODE_FLAG = AncoFlags::ANCO_SFV_NODE;
         return (ancoFlags_ & ANCO_SFV_NODE_FLAG) == ANCO_SFV_NODE_FLAG;
     }
     bool IsAncoNDK() const
     {
-        constexpr uint32_t ANCO_NDK_NODE_FLAG = 0x0111;
+        constexpr uint32_t ANCO_NDK_NODE_FLAG = AncoFlags::ANCO_NDK_NODE;
         return (ancoFlags_ & ANCO_NDK_NODE_FLAG) == ANCO_NDK_NODE_FLAG;
     }
     /* hdiLayer get layer info end */
