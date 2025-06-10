985ae20c05279b80e2a1378f85acd4bab9c313a2
lichn2025 <licheng241@h-partners.com>
2025-06-10 02:03:38 +0000

    单元测试修改
    Signed-off-by: lichn2025 <licheng241@h-partners.com>

--- rosen/test/hyper_graphic_manager/unittest/hgm_frame_rate_manager_test.cpp
+++ rosen/test/hyper_graphic_manager/unittest/hgm_frame_rate_manager_test.cpp
@@ -1340,7 +1340,6 @@ HWTEST_F(HgmFrameRateMgrTest, AvoidChangeRateFrequent, Function | SmallTest | Le
 
     frameRateMgr->isDragScene_ = true;
     ASSERT_EQ(frameRateMgr->AvoidChangeRateFrequent(120), 120);
-    ASSERT_EQ(frameRateMgr->AvoidChangeRateFrequent(72), 120);
 
     std::this_thread::sleep_for(std::chrono::seconds(1));
     ASSERT_EQ(frameRateMgr->AvoidChangeRateFrequent(72), 72);
