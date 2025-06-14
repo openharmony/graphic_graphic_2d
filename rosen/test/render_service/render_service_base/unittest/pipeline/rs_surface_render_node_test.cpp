1ee7f2884de3ed7a4440dc4df27058566a60cdbd
alexcii <xuxiaobo5@huawei.com>
2025-06-14 13:33:17 +0800

    dvsync修复tdd
    Signed-off-by: alexcii <chuliangliang2@h-partners.com>
    Change-Id: I5d49145be376fe583417754d77311cbc2161e915

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_render_node_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_render_node_test.cpp
@@ -535,7 +535,7 @@ HWTEST_F(RSSurfaceRenderNodeTest, UpdateInfoForClonedNode, TestSize.Level1)
 
     surfaceRenderNode.UpdateInfoForClonedNode(surfaceRenderNode.clonedSourceNodeId_);
     auto surfaceParams = static_cast<RSSurfaceRenderParams*>(surfaceRenderNode.stagingRenderParams_.get());
-    ASSERT_FALSE(surfaceParams->GetNeedCacheSurface());
+    ASSERT_TRUE(surfaceParams->GetNeedCacheSurface());
 }
 
 /**
