a8868aee59889c734bea7978115279b72b0eca2b
ZY_encoder <zhouyong10@huawei.com>
2025-06-20 15:27:39 +0800

    fix tdd
    Signed-off-by: ZY_encoder <zhouyong10@huawei.com>

--- frameworks/text/test/unittest/adapter/skia_txt/text_line_base_test.cpp
+++ frameworks/text/test/unittest/adapter/skia_txt/text_line_base_test.cpp
@@ -72,7 +72,11 @@ HWTEST_F(OHDrawingTextLineBaseTest, OHDrawingTextLineBaseTest001, TestSize.Level
     EXPECT_EQ(textLine_.size(), 2);
     ASSERT_NE(textLine_.at(0), nullptr);
     EXPECT_EQ(textLine_.at(0)->GetGlyphCount(), textLine_.at(0)->GetGlyphRuns().at(0)->GetGlyphs().size());
+#ifdef USE_M133_SKIA
+    EXPECT_EQ(textLine_.at(0)->GetGlyphCount(), 5);
+#else
     EXPECT_EQ(textLine_.at(0)->GetGlyphCount(), 7);
+#endif
 }
 
 /*
@@ -85,7 +89,11 @@ HWTEST_F(OHDrawingTextLineBaseTest, OHDrawingTextLineBaseTest002, TestSize.Level
     EXPECT_EQ(textLine_.size(), 2);
     ASSERT_NE(textLine_.at(0), nullptr);
     EXPECT_EQ(textLine_.at(0)->GetTextRange().leftIndex, 0);
+#ifdef USE_M133_SKIA
+    EXPECT_EQ(textLine_.at(0)->GetTextRange().rightIndex, 5);
+#else
     EXPECT_EQ(textLine_.at(0)->GetTextRange().rightIndex, 7);
+#endif
 }
 
 /*
@@ -128,7 +136,11 @@ HWTEST_F(OHDrawingTextLineBaseTest, OHDrawingTextLineBaseTest005, TestSize.Level
     std::unique_ptr<TextLineBase> line
         = textLine_[0]->CreateTruncatedLine(10, OHOS::Rosen::EllipsisModal::HEAD, ellipsisStr);
     EXPECT_NE(line, nullptr);
+#ifdef USE_M133_SKIA
+    EXPECT_EQ(line->GetGlyphCount(), 5);
+#else
     EXPECT_EQ(line->GetGlyphCount(), 7);
+#endif
 
     line = textLine_[0]->CreateTruncatedLine(10, static_cast<OHOS::Rosen::EllipsisModal>(-1), ellipsisStr);
     EXPECT_EQ(line, nullptr);
@@ -136,7 +148,11 @@ HWTEST_F(OHDrawingTextLineBaseTest, OHDrawingTextLineBaseTest005, TestSize.Level
     double ascent = 0;
     double descent = 0;
     double leading = 0;
+#ifdef USE_M133_SKIA
+    EXPECT_FLOAT_EQ(textLine_[0]->GetTypographicBounds(&ascent, &descent, &leading), 36.693878);
+#else
     EXPECT_FLOAT_EQ(textLine_[0]->GetTypographicBounds(&ascent, &descent, &leading), 49.377823);
+#endif
     EXPECT_FLOAT_EQ(ascent, -12.992);
     EXPECT_FLOAT_EQ(descent, 3.4160001);
     EXPECT_FLOAT_EQ(leading, 0.000000);
