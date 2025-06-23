6e6d013bd9ce90836616269b4594053421b6bb2c
sunqizhen <sunqizhen2@huawei.com>
2025-06-23 21:00:15 +0800

    fix sys params bug
    Signed-off-by: sunqizhen <sunqizhen2@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/rs_system_properties.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/rs_system_properties.cpp
@@ -1507,8 +1507,9 @@ int32_t RSSystemProperties::GetHybridRenderSwitch(ComponentEnableSwitch bitSeq)
         ROSEN_LOGD("GetHybridRenderSwitch access to [%{public}s] is denied", VULKAN_CONFIG_FILE_PATH);
         return 0;
     }
-    static uint32_t hybridRenderFeatureSwitch = static_cast<uint32_t>(
-        std::atoll(system::GetParameter("const.graphics.hybridrenderfeatureswitch", "0x00").c_str()));
+    char* endPtr = nullptr;
+    static uint32_t hybridRenderFeatureSwitch =
+        std::strtoul(system::GetParameter("const.graphics.hybridrenderfeatureswitch", "0x00").c_str(), &endPtr, 16);
     static std::vector<int> hybridRenderSystemProperty(std::size(ComponentSwitchTable));
 
     if (bitSeq >= ComponentEnableSwitch::SWITCH_MAX) {
