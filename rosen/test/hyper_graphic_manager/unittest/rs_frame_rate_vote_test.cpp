bc2c6d2dc46bd801f97189eb5c06a4bb08c6ea96
徐华骏 <xuhuajun3@h-partners.com>
2025-07-08 10:56:25 +0000

    update rosen/test/hyper_graphic_manager/unittest/rs_frame_rate_vote_test.cpp.
    Signed-off-by: 徐华骏 <xuhuajun3@h-partners.com>

--- rosen/test/hyper_graphic_manager/unittest/rs_frame_rate_vote_test.cpp
+++ rosen/test/hyper_graphic_manager/unittest/rs_frame_rate_vote_test.cpp
@@ -161,12 +161,10 @@ HWTEST_F(RSFrameRateVoteTest, VideoFrameRateVote002, Function | SmallTest | Leve
         nullptr, nullptr);
     DelayedSingleton<RSFrameRateVote>::GetInstance()->surfaceVideoFrameRateVote_.insert(
         std::pair<uint64_t, std::shared_ptr<RSVideoFrameRateVote>>(1000, rsVideoFrameRateVote));
-
     DelayedSingleton<RSFrameRateVote>::GetInstance()->transactionFlags_ = "";
     DelayedSingleton<RSFrameRateVote>::GetInstance()->lastVotedPid_ = 1000;
     DelayedSingleton<RSFrameRateVote>::GetInstance()->CheckSurfaceAndUi(OHSurfaceSource::OH_SURFACE_SOURCE_DEFAULT);
     DelayedSingleton<RSFrameRateVote>::GetInstance()->lastVotedRate_ = 60;
-
     DelayedSingleton<RSFrameRateVote>::GetInstance()->ffrtQueue_ =
         std::make_shared<ffrt::queue>("frame_rate_vote_queue");
     DelayedSingleton<RSFrameRateVote>::GetInstance()->lastSurfaceNodeId_ = 500;
@@ -175,7 +173,6 @@ HWTEST_F(RSFrameRateVoteTest, VideoFrameRateVote002, Function | SmallTest | Leve
     usleep(500000);
     ASSERT_EQ(DelayedSingleton<RSFrameRateVote>::GetInstance()->lastVotedRate_, 0);
     ASSERT_EQ(DelayedSingleton<RSFrameRateVote>::GetInstance()->surfaceVideoFrameRateVote_.size(), 0);
-
     DelayedSingleton<RSFrameRateVote>::GetInstance()->surfaceVideoFrameRateVote_.insert(
         std::pair<uint64_t, std::shared_ptr<RSVideoFrameRateVote>>(1000, rsVideoFrameRateVote));
     DelayedSingleton<RSFrameRateVote>::GetInstance()->transactionFlags_ = "";
@@ -189,7 +186,6 @@ HWTEST_F(RSFrameRateVoteTest, VideoFrameRateVote002, Function | SmallTest | Leve
     usleep(500000);
     ASSERT_EQ(DelayedSingleton<RSFrameRateVote>::GetInstance()->lastVotedRate_, 60);
     ASSERT_EQ(DelayedSingleton<RSFrameRateVote>::GetInstance()->surfaceVideoFrameRateVote_.size(), 1);
-
     DelayedSingleton<RSFrameRateVote>::GetInstance()->transactionFlags_ = "";
     DelayedSingleton<RSFrameRateVote>::GetInstance()->lastVotedPid_ = 1000;
     DelayedSingleton<RSFrameRateVote>::GetInstance()->lastVotedRate_ = 60;
@@ -205,6 +201,7 @@ HWTEST_F(RSFrameRateVoteTest, VideoFrameRateVote002, Function | SmallTest | Leve
     ASSERT_EQ(DelayedSingleton<RSFrameRateVote>::GetInstance()->surfaceVideoFrameRateVote_.size(), 0);
     rsVideoFrameRateVote = nullptr;
 }
+
 /**
  * @tc.name: ReleaseSurfaceMap001
  * @tc.desc: Verify the result of ReleaseSurfaceMap function
