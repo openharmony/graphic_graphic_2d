1cbd95b03f32d8e97c98b80b8cd958110788b34c
李鹏30067401 <lipeng437@h-partners.com>
2025-07-12 15:38:33 +0800

    修改code check问题
    Signed-off-by: 李鹏30067401 <lipeng437@h-partners.com>

--- rosen/test/render/render/unittest/render/rs_opinc_cache_cache_test.cpp
+++ rosen/test/render/render/unittest/render/rs_opinc_cache_cache_test.cpp
@@ -40,7 +40,7 @@ void RSOpincCacheTest::TearDown() {}
 * @tc.name: OpincSetInAppStateEnd001
 * @tc.desc: test results of OpincSetInAppStateEnd
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, OpincSetInAppStateEnd001, TestSize.Level1)
 {
@@ -63,7 +63,7 @@ HWTEST_F(RSOpincCacheTest, OpincSetInAppStateEnd001, TestSize.Level1)
 * @tc.name: OpincSetInAppStateStart001
 * @tc.desc: test results of OpincSetInAppStateStart
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, OpincSetInAppStateStart001, TestSize.Level1)
 {
@@ -89,6 +89,14 @@ HWTEST_F(RSOpincCacheTest, OpincSetInAppStateStart001, TestSize.Level1)
     unchangeMarkInApp = false;
     opincCache.OpincSetInAppStateStart(unchangeMarkInApp);
     EXPECT_TRUE(opincCache.isUnchangeMarkInApp_);
+
+    renderNode.isSubTreeDirty_ = true;
+    opincCache.OpincQuickMarkStableNode(unchangeMarkInApp, unchangeMarkEnable, false);
+    EXPECT_TRUE(renderNode.isSubTreeDirty_);
+
+    unchangeMarkEnable = false;
+    opincCache.OpincQuickMarkStableNode(unchangeMarkInApp, unchangeMarkEnable, false);
+    EXPECT_TRUE(opincCache.isUnchangeMarkEnable_);
 }
  
 /**
@@ -151,7 +159,7 @@ HWTEST_F(RSOpincCacheTest, OpincQuickMarkStableNode002, TestSize.Level1)
 * @tc.name: OpincQuickMarkStableNode001
 * @tc.desc: test results of OpincQuickMarkStableNode
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, OpincQuickMarkStableNode001, TestSize.Level1)
 {
@@ -199,21 +207,13 @@ HWTEST_F(RSOpincCacheTest, OpincQuickMarkStableNode001, TestSize.Level1)
     opincCache.nodeCacheState_ = NodeCacheState::STATE_CHANGE;
     opincCache.OpincQuickMarkStableNode(unchangeMarkInApp, unchangeMarkEnable, false);
     EXPECT_TRUE(opincCache.isUnchangeMarkEnable_);
-
-    renderNode.isSubTreeDirty_ = true;
-    opincCache.OpincQuickMarkStableNode(unchangeMarkInApp, unchangeMarkEnable, false);
-    EXPECT_TRUE(renderNode.isSubTreeDirty_);
-
-    unchangeMarkEnable = false;
-    opincCache.OpincQuickMarkStableNode(unchangeMarkInApp, unchangeMarkEnable, false);
-    EXPECT_TRUE(opincCache.isUnchangeMarkEnable_);
 }
  
 /**
 * @tc.name: OpincUpdateRootFlag001
 * @tc.desc: test results of OpincUpdateRootFlag
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, OpincUpdateRootFlag001, TestSize.Level1)
 {
@@ -262,19 +262,13 @@ HWTEST_F(RSOpincCacheTest, OpincUpdateRootFlag001, TestSize.Level1)
     opincCache.isUnchangeMarkEnable_ = true;
     opincCache.OpincUpdateRootFlag(unchangeMarkEnable, true);
     EXPECT_TRUE(opincCache.isUnchangeMarkEnable_ == false);
-
-    opincCache.nodeCacheState_ = NodeCacheState::STATE_UNCHANGE;
-    opincCache.isSuggestOpincNode_ = true;
-    renderNode.stagingRenderParams_ = std::make_unique<RSRenderParams>(0);
-    opincCache.OpincUpdateRootFlag(unchangeMarkEnable, true);
-    EXPECT_TRUE(opincCache.IsOpincUnchangeState());
 }
  
 /**
 * @tc.name: IsMarkedRenderGroup001
 * @tc.desc: test results of IsMarkedRenderGroup
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, IsMarkedRenderGroup001, TestSize.Level1)
 {
@@ -287,13 +281,19 @@ HWTEST_F(RSOpincCacheTest, IsMarkedRenderGroup001, TestSize.Level1)
     opincCache.isOpincRootFlag_ = true;
     res = opincCache.IsMarkedRenderGroup(renderNode.GetNodeGroupType() > RSRenderNode::NodeGroupType::NONE);
     EXPECT_TRUE(res);
+
+    opincCache.nodeCacheState_ = NodeCacheState::STATE_UNCHANGE;
+    opincCache.isSuggestOpincNode_ = true;
+    renderNode.stagingRenderParams_ = std::make_unique<RSRenderParams>(0);
+    opincCache.OpincUpdateRootFlag(unchangeMarkEnable, true);
+    EXPECT_TRUE(opincCache.IsOpincUnchangeState());
 }
 
 /**
 * @tc.name: IsOpincUnchangeState001
 * @tc.desc: test results of IsOpincUnchangeState
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, IsOpincUnchangeState001, TestSize.Level1)
 {
@@ -315,7 +315,7 @@ HWTEST_F(RSOpincCacheTest, IsOpincUnchangeState001, TestSize.Level1)
   * @tc.name: OpincGetRootFlag001
   * @tc.desc: test results of OpincGetRootFlag
   * @tc.type: FUNC
-  * @tc.require: issueI9UX8W
+  * @tc.require: issueICLU7S
   */
  HWTEST_F(RSOpincCacheTest, OpincGetRootFlag001, TestSize.Level1)
  {
@@ -333,7 +333,7 @@ HWTEST_F(RSOpincCacheTest, IsOpincUnchangeState001, TestSize.Level1)
 * @tc.name: OpincForcePrepareSubTree001
 * @tc.desc: test results of OpincForcePrepareSubTree
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, OpincForcePrepareSubTree001, TestSize.Level1)
 {
@@ -386,7 +386,7 @@ HWTEST_F(RSOpincCacheTest, OpincForcePrepareSubTree001, TestSize.Level1)
 * @tc.name: MarkSuggestOpincNode001
 * @tc.desc: test results of MarkSuggestOpincNode
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, MarkSuggestOpincNode001, TestSize.Level1)
 {
@@ -400,7 +400,7 @@ HWTEST_F(RSOpincCacheTest, MarkSuggestOpincNode001, TestSize.Level1)
 * @tc.name: IsSuggestOpincNode001
 * @tc.desc: test results of IsSuggestOpincNode
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, IsSuggestOpincNode001, TestSize.Level1)
 {
@@ -410,13 +410,34 @@ HWTEST_F(RSOpincCacheTest, IsSuggestOpincNode001, TestSize.Level1)
 
     opincCache.isSuggestOpincNode_ = true;
     EXPECT_TRUE(opincCache.IsSuggestOpincNode());
+
+    opincCache.unchangeCountUpper_ = 200;
+    opincCache.SetCacheStateByRetrytime();
+    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
+
+    opincCache.unchangeCountUpper_ = 200;
+    opincCache.SetCacheStateByRetrytime();
+    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
+
+    opincCache.unchangeCountUpper_ = 200;
+    opincCache.SetCacheStateByRetrytime();
+    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
+
+    opincCache.tryCacheTimes_ = 4;
+    opincCache.unchangeCountUpper_ = 3;
+    opincCache.SetCacheStateByRetrytime();
+    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 23);
+
+    opincCache.unchangeCountUpper_ = 200;
+    opincCache.SetCacheStateByRetrytime();
+    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
 }
  
 /**
 * @tc.name: NodeCacheStateChange001
 * @tc.desc: test results of NodeCacheStateChange
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, NodeCacheStateChange001, TestSize.Level1)
 {
@@ -446,7 +467,7 @@ HWTEST_F(RSOpincCacheTest, NodeCacheStateChange001, TestSize.Level1)
 * @tc.name: SetCacheStateByRetrytime001
 * @tc.desc: test results of SetCacheStateByRetrytime
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, SetCacheStateByRetrytime001, TestSize.Level1)
 {
@@ -494,34 +515,13 @@ HWTEST_F(RSOpincCacheTest, SetCacheStateByRetrytime001, TestSize.Level1)
     opincCache.unchangeCountUpper_ = 3;
     opincCache.SetCacheStateByRetrytime();
     EXPECT_TRUE(opincCache.unchangeCountUpper_ == 23);
-
-    opincCache.unchangeCountUpper_ = 200;
-    opincCache.SetCacheStateByRetrytime();
-    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
-
-    opincCache.unchangeCountUpper_ = 200;
-    opincCache.SetCacheStateByRetrytime();
-    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
-
-    opincCache.unchangeCountUpper_ = 200;
-    opincCache.SetCacheStateByRetrytime();
-    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
-
-    opincCache.tryCacheTimes_ = 4;
-    opincCache.unchangeCountUpper_ = 3;
-    opincCache.SetCacheStateByRetrytime();
-    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 23);
-
-    opincCache.unchangeCountUpper_ = 200;
-    opincCache.SetCacheStateByRetrytime();
-    EXPECT_TRUE(opincCache.unchangeCountUpper_ == 200);
 }
  
 /**
 * @tc.name: NodeCacheStateReset001
 * @tc.desc: test results of NodeCacheStateReset
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, NodeCacheStateReset001, TestSize.Level1)
 {
@@ -564,7 +564,7 @@ HWTEST_F(RSOpincCacheTest, NodeCacheStateReset001, TestSize.Level1)
 * @tc.name: UpdateSubTreeSupportFlag
 * @tc.desc: test results of UpdateSubTreeSupportFlag
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, UpdateSubTreeSupportFlag, TestSize.Level1)
 {
@@ -635,7 +635,7 @@ HWTEST_F(RSOpincCacheTest, UpdateSubTreeSupportFlag, TestSize.Level1)
 * @tc.name: SetCurNodeTreeSupportFlag
 * @tc.desc: test results of SetCurNodeTreeSupportFlag
 * @tc.type: FUNC
-* @tc.require: issueI9UX8W
+* @tc.require: issueICLU7S
 */
 HWTEST_F(RSOpincCacheTest, SetCurNodeTreeSupportFlag, TestSize.Level1)
 {
