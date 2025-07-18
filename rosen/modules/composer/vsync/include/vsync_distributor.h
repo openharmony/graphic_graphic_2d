dfa9e809ba7512d19080896e818663726783a618
ANRANr <anran8@h-partners.com>
2025-07-18 11:02:06 +0800

    UI弹幕场景帧消费流程增强
    Signed-off-by: ANRANr <anran8@h-partners.com>

--- rosen/modules/composer/vsync/include/vsync_distributor.h
+++ rosen/modules/composer/vsync/include/vsync_distributor.h
@@ -161,7 +161,7 @@ public:
     VsyncError SetUiDvsyncSwitch(bool dvsyncSwitch, const sptr<VSyncConnection>& connection);
     VsyncError SetUiDvsyncConfig(int32_t bufferCount, bool delayEnable, bool nativeDelayEnable);
     int64_t GetUiCommandDelayTime();
-    int64_t GetRsDelayTime();
+    int64_t GetRsDelayTime(int32_t pid);
     void UpdatePendingReferenceTime(int64_t &timeStamp);
     void SetHardwareTaskNum(uint32_t num);
     int64_t GetVsyncCount();
