48801477e45f966c945e53512637c922ea4e6afe
ANRANr <anran8@h-partners.com>
2025-07-21 06:29:42 +0000

    update rosen/modules/composer/vsync/include/vsync_distributor.h.
    Signed-off-by: ANRANr <anran8@h-partners.com>

--- rosen/modules/composer/vsync/include/vsync_distributor.h
+++ rosen/modules/composer/vsync/include/vsync_distributor.h
@@ -161,6 +161,7 @@ public:
     VsyncError SetUiDvsyncSwitch(bool dvsyncSwitch, const sptr<VSyncConnection>& connection);
     VsyncError SetUiDvsyncConfig(int32_t bufferCount, bool delayEnable, bool nativeDelayEnable);
     int64_t GetUiCommandDelayTime();
+    // no input scene delay rs
     int64_t GetRsDelayTime(int32_t pid);
     void UpdatePendingReferenceTime(int64_t &timeStamp);
     void SetHardwareTaskNum(uint32_t num);
