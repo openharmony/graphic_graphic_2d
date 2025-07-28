2a5ca5b7dfafc252679c14f6e7ec3fb46627f7d3
xushengbin <xushengbin2@huawei.com>
2025-07-28 06:45:02 +0000

    差异整改
    Signed-off-by: xushengbin <xushengbin2@huawei.com>

--- rosen/test/render_frame_trace/unittest/render_frame_trace_test.cpp
+++ rosen/test/render_frame_trace/unittest/render_frame_trace_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,10 +15,8 @@
 
 #include <gtest/gtest.h>
 
-#define private public
 #include "render_frame_trace.h"
 #include "render_frame_trace_impl.h"
-#undef private
 
 namespace OHOS {
 namespace Rosen {
