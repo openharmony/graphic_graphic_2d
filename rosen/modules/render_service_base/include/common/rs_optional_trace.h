4eba2ec31fdfa2bd84001ccd8461256dbea487c5
李想 <lixiang542@h-partners.com>
2025-08-06 07:24:54 +0000

    update rosen/modules/render_service_base/include/common/rs_optional_trace.h.
    Signed-off-by: 李想 <lixiang542@h-partners.com>

--- rosen/modules/render_service_base/include/common/rs_optional_trace.h
+++ rosen/modules/render_service_base/include/common/rs_optional_trace.h
@@ -86,10 +86,11 @@ static inline int g_debugLevel = OHOS::Rosen::RSSystemProperties::GetDebugTraceL
 
 #define RS_PROCESS_TRACE(forceEnable, name) RSProcessTrace processTrace(forceEnable, name)
 
-#define RS_OPTIONAL_TRACE_FMT(fmt, ...)                                                                                 \
-    std::unique_ptr<RSOptionalFmtTrace> optionalFmtTrace = OHOS::Rosen::RSSystemProperties::GetDebugFmtTraceEnabled()   \
-                                                            ? std::make_unique<RSOptionalFmtTrace>(fmt, ##__VA_ARGS__)  \
-                                                            : nullptr;
+#define RS_OPTIONAL_TRACE_FMT(fmt, ...)                                 \
+    std::unique_ptr<RSOptionalFmtTrace> optionalFmtTrace =              \
+        OHOS::Rosen::RSSystemProperties::GetDebugFmtTraceEnabled() ?    \
+        std::make_unique<RSOptionalFmtTrace>(fmt, ##__VA_ARGS__) :      \
+        nullptr;
 
 class RenderTrace {
 public:
