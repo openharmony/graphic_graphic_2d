07045d4d20c33ba1bd15adc6a1d82475fed967ea
TetsuOu <wangzhe231@huawei.com>
2025-08-09 19:07:48 +0800

    优化trace点
    Signed-off-by: TetsuOu <wangzhe231@huawei.com>

--- rosen/modules/render_service_base/src/render/rs_motion_blur_filter.cpp
+++ rosen/modules/render_service_base/src/render/rs_motion_blur_filter.cpp
@@ -19,7 +19,6 @@
 #include "draw/surface.h"
 #include "platform/common/rs_log.h"
 #include "platform/common/rs_system_properties.h"
-#include "rs_trace.h"
 #ifdef USE_M133_SKIA
 #include "src/core/SkChecksum.h"
 #else
@@ -53,7 +52,7 @@ RSMotionBlurFilter::~RSMotionBlurFilter() = default;
 void RSMotionBlurFilter::DrawImageRect(Drawing::Canvas& canvas, const std::shared_ptr<Drawing::Image>& image,
     const Drawing::Rect& src, const Drawing::Rect& dst) const
 {
-    RS_TRACE_NAME("RSMotionBlurFilter::MotionBlur");
+    RS_OPTIONAL_TRACE_NAME("RSMotionBlurFilter::MotionBlur");
 
     if (!image || image->GetWidth() == 0 || image->GetHeight() == 0) {
         lastRect_ = Drawing::Rect(0.f, 0.f, 0.f, 0.f);
