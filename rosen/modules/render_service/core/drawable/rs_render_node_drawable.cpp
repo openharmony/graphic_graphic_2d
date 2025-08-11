a3f8d0faec7d465f44bb8f36770bf0a5d2adbdc7
jimliuzhenhuan <liuzhenhuan3@huawei.com>
2025-08-11 08:11:05 +0000

    update rosen/modules/render_service/core/drawable/rs_render_node_drawable.cpp. change %d into %llu
    Signed-off-by: jimliuzhenhuan <liuzhenhuan3@huawei.com>

--- rosen/modules/render_service/core/drawable/rs_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_render_node_drawable.cpp
@@ -817,7 +817,7 @@ void RSRenderNodeDrawable::DrawCachedImage(RSPaintFilterCanvas& canvas, const Ve
         return;
     }
     if (rsFilter != nullptr) {
-        RS_OPTIONAL_TRACE_FMT("RSRenderNodeDrawable::DrawCachedImage image width: %d, height: %d, %s, nodeID = %d",
+        RS_OPTIONAL_TRACE_FMT("RSRenderNodeDrawable::DrawCachedImage image width: %d, height: %d, %s, nodeID = %llu",
             cacheImage->GetWidth(), cacheImage->GetHeight(), rsFilter->GetDescription().c_str(), nodeId_);
         auto foregroundFilter = std::static_pointer_cast<RSDrawingFilterOriginal>(rsFilter);
         foregroundFilter->DrawImageRect(canvas, cacheImage, Drawing::Rect(0, 0, cacheImage->GetWidth(),
