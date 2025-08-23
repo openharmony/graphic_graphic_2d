b5c6bf68c154081358a2e9f81ef6ca8efaf33d73
PengKejie <pengkejie@h-partners.com>
2025-08-23 17:45:44 +0800

    add CreateTransferObj
    Signed-off-by: PengKejie <pengkejie@h-partners.com>

--- frameworks/text/interface/mlb/napi/paragraph_builder_napi/js_paragraph_builder.h
+++ frameworks/text/interface/mlb/napi/paragraph_builder_napi/js_paragraph_builder.h
@@ -34,6 +34,7 @@ public:
     static napi_status SetTypographyCreate(napi_env env, napi_value obj, std::shared_ptr<TypographyCreate> typographyCreate);
 
     static napi_value Init(napi_env env, napi_value exportObj);
+    static napi_value CreateTransferObj(napi_env env, napi_value exportObj);
     static napi_value Constructor(napi_env env, napi_callback_info info);
     static void Destructor(napi_env env, void* nativeObject, void* finalize);
     static napi_value PushStyle(napi_env env, napi_callback_info info);
@@ -53,6 +54,7 @@ private:
     napi_value OnBuildLineTypeset(napi_env env, napi_callback_info info);
     napi_value OnAppendSymbol(napi_env env, napi_callback_info info);
     static thread_local napi_ref constructor_;
+    static thread_local napi_ref noParamConstructor_;
     std::shared_ptr<TypographyCreate> typographyCreate_ = nullptr;
 };
 } // namespace OHOS::Rosen
