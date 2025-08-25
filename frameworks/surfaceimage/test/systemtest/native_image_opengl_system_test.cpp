fae5049c0e74cc0170b85cf22a2da65e21a04a54
刘瓒 <liuzan9@huawei.com>
2025-08-25 07:01:35 +0000

    修复test堆内存未delete问题
    Signed-off-by: 刘瓒 <liuzan9@huawei.com>
    Change-Id: Iacd09acd382d9dba21d58d863dd97bbd744979ad

--- frameworks/surfaceimage/test/systemtest/native_image_opengl_system_test.cpp
+++ frameworks/surfaceimage/test/systemtest/native_image_opengl_system_test.cpp
@@ -422,6 +422,8 @@ void ProducerThreadWithMatrix(OHNativeWindow* nativeWindow, int32_t matrixIndex)
     region->rects = rect;
     ret = OH_NativeWindow_NativeWindowRequestBuffer(nativeWindow, &nativeWindowBuffer, &fenceFd);
     if (ret != GSERROR_OK) {
+        delete rect;
+        delete region;
         return;
     }
     ret = OH_NativeWindow_NativeWindowFlushBuffer(nativeWindow, nativeWindowBuffer, fenceFd, *region);
