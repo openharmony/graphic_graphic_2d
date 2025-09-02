8e5855f23bb09e51a0d221bb7dd8c20caab48f87
gao_yufan <gaoyufan2@huawei.com>
2025-09-02 20:24:45 +0800

    60releaseDivideFix
    Signed-off-by: gao_yufan <gaoyufan2@huawei.com>
    Change-Id: I8822044fc4c4ab79fcb90bbd173125dd58333abf

--- rosen/test/render_service/render_service_client/unittest/feature/ui_capture/rs_divided_ui_capture_test.cpp
+++ rosen/test/render_service/render_service_client/unittest/feature/ui_capture/rs_divided_ui_capture_test.cpp
@@ -15,9 +15,12 @@
 
 #include "gtest/gtest.h"
 
+#include "feature/capture/rs_capture_pixelmap_manager.h"
 #include "feature/ui_capture/rs_divided_ui_capture.h"
 #include "pipeline/rs_render_thread.h"
 #include "pipeline/rs_root_render_node.h"
+#include "render/rs_image_cache.h"
+#include "ui/rs_canvas_node.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -116,6 +119,180 @@ HWTEST_F(RSDividedUICaptureTest, TakeLocalCapture002, TestSize.Level1)
     rsDividedUICapture.nodeId_ = 0;
     auto pixelmap = rsDividedUICapture.TakeLocalCapture();
     EXPECT_TRUE(pixelmap == nullptr);
+}
+
+/**
+ * @tc.name: TakeLocalCapture003
+ * @tc.desc: test TakeLocalCapture FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, TakeLocalCapture003, TestSize.Level1)
+{
+    float scaleX = 1.0;
+    float scaleY = 1.0;
+    auto rsNode = RSCanvasNode::Create();
+    NodeId nodeId = rsNode->GetId();
+    rsNode->SetBounds(10, 10, 600, 1000);
+    rsNode->SetFrame(10, 10, 600, 1000);
+    rsNode->SetBackgroundColor(SK_ColorYELLOW);
+    rsNode->SetIsOnTheTree(true);
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+    RSDividedUICapture rsDividedUICapture(nodeId, scaleX, scaleY);
+    auto pixelmap = rsDividedUICapture.TakeLocalCapture();
+    EXPECT_TRUE(pixelmap == nullptr);
+    rsNode = nullptr;
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+}
+
+/**
+ * @tc.name: TakeLocalCapture004
+ * @tc.desc: test TakeLocalCapture FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, TakeLocalCapture004, TestSize.Level1)
+{
+    float scaleX = 1.0;
+    float scaleY = 1.0;
+    auto rsNode = RSCanvasNode::Create();
+    NodeId nodeId = rsNode->GetId();
+    rsNode->SetBounds(10, 10, 600, 1000);
+    rsNode->SetFrame(10, 10, 600, 1000);
+    rsNode->SetBackgroundColor(SK_ColorYELLOW);
+    rsNode->SetIsOnTheTree(false);
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+    RSDividedUICapture rsDividedUICapture(nodeId, scaleX, scaleY);
+    auto pixelmap = rsDividedUICapture.TakeLocalCapture();
+    EXPECT_TRUE(pixelmap == nullptr);
+    rsNode = nullptr;
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+}
+
+/**
+ * @tc.name: TakeLocalCapture005
+ * @tc.desc: test TakeLocalCapture FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, TakeLocalCapture005, TestSize.Level1)
+{
+    float scaleX = 1.0;
+    float scaleY = 1.0;
+    auto rsNode = RSCanvasNode::Create();
+    NodeId nodeId = rsNode->GetId();
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+    RSDividedUICapture rsDividedUICapture(nodeId, scaleX, scaleY);
+    auto pixelmap = rsDividedUICapture.TakeLocalCapture();
+    EXPECT_TRUE(pixelmap == nullptr);
+    rsNode = nullptr;
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+}
+
+/**
+ * @tc.name: CopyDataToPixelMap001
+ * @tc.desc: test CopyDataToPixelMap FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, CopyDataToPixelMap001, TestSize.Level1)
+{
+    std::shared_ptr<Drawing::Image> img;
+    std::shared_ptr<Media::PixelMap> pixelMap;
+    RSSurfaceCaptureConfig captureConfig;
+    Drawing::Rect rect = {0, 0, 1260, 2720};
+    bool ret = CopyDataToPixelMap(img, pixelMap);
+    EXPECT_EQ(ret, false);
+    auto pixelMap0 = RSCapturePixelMapManager::CreatePixelMap(rect, captureConfig);
+    std::shared_ptr<Media::PixelMap> pixelMap1 = std::move(pixelMap0);
+    EXPECT_EQ(pixelMap1 != nullptr, true);
+    ret = CopyDataToPixelMap(img, pixelMap1);
+    EXPECT_EQ(ret, false);
+}
+
+/**
+ * @tc.name: CopyDataToPixelMap002
+ * @tc.desc: test CopyDataToPixelMap FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, CopyDataToPixelMap002, TestSize.Level1)
+{
+    float scaleX = 1.0;
+    float scaleY = 1.0;
+    auto rsNode = RSCanvasNode::Create();
+    NodeId nodeId = rsNode->GetId();
+    RSTransactionProxy::GetInstance()->FlushImplicitTransaction();
+    sleep(1);
+    RSDividedUICapture rsDividedUICapture(nodeId, scaleX, scaleY);
+    auto img = std::make_shared<Drawing::Image>();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    EXPECT_FALSE(CopyDataToPixelMap(img, pixelMap));
+
+    Media::ImageInfo imageInfo;
+    Media::Size infoSize;
+    infoSize.width = 1;
+    infoSize.height = 1;
+    imageInfo.size = infoSize;
+    imageInfo.pixelFormat = Media::PixelFormat::RGBA_8888;
+    pixelMap->SetImageInfo(imageInfo, true);
+    EXPECT_FALSE(CopyDataToPixelMap(img, pixelMap));
+}
+
+/**
+ * @tc.name: CopyDataToPixelMap003
+ * @tc.desc: test CopyDataToPixelMap FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, CopyDataToPixelMap003, TestSize.Level1)
+{
+    auto img = std::make_shared<Drawing::Image>();
+    std::shared_ptr<Media::PixelMap> pixelMap;
+    RSSurfaceCaptureConfig captureConfig;
+    Drawing::Rect rect = {0, 0, 1260, 2720};
+    bool ret = CopyDataToPixelMap(img, pixelMap);
+    EXPECT_EQ(ret, false);
+    auto pixelMap0 = RSCapturePixelMapManager::CreatePixelMap(rect, captureConfig);
+    std::shared_ptr<Media::PixelMap> pixelMap1 = std::move(pixelMap0);
+    Media::ImageInfo imageInfo;
+    Media::Size infoSize;
+    infoSize.width = 1;
+    infoSize.height = 1;
+    imageInfo.size = infoSize;
+    imageInfo.pixelFormat = Media::PixelFormat::RGBA_8888;
+    pixelMap1->SetImageInfo(imageInfo, true);
+    EXPECT_EQ(pixelMap1 != nullptr, true);
+    ret = CopyDataToPixelMap(img, pixelMap1);
+    EXPECT_EQ(ret, false);
+}
+
+/**
+ * @tc.name: CopyDataToPixelMap004
+ * @tc.desc: test CopyDataToPixelMap FUNC
+ * @tc.type: FUNC
+ * @tc.require: issueICVNI4
+ */
+HWTEST_F(RSDividedUICaptureTest, CopyDataToPixelMap004, TestSize.Level1)
+{
+    RSImageCache& imageCache = RSImageCache::Instance();
+    auto pixelMap = std::make_shared<Media::PixelMap>();
+    auto img = std::make_shared<Drawing::Image>();
+    imageCache.pixelMapCache_.emplace(1, std::make_pair(pixelMap, 1));
+    imageCache.CacheDrawingImage(1, img);
+    pixelMap->allocatorType_ = Media::AllocatorType::HEAP_ALLOC;
+    imageCache.CheckRefCntAndReleaseImageCache(1, pixelMap);
+    EXPECT_FALSE(imageCache.drawingImageCache_.empty());
+    imageCache.drawingImageCache_.clear();
+
+    bool ret = CopyDataToPixelMap(img, pixelMap);
+    EXPECT_EQ(ret, false);
 }
 
 /**
