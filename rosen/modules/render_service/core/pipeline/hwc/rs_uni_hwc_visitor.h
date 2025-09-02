b67cc863730cd6a765c0bd536252e2a851603cfc
徐华骏 <xuhuajun3@h-partners.com>
2025-09-02 03:57:13 +0000

    update rosen/modules/render_service/core/pipeline/hwc/rs_uni_hwc_visitor.h.
    Signed-off-by: 徐华骏 <xuhuajun3@h-partners.com>

--- rosen/modules/render_service/core/pipeline/hwc/rs_uni_hwc_visitor.h
+++ rosen/modules/render_service/core/pipeline/hwc/rs_uni_hwc_visitor.h
@@ -96,7 +96,7 @@ public:
 
     void IncreaseSolidLayerHwcEnableCount() { solidLayerHwcEnableCount_++; }
     size_t GetSolidLayerHwcEnableCount() const { return solidLayerHwcEnableCount_; }
-    bool IsTargetedSolidLayer(RSSurfaceRenderNode& node);
+    bool IsTargetSolidLayer(RSSurfaceRenderNode& node);
 
 private:
     friend class RSUniRenderVisitor;
