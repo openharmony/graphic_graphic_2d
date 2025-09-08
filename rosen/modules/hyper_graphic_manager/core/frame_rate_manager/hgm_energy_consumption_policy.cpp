b56541f7aa7fef9c74bb080e328b186e890299b7
徐华骏 <xuhuajun3@h-partners.com>
2025-09-08 02:00:41 +0000

    update rosen/modules/hyper_graphic_manager/core/hgm_energy_consumption_policy.cpp.
    Signed-off-by: 徐华骏 <xuhuajun3@h-partners.com>

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_energy_consumption_policy.cpp
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_energy_consumption_policy.cpp
@@ -385,7 +385,8 @@ void HgmEnergyConsumptionPolicy::SetCurrentPkgName(const std::vector<std::string
     std::string videoCallLayerNameStr = "";
     for (const auto& pkg: pkgs) {
         std::string pkgName = pkg.substr(0, pkg.find(":"));
-        if (videoCallLayerName_ == "" && (const auto& videoCallLayerName = videoCallLayerConfig.find(pkgName); videoCallLayerName != videoCallLayerConfig.end())) {
+        if (videoCallLayerName_ == "" &&
+            (const auto& videoCallLayerName = videoCallLayerConfig.find(pkgName); videoCallLayerName != videoCallLayerConfig.end())) {
             videoCallLayerNameStr = videoCallLayerName->second;
         }
         bool isVideoApp = videoFrameRateList.find(pkgName) != videoFrameRateList.end();
