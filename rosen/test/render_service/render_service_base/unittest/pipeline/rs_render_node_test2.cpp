ce04d341b49824f5b1d158e771d47e5b4fe8131f
ZhangLitao <zhanglitao11@h-partners.com>
2025-09-16 20:37:23 +0800

    增加维测
    Signed-off-by: ZhangLitao <zhanglitao11@h-partners.com>

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_test2.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_render_node_test2.cpp
@@ -1287,7 +1287,7 @@ HWTEST_F(RSRenderNodeTest2, DumpSubClassNodeTest032, TestSize.Level1)
     std::string outTest7;
     auto canvasDrawingNode = std::make_shared<RSCanvasDrawingRenderNode>(1);
     canvasDrawingNode->DumpSubClassNode(outTest7);
-    EXPECT_EQ(outTest7, ", lastResetSurfaceTime_: 0, opCountAfterReset_: 0");
+    EXPECT_EQ(outTest7, ", lastResetSurfaceTime: 0, opCountAfterReset: 0");
 }
 
 /**
