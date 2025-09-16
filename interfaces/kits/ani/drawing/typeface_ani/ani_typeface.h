b1d6e10ee797dd619e6b8cde39125588d12034cb
dreamyhhh <hangmengxin@huawei.com>
2025-09-16 17:43:55 +0800

    add typeface&tool ani impl
    Signed-off-by: dreamyhhh <hangmengxin@huawei.com>
    Change-Id: Id80f0afd0b28852a7143f396052ce5091983827a

--- interfaces/kits/ani/drawing/typeface_ani/ani_typeface.h
+++ interfaces/kits/ani/drawing/typeface_ani/ani_typeface.h
@@ -35,6 +35,7 @@ public:
     static void Constructor(ani_env* env, ani_object obj);
     static ani_string GetFamilyName(ani_env* env, ani_object obj);
     static ani_object MakeFromFile(ani_env* env, ani_object obj, ani_string filePath);
+    static ani_object MakeFromRawFile(ani_env* env, ani_object obj, ani_object resource);
     static ani_object MakeFromFileWithArguments(ani_env* env, ani_object obj, ani_string filePath,
         ani_object argumentsObj);
 
