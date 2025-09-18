a9ebc873cb5ab5732a60802fc5e1598460dd1651
zhangqiang <zhangqiang349@huawei.com>
2025-09-18 16:19:59 +0800

    & 紧挨类型
    Signed-off-by: zhangqiang <zhangqiang349@huawei.com>

--- rosen/modules/render_service_base/include/pipeline/rs_render_node_gc.h
+++ rosen/modules/render_service_base/include/pipeline/rs_render_node_gc.h
@@ -89,8 +89,8 @@ public:
 
 private:
     GCLevel JudgeGCLevel(uint32_t remainBucketSize);
-    void ReleaseOffTreeNodeForBucket(const RSThresholdDetector<uint32_t>::DetectCallBack &callBack);
-    void ReleaseOffTreeNodeForBucketMap(const RSThresholdDetector<uint32_t>::DetectCallBack &callBack);
+    void ReleaseOffTreeNodeForBucket(const RSThresholdDetector<uint32_t>::DetectCallBack& callBack);
+    void ReleaseOffTreeNodeForBucketMap(const RSThresholdDetector<uint32_t>::DetectCallBack& callBack);
     void AddNodeToBucket(RSRenderNode* ptr);
 
     std::atomic<bool> isEnable_ = true;
