1b437479aca94893864426dba65f42bec215c279
zhangqiang <zhangqiang349@huawei.com>
2025-10-16 15:48:33 +0800

    符合一行不超过120个字符的编码规范
    Signed-off-by: zhangqiang <zhangqiang349@huawei.com>

--- rosen/modules/render_service_base/src/pipeline/rs_draw_cmd.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_draw_cmd.cpp
@@ -286,7 +286,8 @@ void RSExtendImageObject::PreProcessPixelMap(Drawing::Canvas& canvas, const std:
 #endif
 #if defined(RS_ENABLE_VK)
         if (RSSystemProperties::IsUseVukan() &&
-            GetRsImageCache(canvas, pixelMap, reinterpret_cast<SurfaceBuffer*>(pixelMap->GetFd()), sampling, colorSpace)) {
+            GetRsImageCache(canvas, pixelMap, reinterpret_cast<SurfaceBuffer*>(pixelMap->GetFd()),
+                            sampling, colorSpace)) {
             rsImage_->SetDmaImage(image_);
         }
 #endif
