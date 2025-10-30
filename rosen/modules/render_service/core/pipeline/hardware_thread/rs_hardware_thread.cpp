233a22d284cdfa37786c43d24b39387b850353e0
klein_8783 <kechen8783@foxmail.com>
2025-10-30 20:48:40 +0800

    ltpo code using currentId
    Signed-off-by: klein_8783 <kechen8783@foxmail.com>

--- rosen/modules/render_service/core/pipeline/hardware_thread/rs_hardware_thread.cpp
+++ rosen/modules/render_service/core/pipeline/hardware_thread/rs_hardware_thread.cpp
@@ -264,7 +264,8 @@ void RSHardwareThread::CommitAndReleaseLayers(OutputPtr output, const std::vecto
     ResschedEventListener::GetInstance()->ReportFrameToRSS();
 #endif
     RSTaskMessage::RSTask task = [this, output = output, layers = layers, param = param,
-        currentRate = currentRate, hasGameScene = hasGameScene]() {
+        currentRate = currentRate, hasGameScene = hasGameScene,
+        currentId = HgmCore::Instance().GetActiveScreenId()]() {
         PrintHiperfSurfaceLog("counter3", static_cast<uint64_t>(layers.size()));
         int64_t startTime = GetCurTimeCount();
         std::string surfaceName = GetSurfaceNameInLayers(layers);
@@ -309,7 +310,7 @@ void RSHardwareThread::CommitAndReleaseLayers(OutputPtr output, const std::vecto
 
         if (RSSystemProperties::IsSuperFoldDisplay() && output->GetScreenId() == 0) {
             std::vector<LayerInfoPtr> reviseLayers = layers;
-            ChangeLayersForActiveRectOutside(reviseLayers, HgmCore::Instance().GetActiveScreenId());
+            ChangeLayersForActiveRectOutside(reviseLayers, currentId);
             LppVideoHandler::Instance().AddLppLayerId(reviseLayers);
             output->SetLayerInfo(reviseLayers);
         } else {
