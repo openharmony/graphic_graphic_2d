4755b80e78dd96ed13ac658499e7599852eb6821
lishijie77 <lishijie22@huawei.com>
2025-11-06 19:24:58 +0800

    修复tdd
    Signed-off-by: lishijie77 <lishijie22@huawei.com>

--- rosen/test/render_service/render_service_client/unittest/ui/rs_surface_node_test.cpp
+++ rosen/test/render_service/render_service_client/unittest/ui/rs_surface_node_test.cpp
@@ -133,6 +133,7 @@ HWTEST_F(RSSurfaceNodeTest, CreateShadowSurfaceNode, TestSize.Level1)
         RSSurfaceNode::SharedPtr shadowNode = surfaceNode->CreateShadowSurfaceNode();
         EXPECT_TRUE(shadowNode->isShadowNode_);
     }
+#ifdef RS_ENABLE_UNI_RENDER
     {
         RSSurfaceNode::SharedPtr shadowNode2 = surfaceNode->CreateShadowSurfaceNode();
         auto rsUIContext = shadowNode2->GetRSUIContext();
@@ -140,6 +141,7 @@ HWTEST_F(RSSurfaceNodeTest, CreateShadowSurfaceNode, TestSize.Level1)
         ASSERT_TRUE(shadowNode2->isShadowNode_);
         RSUIContextManager::MutableInstance().DestroyContext(rsUIContext->GetToken());
     }
+#endif
 }
 
 /**
