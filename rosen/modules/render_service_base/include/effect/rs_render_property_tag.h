063418f20c44fc366cfdf49c24c005780b18ccb8
zelunwu <wuzelun@huawei.com>
2025-11-11 23:00:21 +0800

    update: 更新文件 rs_render_property_tag.h
    Signed-off-by: zelunwu <wuzelun@huawei.com>

--- rosen/modules/render_service_base/include/effect/rs_render_property_tag.h
+++ rosen/modules/render_service_base/include/effect/rs_render_property_tag.h
@@ -83,7 +83,8 @@ enum class RSNGEffectType : int16_t {
     ALWAYS_SNAPSHOT,
     MASK_TRANSITION,
     VARIABLE_RADIUS_BLUR,
-    CONTENT_LIGHT,
+    CONTENT_LIGHT,    
+    FROSTED_GLASS,
     // mask type
     RIPPLE_MASK,
     DOUBLE_RIPPLE_MASK,
