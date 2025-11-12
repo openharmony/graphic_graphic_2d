bc2569cc1c78e9f98cc1296d9731b2d67ba43bce
zhangqiang <zhangqiang349@huawei.com>
2025-11-12 15:25:40 +0800

    功能代码
    Signed-off-by: zhangqiang <zhangqiang349@huawei.com>

--- rosen/modules/render_service_base/include/pipeline/rs_canvas_drawing_render_node.h
+++ rosen/modules/render_service_base/include/pipeline/rs_canvas_drawing_render_node.h
@@ -112,6 +112,18 @@ private:
     int64_t lastResetSurfaceTime_ = 0;
     size_t opCountAfterReset_ = 0;
 
+    struct CachedReversedOpInfo {
+        std::vector<uint32_t> drawOpTypes;
+        int32_t width = 0;
+        int32_t height = 0;
+        int32_t opItemSize = 0;
+        CachedReversedOpInfo() : drawOpTypes{}, width(0), height(0), opItemSize(0) {}
+    };
+    std::deque<CachedReversedOpInfo> cachedReversedOpTypes_;
+    virtual void DumpSubClassNode(std::string& out) const override;
+    void GetDrawOpItemInfo(const Drawing::DrawCmdListPtr& drawCmdList, size_t opItemSize);
+
+
     friend class RSCanvasDrawingNodeCommandHelper;
     friend class RSRenderNode;
 };
