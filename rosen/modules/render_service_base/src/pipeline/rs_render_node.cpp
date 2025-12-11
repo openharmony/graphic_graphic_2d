2772e2b39b3eb8c2f160671a0822b5d97454f129
ahyls <yanglusheng@h-partners.com>
2025-12-11 07:57:23 +0000

    解决遮挡期间三键显示异常
    Signed-off-by: ahyls <yanglusheng@h-partners.com>
    Change-Id: Iad202d45a5cffb920a6762883acf36b8140a37da

--- rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
@@ -2623,10 +2623,13 @@ void RSRenderNode::RecordCurDirtyStatus()
 void RSRenderNode::AccumulateDirtyStatus()
 {
     GetMutableRenderProperties().AccumulateDirtyStatus();
-    if (curDirtyStatus_ == NodeDirty::CLEAN) {
+    if (curDirtyStatus_ != NodeDirty::CLEAN) {
+        SetDirty();
         return;
     }
-    SetDirty();
+    if (isSubTreeDirty_) {
+        SetParentSubTreeDirty();
+    }
 }
 
 void RSRenderNode::ResetAccumulateDirtyStatus()
