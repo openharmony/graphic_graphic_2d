a03574ba32348a21ccc5ab2309c595acf80fbd9b
saitoasuka_lh <luhan22@huawei.com>
2025-12-17 22:22:29 +0800

    fix GenerateFilterDirtyRegionInfo_001
    Signed-off-by: saitoasuka_lh <luhan22@huawei.com>

--- rosen/test/render_service/render_service/unittest/feature/dirty/rs_uni_dirty_compute_util_test.cpp
+++ rosen/test/render_service/render_service/unittest/feature/dirty/rs_uni_dirty_compute_util_test.cpp
@@ -469,7 +469,7 @@ HWTEST_F(RSUniDirtyComputeUtilTest, GenerateFilterDirtyRegionInfo_001, TestSize.
     FilterDirtyRegionInfo filterInfo =
         RSUniFilterDirtyComputeUtil::GenerateFilterDirtyRegionInfo(*effectNode, std::nullopt, true);
     ASSERT_TRUE(filterInfo.intersectRegion_.Sub(Occlusion::Region(Occlusion::Rect(DEFAULT_RECT2))).IsEmpty());
-    ASSERT_FALSE(filterInfo.filterDirty_.Area() == filterInfo.intersectRegion_.Area());
+    ASSERT_TRUE(filterInfo.filterDirty_.Area() == filterInfo.intersectRegion_.Area());
 }
 
 /**
