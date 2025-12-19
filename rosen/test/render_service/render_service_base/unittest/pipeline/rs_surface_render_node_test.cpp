9b73e705b53505a7ad59eec612c4b4ae4d022cd8
VicZhang1 <zhangshixiong7@h-partners.com>
2025-12-19 09:26:49 +0800

    include missed header file
    Signed-off-by: VicZhang1 <zhangshixiong7@h-partners.com>

--- rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_render_node_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/pipeline/rs_surface_render_node_test.cpp
@@ -29,6 +29,7 @@
 #include "params/rs_surface_render_params.h"
 #include "render_thread/rs_render_thread_visitor.h"
 #include "pipeline/rs_effect_render_node.h"
+#include "pipeline/rs_screen_render_node.h"
 #include "pipeline/rs_surface_render_node.h"
 #include "pipeline/rs_root_render_node.h"
 
