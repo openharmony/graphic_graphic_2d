3d763bff8eeb1ab3a332f284067e5aa9976fed27
zhengxingshen <zhengxingshen@huawei.com>
2025-12-20 18:30:23 +0800

    update: 更新文件 custom_draw_test.cpp
    Signed-off-by: zhengxingshen <zhengxingshen@huawei.com>

--- graphic_test/graphic_test/rs_display_effect/content_display/custom_draw_test.cpp
+++ graphic_test/graphic_test/rs_display_effect/content_display/custom_draw_test.cpp
@@ -56,7 +56,8 @@ GRAPHIC_TEST(CustomDrawTest, CONTENT_DISPLAY_TEST, DrawOnNode_Test)
     auto testNode1 = RSCanvasNode::Create(true);
     auto node = std::static_pointer_cast<RSNode>(testNode1);
     setNode(node, { 0, 0, width, height }, Vector4<Color>(Color(0, 255, 0)));
-    testNode1->DrawOnNode(ModifierNG::RSModifierType::CONTENT_STYLE, [testNode1](std::shared_ptr<Drawing::Canvas> canvas) {
+    testNode1->DrawOnNode(ModifierNG::RSModifierType::CONTENT_STYLE,
+        [testNode1](std::shared_ptr<Drawing::Canvas> canvas) {
         Drawing::Brush brush;
         brush.SetColor(Drawing::Color::COLOR_CYAN);
         canvas->AttachBrush(brush);
