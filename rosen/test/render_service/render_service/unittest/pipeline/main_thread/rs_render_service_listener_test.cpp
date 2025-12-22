1637135203e15a52e190341cd60fce894a6e37a5
zhou_eleven <zhouguoqing12@h-partners.com>
2025-12-22 15:45:54 +0800

    update: 更新文件 rs_render_service_listener_test.cpp
    Signed-off-by: zhou_eleven <zhouguoqing12@h-partners.com>

--- rosen/test/render_service/render_service/unittest/pipeline/main_thread/rs_render_service_listener_test.cpp
+++ rosen/test/render_service/render_service/unittest/pipeline/main_thread/rs_render_service_listener_test.cpp
@@ -20,6 +20,7 @@
 #include "pipeline/main_thread/rs_render_service_listener.h"
 #include "pipeline/rs_surface_render_node.h"
 #include "pipeline/rs_test_util.h"
+#include "hgm_core.h"
 #include "pipeline/main_thread/rs_main_thread.h"
 
 using namespace testing;
