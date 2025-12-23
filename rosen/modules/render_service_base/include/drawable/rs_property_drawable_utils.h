6e953e3c6026698c0196dafe15030e8935282be2
zhoutianer <zhoutianer@huawei.com>
2025-12-23 19:24:34 +0800

    ColorPickerManager结构整改 + 统一反色
    Co-authored-by: Tianer Zhou<zhoutianer@huawei.com>

--- rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h
+++ rosen/modules/render_service_base/include/drawable/rs_property_drawable_utils.h
@@ -65,7 +65,7 @@ public:
     static bool PickColorSyn(Drawing::Canvas* canvas, Drawing::Path& drPath, Drawing::Matrix& matrix,
         RSColor& colorPicked, const int& colorStrategy);
     static bool PickColor(std::shared_ptr<Drawing::GPUContext> context, std::shared_ptr<Drawing::Image> image,
-        Drawing::ColorQuad& colorPicked, ColorPickStrategyType strategy, bool prevDark);
+        Drawing::ColorQuad& colorPicked);
     static std::shared_ptr<Drawing::Image> GpuScaleImage(std::shared_ptr<Drawing::GPUContext> context,
         std::shared_ptr<Drawing::Image> image);
     static void GetDarkColor(RSColor& color);
