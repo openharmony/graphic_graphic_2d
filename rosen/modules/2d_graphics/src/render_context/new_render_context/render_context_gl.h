60646f599eaf15e4d89afba24a6770fb1b88309b
liuwei <liuwei793@h-partners.com>
2025-12-25 18:51:42 +0800

    添加原子变量，保证多线程
    Change-Id: I03457c1db07daa49d5296719bd61e7e6a4f81ed4
    Signed-off-by: liuwei <liuwei793@h-partners.com>

--- rosen/modules/2d_graphics/src/render_context/new_render_context/render_context_gl.h
+++ rosen/modules/2d_graphics/src/render_context/new_render_context/render_context_gl.h
@@ -75,6 +75,7 @@ public:
     bool UpdateStorageSizeIfNecessary();
     bool ResourceMakeCurrent();
     void AddSurface();
+    void DeleteSurface();
     void DestroySharedSource();
     void SetCleanUpHelper(std::function<void()> func);
     static const EGLContext GetResourceContext();
@@ -91,7 +92,7 @@ protected:
     uint32_t colorbuffer_ = 0;
     int32_t storage_width_ = 0;
     int32_t storage_height_ = 0;
-    int32_t surface_count_ = 0;
+    std::atomic<int32_t> surface_count_ = 0;
     bool valid_ = false;
     std::function<void()> cleanUpHelper_ = nullptr;
 #endif
