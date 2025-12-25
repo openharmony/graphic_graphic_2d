59ae184f0f37dd05cca274561d79bb21dcf24c83
liuwei <liuwei793@h-partners.com>
2025-12-25 09:41:30 +0800

    修复iOS中iosurface退出不释放的问题
    Change-Id: I940b81f7f0b1d514ae3c62210adf1e8f788a5543
    Signed-off-by: liuwei <liuwei793@h-partners.com>

--- rosen/modules/2d_graphics/src/render_context/render_context.h
+++ rosen/modules/2d_graphics/src/render_context/render_context.h
@@ -143,6 +143,9 @@ public:
     std::shared_ptr<Drawing::ColorSpace> ColorSpace() const { return color_space_; }
     bool UpdateStorageSizeIfNecessary();
     bool ResourceMakeCurrent();
+    void AddSurface();
+    void DestroySharedSource();
+    void SetCleanUpHelper(std::function<void()> func);
     static const EGLContext GetResourceContext();
 #endif
     static std::shared_ptr<Drawing::ColorSpace> ConvertColorGamutToColorSpace(GraphicColorGamut colorGamut);
@@ -167,7 +170,9 @@ protected:
     uint32_t colorbuffer_ = 0;
     int32_t storage_width_ = 0;
     int32_t storage_height_ = 0;
+    int32_t surface_count_ = 0;
     bool valid_ = false;
+    std::function<void()> cleanUpHelper_ = nullptr;
 #endif
     EGLConfig config_;
     GraphicColorGamut colorSpace_ = GraphicColorGamut::GRAPHIC_COLOR_GAMUT_SRGB;
