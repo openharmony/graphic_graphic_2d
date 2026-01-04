e5aa6942f7d239ad9509f5dcab812f643f5bce4d
kelinyang <yangxiaopeng8@huawei.com>
2026-01-04 16:40:28 +0800

    代码优化
    Signed-off-by: kelinyang <yangxiaopeng8@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_frame_rate_manager.h
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_frame_rate_manager.h
@@ -202,6 +202,8 @@ private:
     void UpdateScreenFrameRate();
     void RegisterUpTimeoutAndDownEvent();
 
+    void UpdateSurfaceTimeInner(const std::string& surfaceName, pid_t pid, UIFWKType uiFwkType);
+
     void GetSupportedRefreshRates(const std::shared_ptr<PolicyConfigData>& configData,
         const std::string& modeKey, std::vector<uint32_t>& targetVec, bool handleAmbientEffect);
     void GetLowBrightVec(const std::shared_ptr<PolicyConfigData>& configData);
