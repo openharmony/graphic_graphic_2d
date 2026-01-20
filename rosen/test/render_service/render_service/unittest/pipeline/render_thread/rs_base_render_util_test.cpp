39748d1e9c739632236ca0830735ba91396600a4
sunriseLL <liwen176@huawei.com>
2026-01-20 15:34:47 +0800

    compile fix
    Signed-off-by: sunriseLL <liwen176@huawei.com>

--- rosen/test/render_service/render_service/unittest/pipeline/render_thread/rs_base_render_util_test.cpp
+++ rosen/test/render_service/render_service/unittest/pipeline/render_thread/rs_base_render_util_test.cpp
@@ -16,6 +16,7 @@
 #include "gtest/gtest.h"
 #include "limit_number.h"
 #include "parameters.h"
+#include "params/rs_screen_render_params.h"
 #include "pipeline/render_thread/rs_base_render_util.h"
 #include "pipeline/rs_test_util.h"
 #include "surface_buffer_impl.h"
