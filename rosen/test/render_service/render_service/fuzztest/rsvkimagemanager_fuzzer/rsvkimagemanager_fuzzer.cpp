ed8e1f362dc66fff772b7e669b5becac7f9cd1fd
fangxiang <fangxiang19@huawei.com>
2026-01-24 08:19:01 +0000

    fix code diff
    Signed-off-by: fangxiang <fangxiang19@huawei.com>
    Change-Id: Id483d8737a46b67139e346fc749b39da7e3e09e0

--- rosen/test/render_service/render_service/fuzztest/rsvkimagemanager_fuzzer/rsvkimagemanager_fuzzer.cpp
+++ rosen/test/render_service/render_service/fuzztest/rsvkimagemanager_fuzzer/rsvkimagemanager_fuzzer.cpp
@@ -130,15 +130,15 @@ void RSVKImageManagerFuzztestVKSemaphore(const uint8_t* data, size_t size)
     auto fakeTid = GetData<uint32_t>();
     if (vkImageManager && buffer1 && drawingSurface) {
         (void)vkImageManager->MapVkImageFromSurfaceBuffer(buffer1, bufferFence1, fakeTid, drawingSurface.get());
-        (void)vkImageManager->UnMapImageFromSurfaceBuffer(buffer1->GetBufferId());
+        vkImageManager->UnMapImageFromSurfaceBuffer(buffer1->GetBufferId());
         (void)vkImageManager->MapVkImageFromSurfaceBuffer(buffer1, nullptr, fakeTid, nullptr);
-        (void)vkImageManager->UnMapImageFromSurfaceBuffer(buffer1->GetBufferId());
+        vkImageManager->UnMapImageFromSurfaceBuffer(buffer1->GetBufferId());
 
         VkDevice device = RsVulkanContext::GetSingleton().GetRsVulkanInterface().device_;
         RsVulkanContext::GetSingleton().GetRsVulkanInterface().device_ = VK_NULL_HANDLE;
         (void)vkImageManager->MapVkImageFromSurfaceBuffer(buffer1, bufferFence1, fakeTid, drawingSurface.get());
         RsVulkanContext::GetSingleton().GetRsVulkanInterface().device_ = device;
-        (void)vkImageManager->UnMapImageFromSurfaceBuffer(buffer1->GetBufferId());
+        vkImageManager->UnMapImageFromSurfaceBuffer(buffer1->GetBufferId());
     }
 }
 } // namespace Rosen
