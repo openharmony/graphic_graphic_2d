a327b582f0c6cd1a818688f3fece94ece7408850
Dizzy1203 <dongwenbo10@huawei.com>
2026-01-26 21:21:32 +0800

    update: 更新文件 hgm_energy_consumption_policy.h
    Signed-off-by: Dizzy1203 <dongwenbo10@huawei.com>

--- rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_energy_consumption_policy.h
+++ rosen/modules/hyper_graphic_manager/core/frame_rate_manager/hgm_energy_consumption_policy.h
@@ -29,6 +29,12 @@
 
 namespace OHOS::Rosen {
 class HgmEnergyConsumptionPolicy {
+
+constexpr uint32_t DRAG_FRAME_RATE_TYPE = REFRESH_DRAG_FRAME_RATE_TYPE | 
+    SWIPER_DRAG_FRAME_RATE_TYPE | SCROLLABLE_DRAG_FRAME_RATE_TYPE | 
+    SCROLLBAR_DRAG_FRAME_RATE_TYPE | SPLIT_DRAG_FRAME_RATE_TYPE | 
+    PICKER_DRAG_FRAME_RATE_TYPE | SCROLLABLE_MULTI_TASK_FRAME_RATE_TYPE;
+
 public:
     static HgmEnergyConsumptionPolicy& Instance();
     void SetEnergyConsumptionConfig(std::unordered_map<std::string, std::string> animationPowerConfig);
