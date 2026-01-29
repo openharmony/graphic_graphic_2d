f0a24e4a086a89601a9049a910d4988bc4ed37a1
zhangqiang <zhangqiang349@huawei.com>
2026-01-29 17:18:58 +0800

    ut
    Signed-off-by: zhangqiang <zhangqiang349@huawei.com>

---
+++ rosen/test/render/render/unittest/render/rs_node_test.cpp
@@ -0,0 +1,389 @@
+/*
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+
+#include "feature/composite_layer/rs_composite_layer_utils.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "animation/rs_animation.h"
+#include "animation/rs_animation_callback.h"
+#include "animation/rs_implicit_animation_param.h"
+#include "animation/rs_implicit_animator.h"
+#include "animation/rs_implicit_animator_map.h"
+#include "animation/rs_transition.h"
+#include "common/rs_vector4.h"
+#include "modifier_ng/appearance/rs_alpha_modifier.h"
+#include "modifier_ng/appearance/rs_background_filter_modifier.h"
+#include "modifier_ng/appearance/rs_foreground_filter_modifier.h"
+#include "modifier_ng/custom/rs_content_style_modifier.h"
+#include "modifier_ng/custom/rs_node_modifier.h"
+#include "render/rs_filter.h"
+#include "render/rs_material_filter.h"
+#include "ui/rs_canvas_node.h"
+#include "ui/rs_display_node.h"
+#include "ui/rs_node.h"
+#include "ui/rs_surface_node.h"
+#include "ui/rs_ui_director.h"
+#include "ui_effect/effect/include/background_color_effect_para.h"
+#include "ui_effect/effect/include/border_light_effect_para.h"
+#include "ui_effect/effect/include/brightness_blender.h"
+#include "ui_effect/effect/include/color_gradient_effect_para.h"
+#include "ui_effect/filter/include/filter_blur_para.h"
+#include "ui_effect/filter/include/filter_content_light_para.h"
+#include "ui_effect/filter/include/filter_displacement_distort_para.h"
+#include "ui_effect/filter/include/filter_edge_light_para.h"
+#include "ui_effect/filter/include/filter_fly_out_para.h"
+#include "ui_effect/filter/include/filter_hdr_para.h"
+#include "ui_effect/filter/include/filter_pixel_stretch_para.h"
+#include "ui_effect/filter/include/filter_water_ripple_para.h"
+#include "ui_effect/property/include/rs_ui_filter_base.h"
+#include "ui_effect/property/include/rs_ui_shader_base.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+constexpr static float FLOAT_DATA_ZERO = 0.0f;
+constexpr static float FLOAT_DATA_POSITIVE = 485.44f;
+constexpr static float FLOAT_DATA_NEGATIVE = -34.4f;
+constexpr static float FLOAT_DATA_MAX = std::numeric_limits<float>::max();
+constexpr static float FLOAT_DATA_MIN = std::numeric_limits<float>::min();
+
+class RSNodeTest : public testing::Test {
+public:
+    constexpr static float floatData[] = {
+        FLOAT_DATA_ZERO, FLOAT_DATA_POSITIVE, FLOAT_DATA_NEGATIVE,
+        FLOAT_DATA_MAX, FLOAT_DATA_MIN,
+        };
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+    Vector4f createV4fWithValue(float value) const
+    {
+        return {value, value, value, value};
+    }
+    void SetBorderDashParamsAndTest(float value) const
+    {
+        SetBorderDashParamsAndTest(createV4fWithValue(value));
+    }
+    void SetOutlineDashParamsAndTest(float value) const
+    {
+        SetOutlineDashParamsAndTest(createV4fWithValue(value));
+    }
+    void SetBorderDashParamsAndTest(const Vector4f& params) const
+    {
+        auto rsNode = RSCanvasNode::Create();
+        rsNode->SetBorderDashWidth(params);
+        rsNode->SetBorderDashGap(params);
+        auto borderDashWidth = rsNode->GetStagingProperties().GetBorderDashWidth();
+        auto borderDashGap = rsNode->GetStagingProperties().GetBorderDashGap();
+        EXPECT_TRUE(borderDashWidth.IsNearEqual(params));
+        EXPECT_TRUE(borderDashGap.IsNearEqual(params));
+    }
+    void SetOutlineDashParamsAndTest(const Vector4f& params) const
+    {
+        auto rsNode = RSCanvasNode::Create();
+        rsNode->SetOutlineDashWidth(params);
+        rsNode->SetOutlineDashGap(params);
+        auto borderOutlineWidth = rsNode->GetStagingProperties().GetOutlineDashWidth();
+        auto borderOutlineGap = rsNode->GetStagingProperties().GetOutlineDashGap();
+        EXPECT_TRUE(borderOutlineWidth.IsNearEqual(params));
+        EXPECT_TRUE(borderOutlineGap.IsNearEqual(params));
+    }
+};
+
+void RSNodeTest::SetUpTestCase() {}
+void RSNodeTest::TearDownTestCase() {}
+void RSNodeTest::SetUp() {}
+void RSNodeTest::TearDown() {}
+
+/**
+ * @tc.name: LifeCycle001
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, LifeCycle001, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and add child
+     */
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+
+    auto child1 = RSCanvasNode::Create();
+    auto child2 = RSCanvasNode::Create();
+    auto child3 = RSCanvasNode::Create();
+    rootNode->AddChild(child1, -1);
+    rootNode->AddChild(child2, 0);
+    child1->AddChild(child3, 1);
+
+    EXPECT_EQ(rootNode->GetId() + 2, child1->GetId());
+
+    /**
+     * @tc.steps: step2. remove child
+     */
+    rootNode->RemoveChild(child2);
+}
+
+/**
+ * @tc.name: LifeCycle002
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, LifeCycle002, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and add child
+     */
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+
+    auto child1 = RSCanvasNode::Create();
+    auto child2 = RSCanvasNode::Create();
+    auto child3 = RSCanvasNode::Create();
+    rootNode->AddChild(child1, -1);
+    rootNode->AddChild(child2, 0);
+    child1->AddChild(child3, 1);
+    /**
+     * @tc.steps: step2. remove child
+     */
+    rootNode->RemoveChild(child2);
+}
+
+/**
+ * @tc.name: LifeCycle003
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, LifeCycle003, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and add child
+     */
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+
+    auto child1 = RSCanvasNode::Create();
+    auto child2 = RSCanvasNode::Create();
+    auto child3 = RSCanvasNode::Create();
+    rootNode->AddChild(child1, -1);
+    rootNode->AddChild(child2, 0);
+    child1->AddChild(child3, 1);
+    /**
+     * @tc.steps: step2. remove child
+     */
+    rootNode->RemoveFromTree();
+}
+
+/**
+ * @tc.name: LifeCycle004
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, LifeCycle004, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and add child
+     */
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+
+    auto child1 = RSCanvasNode::Create();
+    auto child2 = RSCanvasNode::Create();
+    auto child3 = RSCanvasNode::Create();
+    rootNode->AddChild(child1, -1);
+    rootNode->AddChild(child2, 0);
+    child1->AddChild(child3, 1);
+    /**
+     * @tc.steps: step2. remove child
+     */
+    rootNode->ClearChildren();
+}
+
+/**
+ * @tc.name: LifeCycle005
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, LifeCycle005, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and add child
+     */
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+
+    auto child1 = RSCanvasNode::Create();
+    auto child2 = RSCanvasNode::Create();
+    auto child3 = RSCanvasNode::Create();
+    rootNode->AddChild(child1, -1);
+    rootNode->AddChild(child2, 0);
+    child1->AddChild(child3, 1);
+    /**
+     * @tc.steps: step2. remove child
+     */
+    rootNode->RemoveChild(child3);
+}
+
+/**
+ * @tc.name: test destruction001
+ * @tc.desc:
+ * @tc.type:FUNC
+ * @tc.require: issueI9KDPI
+ */
+HWTEST_F(RSNodeTest, destruction001, TestSize.Level1)
+{
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+    rootNode->rsUIContext_ = std::make_shared<RSUIContext>();
+    rootNode->skipDestroyCommandInDestructor_ = true;
+}
+
+/**
+ * @tc.name: test destruction002
+ * @tc.desc:
+ * @tc.type:FUNC
+ * @tc.require: issueICL3ND
+ */
+HWTEST_F(RSNodeTest, destruction002, TestSize.Level1)
+{
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+    rootNode->rsUIContext_ = std::make_shared<RSUIContext>();
+    rootNode->skipDestroyCommandInDestructor_ = false;
+}
+
+/**
+ * @tc.name: test destruction003
+ * @tc.desc:
+ * @tc.type:FUNC
+ * @tc.require: issueICL3ND
+ */
+HWTEST_F(RSNodeTest, destruction003, TestSize.Level1)
+{
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+    rootNode->skipDestroyCommandInDestructor_ = true;
+}
+
+/**
+ * @tc.name: test destruction004
+ * @tc.desc:
+ * @tc.type:FUNC
+ * @tc.require: issueICL3ND
+ */
+HWTEST_F(RSNodeTest, destruction004, TestSize.Level1)
+{
+    auto rootNode = RSCanvasNode::Create();
+    ASSERT_TRUE(rootNode != nullptr);
+}
+
+/**
+ * @tc.name: Recording001
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, Recording001, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and RSUIDirector
+     */
+    auto rsNode = RSCanvasNode::Create();
+
+    /**
+     * @tc.steps: step2. begin recording
+     */
+    EXPECT_FALSE(rsNode->IsRecording());
+    rsNode->BeginRecording(500, 400);
+    EXPECT_TRUE(rsNode->IsRecording());
+}
+
+/**
+ * @tc.name: SetPaintOrder001
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, SetPaintOrder001, TestSize.Level1)
+{
+    /**
+     * @tc.steps: step1. create RSNode and RSUIDirector
+     */
+    auto rsNode = RSCanvasNode::Create();
+    ASSERT_NE(rsNode, nullptr);
+    rsNode->SetPaintOrder(true);
+}
+
+/**
+ * @tc.name: SetandGetBounds001
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, SetandGetBounds001, TestSize.Level1)
+{
+    auto rsNode = RSCanvasNode::Create();
+    rsNode->SetBounds(floatData[0], floatData[1], floatData[2], floatData[3]);
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().x_, floatData[0]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().y_, floatData[1]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().z_, floatData[2]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().w_, floatData[3]));
+}
+
+/**
+ * @tc.name: SetandGetBounds002
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, SetandGetBounds002, TestSize.Level1)
+{
+    auto rsNode = RSCanvasNode::Create();
+    rsNode->SetBounds(floatData[3], floatData[1], floatData[2], floatData[0]);
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().x_, floatData[3]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().y_, floatData[1]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().z_, floatData[2]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().w_, floatData[0]));
+}
+
+/**
+ * @tc.name: SetandGetBounds003
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, SetandGetBounds003, TestSize.Level1)
+{
+    auto rsNode = RSCanvasNode::Create();
+    rsNode->SetBounds(floatData[3], floatData[2], floatData[1], floatData[0]);
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().x_, floatData[3]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().y_, floatData[2]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().z_, floatData[1]));
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().w_, floatData[0]));
+}
+
+/**
+ * @tc.name: SetandGetBoundsWidth001
+ * @tc.desc:
+ * @tc.type:FUNC
+ */
+HWTEST_F(RSNodeTest, SetandGetBoundsWidth001, TestSize.Level1)
+{
+    auto rsNode = RSCanvasNode::Create();
+    rsNode->SetBoundsWidth(floatData[1]);
+    rsNode->SetBoundsWidth(floatData[1]);
+    EXPECT_TRUE(ROSEN_EQ(rsNode->GetStagingProperties().GetBounds().z_, floatData[1]));
+}
+
+} // namespace OHOS::Rosen
