bdddfa6da00318646746c8f81538dc51e1035189
glb0 <gaolibin3@h-partners.com>
2026-02-11 17:23:55 +0800

    fix unirender tdd
    Signed-off-by: glb0 <gaolibin3@h-partners.com>

--- rosen/test/render_service/render_service/unittest/pipeline/main_thread/rs_main_thread_test.cpp
+++ rosen/test/render_service/render_service/unittest/pipeline/main_thread/rs_main_thread_test.cpp
@@ -1857,6 +1857,7 @@ HWTEST_F(RSMainThreadTest, UniRender003, TestSize.Level1)
  * @tc.type: FUNC
  * @tc.require:
  */
+#if define(RS_ENABLE_UNI_RENDER)
 HWTEST_F(RSMainThreadTest, UniRender004, TestSize.Level1)
 {
     auto mainThread = RSMainThread::Instance();
@@ -1885,6 +1886,7 @@ HWTEST_F(RSMainThreadTest, UniRender004, TestSize.Level1)
     mainThread->UniRender(rootNode);
     ASSERT_TRUE(mainThread->doDirectComposition_);
 }
+#endif
 
 /**
  * @tc.name: IfStatusBarDirtyOnly001
