6fca168df6f349e28d65001b1e0d4a9da320137a
wangjincheng <wangjincheng13@huawei.com>
2026-02-24 17:08:13 +0800

    修复RemoveDrawableFromCache死锁风险
    Signed-off-by: wangjincheng <wangjincheng13@huawei.com>

--- rosen/modules/render_service_base/src/drawable/rs_render_node_drawable_adapter.cpp
+++ rosen/modules/render_service_base/src/drawable/rs_render_node_drawable_adapter.cpp
@@ -97,6 +97,7 @@ RSRenderNodeDrawableAdapter::SharedPtr RSRenderNodeDrawableAdapter::OnGenerate(
     }
     static const auto Destructor = [](RSRenderNodeDrawableAdapter* ptr) {
         RemoveDrawableFromCache(ptr->nodeId_); // Remove from cache before deleting
+        ptr->ClearCustomResource();
         RSRenderNodeGC::DrawableDestructor(ptr);
     };
     auto id = node->GetId();
@@ -678,12 +679,7 @@ int8_t RSRenderNodeDrawableAdapter::GetSkipIndex() const
 void RSRenderNodeDrawableAdapter::RemoveDrawableFromCache(const NodeId nodeId)
 {
     std::lock_guard<std::mutex> lock(cacheMapMutex_);
-    if (const auto cacheIt = RenderNodeDrawableCache_.find(nodeId); cacheIt != RenderNodeDrawableCache_.end()) {
-        if (const auto drawable = cacheIt->second.lock()) {
-            drawable->ClearCustomResource();
-        }
-        RenderNodeDrawableCache_.erase(cacheIt);
-    }
+    RenderNodeDrawableCache_.erase(nodeId);
 }
 
 void RSRenderNodeDrawableAdapter::RegisterClearSurfaceFunc(ClearSurfaceTask task)
