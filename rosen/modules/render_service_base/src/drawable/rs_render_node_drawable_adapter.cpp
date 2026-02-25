552ece65933e5443870967dc186b374fabb2670b
ZhangLitao <zhanglitao11@h-partners.com>
2026-02-25 10:47:52 +0800

    Revert 28518
    Signed-off-by: ZhangLitao <zhanglitao11@h-partners.com>

--- rosen/modules/render_service_base/src/drawable/rs_render_node_drawable_adapter.cpp
+++ rosen/modules/render_service_base/src/drawable/rs_render_node_drawable_adapter.cpp
@@ -101,20 +101,20 @@ RSRenderNodeDrawableAdapter::SharedPtr RSRenderNodeDrawableAdapter::OnGenerate(
         RSRenderNodeGC::DrawableDestructor(ptr);
     };
     auto id = node->GetId();
-    // Try to get a cached drawable if it exists.   
-    {   
-        std::lock_guard<std::mutex> lock(cacheMapMutex_);   
-        if (const auto cacheIt = RenderNodeDrawableCache_.find(id); cacheIt != RenderNodeDrawableCache_.end()) {    
-            if (const auto ptr = cacheIt->second.lock()) {  
-                ROSEN_LOGE("%{public}s, node id in Cache is %{public}" PRIu64 
-                    ", nodeType: %{public}u, drawableType: %{public}u", __func__, id, node->GetType(), 
-                    ptr->GetNodeType()); 
-                if (node->GetType() == ptr->GetNodeType()) { 
-                    return ptr; 
-                } 
-            } 
-            RenderNodeDrawableCache_.erase(cacheIt); 
-        }   
+    // Try to get a cached drawable if it exists.
+    {
+        std::lock_guard<std::mutex> lock(cacheMapMutex_);
+        if (const auto cacheIt = RenderNodeDrawableCache_.find(id); cacheIt != RenderNodeDrawableCache_.end()) {
+            if (const auto ptr = cacheIt->second.lock()) {
+                ROSEN_LOGE("%{public}s, node id in Cache is %{public}" PRIu64
+                    ", nodeType: %{public}u, drawableType: %{public}u", __func__, id, node->GetType(),
+                    ptr->GetNodeType());
+                if (node->GetType() == ptr->GetNodeType()) {
+                    return ptr;
+                }
+            }
+            RenderNodeDrawableCache_.erase(cacheIt);
+        }
     }
     // If we don't have a cached drawable, try to generate a new one and cache it.
     const auto it = GeneratorMap.find(node->GetType());
