2ead60a512270323e5c5f1b8920cb53c2121612b
liugan <liugan8@huawei.com>
2026-03-10 19:39:09 +0800

    补充TDD
    Signed-off-by: liugan <liugan8@huawei.com>

---
+++ rosen/test/render/render/unittest/profiler/render_marshalling_helper_test.cpp
@@ -0,0 +1,462 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, Hardware
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "gtest/hwext/gtest-tag.h"
+#include "parameters.h"
+#include "render_service_profiler/rs_profiler.h"
+#include "rs_profiler.h"
+#include "test_utils.h"
+
+#include "transaction/rs_marshalling_helper.h"
+#include "utils/data.h"
+
+#ifdef ROSEN_OHOS
+#include "buffer_utils.h"
+#endif
+#include "recording/mask_cmd_list.h"
+#include "rs_profiler.h"
+
+#include "property/rs_properties_def.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Rosen {
+class RenderMarshallingReliabilityTest : public testing::Test {
+    static bool profilerEnabled_;
+
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp() override;
+    void TearDown() override;
+};
+
+bool RenderMarshallingReliabilityTest::profilerEnabled_ = false;
+
+void RenderMarshallingReliabilityTest::SetUpTestCase()
+{
+    profilerEnabled_ = system::GetBoolParameter("persist.graphic.profiler.enabled", false);
+}
+void RenderMarshallingReliabilityTest::TearDownTestCase()
+{
+    system::SetParameter("persist.graphic.profiler.enabled", profilerEnabled_ ? "1" : "0");
+}
+void RenderMarshallingReliabilityTest::SetUp() {}
+void RenderMarshallingReliabilityTest::TearDown() {}
+
+#ifdef USE_VIDEO_PROCESSING_ENGINE
+/**
+ * @tc.name: ConsistencyWithSmallData
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithSmallDataWithoutProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "0");
+    // data should be std::shared_ptr<Drawing::Data>
+    // because Marshalling working for std::shared_ptr<Drawing::Data>
+    auto data = std::make_shared<Drawing::Data>();
+    const size_t length = 100;
+    const size_t position = 108;
+
+    void* allocated = malloc(length);
+    EXPECT_TRUE(data->BuildFromMalloc(allocated, length));
+
+    EXPECT_TRUE(CheckConsistencyWithData(data, position));
+}
+
+/**
+ * @tc.name: ConsistencyWithSmallPixelMapWithoutProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithSmallPixelMapWithoutProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "0");
+
+    Media::InitializationOptions options;
+    options.size.width = 1;
+    options.size.height = 1;
+    options.pixelFormat = Media::PixelFormat::RGBA_8888;
+    const size_t position = 88; // or 92
+    const size_t pixelMapSize = 84;
+
+    std::shared_ptr pixelMap = Media::PixelMap::Create(options);
+    EXPECT_TRUE(pixelMap != nullptr);
+    EXPECT_TRUE(CheckConsistencyWithPixelMap(pixelMap, position, pixelMapSize));
+}
+
+/**
+ * @tc.name: ConsistencyWithMediumPixelMapWithoutProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithMediumPixelMapWithoutProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "0");
+
+    Media::InitializationOptions options;
+    options.size.width = 860;
+    options.size.height = 520;
+    options.pixelFormat = Media::PixelFormat::RGBA_8888;
+    const size_t position = 88;
+    const size_t pixelMapSize = 84;
+
+    std::shared_ptr pixelMap = Media::PixelMap::Create(options);
+    EXPECT_TRUE(pixelMap != nullptr);
+    EXPECT_TRUE(CheckConsistencyWithPixelMap(pixelMap, position, pixelMapSize));
+}
+
+/**
+ * @tc.name: ConsistencyWithMediumData
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithMediumDataWithoutProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "0");
+    // data should be std::shared_ptr<Drawing::Data>
+    // because Marshalling working for std::shared_ptr<Drawing::Data>
+    auto data = std::make_shared<Drawing::Data>();
+    const size_t length = 40'000;
+    const size_t position = 36;
+
+    void* allocated = malloc(length);
+    EXPECT_TRUE(data->BuildFromMalloc(allocated, length));
+
+    EXPECT_TRUE(CheckConsistencyWithData(data, position));
+}
+
+/**
+ * @tc.name: ConsistencyWithBigPixelMapWithoutProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithBigPixelMapWithoutProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "0");
+
+    Media::InitializationOptions options;
+    options.size.width = 2123;
+    options.size.height = 1987;
+    options.pixelFormat = Media::PixelFormat::RGBA_8888;
+    const size_t position = 88;
+    const size_t pixelMapSize = 84;
+
+    std::shared_ptr pixelMap = Media::PixelMap::Create(options);
+    EXPECT_TRUE(pixelMap != nullptr);
+    EXPECT_TRUE(CheckConsistencyWithPixelMap(pixelMap, position, pixelMapSize));
+}
+
+/**
+ * @tc.name: ConsistencyWithSmallPixelMapWithProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithSmallPixelMapWithProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "1");
+
+    Media::InitializationOptions options;
+    options.size.width = 1;
+    options.size.height = 1;
+    options.pixelFormat = Media::PixelFormat::RGBA_8888;
+    const size_t position = 96;
+    const size_t pixelMapSize = 92;
+
+    std::shared_ptr pixelMap = Media::PixelMap::Create(options);
+    EXPECT_TRUE(pixelMap != nullptr);
+    EXPECT_TRUE(CheckConsistencyWithPixelMap(pixelMap, position, pixelMapSize));
+}
+
+/**
+ * @tc.name: ConsistencyWithMediumPixelMapWithProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithMediumPixelMapWithProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "1");
+
+    Media::InitializationOptions options;
+    options.size.width = 860;
+    options.size.height = 520;
+    options.pixelFormat = Media::PixelFormat::RGBA_8888;
+    const size_t position = 96;
+    const size_t pixelMapSize = 92;
+
+    std::shared_ptr pixelMap = Media::PixelMap::Create(options);
+    EXPECT_TRUE(pixelMap != nullptr);
+    EXPECT_TRUE(CheckConsistencyWithPixelMap(pixelMap, position, pixelMapSize));
+}
+
+/**
+ * @tc.name: ConsistencyWithSmallDataWithProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithSmallDataWithProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "1");
+    // data should be std::shared_ptr<Drawing::Data>
+    // because Marshalling working for std::shared_ptr<Drawing::Data>
+    auto data = std::make_shared<Drawing::Data>();
+    const size_t length = 100;
+    const size_t position = 108;
+
+    void* allocated = malloc(length);
+    EXPECT_TRUE(data->BuildFromMalloc(allocated, length));
+
+    EXPECT_TRUE(CheckConsistencyWithData(data, position));
+}
+
+/**
+ * @tc.name: ConsistencyWithMediumDataWithProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithMediumDataWithProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "1");
+    Parcel parcel;
+    // data should be std::shared_ptr<Drawing::Data>
+    // because Marshalling working for std::shared_ptr<Drawing::Data>
+    auto data = std::make_shared<Drawing::Data>();
+    const size_t length = 40'000;
+    const size_t position = 44;
+
+    void* allocated = malloc(length);
+    EXPECT_TRUE(data->BuildFromMalloc(allocated, length));
+
+    EXPECT_TRUE(CheckConsistencyWithData(data, position));
+}
+
+/**
+ * @tc.name: ConsistencyWithBigPixelMapWithProfiler
+ * @tc.desc: Verify function marshal, unmarshal and skip is consistent.
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderMarshallingReliabilityTest, ConsistencyWithBigPixelMapWithProfiler, TestSize.Level1 | Reliability)
+{
+    OHOS::system::SetParameter("persist.graphic.profiler.enabled", "1");
+
+    Media::InitializationOptions options;
+    options.size.width = 2123;
+    options.size.height = 1987;
+    options.pixelFormat = Media::PixelFormat::RGBA_8888;
+    const size_t position = 96;
+    const size_t pixelMapSize = 92;
+
+    std::shared_ptr pixelMap = Media::PixelMap::Create(options);
+    EXPECT_TRUE(pixelMap != nullptr);
+    EXPECT_TRUE(CheckConsistencyWithPixelMap(pixelMap, position, pixelMapSize));
+}
+
+/**
+ * @tc.name: UnmarshallingEmpty
+ * @tc.desc: Verify function UnmarshallingEmpty
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST(RSMarshallingHelperTest, UnmarshallingEmpty, Level1)
+{
+    Parcel parcel;
+    uint64_t val = 0;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusId(parcel, val);
+    EXPECT_FALSE(ret);
+    EXPECT_EQ(val, 0);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.desc: Verify function UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST(RSMarshallingHelperTest, UnmarshallingPidPlusIdNoChangeIfZero0, Level1)
+{
+    Parcel parcel;
+    uint64_t val = 0;
+    parcel.WriteUint64(val);
+    val = 42;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusIdNoChangeIfZero(parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, 0);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.desc: Verify function UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST(RSMarshallingHelperTest, UnmarshallingPidPlusIdNoChangeIfZero, Level1)
+{
+    Parcel parcel;
+    uint64_t val = 42;
+    parcel.WriteUint64(val);
+    val = 0;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusIdNoChangeIfZero(parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, 42);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusId0
+ * @tc.desc: Verify function UnmarshallingPidPlusId
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST(RSMarshallingHelperTest, UnmarshallingPidPlusId0, Level1)
+{
+    Parcel parcel;
+    uint64_t val = 0;
+    parcel.WriteUint64(val);
+    val = 42;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusId(parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, 0);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusId
+ * @tc.desc: Verify function UnmarshallingPidPlusId
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST(RSMarshallingHelperTest, UnmarshallingPidPlusId, Level1)
+{
+    Parcel parcel;
+    uint64_t val = 42;
+    parcel.WriteUint64(val);
+    val = 0;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusId(parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, 42);
+}
+
+class RSMarshallingHelperMockParcelTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+        RSProfiler::testing_ = true;
+        RSProfiler::SetMode(Mode::READ);
+    }
+
+    static void TearDownTestCase()
+    {
+        RSProfiler::testing_ = false;
+        RSProfiler::SetMode(Mode::NONE);
+    }
+
+    void SetUp() override
+    {
+        parcel = new (parcelMemory + 1) Parcel;
+    }
+
+    void TearDown() override
+    {
+        parcel->~Parcel();
+    }
+
+private:
+    Parcel* parcel;
+    uint8_t parcelMemory[sizeof(Parcel) + 1];
+
+    static constexpr const uint64_t FLAG = 1uLL << 62u;
+};
+
+/**
+ * @tc.name: UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.desc: Verify function UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RSMarshallingHelperMockParcelTest, UnmarshallingPidPlusIdNoChangeIfZero0, Level1)
+{
+    uint64_t val = 0;
+    parcel->WriteUint64(val);
+    val = 42;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusIdNoChangeIfZero(*parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, 0);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.desc: Verify function UnmarshallingPidPlusIdNoChangeIfZero
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RSMarshallingHelperMockParcelTest, UnmarshallingPidPlusIdNoChangeIfZero, Level1)
+{
+    uint64_t val = 42;
+    parcel->WriteUint64(val);
+    val = 0;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusIdNoChangeIfZero(*parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, FLAG | 42);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusId0
+ * @tc.desc: Verify function UnmarshallingPidPlusId
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RSMarshallingHelperMockParcelTest, UnmarshallingPidPlusId0, Level1)
+{
+    uint64_t val = 0;
+    parcel->WriteUint64(val);
+    val = 42;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusId(*parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, FLAG | 0);
+}
+
+/**
+ * @tc.name: UnmarshallingPidPlusId
+ * @tc.desc: Verify function UnmarshallingPidPlusId
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RSMarshallingHelperMockParcelTest, UnmarshallingPidPlusId, Level1)
+{
+    uint64_t val = 42;
+    parcel->WriteUint64(val);
+    val = 0;
+    bool ret = RSMarshallingHelper::UnmarshallingPidPlusId(*parcel, val);
+    EXPECT_TRUE(ret);
+    EXPECT_EQ(val, FLAG | 42);
+}
+#endif
+} // namespace Rosen
+} // namespace OHOS
---
+++ rosen/test/render/render/unittest/profiler/render_profiler_network_test.cpp
@@ -0,0 +1,208 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "gtest/gtest.h"
+#include "rs_profiler_network.h"
+#include "rs_profiler_packet.h"
+
+using namespace testing;
+using namespace testing::ext;
+
+namespace OHOS::Rosen {
+class RenderProfilerNetworkTest : public testing::Test {
+public:
+    static void SetUpTestCase() {};
+    static void TearDownTestCase() {};
+    void SetUp() override;
+    void TearDown() override {};
+};
+
+void RenderProfilerNetworkTest::SetUp()
+{
+    Network::outgoing_ = {};
+    Network::incoming_ = {};
+    Network::isRunning_ = true;
+}
+
+/*
+ * @tc.name: RSProfilerNetworkPushPopCommandTest
+ * @tc.desc: Test public method PushPopCommand
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkPushPopCommandTest, TestSize.Level1)
+{
+    std::vector<std::string> command { "qwe", "123", "asd", "456" };
+    Network::PushCommand(command);
+    EXPECT_EQ(Network::incoming_.front(), command);
+
+    std::vector<std::string> args;
+    Network::PopCommand(args);
+    EXPECT_EQ(args, command);
+    EXPECT_TRUE(Network::incoming_.empty());
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendBinaryTest
+ * @tc.desc: Test public method SendBinary
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendBinaryTest, TestSize.Level1)
+{
+    std::vector<char> data { 0, 1, 2, 3, 4, 5 };
+    Network::SendBinary(nullptr, data.size());
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendBinary(data.data(), 0);
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendBinary(data.data(), data.size());
+
+    auto type = static_cast<char>(Packet::BINARY);
+    std::vector<char> sendData { type, 11, 0, 0, 0, 0, 1, 2, 3, 4, 5 };
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkProcessCommandTest
+ * @tc.desc: Test public method ProcessCommand
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkProcessCommandTest, TestSize.Level1)
+{
+    std::string command = "";
+    Network::ProcessCommand(command.data(), command.size());
+    EXPECT_TRUE(Network::incoming_.empty());
+
+    command = "qwe 123";
+    Network::ProcessCommand(command.data(), command.size());
+    std::vector<std::string> expected { "qwe", "123" };
+    EXPECT_EQ(Network::incoming_.front(), expected);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendMessageTest
+ * @tc.desc: Test public method SendMessage
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendMessageTest, TestSize.Level1)
+{
+    Network::SendMessage("");
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendMessage("hello");
+    auto type = static_cast<char>(Packet::LOG);
+    std::vector<char> sendData { type, 10, 0, 0, 0, 'h', 'e', 'l', 'l', 'o' };
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendDclPathTest
+ * @tc.desc: Test public method SendDclPath
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendDclPathTest, TestSize.Level1)
+{
+    Network::SendDclPath("");
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendDclPath("path");
+    auto type = static_cast<char>(Packet::BINARY);
+    auto subtype = static_cast<char>(PackageID::RS_PROFILER_DCL_BINARY);
+    std::vector<char> sendData { type, 10, 0, 0, 0, subtype, 'p', 'a', 't', 'h' };
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendRdcPathTest
+ * @tc.desc: Test public method SendRdcPath
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendRdcPathTest, TestSize.Level1)
+{
+    Network::SendRdcPath("");
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendRdcPath("path");
+    auto type = static_cast<char>(Packet::BINARY);
+    auto subtype = static_cast<char>(PackageID::RS_PROFILER_RDC_BINARY);
+    std::vector<char> sendData { type, 10, 0, 0, 0, subtype, 'p', 'a', 't', 'h' };
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendSkpTest
+ * @tc.desc: Test public method SendSkp
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendSkpTest, TestSize.Level1)
+{
+    std::vector<char> data { 0, 1, 2, 3, 4, 5 };
+    Network::SendSkp(nullptr, data.size());
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendSkp(data.data(), 0);
+    EXPECT_TRUE(Network::outgoing_.empty());
+    Network::SendSkp(data.data(), data.size());
+    auto type = static_cast<char>(Packet::BINARY);
+    auto subtype = static_cast<char>(PackageID::RS_PROFILER_SKP_BINARY);
+    std::vector<char> sendData { type, 12, 0, 0, 0, subtype, 0, 1, 2, 3, 4, 5 };
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendSingleNodePerfTest
+ * @tc.desc: Test public method SendRSTreeSingleNodePerf
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendSingleNodePerfTest, TestSize.Level1)
+{
+    auto type = static_cast<char>(Packet::BINARY);
+    auto subtype = static_cast<char>(PackageID::RS_PROFILER_RSTREE_SINGLE_NODE_PERF);
+    std::vector<char> sendData { type, 22, 0, 0, 0, subtype, 10, 0, 0, 0, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0 };
+    Network::SendRSTreeSingleNodePerf(10, 80);
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+
+/*
+ * @tc.name: RSProfilerNetworkSendPerfNodeListTest
+ * @tc.desc: Test public method SendRSTreePerfNodeList
+ * @tc.type: FUNC
+ * @tc.require: 22738
+ */
+HWTEST_F(RenderProfilerNetworkTest, RSProfilerNetworkSendPerfNodeListTest, TestSize.Level1)
+{
+    std::unordered_set<uint64_t> nodelist;
+    auto type = static_cast<char>(Packet::BINARY);
+    auto subtype = static_cast<char>(PackageID::RS_PROFILER_RSTREE_PERF_NODE_LIST);
+    std::vector<char> sendData { type, 6, 0, 0, 0, subtype };
+    Network::SendRSTreePerfNodeList(nodelist);
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+
+    nodelist = { 1, 2 };
+    sendData = { type, 22, 0, 0, 0, subtype, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+    Network::SendRSTreePerfNodeList(nodelist);
+    ASSERT_FALSE(Network::outgoing_.empty());
+    EXPECT_EQ(Network::outgoing_.back(), sendData);
+}
+} // namespace OHOS::Rosen
