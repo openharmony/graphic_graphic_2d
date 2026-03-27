78829c1094afda891b4e9b265f4131b27c3882e0
gcw_yxxmv6uE <guojunfu@huawei.com>
2026-03-27 14:15:33 +0800

    update: 更新文件 ani_filter.cpp
    Signed-off-by: gcw_yxxmv6uE <guojunfu@huawei.com>

--- interfaces/kits/ani/effect_kit/src/ani_filter.cpp
+++ interfaces/kits/ani/effect_kit/src/ani_filter.cpp
@@ -116,8 +116,8 @@ ani_object AniFilter::EllipticalGradientBlur(ani_env *env, ani_object obj, ani_d
         return AniEffectKitUtils::CreateAniUndefined(env);
     }
     ani_double positionParam;
-    ani_ref positionRef;
-    ani_ref degreeRef;
+    ani_ref positionRef = nullptr;
+    ani_ref degreeRef = nullptr;
     auto ret = env->Object_CallMethodByName_Double(
         static_cast<ani_object>(positionRef),
         "toDouble", ":d", &positionParam);
