535611c9f10b960a56e8837d6d772f33d5396a4d
Askr_w <972048870@qq.com>
2026-03-30 14:45:40 +0800

    update: 更新文件 drawing_canvas_test.cpp bugfix
    Signed-off-by: Askr_w <972048870@qq.com>

--- rosen/test/2d_graphics/unittest/ndk/drawing_canvas_test.cpp
+++ rosen/test/2d_graphics/unittest/ndk/drawing_canvas_test.cpp
@@ -3201,6 +3201,41 @@ HWTEST_F(NativeDrawingCanvasTest, NativeDrawingCanvasTest_DrawSingleCharacterWit
 }
 
 /*
+ * @tc.name: NativeDrawingCanvasTest_DrawGlyphs001
+ * @tc.desc: test for draw glyphs
+ * @tc.type: FUNC
+ * @tc.require: ICG6L3
+ */
+HWTEST_F(NativeDrawingCanvasTest, NativeDrawingCanvasTest_DrawGlyphs001, TestSize.Level1)
+{
+    OH_Drawing_Font *font = OH_Drawing_FontCreate();
+    EXPECT_NE(font, nullptr);
+    int glyphIds[] = {71, 72, 73};
+    OH_Drawing_Point2D positions[] = {{0, 20}, {0, 40}, {0,60}};
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 0, 3, font), OH_DRAWING_SUCCESS);
+
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 0, -1, font),
+              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, -1, positions, 3, 0, 3, font),
+              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, -1, 3, font),
+              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 1, positions, 3, 0, 3, font),
+              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 1, 3, font),
+              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
+
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(nullptr, glyphIds, 3, 0, positions, 3, 0, 3, font),
+              OH_DRAWING_ERROR_INVALID_PARAMETER);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 0, 3, nullptr),
+              OH_DRAWING_ERROR_INVALID_PARAMETER);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, nullptr, 3, 0, positions, 3, 0, 3, font),
+              OH_DRAWING_ERROR_INVALID_PARAMETER);
+    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, nullptr, 3, 0, 3, font),
+              OH_DRAWING_ERROR_INVALID_PARAMETER);
+}
+
+/*
  * @tc.name: NativeDrawingCanvasTest_DrawSingleCharacterWithFeatures001
  * @tc.desc: test for draw single character with Features with canvas pixelmap
  * @tc.type: FUNC
@@ -3238,41 +3273,6 @@ HWTEST_F(NativeDrawingCanvasTest, NativeDrawingCanvasTest_DrawSingleCharacterWit
     OH_PixelmapNative_Release(pixelMap);
     free(buffer);
     OH_PixelmapInitializationOptions_Release(options);
-}
-
-/*
- * @tc.name: NativeDrawingCanvasTest_DrawGlyphs001
- * @tc.desc: test for draw glyphs
- * @tc.type: FUNC
- * @tc.require: ICG6L3
- */
-HWTEST_F(NativeDrawingCanvasTest, NativeDrawingCanvasTest_DrawGlyphs001, TestSize.Level1)
-{
-    OH_Drawing_Font *font = OH_Drawing_FontCreate();
-    EXPECT_NE(font, nullptr);
-    int glyphIds[] = {71, 72, 73};
-    OH_Drawing_Point2D positions[] = {{0, 20}, {0, 40}, {0,60}};
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 0, 3, font), OH_DRAWING_SUCCESS);
-
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 0, -1, font),
-              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, -1, positions, 3, 0, 3, font),
-              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, -1, 3, font),
-              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 1, positions, 3, 0, 3, font),
-              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 1, 3, font),
-              OH_DRAWING_ERROR_PARAMETER_OUT_OF_RANGE);
-
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(nullptr, glyphIds, 3, 0, positions, 3, 0, 3, font),
-              OH_DRAWING_ERROR_INVALID_PARAMETER);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, positions, 3, 0, 3, nullptr),
-              OH_DRAWING_ERROR_INVALID_PARAMETER);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, nullptr, 3, 0, positions, 3, 0, 3, font),
-              OH_DRAWING_ERROR_INVALID_PARAMETER);
-    EXPECT_EQ(OH_Drawing_CanvasDrawGlyphs(canvas_, glyphIds, 3, 0, nullptr, 3, 0, 3, font),
-              OH_DRAWING_ERROR_INVALID_PARAMETER);
 }
 } // namespace Drawing
 } // namespace Rosen
