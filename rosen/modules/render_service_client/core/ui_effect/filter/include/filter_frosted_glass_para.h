0d3d1ee4a7502a44fda4382692ef90e392b35295
zelunwu <wuzelun@huawei.com>
2026-03-31 09:02:26 +0800

    update: 更新文件 filter_frosted_glass_para.h
    Signed-off-by: zelunwu <wuzelun@huawei.com>

--- rosen/modules/render_service_client/core/ui_effect/filter/include/filter_frosted_glass_para.h
+++ rosen/modules/render_service_client/core/ui_effect/filter/include/filter_frosted_glass_para.h
@@ -71,6 +71,16 @@ public:
         return weightsEdl_;
     }
 
+    void SetMaxColor(float maxColor)
+    {
+        maxColor_ = maxColor;
+    }
+
+    float GetMaxColor() const
+    {
+        return maxColor_;
+    }
+
     void SetBgRates(Vector2f& bgRates)
     {
         bgRates_ = bgRates;
@@ -378,7 +388,8 @@ public:
 private:
     Vector2f blurParams_ = Vector2f(0.0f, 0.0f);
     Vector2f weightsEmboss_ = Vector2f(0.0f, 0.0f); // (envLight, sd)
-    Vector2f weightsEdl_ = Vector2f(0.0f, 0.0f); // (envLight, sd)
+    Vector2f weightsEdl_ = Vector2f(0.0f, 0.0f);
+    Vector2f maxColor_ = 15.0f;
     // Background darken parameters
     Vector2f bgRates_ = Vector2f(0.0f, 0.0f);
     Vector3f bgKBS_ = Vector3f(0.0f, 0.0f, 0.0f);
