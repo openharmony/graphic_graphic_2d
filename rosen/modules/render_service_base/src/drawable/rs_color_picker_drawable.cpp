9e7639ffcb46321420e030eac3ed65f9768d2027
Tianer Zhou <ezhoureal@gmail.com>
2026-04-01 07:35:29 +0000

    add debug trace
    Signed-off-by: Tianer Zhou <ezhoureal@gmail.com>
    
    Co-Authored-By: Agent
    Change-Id: Iad5cbfac4959fc0c87450ccbf42620e08c22aa38

--- rosen/modules/render_service_base/src/drawable/rs_color_picker_drawable.cpp
+++ rosen/modules/render_service_base/src/drawable/rs_color_picker_drawable.cpp
@@ -152,6 +152,7 @@ void RSColorPickerDrawable::ResetColorMemory()
 void RSColorPickerDrawable::SetState(DrawableV2::ColorPickerState state)
 {
     const DrawableV2::ColorPickerState currentState = stagingState_;
+    RS_OPTIONAL_TRACE_NAME_FMT("ColorPicker: try transitioning to state %d from %d", state, currentState);
 
     // Validate state transitions
     switch (currentState) {
