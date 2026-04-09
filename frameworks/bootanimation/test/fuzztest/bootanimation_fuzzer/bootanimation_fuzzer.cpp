b805273819c6b118e6758359723da254a57e9975
OningO <zhangning134@huawei.com>
2026-04-09 11:40:24 +0800

    update: 更新文件 bootanimation_fuzzer.cpp
    Signed-off-by: OningO <zhangning134@huawei.com>

--- frameworks/bootanimation/test/fuzztest/bootanimation_fuzzer/bootanimation_fuzzer.cpp
+++ frameworks/bootanimation/test/fuzztest/bootanimation_fuzzer/bootanimation_fuzzer.cpp
@@ -24,7 +24,6 @@ namespace OHOS {
     {
         bool isEnabled = fdp.ConsumeBool();
         BootAnimationUtils::SetBootAnimationSoundEnabled(isEnabled);
-        BootAnimationUtils::GetBootAnimationSoundEnabled();
     }
 }
 
