c25904a76d377458925b094a5e86ed5259226922
li_bing_bing_ccc <libingbing12@huawei.com>
2026-04-13 14:54:27 +0800

    fixipc
    Signed-off-by: li_bing_bing_ccc <libingbing12@huawei.com>
    Change-Id: Idf65dfc5c2c4f30056a3e3984d17135318411f9b

--- rosen/test/render_service/render_service_base/unittest/platform/ohos/rs_client_to_service_connection_proxy_test.cpp
+++ rosen/test/render_service/render_service_base/unittest/platform/ohos/rs_client_to_service_connection_proxy_test.cpp
@@ -240,6 +240,46 @@ HWTEST_F(RSClientToServiceConnectionProxyTest, RemoveVirtualScreenBlackList, Tes
 }
 
 /**
+ * @tc.name: SetVirtualScreenTypeBlackListTest
+ * @tc.desc: test SetVirtualScreenTypeBlackList
+ * @tc.type: FUNC
+ * @tc.require: issue23264
+ */
+HWTEST_F(RSClientToServiceConnectionProxyTest, SetVirtualScreenTypeBlackListTest, TestSize.Level2)
+{
+    int32_t repCode = 0;
+    std::vector<NodeType> typeBlackListVector;
+    ASSERT_NE(proxy, nullptr);
+    ASSERT_EQ(proxy->SetVirtualScreenTypeBlackList(0, typeBlackListVector, repCode), 0);
+}
+
+/**
+ * @tc.name: AddVirtualScreenWhiteListTest
+ * @tc.desc: test AddVirtualScreenWhiteList
+ * @tc.type: FUNC
+ * @tc.require: issue23264
+ */
+HWTEST_F(RSClientToServiceConnectionProxyTest, AddVirtualScreenWhiteListTest, TestSize.Level2)
+{
+    int32_t repCode = 0;
+    ASSERT_NE(proxy, nullptr);
+    ASSERT_EQ(proxy->AddVirtualScreenWhiteList(0, {}, repCode), 0);
+}
+
+/**
+ * @tc.name: RemoveVirtualScreenWhiteListTest
+ * @tc.desc: test RemoveVirtualScreenWhiteList
+ * @tc.type: FUNC
+ * @tc.require: issue23264
+ */
+HWTEST_F(RSClientToServiceConnectionProxyTest, RemoveVirtualScreenWhiteListTest, TestSize.Level2)
+{
+    int32_t repCode = 0;
+    ASSERT_NE(proxy, nullptr);
+    ASSERT_EQ(proxy->RemoveVirtualScreenWhiteList(0, {}, repCode), 0);
+}
+
+/**
  * @tc.name: RemoveVirtualScreen Test
  * @tc.desc: RemoveVirtualScreen Test
  * @tc.type:FUNC
