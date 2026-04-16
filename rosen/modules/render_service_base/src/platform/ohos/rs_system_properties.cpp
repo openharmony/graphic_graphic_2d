d276e20676ba83941a2973edaa764ae3bf072bdc
lishijie77 <lishijie22@huawei.com>
2026-04-16 16:30:11 +0800

    修复开机流程
    Signed-off-by: lishijie77 <lishijie22@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/rs_system_properties.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/rs_system_properties.cpp
@@ -257,8 +257,7 @@ bool RSSystemProperties::GetAnimationDelayOptimizeEnabled()
 bool RSSystemProperties::GetRSClientMultiInstanceEnabled()
 {
     static bool isMultiInstance = system::GetParameter("persist.rosen.rsclientmultiinstance.enabled", "1") != "0";
-    static bool isPhone = system::GetParameter("const.product.devicetype", "phone") == "phone";
-    return isMultiInstance && isPhone;
+    return isMultiInstance;
 }
 
 bool RSSystemProperties::GetRSScreenRoundCornerEnable()
