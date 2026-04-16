8a9d59f04bcf636267713655467b70520fa06a70
zhangxiangwei8 <zhangxiangwei8@huawei.com>
2026-04-16 20:24:19 +0800

    fix dos Co-Authored-By: Agent
    Signed-off-by: zhangxiangwei8 <zhangxiangwei8@huawei.com>

--- rosen/modules/render_service_base/src/command/rs_root_node_command.cpp
+++ rosen/modules/render_service_base/src/command/rs_root_node_command.cpp
@@ -28,6 +28,11 @@ namespace Rosen {
 
 void RootNodeCommandHelper::Create(RSContext& context, NodeId id, bool isTextureExportNode)
 {
+    if (context.GetNodeMap().GetNodeCountByPid(ExtractPid(id)) > MAX_NODE_COUNT_PER_PID) {
+        RS_LOGE_LIMIT(__func__, __line__,
+            "GetNodeCountByPid > %{public}u, pid:%{public}d", MAX_NODE_COUNT_PER_PID, ExtractPid(id));
+        return;
+    }
     auto node = std::shared_ptr<RSRootRenderNode>(new RSRootRenderNode(id,
         context.weak_from_this(), isTextureExportNode), RSRenderNodeGC::NodeDestructor);
     if (context.GetMutableNodeMap().UnRegisterUnTreeNode(id)) {
