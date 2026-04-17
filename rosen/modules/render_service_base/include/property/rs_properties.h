04902a60445ebf76c81082e2aae77efe86ddc215
zelunwu <wuzelun@huawei.com>
2026-04-17 15:23:07 +0800

    update: 更新文件 rs_properties.h
    Signed-off-by: zelunwu <wuzelun@huawei.com>

--- rosen/modules/render_service_base/include/property/rs_properties.h
+++ rosen/modules/render_service_base/include/property/rs_properties.h
@@ -1090,7 +1090,7 @@ private:
     bool needHwcFilter_ = false;
     bool needForceSubmit_ = false;
     bool hasHarmonium_ = false;
-    bool HasSpatialGlassEffect_ = false;
+    bool hasSpatialGlassEffect_ = false;
     bool useUnion_ = false;
     float gravityPullStrength_ = 0.0f;
     float gravityHotZone_ = 0.0f;
