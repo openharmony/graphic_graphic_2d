09187c97cb582a6bb32222a9d425164d0ce54607
chitoylala <chitoylala@noreply.gitcode.com>
2026-04-18 16:46:15 +0800

    layer cache manager dfx
    Signed-off-by: peiyuanshuai <1463484785@qq.com>

--- rosen/modules/render_service_base/include/platform/common/rs_system_properties.h
+++ rosen/modules/render_service_base/include/platform/common/rs_system_properties.h
@@ -327,6 +327,7 @@ public:
     static bool GetOpincCacheMemThresholdEnabled();
     static bool GetLayerPartRenderEnabled();
     static bool GetLayerPartRenderDebugEnabled();
+    static bool GetLayerDebugEnabled();
     static bool GetFilterCacheMemThresholdEnabled();
     static bool GetSkipDisplayIfScreenOffEnabled();
     static bool GetBatchRemovingOnRemoteDiedEnabled();
