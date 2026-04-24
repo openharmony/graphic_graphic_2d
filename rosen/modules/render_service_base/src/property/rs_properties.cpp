0ce5ce6964df9c465a6e66b883b93b7aa0d18ed1
zelunwu <wuzelun@huawei.com>
2026-04-24 14:30:21 +0800

    update: 更新文件 rs_properties.cpp
    Signed-off-by: zelunwu <wuzelun@huawei.com>

--- rosen/modules/render_service_base/src/property/rs_properties.cpp
+++ rosen/modules/render_service_base/src/property/rs_properties.cpp
@@ -5668,7 +5668,9 @@ void RSProperties::SetBackgroundNGShader(const std::shared_ptr<RSNGRenderShaderB
     if (renderShader != nullptr && renderShader->ContainsType(RSNGEffectType::FROSTED_GLASS_EFFECT)) {
         filterNeedUpdate_ = true;
     }
-    hasSpatialGlassEffect_ = renderShader->ContainsType(RSNGEffectType::SPATIAL_GLASS_EFFECT);
+    if (renderShader != nullptr) {
+        hasSpatialGlassEffect_ = renderShader->ContainsType(RSNGEffectType::SPATIAL_GLASS_EFFECT);
+    }
     if (hasSpatialGlassEffect_) {
         filterNeedUpdate_ = true;
     }
