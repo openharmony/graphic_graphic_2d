8b6b259d2d45c3b0ac4257825fe17b61a1e4af36
chengqian <chengqian51@huawei.com>
2026-05-06 14:13:35 +0800

    drawing字体语言和特征查询需求开发
    Signed-off-by: chengqian <chengqian51@huawei.com>

---
+++ rosen/modules/2d_graphics/include/text/font_feature_query.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FONT_FEATURE_QUERY_H
+#define FONT_FEATURE_QUERY_H
+
+#include <string>
+#include <vector>
+
+#include "text/font_style.h"
+#include "typeface.h"
+
+namespace OHOS {
+namespace Rosen {
+namespace Drawing {
+class DRAWING_API FontFeatureQuery {
+public:
+    /**
+     * @brief            Returns all supported and valid font feature in given typeface.
+     * @param typeface   given typeface.
+     * @return           vector of all supported and valid font feature in given typeface
+     */
+    static std::vector<std::string> GenerateFontSupportedFeatures(const std::shared_ptr<Typeface>& typeface);
+
+    /**
+     * @brief            Returns all supported and valid font feature in given fontPath.
+     * @param fontPath   given fontPath.
+     * @return           vector of all supported and valid font feature in given fontPath
+     */
+    static std::vector<std::string> GenerateFontSupportedFeatures(const std::string& fontPath);
+
+    /**
+     * @brief            Returns all supported and valid font feature in given fontData.
+     * @param fontData   given fontData.
+     * @return           vector of all supported and valid font feature in given fontData
+     */
+    static std::vector<std::string> GenerateFontSupportedFeatures(const std::vector<uint8_t>& fontData);
+};
+} // Drawing
+} // Rosen
+} // OHOS
+#endif // FONT_FEATURE_QUERY_H
