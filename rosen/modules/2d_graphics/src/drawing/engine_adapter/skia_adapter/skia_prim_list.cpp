5328a8f20182795fddee77a8f64a91465418be10
small_ling <linghaoyu1@huawei.com>
2026-05-25 02:36:14 +0000

    Add fuzz test cases for PrimList module
    Add PrimListFuzzTest, PrimListCanvasFuzzTest and PrimListCanvasFuzzTestNullptr
    to cover PrimList APIs and Canvas PrimList collecting interfaces.
    
    Co-Authored-By: Agent
    Signed-off-by: small_ling <linghaoyu1@huawei.com>

--- rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_prim_list.cpp
+++ rosen/modules/2d_graphics/src/drawing/engine_adapter/skia_adapter/skia_prim_list.cpp
@@ -14,6 +14,7 @@
  */
 
 #include "skia_prim_list.h"
+#include "utils/log.h"
 
 namespace OHOS {
 namespace Rosen {
@@ -23,15 +24,18 @@ SkiaPrimList::SkiaPrimList() noexcept {}
 
 void SkiaPrimList::UpdateAlphaFactor(float alphaFactor)
 {
+    LOGD("SkiaPrimList does not support UpdateAlphaFactor.");
 }
 
 size_t SkiaPrimList::GetPrimCount() const
 {
+    LOGD("SkiaPrimList does not support GetPrimCount.");
     return 0;
 }
 
 std::vector<std::pair<const char*, size_t>> SkiaPrimList::GetPrimCountPerType() const
 {
+    LOGD("SkiaPrimList does not support GetPrimCountPerType.");
     return {};
 }
 
