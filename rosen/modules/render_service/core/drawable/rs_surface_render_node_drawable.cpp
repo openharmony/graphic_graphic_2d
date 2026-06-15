1859322a043e779c24c034d17ad50523885122ba
ahyls <yanglusheng@h-partners.com>
2026-06-15 19:26:36 +0800

    update: 更新文件 rs_surface_render_node_drawable.cpp
    Signed-off-by: ahyls <yanglusheng@h-partners.com>

--- rosen/modules/render_service/core/drawable/rs_surface_render_node_drawable.cpp
+++ rosen/modules/render_service/core/drawable/rs_surface_render_node_drawable.cpp
@@ -935,10 +935,10 @@ void RSSurfaceRenderNodeDrawable::OnDraw(Drawing::Canvas& canvas)
         std::static_pointer_cast<RSScreenRenderNodeDrawable>(surfaceParams->GetAncestorScreenDrawable().lock());
     auto screenParams = screenDrawable ?
         static_cast<RSScreenRenderParams*>(screenDrawable->GetRenderParams().get()) : nullptr;
-    bool isVirtualExpandScreenMirrored = screenParams &&
+    bool isSingleAppCast = screenParams &&
         screenParams->GetCompositeType() == CompositeType::UNI_RENDER_EXPAND_COMPOSITE &&
         screenParams->HasMirrorScreen();
-    needOffscreen = needOffscreen && !isVirtualExpandScreenMirrored;
+    needOffscreen = needOffscreen && !isSingleAppCast;
 
     needOffscreen = needOffscreen || (captureConfig ? captureConfig->isSyncRender : false);
     if (captureConfig && captureConfig->isSyncRender) {
