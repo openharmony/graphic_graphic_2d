39f4bb7a8b4a90446b39ec0b11740faa8b9a074f
lishijie77 <lishijie22@huawei.com>
2026-06-25 11:25:45 +0800

    update: 更新文件 rs_ipc_persistence_data.h update
    Signed-off-by: lishijie77 <lishijie22@huawei.com>

--- rosen/modules/render_service/main/render_process/transaction/ipc_persistence/rs_ipc_persistence_data.h
+++ rosen/modules/render_service/main/render_process/transaction/ipc_persistence/rs_ipc_persistence_data.h
@@ -29,7 +29,7 @@ namespace Rosen {
 class SetWatermarkPersistenceData : public RSIpcPersistenceDataBase {
 public:
     SetWatermarkPersistenceData() = default;
-    SetWatermarkPersistenceData(pid_t pid, const std::string &name, std::shared_ptr<Media::PixelMap> watermark,
+    SetWatermarkPersistenceData(pid_t pid, const std::string& name,, std::shared_ptr<Media::PixelMap> watermark,
         bool success, uint32_t rowCount = 0, uint32_t colCount = 0)
         : pid_(pid), name_(name), watermark_(watermark), success_(success), rowCount_(rowCount), colCount_(colCount)
     {}
