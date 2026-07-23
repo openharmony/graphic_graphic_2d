236f2503c2fa237149bdbfc172c10586a51e8c6f
Niko-Yuan <yuanfangqi7@huawei.com>
2026-07-23 15:56:04 +0800

    unittest fix
    Co-Authored-By: Agent
    Signed-off-by: Niko-Yuan <yuanfangqi7@huawei.com>

--- rosen/test/render_service/render_server/unittest/rs_render_service_test.cpp
+++ rosen/test/render_service/render_server/unittest/rs_render_service_test.cpp
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, Hardware
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -35,6 +35,8 @@
 #include "vsync_distributor.h"
 #include "pipeline/render_thread/rs_uni_render_thread.h"
 #include "platform/common/rs_system_properties.h"
+#include "feature/color_picker/rs_color_picker_thread.h"
+#include "common/rs_background_thread.h"
 
 using namespace testing;
 using namespace testing::ext;
@@ -67,6 +69,13 @@ void RSRenderServiceTest::SetUpTestCase()
 
 void RSRenderServiceTest::TearDownTestCase()
 {
+    if (RSSystemProperties::IsUseVulkan()) {
+        RSUniRenderThread::Instance().uniRenderEngine_.reset();
+        RSColorPickerThread::Instance().gpuContext_.reset();
+        RSColorPickerThread::Instance().renderContext_.reset();
+        RSBackgroundThread::Instance().gpuContext_.reset();
+        RSBackgroundThread::Instance().renderContext_.reset();
+    }
     screenManager_ = nullptr;
     vsyncManager_ = nullptr;
 }
