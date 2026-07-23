1d97451bfcc7b8977013d8b45d6cb47db55a4f81
jk2236178279 <jiangkai71@huawei.com>
2026-07-23 11:38:52 +0800

    RSRenderFrameRateLinker线程安全修复
    Signed-off-by: jk2236178279 <jiangkai71@huawei.com>
    Change-Id: I2573e05e491714201ebdd7e9191b3287a12b2ed2

--- rosen/modules/hyper_graphic_manager/core/utils/hgm_state_machine.h
+++ rosen/modules/hyper_graphic_manager/core/utils/hgm_state_machine.h
@@ -163,7 +163,7 @@ template<typename State, typename Event>
 void HgmStateMachine<State, Event>::UnRegisterStateChangeCallback(
     StateChangeCallbacksType& callbacks, State state, int32_t callbackId) {
     if (auto iter = callbacks.find(state); iter != callbacks.end()) {
-        if (auto toDelCallbackIter = iter->second; toDelCallbackIter != iter->second.end()) {
+        if (auto toDelCallbackIter = iter->second.find(callbackId); toDelCallbackIter != iter->second.end()) {
             iter->second.erase(toDelCallbackIter);
         }
     }
