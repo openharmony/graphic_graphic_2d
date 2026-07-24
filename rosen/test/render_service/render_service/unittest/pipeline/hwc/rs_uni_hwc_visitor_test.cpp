5f474ea3fadeb5f334b44cd4854579e629beb625
zhengxingshen <zhengxingshen@huawei.com>
2026-07-24 12:04:29 +0800

    fixUT7.0
    Signed-off-by: zhengxingshen <zhengxingshen@huawei.com>

--- rosen/test/render_service/render_service/unittest/pipeline/hwc/rs_uni_hwc_visitor_test.cpp
+++ rosen/test/render_service/render_service/unittest/pipeline/hwc/rs_uni_hwc_visitor_test.cpp
@@ -1579,35 +1579,6 @@ HWTEST_F(RSUniHwcVisitorTest, UpdateHardwareStateByHwcNodeBackgroundAlpha002, Te
 }
 
 /**
- * @tc.name: UpdateHardwareStateByHwcNodeBackgroundAlpha003
- * @tc.desc: Test RSUniHwcVisitorTest.UpdateHardwareStateByHwcNodeBackgroundAlpha
- * @tc.type: FUNC
- * @tc.require: IAHFXD
- */
-HWTEST_F(RSUniHwcVisitorTest, UpdateHardwareStateByHwcNodeBackgroundAlpha003, TestSize.Level1)
-{
-    RSSurfaceRenderNodeConfig surfaceConfig;
-    surfaceConfig.id = 1;
-    auto surfaceNode1 = std::make_shared<RSSurfaceRenderNode>(surfaceConfig);
-    ASSERT_NE(surfaceNode1, nullptr);
-
-    auto surfaceNode2 = std::make_shared<RSSurfaceRenderNode>(surfaceConfig);
-    ASSERT_NE(surfaceNode2, nullptr);
-    surfaceNode2->SetNodeHasBackgroundColorAlpha(true);
-
-    std::vector<std::weak_ptr<RSSurfaceRenderNode>> hwcNodes;
-    hwcNodes.push_back(std::weak_ptr<RSSurfaceRenderNode>(surfaceNode1));
-    hwcNodes.push_back(std::weak_ptr<RSSurfaceRenderNode>(surfaceNode2));
-    auto rsUniRenderVisitor = std::make_shared<RSUniRenderVisitor>();
-    ASSERT_NE(rsUniRenderVisitor, nullptr);
-    auto rsUniHwcVisitor = std::make_shared<RSUniHwcVisitor>(*rsUniRenderVisitor);
-    ASSERT_NE(rsUniHwcVisitor, nullptr);
-    RectI rect;
-    bool isHardwareEnableByBackgroundAlpha = false;
-    rsUniHwcVisitor->UpdateHardwareStateByHwcNodeBackgroundAlpha(hwcNodes, rect, isHardwareEnableByBackgroundAlpha);
-}
-
-/**
  * @tc.name: UpdateHardwareStateByHwcNodeBackgroundAlpha004
  * @tc.desc: Test RSUniHwcVisitorTest.UpdateHardwareStateByHwcNodeBackgroundAlpha
  * @tc.type: FUNC
