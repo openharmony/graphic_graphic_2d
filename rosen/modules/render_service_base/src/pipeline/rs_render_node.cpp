a6924c37f2e01d77f1b82954e8c67915989cce2b
peiyuanshuai <1463484785@qq.com>
2026-07-27 15:10:07 +0800

    优化代码
    Signed-off-by: peiyuanshuai <1463484785@qq.com>

--- rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
+++ rosen/modules/render_service_base/src/pipeline/rs_render_node.cpp
@@ -4818,7 +4818,7 @@ void RSRenderNode::OnSync()
     bool isLayerNode = nodeGroupType_ == NodeGroupType::GROUPED_BY_LAYER &&
                        stagingRenderParams_->GetDrawingCacheType() != RSDrawingCacheType::DISABLED_CACHE;
     if (isLayerNode) {
-        auto& layerParams = stagingRenderParams_->GetLayerParams();
+        const auto& layerParams = stagingRenderParams_->GetLayerParams();
         bool isLayerCacheDisabled = (layerParams && layerParams->isUnSupportLayer) || IsNodeParentHasUIFirstCache();
         if (isLayerCacheDisabled) {
             stagingRenderParams_->SetDrawingCacheType(RSDrawingCacheType::DISABLED_CACHE);
