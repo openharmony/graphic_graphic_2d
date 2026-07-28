d974c93a7d13d2452506d111e5ce2131f47e37bd
YaolinHou <houyaolin@huawei.com>
2026-07-28 20:30:15 +0800

    蓝黄同步
    Signed-off-by: YaolinHou <houyaolin@huawei.com>

--- rosen/modules/render_service/core/feature/hwc/hpae_offline/rs_hpae_offline_device.cpp
+++ rosen/modules/render_service/core/feature/hwc/hpae_offline/rs_hpae_offline_device.cpp
@@ -537,7 +537,7 @@ bool RSHpaeOfflineDevice::PostProcessOfflineTask(
     auto* params = surfaceNode->GetStagingRenderParams().get();
     auto surfaceParams = static_cast<RSSurfaceRenderParams*>(params);
     RS_OFFLINE_LOGD("start to proces offline surface (by node), task[%{public}" PRIu64 "-%{public}" PRIu64 "]",
-        taskId.first, taskId.second);
+            taskId.first, taskId.second);
     return PostOfflineTaskCommon(context, surfaceParams, taskId);
 }
 
