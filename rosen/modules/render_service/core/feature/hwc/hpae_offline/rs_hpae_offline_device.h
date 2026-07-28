fd4451ddaa36ea80e5e2f0191c100a9335567bfb
YaolinHou <houyaolin@huawei.com>
2026-07-28 19:14:22 +0800

    蓝黄同步
    Signed-off-by: YaolinHou <houyaolin@huawei.com>

--- rosen/modules/render_service/core/feature/hwc/hpae_offline/rs_hpae_offline_device.h
+++ rosen/modules/render_service/core/feature/hwc/hpae_offline/rs_hpae_offline_device.h
@@ -32,7 +32,7 @@ namespace Rosen {
 enum OfflineContextType : uint32_t {
     INVALID = 0,
     SCALE,
-    AI2020
+    AI2020,
 };
 
 struct OfflineProcessInputInfo {
@@ -90,7 +90,7 @@ public:
     explicit RSHpaeOfflineContext(OfflineContextType type);
     ~RSHpaeOfflineContext();
 
-    bool isSkipDraw();
+    bool IsSkipDraw();
 
     OfflineContextType contextType = OfflineContextType::INVALID;
     NodeId nodeId = 0;
@@ -109,8 +109,8 @@ public:
     std::atomic<bool> preAllocBufferSucc = false;
     std::atomic<size_t> invalidFrames = 0;
     std::atomic<size_t> maxInvalidFrames = 0;
-    int32_t heteroEnableFrames = 0;
-    bool isSetHeteroEnable = false;
+    std::atomic<size_t> heteroEnableFrames = 0;
+    std::atomic<bool> isSetHeteroEnable = false;
     std::atomic<bool> skipDraw = false;
     std::atomic<bool> hasDrawn = false;
     std::mutex offlineConfigMutex;
