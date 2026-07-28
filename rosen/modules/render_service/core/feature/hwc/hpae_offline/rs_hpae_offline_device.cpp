df2e821faa684f876141be050499790a0ae8aef9
YaolinHou <houyaolin@huawei.com>
2026-07-28 20:31:04 +0800

    蓝黄同步
    Signed-off-by: YaolinHou <houyaolin@huawei.com>

--- rosen/modules/render_service/core/feature/hwc/hpae_offline/rs_hpae_offline_device.cpp
+++ rosen/modules/render_service/core/feature/hwc/hpae_offline/rs_hpae_offline_device.cpp
@@ -564,7 +564,7 @@ bool RSHpaeOfflineDevice::PostProcessOfflineTask(
     auto* params = surfaceDrawable->GetRenderParams().get();
     auto surfaceParams = static_cast<RSSurfaceRenderParams*>(params);
     RS_OFFLINE_LOGD("post offline task[%{public}" PRIu64 "-%{public}" PRIu64 "] by drawable",
-        taskId.first, taskId.second);
+            taskId.first, taskId.second);
     return PostOfflineTaskCommon(context, surfaceParams, taskId);
 }
 
