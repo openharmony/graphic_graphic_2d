58be8f89e66793c011d77e6a8b83d00718f5a119
kelinyang <yangxiaopeng8@huawei.com>
2026-07-31 11:25:37 +0800

    tdd优化
    Co-Authored-By: Agent
    Signed-off-by: kelinyang <yangxiaopeng8@huawei.com>
    
    AI[0%] Human Fixed[0%] Human[100%] AI Adopted[0%]

--- rosen/test/render_service/render_service/unittest/feature/hyper_graphic_manager/rs_vblank_idle_corrector_test.cpp
+++ rosen/test/render_service/render_service/unittest/feature/hyper_graphic_manager/rs_vblank_idle_corrector_test.cpp
@@ -17,6 +17,7 @@
 
 #include "hyper_graphic_manager/rs_vblank_idle_corrector.h"
 #include "hgm_core.h"
+#include "mock_hgm_frame_rate_manager.h"
 #include "screen_manager/rs_screen_manager.h"
 
 using namespace testing;
@@ -112,10 +113,13 @@ HWTEST_F(RSVBlankIdleCorrectorTest, ProcessScreenConstraintTest, TestSize.Level1
     rsVBlankIdleCorrector->ProcessScreenConstraint(id, timestamp, 0);
 
     rsVBlankIdleCorrector->SetScreenVBlankIdle();
-    frameRateMgr->isAdaptive_ = SupportASStatus::SUPPORT_AS;
-    frameRateMgr->isGameNodeOnTree_ = true;
+    std::shared_ptr<Mock::MockHgmFrameRateManager> mock = std::make_shared<Mock::MockHgmFrameRateManager>();
+    EXPECT_CALL(*mock, AdaptiveStatus()).WillRepeatedly(testing::Return(SupportASStatus::SUPPORT_AS));
+    EXPECT_CALL(*mock, IsGameNodeOnTree()).WillRepeatedly(testing::Return(true));
+    EXPECT_CALL(*mock, IsNeedAdaptiveAfterUpdateMode()).WillRepeatedly(testing::Return(true));
+    hgmCore.hgmFrameRateMgr_ = mock;
     rsVBlankIdleCorrector->ProcessScreenConstraint(id, timestamp, constraintTime);
-    EXPECT_EQ(rsVBlankIdleCorrector->idleFrameCount_, 1);
+    EXPECT_EQ(rsVBlankIdleCorrector->idleFrameCount_, 2);
 
     hgmCore.hgmFrameRateMgr_ = nullptr;
     rsVBlankIdleCorrector->ProcessScreenConstraint(id, timestamp, constraintTime);
