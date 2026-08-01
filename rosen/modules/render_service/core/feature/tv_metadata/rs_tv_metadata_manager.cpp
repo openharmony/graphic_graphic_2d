cdd3ef18655c41b861190b19bc9a101a0a8eef2e
chen_bailin <2411398810@qq.com>
2026-08-01 14:22:45 +0800

    sync code
    Signed-off-by: chen_bailin <2411398810@qq.com>

--- rosen/modules/render_service/core/feature/tv_metadata/rs_tv_metadata_manager.cpp
+++ rosen/modules/render_service/core/feature/tv_metadata/rs_tv_metadata_manager.cpp
@@ -36,9 +36,10 @@ constexpr uint8_t WIND_SIZE_SMALLSCREEN = 1;
 constexpr uint8_t SCALER_MODE_GPU = 3;
 constexpr uint8_t VIDEO_PLAYING_SCENE = 1;
 constexpr uint8_t VIDEO_PLAYING_NO_AIHDR_SCENE = 2;
-constexpr uint8_t PRIORITY_FOR_FULLSCREEN = 1;
+constexpr uint8_t PRIORITY_FOR_FULLSCREEN = 2;
 constexpr uint8_t PRIORITY_FOR_SMALLSCREEN = 1;
-constexpr uint8_t PRIORITY_FOR_VIDEO_EFFECT = 2;
+constexpr uint8_t PRIORITY_FOR_VPE = 1;
+constexpr uint8_t PRIORITY_FOR_VIDEO_EFFECT = 4;
 constexpr uint8_t RESERVED_INDEX_FOR_NODE_ID = 0;
 constexpr uint8_t RESERVED_INDEX_FOR_CACHE = 1;
 constexpr uint8_t RESERVED_INDEX_FOR_DIM = 2;
@@ -270,6 +271,9 @@ uint8_t RSTvMetadataManager::GetPriority(TvPQMetadata& metadata)
         return priority;
     }
     priority += PRIORITY_FOR_SMALLSCREEN;
+    if (metadata.sceneTag != VIDEO_PLAYING_NO_AIHDR_SCENE) {
+        priority += PRIORITY_FOR_VPE;
+    }
     if (metadata.vidWinSize == WIND_SIZE_FULLSCREEN) {
         priority += PRIORITY_FOR_FULLSCREEN;
     }
