c04738cac661f9bad63973d73a598b843a335a41
A7355007 <xuhuajun3@h-partners.com>
2026-08-06 17:01:31 +0800

    update: 更新文件 rs_surface_render_node_three_util_test.cpp
    Signed-off-by: A7355007 <xuhuajun3@h-partners.com>

--- rosen/test/render/render/unittest/render/rs_surface_render_node_three_util_test.cpp
+++ rosen/test/render/render/unittest/render/rs_surface_render_node_three_util_test.cpp
@@ -754,13 +754,13 @@ HWTEST_F(RSSurfaceRenderNodeThreeUtilTest, ResetIsBufferFlushed, TestSize.Level1
     std::shared_ptr<RSSurfaceRenderNode> testNode = std::make_shared<RSSurfaceRenderNode>(id, context);
     ASSERT_NE(testNode, nullptr);
     testNode->stagingRenderParams_ = nullptr;
-    testNode->IsBufferFlushed_ = true;
+    testNode->isBufferFlushed_ = true;
     ASSERT_EQ(testNode->stagingRenderParams_, nullptr);
     testNode->ResetIsBufferFlushed();
 
     testNode->stagingRenderParams_ = std::make_unique<RSSurfaceRenderParams>(id + 1);
     ASSERT_NE(testNode->stagingRenderParams_, nullptr);
-    testNode->IsBufferFlushed_ = true;
+    testNode->isBufferFlushed_ = true;
     testNode->ResetIsBufferFlushed();
     auto surfaceParams = static_cast<RSSurfaceRenderParams*>(testNode->stagingRenderParams_.get());
     ASSERT_FALSE(surfaceParams->GetIsBufferFlushed());
@@ -777,13 +777,13 @@ HWTEST_F(RSSurfaceRenderNodeThreeUtilTest, ResetSurfaceNodeStates, TestSize.Leve
     std::shared_ptr<RSSurfaceRenderNode> testNode = std::make_shared<RSSurfaceRenderNode>(id, context);
     ASSERT_NE(testNode, nullptr);
     testNode->stagingRenderParams_ = nullptr;
-    testNode->IsBufferFlushed_ = true;
+    testNode->isBufferFlushed_ = true;
     ASSERT_EQ(testNode->stagingRenderParams_, nullptr);
     testNode->ResetSurfaceNodeStates();
 
     testNode->stagingRenderParams_ = std::make_unique<RSSurfaceRenderParams>(id + 1);
     ASSERT_NE(testNode->stagingRenderParams_, nullptr);
-t   estNode->IsBufferFlushed_ = true;
+    testNode->isBufferFlushed_ = true;
 
     testNode->ResetSurfaceNodeStates();
     auto surfaceParams = static_cast<RSSurfaceRenderParams*>(testNode->stagingRenderParams_.get());
