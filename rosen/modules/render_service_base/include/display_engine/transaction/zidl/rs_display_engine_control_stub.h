8bb5f13a98a83a6efdb467d672945026285e7ad0
wujiahao531 <wujiahao15@huawei.com>
2026-08-06 18:52:55 +0800

    update: 更新文件 rs_display_engine_control_stub.h
    Signed-off-by: wujiahao531 <wujiahao15@huawei.com>

--- rosen/modules/render_service_base/include/display_engine/transaction/zidl/rs_display_engine_control_stub.h
+++ rosen/modules/render_service_base/include/display_engine/transaction/zidl/rs_display_engine_control_stub.h
@@ -20,17 +20,22 @@
 #include <message_option.h>
 #include <message_parcel.h>
 
-#include <display_engine/transaction/rs_idisplay_engine_control_ipc_interface_code.h>
-#include <display_engine/transaction/zidl/rs_idisplay_engine_control.h>
+#include "common/rs_macros.h"
+#include "display_engine/ipc_callbacks/rs_ide_status_change_callback.h"
+#include "display_engine/transaction/rs_idisplay_engine_control_ipc_interface_code.h"
+#include "display_engine/transaction/zidl/rs_idisplay_engine_control.h"
 
 namespace OHOS {
 namespace Rosen {
-class RSDisplayEngineControlStub : public IRemoteStub<RSIDisplayEngineControl> {
+class RSB_EXPORT RSDisplayEngineControlStub : public IRemoteStub<RSIDisplayEngineControl> {
 public:
     RSDisplayEngineControlStub() = default;
     ~RSDisplayEngineControlStub() noexcept override = default;
  
     int OnRemoteRequest(uint32_t code, MessageParcel& data, MessageParcel& reply, MessageOption& option) override;
+    int32_t NotifyDEStatusChange(const uint32_t sceneKey, const std::vector<uint8_t>& values) override;
+ 	int32_t RegisterDEStatusChangeCallback(const sptr<RSIDEStatusChangeCallback>& callback) override;
+ 	int32_t UnregisterDEStatusChangeCallback() override;
 };
 } // namespace Rosen
 } // namespace OHOS
