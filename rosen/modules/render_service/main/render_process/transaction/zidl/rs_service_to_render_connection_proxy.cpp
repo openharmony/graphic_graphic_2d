b47f70526db9a475ca628ed8ad701e922a4f5e0b
chen_bailin <2411398810@qq.com>
2026-08-07 15:15:26 +0800

    sync code
    Signed-off-by: chen_bailin <2411398810@qq.com>

--- rosen/modules/render_service/main/render_process/transaction/zidl/rs_service_to_render_connection_proxy.cpp
+++ rosen/modules/render_service/main/render_process/transaction/zidl/rs_service_to_render_connection_proxy.cpp
@@ -35,7 +35,7 @@ namespace OHOS {
 namespace Rosen {
 namespace {
 #ifdef RS_ENABLE_TV_PQ_METADATA
-static constexpr uint32_t MAX_VIDEO_INFO_SIZE = 32; // video rate info max map size
+constexpr uint32_t MAX_VIDEO_INFO_SIZE = 32; // video rate info max map size
 #endif
 static constexpr uint32_t MAX_APS_PARAMS_SIZE = 128;
 }
@@ -1122,7 +1122,7 @@ ErrCode RSServiceToRenderConnectionProxy::SendVideoRateInfo(
     const std::unordered_map<std::string, std::string>& videoRateInfo)
 {
     auto mapSize = videoRateInfo.size();
-    if (mapSize <= 0 || mapSize > MAX_VIDEO_INFO_SIZE) {
+    if (mapSize == 0 || mapSize > MAX_VIDEO_INFO_SIZE) {
         ROSEN_LOGE("SendVideoRateInfo: map size err.");
         return ERR_INVALID_VALUE;
     }
