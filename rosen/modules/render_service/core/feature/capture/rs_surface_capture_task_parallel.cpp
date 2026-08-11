cd052d21928e8450811c745d5c048e115107f406
jianghongxi <jianghongxi3@h-partners.com>
2026-08-11 17:56:09 +0800

    update: 更新文件 rs_surface_capture_task_parallel.cpp
    Signed-off-by: jianghongxi <jianghongxi3@h-partners.com>

--- rosen/modules/render_service/core/feature/capture/rs_surface_capture_task_parallel.cpp
+++ rosen/modules/render_service/core/feature/capture/rs_surface_capture_task_parallel.cpp
@@ -565,12 +565,12 @@ std::unique_ptr<Media::PixelMap> RSSurfaceCaptureTaskParallel::CreatePixelMapByD
     }
     opts.size.width = ceil(pixmapWidth * captureConfig_.scaleX);
     opts.size.height = ceil(pixmapHeight * captureConfig_.scaleY);
-    RS_LOGI("%{public}s NodeId[%{public}" PRIu64 "], size[%{public}u, %{public}u],"	 
-        " scale[%{public}f, %{public}f], rect[%{public}f, %{public}f, %{public}f, %{public}f],"	 
-        " dma[%{public}d], rot[%{public}d], corr[%{public}d],"	 
-        " blackList[%{public}zu], isHDR[%{public}d]",	 
-        __func__, node->GetId(), pixmapWidth, pixmapHeight, captureConfig_.scaleX, captureConfig_.scaleY,	 
-        rect.GetLeft(), rect.GetTop(), rect.GetWidth(), rect.GetHeight(), captureConfig_.useDma, screenRotation_,	 
+    RS_LOGI("%{public}s NodeId[%{public}" PRIu64 "], size[%{public}u, %{public}u],"
+        " scale[%{public}f, %{public}f], rect[%{public}f, %{public}f, %{public}f, %{public}f],"
+        " dma[%{public}d], rot[%{public}d], corr[%{public}d],"
+        " blackList[%{public}zu], isHDR[%{public}d]",
+        __func__, node->GetId(), pixmapWidth, pixmapHeight, captureConfig_.scaleX, captureConfig_.scaleY,
+        rect.GetLeft(), rect.GetTop(), rect.GetWidth(), rect.GetHeight(), captureConfig_.useDma, screenRotation_,
         screenCorrection_, captureConfig_.blackList.size(), isHDRCapture);
     std::unique_ptr<Media::PixelMap> pixelMap = Media::PixelMap::Create(opts);
     if (pixelMap) {
