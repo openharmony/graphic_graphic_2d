ef9704278b79e684aa656c25ee216fa797cded68
sunriseLL <liwen176@huawei.com>
2026-08-14 11:33:31 +0800

    update: 更新文件 rs_main_thread.cpp
    Signed-off-by: sunriseLL <liwen176@huawei.com>

--- rosen/modules/render_service/core/pipeline/main_thread/rs_main_thread.cpp
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_main_thread.cpp
@@ -1059,7 +1059,7 @@ void RSMainThread::OnScreenPropertyChanged(
     HandlePowerStatusChanged(id, type, property);
     HandlePhysicalModeParamsChanged(id, type, property);
     UpdateScreenProperty(id, type, property);
-    HandleProtectiveSolidNode(id);
+    // [Planning]: HandleProtectiveSolidNode
 }
 
 void RSMainThread::ReleaseImageMem()
