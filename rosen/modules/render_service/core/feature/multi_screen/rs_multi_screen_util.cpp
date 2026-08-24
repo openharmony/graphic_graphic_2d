0edb37fa80c27934ac7037e5f5c49d681e829302
jianghongxi <jianghongxi3@h-partners.com>
2026-08-24 11:35:50 +0800

    update: 更新文件 rs_multi_screen_util.cpp
    Signed-off-by: jianghongxi <jianghongxi3@h-partners.com>

--- rosen/modules/render_service/core/feature/multi_screen/rs_multi_screen_util.cpp
+++ rosen/modules/render_service/core/feature/multi_screen/rs_multi_screen_util.cpp
@@ -683,7 +683,7 @@ void RSMultiScreenUtil::DrawVirtualMirrorFromCache(
     auto widthRatio = mirroredScreenProperty.GetRogWidthRatio();
     auto heightRatio = mirroredScreenProperty.GetRogHeightRatio();
     if (mirroredScreenProperty.GetSamplingMode() == ScreenSamplingMode::DEVICE_GPU &&
-        ROSEN_GE(widthRatio, 0.f) && ROSEN_GE(heightRatio, 0.f)) {
+        ROSEN_GNE(widthRatio, 0.f) && ROSEN_GNE(heightRatio, 0.f)) {
         curCanvas->Scale(1.0f / widthRatio, 1.0f / heightRatio);
     }
 
