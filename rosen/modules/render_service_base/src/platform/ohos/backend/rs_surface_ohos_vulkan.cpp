d7ce85fad4b2dabcc5c9d3d553c574c78dc79c31
sunriseLL <liwen176@huawei.com>
2026-08-29 18:49:45 +0800

    update: 更新文件 rs_surface_ohos_vulkan.cpp
    Signed-off-by: sunriseLL <liwen176@huawei.com>

--- rosen/modules/render_service_base/src/platform/ohos/backend/rs_surface_ohos_vulkan.cpp
+++ rosen/modules/render_service_base/src/platform/ohos/backend/rs_surface_ohos_vulkan.cpp
@@ -674,9 +674,6 @@ bool RSSurfaceOhosVulkan::FlushBuffer(std::unique_ptr<RSSurfaceFrame>& frame, ui
     callbackInfo->mFenceFd = ::dup(fenceFd);
     RsVulkanInterface::callbackSemaphoreInfofdDupCnt_.fetch_add(+1, std::memory_order_relaxed);
     mReservedFlushFd = ::dup(fenceFd);
-    if (mReservedFlushFd == -1) {
-        ROSEN_LOGE("RSSurfaceOhosVulkan::FlushBuffer dup fence failed, errno=%{public}d", errno);
-    }
     fdsan_exchange_owner_tag(mReservedFlushFd, 0, LOG_DOMAIN);
 
     auto frameOhosVulkan = static_cast<RSSurfaceFrameOhosVulkan*>(frame.get());
