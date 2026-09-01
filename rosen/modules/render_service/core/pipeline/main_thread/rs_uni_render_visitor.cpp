edd28f89f819f40ed26486626bd9a207cc21537d
testwubo <wubo105@huawei.com>
2026-09-01 17:49:57 +0800

    fix(render_service): 快门式3D强制关闭HDR时同步关闭当帧HDRPresent
    HandlePixelFormat中快门式3D模式下强制关闭HDR时，除SetHdrStatus外，
    同步将isHdrOn清零，避免底层IsHdrOn返回true时HDRPresent状态残留。
    
    Co-Authored-By: Agent
    Signed-off-by: testwubo <wubo105@huawei.com>

--- rosen/modules/render_service/core/pipeline/main_thread/rs_uni_render_visitor.cpp
+++ rosen/modules/render_service/core/pipeline/main_thread/rs_uni_render_visitor.cpp
@@ -457,7 +457,8 @@ void RSUniRenderVisitor::HandlePixelFormat(RSScreenRenderNode& node)
     bool forceCloseHDR = node.GetForceCloseHdr();
     bool isCloseHdr = forceCloseHDR || (RSLuminanceControl::Get().IsHardwareHdrDisabled() && !drmNodes_.empty());
 #ifdef RS_ENABLE_TV_SHUTTER_3D
-    isCloseHdr |= RSTvShutter3DManager::Instance().ShouldForceCloseHdr(node);
+    bool isCloseHdrForTv3D = RSTvShutter3DManager::Instance().ShouldForceCloseHdr(node);
+    isCloseHdr |= isCloseHdrForTv3D;
 #endif
     RSLuminanceControl::Get().SetHdrStatus(screenId, isCloseHdr ? HdrStatus::NO_HDR : node.GetDisplayHdrStatus());
     bool isHdrOn = RSLuminanceControl::Get().IsHdrOn(screenId);
@@ -474,6 +475,9 @@ void RSUniRenderVisitor::HandlePixelFormat(RSScreenRenderNode& node)
     if (!hasUniRenderHdrSurface && !RSLuminanceControl::Get().IsHardwareHdrDisabled()) {
         isHdrOn = false;
     }
+#ifdef RS_ENABLE_TV_SHUTTER_3D
+    isHdrOn &= !isCloseHdrForTv3D;
+#endif
     node.SetLastDisplayHdrStatus(node.GetDisplayHdrStatus());
     node.SetHDRPresent(isHdrOn);
     hasDisplayHdrOn_ |= isHdrOn;
