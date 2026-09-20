32e421ef531259b812a67127af2ad2517ccab8e1
ZT003 <zhanglitao11@h-partners.com>
2026-09-20 18:27:42 +0800

    RSCanvasDrawingNode::GetBitmap接口逻辑整改
    Signed-off-by: ZT003 <zhanglitao11@h-partners.com>

--- rosen/modules/render_service_client/core/ui/rs_canvas_drawing_node.cpp
+++ rosen/modules/render_service_client/core/ui/rs_canvas_drawing_node.cpp
@@ -411,9 +411,6 @@ bool RSCanvasDrawingNode::GetBitmap(Drawing::Bitmap& bitmap,
             }
             auto getBitmapTask = [&node, &bitmap]() { bitmap = node->GetBitmap(); };
             RSRenderThread::Instance().PostSyncTask(getBitmapTask);
-            if (!bitmap.IsValid()) {
-                return false;
-            }
         }
     }
     if (drawCmdList == nullptr) {
